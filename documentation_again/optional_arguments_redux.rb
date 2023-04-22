413b4696e4c5280661b2ef8982d2f94d49aed850
Justin Greger <justingreger@gmail.com>
2023-04-21 20:39:59 -0400

    Description of Exercise

---
+++ documentation_again/optional_arguments_redux.rb
@@ -0,0 +1,18 @@
+=begin
+
+Description of Exercise:
+Assume you have the following code:
+require 'date'
+
+puts Date.civil
+puts Date.civil(2016)
+puts Date.civil(2016, 5)
+puts Date.civil(2016, 5, 13)
+
+What will each of the 4 puts statements print?
+
+
+
+
+
+=end
