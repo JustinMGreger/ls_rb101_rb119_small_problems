fcd9158f5231f5363053ccc5ff42398f81e88d85
Justin Greger <justingreger@gmail.com>
2023-04-21 20:46:50 -0400

    require 'date'

--- documentation_again/optional_arguments_redux.rb
+++ documentation_again/optional_arguments_redux.rb
@@ -2,6 +2,8 @@
 
 Description of Exercise:
 Assume you have the following code:
+
+Initial Given Syntax:
 require 'date'
 
 puts Date.civil
@@ -11,8 +13,17 @@ puts Date.civil(2016, 5, 13)
 
 What will each of the 4 puts statements print?
 
+Explanaiton of Initial Given Syntax:
 
+Syntax:
+require 'date'
 
+Explanation:
+require means search for a file and if found then read and execute the file.
+'' is a string representing the name of the file.
+date is the name of a file.
+this means:
+search for the file named date and if found then read and execute the date file.
 
 
 =end
