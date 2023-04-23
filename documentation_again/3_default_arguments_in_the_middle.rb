0d9fd42ef5ac802afb2f74caa459761d379ee13f
Justin Greger <justingreger@gmail.com>
2023-04-22 21:55:31 -0400

    outline

--- documentation_again/3_default_arguments_in_the_middle.rb
+++ documentation_again/3_default_arguments_in_the_middle.rb
@@ -3,12 +3,21 @@
 Description of Exercise:
 Consider the following method and a call to that method:
 
+Initial Syntax Given:
 def my_method(a, b = 2, c = 3, d)
   p [a, b, c, d]
 end
 
 my_method(4, 5, 6)
 
+Description of Exercise:
 Use the ruby documentation to determine what this code will print.
+
+Explanation of Initial Syntax Given:
+
+Syntax:
+def my_method(a, b = 2, c = 3, d)
+
+  Explanation:
 
 =end
