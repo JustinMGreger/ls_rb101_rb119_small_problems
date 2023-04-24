ce8473b18dab92740ac08b6f826245e2cb7d50bf
Justin Greger <justingreger@gmail.com>
2023-04-23 21:16:16 -0400

    Initial Given Syntax:

--- documentation_again/4_mandatory_blocks.rb
+++ documentation_again/4_mandatory_blocks.rb
@@ -7,5 +7,10 @@ Assume you have the following code:
 Initial Given Syntax:
 a = [1, 4, 8, 11, 15, 19]
 
+Explanation of Initial Given Syntax:
+a is a variable.
+= means assignment.
+[1, 4, 8, 11, 15, 19] is a array of integer elements.
+this means the variable a is assigned the [1, 4, 8, 11, 15, 19] array of integer elements.
 
 =end
