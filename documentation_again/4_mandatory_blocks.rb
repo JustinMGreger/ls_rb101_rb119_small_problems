07f876bbdf32336cdcc3f5c1017f3337a718c5a2
Justin Greger <justingreger@gmail.com>
2023-04-23 21:11:56 -0400

    Initial Given Syntax:

--- documentation_again/4_mandatory_blocks.rb
+++ documentation_again/4_mandatory_blocks.rb
@@ -4,5 +4,8 @@ Description of Exercise:
 The Array#bsearch method is used to search ordered Arrays more quickly than #find and #select can. 
 Assume you have the following code:
 
+Initial Given Syntax:
+a = [1, 4, 8, 11, 15, 19]
+
 
 =end
