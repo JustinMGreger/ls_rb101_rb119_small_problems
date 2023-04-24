8bc816a8fb4cb222fc2e29d78f5541fec2f390c8
Justin Greger <justingreger@gmail.com>
2023-04-23 21:10:55 -0400

    Description of Exercise:

---
+++ documentation_again/4_mandatory_blocks.rb
@@ -0,0 +1,8 @@
+=begin
+
+Description of Exercise:
+The Array#bsearch method is used to search ordered Arrays more quickly than #find and #select can. 
+Assume you have the following code:
+
+
+=end
