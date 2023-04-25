e9d8bb1dd67f8a3f5a5a463e00ed2ac42df2df72
Justin Greger <justingreger@gmail.com>
2023-04-24 20:41:23 -0400

    PEDAC

--- documentation_again/4_mandatory_blocks.rb
+++ documentation_again/4_mandatory_blocks.rb
@@ -1,5 +1,39 @@
 =begin
 
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+
+PEDAC stands for “[Understand the] Problem, 
+ Identify expected input and output
+ Make the requirements explicit
+ Identify rules
+ Mental model of the problem (optional)
+
+Examples / Test Cases, 
+ Validate understanding of the problem
+
+Data Structure, 
+ How we represent data that we will work with when converting the input to output.
+
+Algorithm, 
+ Steps for converting input to output
+
+and Code.” 
+ Implementation of Algorithm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Description of Exercise:
 The Array#bsearch method is used to search ordered Arrays more quickly than #find and #select can. 
 Assume you have the following code:
