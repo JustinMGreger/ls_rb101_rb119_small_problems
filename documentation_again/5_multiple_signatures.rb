6f36eab91017168371dd02e187b9cd92434e4484
Justin Greger <justingreger@gmail.com>
2023-04-25 20:25:34 -0400

    Explanation of Initial Given Syntax:

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -3,10 +3,17 @@
 Description of Exercise:
 What do each of these puts statements output?
 
-Given Initial Syntax:
+Initial Given Syntax:
 a = %w(a b c d e)
 puts a.fetch(7)
 puts a.fetch(7, 'beats me')
 puts a.fetch(7) { |index| index**2 }
+
+Explanation of Initial Given Syntax:
+a = %w(a b c d e)
+puts a.fetch(7)
+puts a.fetch(7, 'beats me')
+puts a.fetch(7) { |index| index**2 }
+
 
 =end
