1abca27dc42a61af58f7c0a9516fa833f594e7f0
Justin Greger <justingreger@gmail.com>
2023-04-25 21:28:13 -0400

    [a, b, c, d, e]

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -21,6 +21,8 @@ a is a variable.
 (a b c d e) are string elements.
 this means: 
 the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace 
+the result of which is:
+["a", "b", "c", "d", "e"]
 
 Syntax:
 puts a.fetch(7)
@@ -62,13 +64,33 @@ of string literals, where each string is separated by whitespace and if this fai
 the output of which is:
 beats me
 
-
-
-
-
-
-
+Syntax:
 puts a.fetch(7) { |index| index**2 }
 
+Explanation:
+puts is put s which is put string which is print string to the screen.
+a is a variable.
+= means assignment.
+%w is used to create an array of string literals, where each string is separated by whitespace.
+(a b c d e) are string elements.
+this means: 
+the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace 
+.fetch method is used to retrieve the value of a specified key from a hash. 
+(7) is a integer argument to the fetch method.
+{ |index| index**2 } is a block of code passed to the fetch method where index is the index of the elements which are
+then raised to the power of 2 that is multiplied by itself.
+this means:
+print to the screen
+
+
+
+
+the result of retrieving 
+the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace 
+
+the value of a specified key from a hash. 
+(7) is a integer argument to the fetch method.
+{ |index| index**2 } is a block of code passed to the fetch method where index is the index of the elements which are
+then raised to the power of 2 that is multiplied by itself.
 
 =end
