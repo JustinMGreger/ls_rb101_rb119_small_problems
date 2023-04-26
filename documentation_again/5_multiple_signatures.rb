20e3c00d79923d711fcb912abd03a3dab40cb557
Justin Greger <justingreger@gmail.com>
2023-04-25 21:05:49 -0400

    puts a.fetch(7)

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -22,8 +22,49 @@ a is a variable.
 this means: 
 the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace 
 
+Syntax:
 puts a.fetch(7)
+
+Explanation:
+puts is put s which is put string which is print string to the screen.
+a is a variable.
+= means assignment.
+%w is used to create an array of string literals, where each string is separated by whitespace.
+(a b c d e) are string elements.
+this means: 
+the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace 
+.fetch method is used to retrieve the value of a specified key from a hash. 
+(7) is a integer argument to the fetch method.
+this means:
+print to the screen the result of retrieving the value of integer 7 from the variable which a is assigned to the 
+(a b c d e) array of string literals, where each string is separated by whitespace.
+the output of which is:
+`fetch': index 7 outside of array bounds: -5...5 (IndexError)
+	from ruby.rb:2:in `<main>'
+
+Syntax:
 puts a.fetch(7, 'beats me')
+
+Explanation:
+puts is put s which is put string which is print string to the screen.
+a is a variable.
+= means assignment.
+%w is used to create an array of string literals, where each string is separated by whitespace.
+(a b c d e) are string elements.
+this means: 
+the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace 
+.fetch method is used to retrieve the value of a specified key from a hash. 
+(7, 'beats me') are arguments to the fetch method.
+
+
+
+
+
+
+
+
+
+
 puts a.fetch(7) { |index| index**2 }
 
 
