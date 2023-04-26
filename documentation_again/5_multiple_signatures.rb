0d154a9ac67312f077697f6d3f574f1a94e99245
Justin Greger <justingreger@gmail.com>
2023-04-25 20:32:57 -0400

    a = %w(a b c d e)

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -10,7 +10,18 @@ puts a.fetch(7, 'beats me')
 puts a.fetch(7) { |index| index**2 }
 
 Explanation of Initial Given Syntax:
-a = %w(a b c d e)
+
+Syntax:
+a = %w(a b c d e) 
+
+Explanation:
+a is a variable.
+= means assignment.
+%w is used to create an array of string literals, where each string is separated by whitespace.
+(a b c d e) are string elements.
+this means: 
+the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace 
+
 puts a.fetch(7)
 puts a.fetch(7, 'beats me')
 puts a.fetch(7) { |index| index**2 }
