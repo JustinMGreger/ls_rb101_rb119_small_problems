7bd38096a635df71d663727a6247a1d3c90f70de
Justin Greger <justingreger@gmail.com>
2023-04-25 20:22:29 -0400

    Given Initial Syntax:

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -3,6 +3,10 @@
 Description of Exercise:
 What do each of these puts statements output?
 
-
+Given Initial Syntax:
+a = %w(a b c d e)
+puts a.fetch(7)
+puts a.fetch(7, 'beats me')
+puts a.fetch(7) { |index| index**2 }
 
 =end
