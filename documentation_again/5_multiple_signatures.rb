b6a29ea301660d45f4af3056db9800af7ac17656
Justin Greger <justingreger@gmail.com>
2023-04-26 19:25:31 -0400

    completed

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -38,11 +38,11 @@ the variable a is assigned to the (a b c d e) array of string literals, where ea
 the result of which is:
 ["a", "b", "c", "d", "e"]
 .fetch method is used to retrieve the element at the specified index.
-
 (7) is a integer argument to the fetch method.
 this means:
-print to the screen the result of retrieving the value of integer 7 from the variable which a is assigned to the 
-(a b c d e) array of string literals, where each string is separated by whitespace.
+print to the screen the result of retrieving the element at index 7 from the 
+variable a which is assigned to the ["a", "b", "c", "d", "e"] array of string literals with each element seperated by 
+whitespace.
 the output of which is:
 `fetch': index 7 outside of array bounds: -5...5 (IndexError)
 	from ruby.rb:2:in `<main>'
@@ -63,9 +63,9 @@ the result of which is:
 .fetch method is used to retrieve the element at the specified index.
 (7, 'beats me') are arguments to the fetch method where 7 is a integer and 'beats me' is a string.
 this means:
-print to the screen the retrieved element at index 7 for the variable a is assigned to the (a b c d e) array
-of string literals, where each string is separated by whitespace and if this fails then print to the screen the string
-'beats me'.
+print to the screen the result of retrieving the element at index 7 from the 
+variable a which is assigned to the ["a", "b", "c", "d", "e"] array of string literals with each element seperated by 
+whitespace and if this fails then print to the screen the string 'beats me'.
 the output of which is:
 beats me
 
@@ -87,9 +87,9 @@ the result of which is:
 { |index| index**2 } is a block of code passed to the fetch method where index is the index of the elements which are
 then raised to the power of 2 that is multiplied by itself.
 this means:
-print to the screen the retrieved element at index 7 for the variable a is assigned to the (a b c d e) array
-of string literals, where each string is separated by whitespace and if this fails then print to the screen the result of
-the index 7 raised to the power of 2 that is multiplied by itself which is the integer 49.
+print to the screen the result of retrieving the element at index 7 from the variable a which is assigned to the 
+["a", "b", "c", "d", "e"] array of string literals with each element seperated by whitespace and if this fails then print 
+to the screen the result of the index 7 raised to the power of 2 that is multiplied by itself which is the integer 49.
 the output of which is:
 49
 
