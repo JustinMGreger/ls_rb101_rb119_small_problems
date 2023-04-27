682fe88d3ab488e21ab8453195bfba55b8419fd7
Justin Greger <justingreger@gmail.com>
2023-04-26 21:29:24 -0400

    10 is a integer.

--- documentation_again/6_keyword_arguments.rb
+++ documentation_again/6_keyword_arguments.rb
@@ -11,6 +11,6 @@ Explanation:
 .step method is used to iterate over a range of numbers with a specific step value which follows the syntax:
 start_value.step(end_value, step_value) { |current_value| # code to be executed }
 to: means the exclusive limit of the range.
-
+10 is a integer.
 
 =end
