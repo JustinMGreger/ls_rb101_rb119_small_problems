cf061ffb3150ad30353c7be79ac40f52320e44ac
Justin Greger <justingreger@gmail.com>
2023-04-27 21:16:48 -0400

    the result of which is:

--- documentation_again/7_parent_class.rb
+++ documentation_again/7_parent_class.rb
@@ -49,7 +49,24 @@ the variable s is assigned the string 'abc'.
 this means:
 print to the screen the variable s is assigned the string 'abc' which is returned as an array of all the public instance 
 methods available for an object and then returned as a string representation of the object.
-
+the result of which is:
+[:each_grapheme_cluster, :slice, :slice!, :rpartition, :encoding, :force_encoding, :b, :valid_encoding?, :ascii_only?, 
+:hash, :unicode_normalized?, :unicode_normalize, :unicode_normalize!, :encode, :encode!, :to_r, :include?, :%, :*, :+, 
+:to_c, :count, :partition, :unpack, :unpack1, :+@, :-@, :<=>, :<<, :==, :===, :sum, :=~, :[], :[]=, :next, :empty?, :eql?,
+:casecmp, :casecmp?, :insert, :bytesize, :match, :match?, :succ!, :next!, :upto, :index, :byteindex, :rindex, :byterindex,
+:replace, :clear, :chr, :getbyte, :setbyte, :byteslice, :bytesplice, :scrub, :scrub!, :dedup, :freeze, :undump, :intern, 
+:length, :size, :succ, :downcase, :capitalize, :upcase, :dump, :upcase!, :inspect, :swapcase!, :oct, :downcase!, 
+:capitalize!, :swapcase, :to_str, :codepoints, :split, :lines, :hex, :chars, :to_s, :to_i, :to_f, :reverse!, :concat, 
+:grapheme_clusters, :reverse, :bytes, :start_with?, :prepend, :crypt, :ljust, :gsub, :end_with?, :scan, :strip, :to_sym, 
+:center, :sub, :lstrip, :chop, :rjust, :ord, :sub!, :rstrip, :delete_prefix, :chomp, :strip!, :gsub!, :chop!, :chomp!, 
+:delete_suffix, :lstrip!, :rstrip!, :delete_prefix!, :delete_suffix!, :tr, :tr_s, :delete, :squeeze, :tr!, :tr_s!, 
+:delete!, :squeeze!, :each_line, :each_byte, :each_char, :each_codepoint, :clamp, :between?, :<=, :>=, :<, :>, 
+:singleton_class, :dup, :itself, :methods, :singleton_methods, :protected_methods, :private_methods, :public_methods, 
+:instance_variables, :instance_variable_get, :instance_variable_set, :instance_variable_defined?, 
+:remove_instance_variable, :instance_of?, :kind_of?, :is_a?, :display, :public_send, :extend, :class, :clone, :tap, 
+:frozen?, :yield_self, :then, :!~, :nil?, :respond_to?, :public_method, :method, :singleton_method, 
+:define_singleton_method, :object_id, :send, :to_enum, :enum_for, :!, :equal?, :__send__, :!=, :instance_eval, 
+:instance_exec, :__id__]
 
 
 
