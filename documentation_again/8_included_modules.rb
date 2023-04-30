7fba117edc6848898b8ac2f88d524165c522a800
Justin Greger <justingreger@gmail.com>
2023-04-30 16:57:35 -0400

    puts a.min

--- documentation_again/8_included_modules.rb
+++ documentation_again/8_included_modules.rb
@@ -17,9 +17,18 @@ a is a variable.
 = means assignment.
 [5, 9, 3, 11] is a array of integers.
 
-
-
-
+Syntax:
 puts a.min
+
+Explanation:
+puts means put s which is put string which is print to the screen the string.
+a is a variable.
+= means assignment.
+[5, 9, 3, 11] is a array of integers.
+.min method returns the minimum value.
+
+
+
+
 
 =end
