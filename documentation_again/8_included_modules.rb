33c807a1112861444687d167ca1fe02e11b839ce
Justin Greger <justingreger@gmail.com>
2023-04-29 21:55:15 -0400

    a = [5, 9, 3, 11]

--- documentation_again/8_included_modules.rb
+++ documentation_again/8_included_modules.rb
@@ -7,4 +7,19 @@ Initial Given Syntax:
 a = [5, 9, 3, 11]
 puts a.min
 
+Explanation of Initial Given Syntax:
+
+Syntax:
+a = [5, 9, 3, 11]
+
+Explanation:
+a is a variable.
+= means assignment.
+[5, 9, 3, 11] is a array of integers.
+
+
+
+
+puts a.min
+
 =end
