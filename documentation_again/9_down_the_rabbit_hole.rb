ee065a2b7f6d82bf523aa47092261d70dbf075a9
Justin Greger <justingreger@gmail.com>
2023-05-01 19:30:29 -0400

    require 'yaml'

--- documentation_again/9_down_the_rabbit_hole.rb
+++ documentation_again/9_down_the_rabbit_hole.rb
@@ -7,6 +7,18 @@ In a very early assignment at Launch School,
 you are tasked with writing a program that loads some text messages from a YAML file. 
 We do this with YAML::load_file:
 
+Initial Given Syntax:
+
+Syntax:
+require 'yaml'
+
+Explanation:
+require means find and load a file.
+'yaml' is YAML Ain’t Markup Language which is a Ruby module that provides a Ruby interface for data serialization in YAML
+format.
+this means: find and load the YAML module.
+
+
 
 
 
