adc0c3dac866f73db76ad9a98e7b9e9285305884
Justin Greger <justingreger@gmail.com>
2023-05-02 19:39:33 -0400

    Description of Exercise:

---
+++ easy_1/1_repeat_yourself.rb
@@ -0,0 +1,9 @@
+=begin
+
+Description of Exercise:
+Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the 
+integer indicates.
+
+Example:
+
+=end
