49d2da7236082aaa89fb54bad36cfb48d07493fb
Justin Greger <justingreger@gmail.com>
2023-05-01 20:01:05 -0400

    MESSAGES = YAML.load_file('calculator_messages.yml')

--- documentation_again/9_down_the_rabbit_hole.rb
+++ documentation_again/9_down_the_rabbit_hole.rb
@@ -8,6 +8,8 @@ you are tasked with writing a program that loads some text messages from a YAML 
 We do this with YAML::load_file:
 
 Initial Given Syntax:
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
 
 Syntax:
 require 'yaml'
@@ -17,6 +19,22 @@ require means find and load a file.
 'yaml' is YAML Ain’t Markup Language which is a Ruby module that provides a Ruby interface for data serialization in YAML
 format.
 this means: find and load the YAML module.
+
+Syntax:
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+Explanation:
+MESSAGES is a constant.
+= means assignment.
+YAML is YAML Ain’t Markup Language which is a Ruby module that provides a Ruby interface for data serialization in YAML
+format.
+.load_file('calculator_messages.yml') according to the ruby docs:
+https://ruby-doc.org/stdlib-1.8.6/libdoc/yaml/rdoc/YAML.html#method-c-load_file
+means: load_file( filepath ) which is used to load a document from the file located at filepath.
+'calculator_messages.yml' is a YAML file.
+this means:
+the constant MESSAGES is assigned the result of loading the calculator_messages.yml YAML file and using the YAML Ruby 
+module to provide a Ruby interface for data serialization in YAML format.
 
 
 
