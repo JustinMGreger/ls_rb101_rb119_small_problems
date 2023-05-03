ce3593b911d3b87c88433c3968a4bc84346c5f1e
Justin Greger <justingreger@gmail.com>
2023-05-02 20:14:28 -0400

    PEDAC

--- easy_1/1_repeat_yourself.rb
+++ easy_1/1_repeat_yourself.rb
@@ -12,4 +12,35 @@ Hello
 Hello
 Hello
 
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input:
+Write a method that takes two arguments, a string and a positive integer
+ 
+and output:
+print the string as many times as the integer indicates.
+ 
+ 
+Make the requirements explicit Identify rules:
+
+
+
+
+Mental model of the problem (optional)
+
+Examples / Test Cases, 
+ Validate understanding of the problem
+
+Data Structure, 
+ How we represent data that we will work with when converting the input to output.
+
+Algorithm, 
+ Steps for converting input to output
+
+and Code.” 
+ Implementation of Algorithm
+
+ 
+
+
+
 =end
