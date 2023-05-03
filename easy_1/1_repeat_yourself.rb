bb94db0812ec428eea0903d0e2dd6b7273071803
Justin Greger <justingreger@gmail.com>
2023-05-02 20:59:32 -0400

    Mental model of the problem (optional):

--- easy_1/1_repeat_yourself.rb
+++ easy_1/1_repeat_yourself.rb
@@ -23,25 +23,73 @@ Make the requirements explicit Identify rules:
 Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the 
 integer indicates.
 
+Mental model of the problem (optional):
 
+Example:
+repeat('Hello', 3)
+Output:
+Hello
+Hello
+Hello
 
+Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the 
+integer indicates.
 
+syntax:
+repeat('Hello', 3)
 
-Mental model of the problem (optional)
+explanation:
+repeat is a method.
+('Hello', 3) are arguments that is a string and a integer.
+this means:
+repeat calls the repeat method that accepts the ('Hello', 3) arguments where 'Hello' is a string and 3 is a integer.
+the syntax is similar to:
+def repeat(string, integer)
 
-Examples / Test Cases, 
- Validate understanding of the problem
+Syntax:
+Hello
+Hello
+Hello
 
-Data Structure, 
- How we represent data that we will work with when converting the input to output.
+Explanation:
+Hello is a string that has been printed to the screen 3 times.
+Hello is a string.
+printed to the screen is puts.
+3 is a integer.
+times is the .times method.
+which would be:
+puts string integer.times
+the syntax is similar to:
+integer.times do
+  puts string
+end
 
-Algorithm, 
- Steps for converting input to output
+combining both:
+def repeat(string, integer)
+integer.times do
+  puts string
+end
 
-and Code.” 
- Implementation of Algorithm
+refactoring it:
+def repeat(string, integer)
+  integer.times do
+    puts string
+  end
+end
 
- 
+refactored with the original example as a call:
+def repeat(string, integer)
+  integer.times do
+    puts string
+  end
+end
+
+repeat('Hello', 3)
+
+the output of which is:
+Hello
+Hello
+Hello
 
 
 
