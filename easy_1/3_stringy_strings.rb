d1ae8c4951da952ac429a685b145d76ccf520e0e
Justin Greger <justingreger@gmail.com>
2023-05-05 21:22:33 -0400

    Make the requirements explicit Identify rules:

--- easy_1/3_stringy_strings.rb
+++ easy_1/3_stringy_strings.rb
@@ -30,8 +30,24 @@ puts stringy(7) == '1010101'
 The tests above should print true.
 
 Make the requirements explicit Identify rules:
+Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, 
+always starting with 1. 
+The length of the string should match the given integer.
+Examples:
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
+The tests above should print true.
 
 Mental model of the problem (optional):
+
+
+
+
+
+
+
 
 Examples / Test Cases, Validate understanding of the problem:
 
