0bd00aa7c9500a39d65f5cddb48780542be3b496
Justin Greger <justingreger@gmail.com>
2023-05-05 21:16:01 -0400

    PEDAC

--- easy_1/3_stringy_strings.rb
+++ easy_1/3_stringy_strings.rb
@@ -13,4 +13,23 @@ puts stringy(7) == '1010101'
 Description of Exercise:
 The tests above should print true.
 
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+
+and output:
+
+Make the requirements explicit Identify rules:
+
+Mental model of the problem (optional):
+
+Examples / Test Cases, Validate understanding of the problem:
+
+Data Structure, How we represent data that we will work with when converting the input to output:
+
+Algorithm, Steps for converting input to output:
+
+and Code.” Implementation of Algorithm:
+
 =end
