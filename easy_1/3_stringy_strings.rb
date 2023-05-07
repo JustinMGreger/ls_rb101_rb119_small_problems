f906d55b02ad10c1143a17d69561758b5daa09fb
Justin Greger <justingreger@gmail.com>
2023-05-06 20:34:27 -0400

    Mental model of the problem (optional):

--- easy_1/3_stringy_strings.rb
+++ easy_1/3_stringy_strings.rb
@@ -41,6 +41,49 @@ puts stringy(7) == '1010101'
 The tests above should print true.
 
 Mental model of the problem (optional):
+Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, 
+always starting with 1. 
+The length of the string should match the given integer.
+Examples:
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
+The tests above should print true.
+
+Syntax:
+puts stringy(6) == '101010'
+
+Explanation:
+puts is put s which is put string which is print to the screen the string.
+stringy is a method.
+(6) is a argument passed to the method.
+== means equal to.
+'101010' is a string.
+this means:
+print to the screen the result of the stringy method accepting the argument 6 which is the string '101010'.
+
+
+Instruction:
+Write a method that takes one argument, a positive integer, 
+
+Syntax:
+def stringy(parameter)
+
+Instruction:
+and returns a string of alternating 1s and 0s, always starting with 1. 
+
+Syntax
+string '1' or '0' 
+
+
+
+
+
+
+
+The length of the string should match the given integer.
+string.length == integer
 
 
 
