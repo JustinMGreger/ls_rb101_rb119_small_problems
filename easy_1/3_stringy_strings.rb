0df265a16096cd5f3e4c41a5482550a0d05f48c6
Justin Greger <justingreger@gmail.com>
2023-05-07 21:18:17 -0400

    completed

--- easy_1/3_stringy_strings.rb
+++ easy_1/3_stringy_strings.rb
@@ -261,5 +261,28 @@ Instruction:
 If the method is called with this argument set to 0, the method should return a String of alternating 0s and 1s, 
 but starting with 0 instead of 1.
 
+Syntax
+def stringy(parameter, optional_parameter = 1)
+  integers_array = []
+  parameter.times do |index|
+    integer = index.even? ? optional_parameter : (optional_parameter - 1).abs
+    integers_array << integer
+  end
+  integers_array.join
+end
+
+tests:
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
+
+the output of which is:
+true
+true
+true
+true
+
+the code works.
 
 =end
