84cb9415590a16b7ca65e18b200ce6b257c6445a
Justin Greger <justingreger@gmail.com>
2023-05-09 19:59:26 -0400

    Algorithm, Steps for converting input to output:

--- easy_1/4_whats_my_bonus.rb
+++ easy_1/4_whats_my_bonus.rb
@@ -73,12 +73,17 @@ puts calculate_bonus(1000, false) == 0
 puts calculate_bonus(50000, true) == 25000
 The tests above should print true.
 
-
-
-
 Data Structure, How we represent data that we will work with when converting the input to output:
+array or not determined. 
 
 Algorithm, Steps for converting input to output:
+1. Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary.
+2. If the boolean is true, the bonus should be half of the salary. 
+3. If the boolean is false, the bonus should be 0.
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
+The tests above should print true.
 
 and Code.” Implementation of Algorithm:
 
