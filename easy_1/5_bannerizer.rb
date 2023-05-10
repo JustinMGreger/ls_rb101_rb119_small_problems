f2e3c236df6cde73d4c217330e67ccc9d0ab1510
Justin Greger <justingreger@gmail.com>
2023-05-10 19:30:45 -0400

    Make the requirements explicit Identify rules:

--- easy_1/5_bannerizer.rb
+++ easy_1/5_bannerizer.rb
@@ -29,6 +29,21 @@ and output:
 print a short line of text within a box.
 
 Make the requirements explicit Identify rules:
+Write a method that will take a short line of text, and print it within a box.
+Example:
+print_in_box('To boldly go where no one has gone before.')
++--------------------------------------------+
+|                                            |
+| To boldly go where no one has gone before. |
+|                                            |
++--------------------------------------------+
+print_in_box('')
++--+
+|  |
+|  |
+|  |
++--+
+You may assume that the input will always fit in your terminal window.
 
 Mental model of the problem (optional):
 
