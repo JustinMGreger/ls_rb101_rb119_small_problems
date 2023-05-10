7fe5d34185ffc6a3659d382bee16499ba55ca5cd
Justin Greger <justingreger@gmail.com>
2023-05-10 19:22:19 -0400

    PEDAC

--- easy_1/5_bannerizer.rb
+++ easy_1/5_bannerizer.rb
@@ -17,4 +17,25 @@ print_in_box('')
 |  |
 +--+
 You may assume that the input will always fit in your terminal window.
+
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+
+and output:
+
+Make the requirements explicit Identify rules:
+
+Mental model of the problem (optional):
+
+Examples / Test Cases, Validate understanding of the problem:
+
+Data Structure, How we represent data that we will work with when converting the input to output:
+
+Algorithm, Steps for converting input to output:
+
+and Code.” Implementation of Algorithm:
+
 =end
