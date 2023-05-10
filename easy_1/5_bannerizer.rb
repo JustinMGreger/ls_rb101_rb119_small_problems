e0eede0de7f0475ddbb6abfac1d238cf7920c85f
Justin Greger <justingreger@gmail.com>
2023-05-10 19:18:13 -0400

    Example:

--- easy_1/5_bannerizer.rb
+++ easy_1/5_bannerizer.rb
@@ -4,5 +4,17 @@ Description of Exercise:
 Write a method that will take a short line of text, and print it within a box.
 
 Example:
-
+print_in_box('To boldly go where no one has gone before.')
++--------------------------------------------+
+|                                            |
+| To boldly go where no one has gone before. |
+|                                            |
++--------------------------------------------+
+print_in_box('')
++--+
+|  |
+|  |
+|  |
++--+
+You may assume that the input will always fit in your terminal window.
 =end
