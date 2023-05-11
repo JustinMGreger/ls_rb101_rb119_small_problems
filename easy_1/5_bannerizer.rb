6ae9c1cce0a0f905b532ecc639f31c8dc6b246cd
Justin Greger <justingreger@gmail.com>
2023-05-10 20:04:46 -0400

    Mental model of the problem (optional):

--- easy_1/5_bannerizer.rb
+++ easy_1/5_bannerizer.rb
@@ -46,8 +46,63 @@ print_in_box('')
 You may assume that the input will always fit in your terminal window.
 
 Mental model of the problem (optional):
+Write a method that will take a short line of text, and print it within a box.
+Example:
+print_in_box('To boldly go where no one has gone before.')
++--------------------------------------------+
+|                                            |
+| To boldly go where no one has gone before. |
+|                                            |
++--------------------------------------------+
+print_in_box('')
++--+
+|  |
+|  |
+|  |
++--+
+You may assume that the input will always fit in your terminal window.
+
+Syntax:
+def method
+  take a short line of text, and print it within a box.
+end
+
+Instruction:
+Example:
+print_in_box('To boldly go where no one has gone before.')
++--------------------------------------------+ 1
+|                                            | 2
+| To boldly go where no one has gone before. | 3
+|                                            | 4
++--------------------------------------------+ 5
+print_in_box('')
++--+ 1
+|  | 2
+|  | 3
+|  | 4
++--+ 5
+
+Syntax:
+def print_in_box()
+  take a short line of text, and print it within a box.
+end
+
+5 rows on third row so centered?
+
+Syntax:
+def print_in_box()
+  take a short line of text, center and print it within a box.
+end
 
 Examples / Test Cases, Validate understanding of the problem:
+
+
+
+
+
+
+
+
 
 Data Structure, How we represent data that we will work with when converting the input to output:
 
