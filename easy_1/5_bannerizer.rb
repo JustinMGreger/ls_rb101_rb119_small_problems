6f804896dfa0ddfc28da1a3781da7890cbad5059
Justin Greger <justingreger@gmail.com>
2023-05-19 21:16:58 -0400

    def print_in_box(message)

--- easy_1/5_bannerizer.rb
+++ easy_1/5_bannerizer.rb
@@ -1131,4 +1131,45 @@ end
 
 Explanation of Syntax of Solution:
 
+Syntax:
+def print_in_box(message)
+
+Explanation:
+def means definition.
+print_in_box is a method.
+(message) is a parameter.
+this means:
+the method print_in_box accepts the message parameter and is defined as the following:
+
+
+
+
+  max_width = 76
+  max_message_width = max_width - 4 
+
+  words = message.split(' ')
+  lines = ['']
+  words.each do |word|
+    while word.length > max_message_width
+      lines[-1] += word.slice!(0..max_message_width-1) + ' '
+      lines << '' if word.length > 0
+    end
+    if (lines[-1] + word + ' ').length > max_message_width
+      lines << ''
+    end
+    lines[-1] += word + ' ' unless word.empty?
+  end
+  message = lines.map(&:strip).join("\n")
+
+  max_line_length = [max_message_width, message.split("\n").map(&:length).max].min
+  horizontal_rule = "+#{'-' * (max_line_length + 2)}+"
+  empty_line = "|#{' ' * (max_line_length + 2)}|"
+
+  puts horizontal_rule
+  puts empty_line
+  message.split("\n").each { |line| puts "| #{line.ljust(max_line_length)} |" }
+  puts empty_line
+  puts horizontal_rule
+end
+
 =end
