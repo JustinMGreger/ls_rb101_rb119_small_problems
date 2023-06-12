9969eaaeed901d9842dc3b444781ad0ab553979e
Justin Greger <justingreger@gmail.com>
2023-06-11 21:23:57 -0400

    Description of Exercise:

--- easy_1/6_right_triangles.rb
+++ easy_1/6_right_triangles.rb
@@ -1,4 +1,10 @@
 =begin
+Description of Exercise:
+Write a method that takes a positive integer, n, as an argument, and displays a right triangle whose sides each have n 
+stars. 
+The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the 
+triangle, and the other end at the upper-right.
 
+Examples:
 
 =end
