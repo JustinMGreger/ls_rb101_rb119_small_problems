a1d62161325b357965f6599c6766de78d4c144ac
Justin Greger <justingreger@gmail.com>
2023-06-11 21:58:57 -0400

    somehow the first * is n spaces to the right.

--- easy_1/6_right_triangles.rb
+++ easy_1/6_right_triangles.rb
@@ -51,18 +51,19 @@ triangle, and the other end at the upper-right.
 Examples:
 triangle(5)     this is the method name.
 
-    *            1 stars
-   **            2 stars
-  ***            3 stars
- ****            4 stars
-*****            5 stars
+    *            1 stars  * is 5 spaces to the right.
+   **            2 stars  * is 5 spaces to the right. 
+  ***            3 stars  * is 5 spaces to the right.
+ ****            4 stars  * is 5 spaces to the right.
+*****            5 stars  * is 5 spaces to the right.
+12345
 
 Syntax so far:
 def triangle(n)
   n is the number of lines
   n is the number of * printed.
 end
-
+somehow the first * is n spaces to the right.
 
 
 triangle(9)
