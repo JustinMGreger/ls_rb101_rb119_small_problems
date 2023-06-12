84928ef082383c39c31dc8109780c88927b829af
Justin Greger <justingreger@gmail.com>
2023-06-11 21:39:50 -0400

    Make the requirements explicit Identify rules:

--- easy_1/6_right_triangles.rb
+++ easy_1/6_right_triangles.rb
@@ -35,6 +35,11 @@ and output: displays a right triangle whose sides each have n stars. The hypoten
 the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
 
 Make the requirements explicit Identify rules:
+Description of Exercise:
+Write a method that takes a positive integer, n, as an argument, 
+and displays a right triangle whose sides each have n stars. 
+The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the 
+triangle, and the other end at the upper-right.
 
 Mental model of the problem (optional):
 
