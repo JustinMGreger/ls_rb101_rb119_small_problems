579aad72f8ed2535dfabc8c131985286af558105
Justin Greger <justingreger@gmail.com>
2023-06-12 20:08:38 -0400

    test & output

--- easy_1/6_right_triangles.rb
+++ easy_1/6_right_triangles.rb
@@ -74,19 +74,40 @@ triangle(9)
      **** 4 stars * is 9 spaces to the right. * is 8 spaces to the right. * is 7 spaces to the right. * is 6 spaces to the right.
     ***** 5 stars * is 9 spaces to the right. * is 8 spaces to the right. * is 7 spaces to the right. * is 6 spaces to the right. * is 5 spaces to the right.
    ****** 6 stars * is 9 spaces to the right. * is 8 spaces to the right. * is 7 spaces to the right. * is 6 spaces to the right. * is 5 spaces to the right. * is 4 spaces to the right.
-  ******* 7 stars * is 9 spaces to the right. * is 8 spaces to the right. * is 7 spaces to the right. * is 6 spaces to the right. * is 5 spaces to the right. 
- ******** 8 stars * is 9 spaces to the right. * is 8 spaces to the right. * is 7 spaces to the right. * is 6 spaces to the right. * is 5 spaces to the right.
-********* 9 stars * is 9 spaces to the right. * is 8 spaces to the right. * is 7 spaces to the right. * is 6 spaces to the right. * is 5 spaces to the right.
-123456789 10 stars * is 9 spaces to the right. * is 8 spaces to the right. * is 7 spaces to the right. * is 6 spaces to the right. * is 5 spaces to the right.
+  ******* 7 stars * is 9 spaces to the right. * is 8 spaces to the right. * is 7 spaces to the right. * is 6 spaces to the right. * is 5 spaces to the right. * is 4 spaces to the right. * is 3 spaces to the right.
+ ******** 8 stars * is 9 spaces to the right. * is 8 spaces to the right. * is 7 spaces to the right. * is 6 spaces to the right. * is 5 spaces to the right. * is 4 spaces to the right. * is 3 spaces to the right. * is 2 spaces to the right.
+********* 9 stars * is 9 spaces to the right. * is 8 spaces to the right. * is 7 spaces to the right. * is 6 spaces to the right. * is 5 spaces to the right. * is 4 spaces to the right. * is 3 spaces to the right. * is 2 spaces to the right. * is 1 spaces to the right.
+123456789 
 
 Examples / Test Cases, Validate understanding of the problem:
+the method when passed the arguments 5 & 9 respectively have to have this output of the solution does not solve the 
+problem.
 
+test:
+triangle(5)
 
+output:
 
+    *
+   **
+  ***
+ ****
+*****
 
+test:
+triangle(9)
 
+output:
 
-
+        *
+       **
+      ***
+     ****
+    *****
+   ******
+  *******
+ ********
+*********
 
 Data Structure, How we represent data that we will work with when converting the input to output:
 
