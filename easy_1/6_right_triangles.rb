cb7671ede9634339e6aeccbfee63f935180a121c
Justin Greger <justingreger@gmail.com>
2023-06-15 19:31:46 -0400

    Syntax of Solution:

--- easy_1/6_right_triangles.rb
+++ easy_1/6_right_triangles.rb
@@ -643,4 +643,43 @@ method that is everything after this is outside of the triangle method.
 Further Exploration:
 Try modifying your solution again so that you can display the triangle with the right angle at any corner of the grid.
 
+Syntax of Solution:
+def triangle()
+  puts "How many stars do you want?"
+  num = gets.to_i
+
+  puts "What corner do you want the triangle to be in :top_right, :top_left, :bottom_right, :bottom_left?"
+  corner = gets.strip.to_sym
+
+  num.times do |n|
+    case corner
+    when :top_right
+      spaces = num - n - 1
+      stars = n + 1
+    when :top_left
+      spaces = 0
+      stars = n + 1
+    when :bottom_right
+      spaces = n
+      stars = num - n
+    when :bottom_left
+      spaces = 0
+      stars = num - n
+    else
+      puts "Invalid corner!"
+      return
+    end
+    puts (' ' * spaces) + ('*' * stars)
+  end
+end
+
+triangle()
+
+test:
+
+test:
+
+test:
+
+test:
 =end
