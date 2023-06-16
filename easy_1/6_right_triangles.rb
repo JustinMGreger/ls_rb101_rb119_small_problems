5601e1c979fa09821a57d9ae53f1f2ab9f43c0e5
Justin Greger <justingreger@gmail.com>
2023-06-15 20:05:26 -0400

    Explanation of Syntax of Solution:

--- easy_1/6_right_triangles.rb
+++ easy_1/6_right_triangles.rb
@@ -819,4 +819,36 @@ output:
 result:
 success.
 
+
+Explanation of Syntax of Solution:
+def triangle()
+  puts "How many stars do you want?"
+  num = gets.to_i
+
+  puts "What corner do you want the triangle to be in :top_right, :top_left, :bottom_right, :bottom_left?"
+  corner = gets.strip.to_sym
+
+  num.times do |n|
+    case corner
+    when :top_right
+      spaces = num - n - 1
+      stars = n + 1
+    when :top_left
+      spaces = 0
+      stars = n + 1
+    when :bottom_right
+      spaces = n
+      stars = num - n
+    when :bottom_left
+      spaces = 0
+      stars = num - n
+    else
+      puts "Invalid corner!"
+      return
+    end
+    puts (' ' * spaces) + ('*' * stars)
+  end
+end
+
+triangle()
 =end
