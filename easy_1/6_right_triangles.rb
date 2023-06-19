393af676ff8b743b3815256d5f33538f26a2159a
Justin Greger <justingreger@gmail.com>
2023-06-19 16:56:37 -0400

    broke the method into multiple methods

--- easy_1/6_right_triangles.rb
+++ easy_1/6_right_triangles.rb
@@ -1,29 +1,41 @@
-def triangle()
+def ask_for_stars
   puts 'How many stars do you want?'
-  num = gets.to_i
+  gets.to_i
+end
 
+def ask_for_corner
   puts 'What corner do you want the triangle to be in :top_right, :top_left, :bottom_right, :bottom_left?'
-  corner = gets.strip.to_sym
+  gets.strip.to_sym
+end
+
+def calculate_spaces_and_stars(corner, num, n)
+  case corner
+  when :top_right
+    [num - n - 1, n + 1]
+  when :top_left
+    [0, n + 1]
+  when :bottom_right
+    [n, num - n]
+  when :bottom_left
+    [0, num - n]
+  else
+    puts 'Invalid corner!'
+    nil
+  end
+end
+
+def print_line(spaces, stars)
+  puts (' ' * spaces) + ('*' * stars)
+end
+
+def triangle()
+  num = ask_for_stars
+  corner = ask_for_corner
 
   num.times do |n|
-    case corner
-    when :top_right
-      spaces = num - n - 1
-      stars = n + 1
-    when :top_left
-      spaces = 0
-      stars = n + 1
-    when :bottom_right
-      spaces = n
-      stars = num - n
-    when :bottom_left
-      spaces = 0
-      stars = num - n
-    else
-      puts 'Invalid corner!'
-      return
-    end
-    puts (' ' * spaces) + ('*' * stars)
+    spaces, stars = calculate_spaces_and_stars(corner, num, n)
+    return if spaces.nil? || stars.nil?
+    print_line(spaces, stars)
   end
 end
 
