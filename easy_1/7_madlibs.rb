a6c96bfb650e82eaa83162ae1089d3c27dc452c1
Justin Greger <justingreger@gmail.com>
2023-06-19 19:35:46 -0400

    example

---
+++ easy_1/7_madlibs.rb
@@ -0,0 +1,8 @@
+puts 'Enter a noun: dog'
+noun = gets
+puts 'Enter a verb: walk'
+verb = gets 
+puts 'Enter an adjective: blue'
+puts 'Enter an adverb: quickly'
+
+Do you walk your blue dog quickly? That's hilarious!
