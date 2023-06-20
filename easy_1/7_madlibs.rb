f686536f2de2a7a994ea41c9e786bc156c891c5c
Justin Greger <justingreger@gmail.com>
2023-06-19 20:28:29 -0400

    sample of 3 sentences of string interpolation

--- easy_1/7_madlibs.rb
+++ easy_1/7_madlibs.rb
@@ -7,4 +7,8 @@ adjective = gets.chomp
 puts 'Enter an adverb: '
 adverb = gets.chomp
 
-print "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
+sentence_1 = "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
+sentence_2 = "#{noun} #{verb} #{adverb} #{adjective}"
+sentence_3 =   "#{adverb} #{noun} #{verb} #{adjective}"
+
+puts [sentence_1, sentence_2, sentence_3].sample
