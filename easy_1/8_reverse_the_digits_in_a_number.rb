425cc9f8178c30a794096b6242711b1cbc1f397c
Justin Greger <justingreger@gmail.com>
2023-06-21 21:48:03 -0400

    tests

--- easy_1/8_reverse_the_digits_in_a_number.rb
+++ easy_1/8_reverse_the_digits_in_a_number.rb
@@ -37,3 +37,15 @@ def reversed_number(12345)
   and output:
   54321
   
+
+  Examples / Test Cases, Validate understanding of the problem:
+  
+  reversed_number(12345) == 54321
+  reversed_number(12213) == 31221
+  reversed_number(456) == 654
+  reversed_number(12000) == 21 # No leading zeros in return value!
+  reversed_number(12003) == 30021
+  reversed_number(1) == 1
+  
+  If the results are the same as the above then the understanding is correct.
+  
