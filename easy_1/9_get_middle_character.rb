20cc53a2e0dfe3d41fbceacf20552878233a14c5
Justin Greger <justingreger@gmail.com>
2023-06-23 18:49:53 -0400

    PEDAC

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -1,6 +1,7 @@
-Write a method that takes a non-empty string argument, and returns the middle character or characters of the argument. If
-the argument has an odd length, you should return exactly one character. If the argument has an even length, you should 
-return exactly two characters.
+Write a method that takes a non-empty string argument, 
+and returns the middle character or characters of the argument. 
+If the argument has an odd length, you should return exactly one character. 
+If the argument has an even length, you should return exactly two characters.
 
 Examples:
 
@@ -9,3 +10,14 @@ center_of('Launch School') == ' '
 center_of('Launch') == 'un'
 center_of('Launchschool') == 'hs'
 center_of('x') == 'x'
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+and output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
