9573bfa213e944c43f94170afd231b6119223ece
Justin Greger <justingreger@gmail.com>
2023-06-24 17:00:57 -0400

    if string.size.odd?

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -36,9 +36,34 @@ center_of is a method.
 this means:
 the center_of method accepts the (string) parameter and is defined as follows:
 
-
+Syntax:
   center_index = string.size / 2
+
+Explanation:
+center_index is a variable
+= means assignment
+string is a parameter
+.size means return the number of characters in the string.
+/ is division.
+2 is a integer.
+this means:
+the center_index variable is assigned the result of returning the number of characters in the string parameter then 
+dividing that number by the integer 2.
+
+Syntax:
   if string.size.odd?
+
+Explanation:
+if means if a condition is true then execute the code:
+string is a parameter
+.size means return the number of characters in the string.
+.odd? return true if integer is odd.
+this means:
+if the result of returning the number of characters in the string parameter and then return true if that integer is odd is
+true then execute the following code:
+
+
+
     string[center_index]
   else
     string[center_index - 1, 2]
@@ -47,3 +72,7 @@ end
 
 this means compilation:
 the center_of method accepts the (string) parameter and is defined as follows:
+the center_index variable is assigned the result of returning the number of characters in the string parameter then 
+dividing that number by the integer 2.
+if the result of returning the number of characters in the string parameter and then return true if that integer is odd is
+true then execute the following code:
