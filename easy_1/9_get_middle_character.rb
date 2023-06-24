c13cd24a9d3013eb7120d579cbdf9c665773ae6b
Justin Greger <justingreger@gmail.com>
2023-06-24 17:32:44 -0400

    string[center_index - 1, 2]

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -62,11 +62,56 @@ this means:
 if the result of returning the number of characters in the string parameter and then return true if that integer is odd is
 true then execute the following code:
 
-
-
+Syntax:
     string[center_index]
+
+Explanation:
+string is a parameter
+[center_index] this means extract the character in string at the index of center_index.
+center_index is a variable
+= means assignment
+string is a parameter
+.size means return the number of characters in the string.
+/ is division.
+2 is a integer.
+this means:
+the center_index variable is assigned the result of returning the number of characters in the string parameter then 
+dividing that number by the integer 2.
+this means:
+extract the character in the string parameter at the index of the center_index variable which is assigned the result of 
+returning the number of characters in the string parameter then dividing that number by the integer 2.
+
+Syntax:
   else
+
+Explanation:
+if the above if false then execute the following code:
+
+Syntax:
     string[center_index - 1, 2]
+
+Explanation:
+string is a parameter
+[center_index - 1, 2] are two string arugments.
+center_index - 1 is a string arugment specifies the starting index that contains the result of:
+center_index is a variable
+= means assignment
+string is a parameter
+.size means return the number of characters in the string.
+/ is division.
+2 is a integer.
+this means:
+the center_index variable is assigned the result of returning the number of characters in the string parameter then 
+dividing that number by the integer 2.
+- is subtraction.
+1 is a integer.
+2 is a string argument that specifies the length of the substring to be extracted.
+this means:
+extract a substring from the string parameter that starts at the position that is the result of the center_index variable
+which is assigned the result of returning the number of characters in the string parameter then dividing that number by 
+the integer 2 subtracted by 1 and has a length of 2.
+
+
   end
 end
 
@@ -76,3 +121,9 @@ the center_index variable is assigned the result of returning the number of char
 dividing that number by the integer 2.
 if the result of returning the number of characters in the string parameter and then return true if that integer is odd is
 true then execute the following code:
+extract the character in the string parameter at the index of the center_index variable which is assigned the result of 
+returning the number of characters in the string parameter then dividing that number by the integer 2.
+if the above if false then execute the following code:
+extract a substring from the string parameter that starts at the position that is the result of the center_index variable
+which is assigned the result of returning the number of characters in the string parameter then dividing that number by 
+the integer 2 subtracted by 1 and has a length of 2.
