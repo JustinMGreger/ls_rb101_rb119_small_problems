0f9857a938ac2ac0d1269f08f5c9b201cd86b761
Justin Greger <justingreger@gmail.com>
2023-06-23 20:11:08 -0400

    If the argument has an odd length, you should return exactly one character.

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -55,6 +55,6 @@ and Code.” Implementation of Algorithm:
 
 def center_of method(str)
   returns the middle character(s) of the argument. 
-3. If the argument has an odd length, you should return exactly one character. 
+  If the argument has an odd length, you should return exactly one character. 
 4. If the argument has an even length, you should return exactly two characters.
 5. end method.
