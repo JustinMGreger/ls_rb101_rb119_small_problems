76e7f3220f3b2483e33612b83cb5f638c1010e79
Justin Greger <justingreger@gmail.com>
2023-06-24 16:35:32 -0400

    LS Solution:

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -9,3 +9,12 @@ def center_of(str)
   puts middle_character
 end
  
+LS Solution:
+def center_of(string)
+  center_index = string.size / 2
+  if string.size.odd?
+    string[center_index]
+  else
+    string[center_index - 1, 2]
+  end
+end
