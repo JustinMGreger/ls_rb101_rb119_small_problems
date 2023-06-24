03d5b5141fe90bb710eb3c1ee191211ae1f15828
Justin Greger <justingreger@gmail.com>
2023-06-24 18:03:23 -0400

    def center_of(str)

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -9,3 +9,29 @@ def center_of(str)
                      end
   puts middle_character
 end
+
+Explanation of Syntax:
+Syntax:
+def center_of(str)
+
+Explanation:
+def means definition.
+center_of is a method.
+(str) is a parameter.
+this means:
+the center_of method accepts the (str) parameter and is defined as the following:
+
+
+
+
+  middle_index = str.length / 2
+  middle_character = if str.length.odd?
+                       "'#{str[middle_index]}'"
+                     else
+                       "'#{str[middle_index - 1..middle_index]}'"
+                     end
+  puts middle_character
+end
+
+this means compilation:
+the center_of method accepts the (str) parameter and is defined as the following:
