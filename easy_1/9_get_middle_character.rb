24cd87168998f070e154798ea79d020ee7ee1f9d
Justin Greger <justingreger@gmail.com>
2023-06-23 21:16:22 -0400

    argument is str

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -54,7 +54,7 @@ Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
 
 def center_of method(str)
-  returns the middle character(s) of the argument. 
-  If the argument has an odd length, you should return exactly one character. 
-  If the argument has an even length, you should return exactly two characters.
+  returns the middle character(s) str 
+  If str has an odd length, you should return exactly one character. 
+  If str has an even length, you should return exactly two characters.
 end 
