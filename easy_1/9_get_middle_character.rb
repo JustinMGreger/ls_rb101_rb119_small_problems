56f540b89d5e8ef56a32ee08c98d5c849d488b13
Justin Greger <justingreger@gmail.com>
2023-06-24 19:24:21 -0400

    #{str[middle_index]}

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -51,12 +51,36 @@ this means:
 the middle_character variable is assigned the result of: if return boolean true if the result of returning the number of 
 characters in the str parameter is odd is boolean true then execute the following code:
 
-  
+Syntax:  
+"'#{str[middle_index]}'"
+
+Explanation:
+"'#{str[middle_index]}'" is a string which contains:
+'#{str[middle_index]}' is a string which contains:
+#{str[middle_index]} string interpolation which contains:
+str[middle_index] which contains:
+str is a parameter. 
+[middle_index] means extract at the middle_index index which contains:
+middle_index is a variable.
+= is assignment.
+str is a parameter.
+.length returns the number of characters in a string.
+/ is division.
+2 is a integer.
+this means:
+the middle_index variable is assigned the result of returning the number of characters in the str parameter that is then
+divided by the integer 2.
+this means:
+the "'#{str[middle_index]}'" string contains: the '#{str[middle_index]}' string which contains: #{str[middle_index]} 
+string interpolation which contains: the result of extracting the character at the [middle_index] index which contains: 
+the middle_index variable which is assigned the result of returning the number of characters in the str parameter that is 
+then divided by the integer 2 of the str parameter.
 
 
 
 
-                       "'#{str[middle_index]}'"
+
+
                      else
                        "'#{str[middle_index - 1..middle_index]}'"
                      end
@@ -69,3 +93,7 @@ the middle_index variable is assigned the result of returning the number of char
 divided by the integer 2.
 the middle_character variable is assigned the result of: if return boolean true if the result of returning the number of 
 characters in the str parameter is odd is boolean true then execute the following code:
+the "'#{str[middle_index]}'" string contains: the '#{str[middle_index]}' string which contains: #{str[middle_index]} 
+string interpolation which contains: the result of extracting the character at the [middle_index] index which contains: 
+the middle_index variable which is assigned the result of returning the number of characters in the str parameter that is 
+then divided by the integer 2 of the str parameter.
