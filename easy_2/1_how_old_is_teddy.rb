3e01940ee0b973a51746822075c75908db2b3051
Justin Greger <justingreger@gmail.com>
2023-06-24 21:02:47 -0400

    deleted non essential code

--- easy_2/1_how_old_is_teddy.rb
+++ easy_2/1_how_old_is_teddy.rb
@@ -1,11 +1,3 @@
-Build a program that randomly generates and prints Teddy's age. 
-To get the age, you should generate a random number between 20 and 200.
-
-Example Output
-
-Teddy is 69 years old!
-
-Ideas:
 
 def teddy_age
   random_age = rand (20..200)
