a4ab37d44cef8fc29bb5620ef3b80abd8b43eba1
Justin Greger <justingreger@gmail.com>
2023-06-24 21:12:46 -0400

    double to single quotes

--- easy_2/1_how_old_is_teddy.rb
+++ easy_2/1_how_old_is_teddy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def teddy_age
-  puts "enter a name: "
+  puts 'enter a name: '
   name = gets.chomp
   random_age = rand(20..200)
   puts "#{name} is #{random_age} years old!"
