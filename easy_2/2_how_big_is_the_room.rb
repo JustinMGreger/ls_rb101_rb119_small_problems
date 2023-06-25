f3c49e286dc89b4c4f714c1abcf548837456e7fc
Justin Greger <justingreger@gmail.com>
2023-06-24 21:29:13 -0400

    length = 10

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -7,7 +7,7 @@ Do not worry about validating the input at this time.
 
 def room_size
 puts "Enter the length of the room in meters: "
-10
+length = 10
 puts "Enter the width of the room in meters: "
 7
 puts "The area of the room is 70.0 square meters (753.47 square feet)."
