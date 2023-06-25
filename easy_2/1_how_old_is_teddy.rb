db9026f2bb6ba08f74477dc639887ef4894c7bfe
Justin Greger <justingreger@gmail.com>
2023-06-24 20:58:42 -0400

    age inside the method

--- easy_2/1_how_old_is_teddy.rb
+++ easy_2/1_how_old_is_teddy.rb
@@ -8,13 +8,13 @@ Teddy is 69 years old!
 Ideas:
 
 def teddy_age
-
+  age = generate a random number between 20 and 200.
   puts "Teddy is #{age} years old!"
 end  
 
 
-  that randomly generates and prints Teddy's age. 
-  age = generate a random number between 20 and 200.
+
+
 
 Example Output
 
