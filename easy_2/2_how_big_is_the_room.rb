11d17d32149a1b7449e5ff1998fde1ccf544797c
Justin Greger <justingreger@gmail.com>
2023-06-25 19:25:26 -0400

    removed   sqmeters_to_sqfeet = 10.7639

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 def room_size
-  sqmeters_to_sqfeet = 10.7639
   puts 'Enter the length of the room in feet: '
   length = gets.to_f
   puts 'Enter the width of the room in feet: '
