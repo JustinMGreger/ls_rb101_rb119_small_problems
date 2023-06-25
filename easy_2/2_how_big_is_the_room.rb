39ea62b30050a4ef2d8c325c67c727af8d43e427
Justin Greger <justingreger@gmail.com>
2023-06-25 19:00:48 -0400

    puts 'Enter the width of the room in feet: '

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -4,7 +4,7 @@ def room_size
   sqmeters_to_sqfeet = 10.7639
   puts 'Enter the length of the room in feet: '
   length = gets.to_f
-  puts 'Enter the width of the room in meters: '
+  puts 'Enter the width of the room in feet: '
   width = gets.to_f
   area_square_meter = (length * width).round(2)
   puts "The area of the room is #{area_square_meter} square meters (#{area_square_feet} square feet)."
@@ -14,5 +14,4 @@ room_size
 
 
 Further Exploration
-Modify this program to ask for the input measurements in feet, 
 and display the results in square feet, square inches, and square centimeters.
