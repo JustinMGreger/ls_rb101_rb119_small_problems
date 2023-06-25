03ac5289c03bb3dde60386b01d1ce47b5ab2dfce
Justin Greger <justingreger@gmail.com>
2023-06-24 21:51:17 -0400

    area_square_feet  = (area_square_meter * 10.7639).round(2)

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -11,7 +11,7 @@ def room_size
   puts "Enter the width of the room in meters: "
   width = gets.to_f
   area_square_meter = (length * width).round(2)
-  area_square_feet  = area_square_meter * 10.7639
+  area_square_feet  = (area_square_meter * 10.7639).round(2)
   puts "The area of the room is #{area_square_meter} square meters (#{area_square_feet} square feet)."
 end
 
