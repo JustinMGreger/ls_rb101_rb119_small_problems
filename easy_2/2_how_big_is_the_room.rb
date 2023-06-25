bc1de350a6706c9482f020c815ef044aca71284a
Justin Greger <justingreger@gmail.com>
2023-06-24 21:31:59 -0400

    shifted to the left

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -6,10 +6,10 @@ Note: 1 square meter == 10.7639 square feet
 Do not worry about validating the input at this time.
 
 def room_size
-puts "Enter the length of the room in meters: "
-length = 10
-puts "Enter the width of the room in meters: "
-width = 7
-area = length * width
-puts "The area of the room is 70.0 square meters (753.47 square feet)."
+  puts "Enter the length of the room in meters: "
+  length = 10
+  puts "Enter the width of the room in meters: "
+  width = 7
+  area = length * width
+  puts "The area of the room is 70.0 square meters (753.47 square feet)."
 end
