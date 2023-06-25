fa954fdd07e005a3c5cd7e10e43822a85b4d04e7
Justin Greger <justingreger@gmail.com>
2023-06-25 18:59:20 -0400

    removed sqmeters_to_sqfeet

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -7,7 +7,6 @@ def room_size
   puts 'Enter the width of the room in meters: '
   width = gets.to_f
   area_square_meter = (length * width).round(2)
-  area_square_feet  = (area_square_meter * sqmeters_to_sqfeet).round(2)
   puts "The area of the room is #{area_square_meter} square meters (#{area_square_feet} square feet)."
 end
 
