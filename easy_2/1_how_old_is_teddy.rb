b44cfda285d3c97937fbb2f849a304dcf0b659a4
Justin Greger <justingreger@gmail.com>
2023-06-24 20:54:42 -0400

    age = generate a random number between 20 and 200.

--- easy_2/1_how_old_is_teddy.rb
+++ easy_2/1_how_old_is_teddy.rb
@@ -3,3 +3,12 @@ To get the age, you should generate a random number between 20 and 200.
 
 Example Output
 
+Teddy is 69 years old!
+
+Ideas:
+Build a program that randomly generates and prints Teddy's age. 
+age = generate a random number between 20 and 200.
+
+Example Output
+
+Teddy is 69 years old!
