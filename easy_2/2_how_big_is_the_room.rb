a7f2b3a112b7da75f1a4cc06b6d2309724dbf122
Justin Greger <justingreger@gmail.com>
2023-06-24 22:00:47 -0400

    sqmeters_to_sqfeet = 10.7639

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 def room_size
+  sqmeters_to_sqfeet = 10.7639
   puts 'Enter the length of the room in meters: '
   length = gets.to_f
   puts 'Enter the width of the room in meters: '
   width = gets.to_f
   area_square_meter = (length * width).round(2)
-  area_square_feet  = (area_square_meter * 10.7639).round(2)
+  area_square_feet  = (area_square_meter * sqmeters_to_sqfeet).round(2)
   puts "The area of the room is #{area_square_meter} square meters (#{area_square_feet} square feet)."
 end
 
