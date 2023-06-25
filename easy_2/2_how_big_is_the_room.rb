8c8100c9269622187217705e724b131f68f56a04
Justin Greger <justingreger@gmail.com>
2023-06-25 19:07:46 -0400

    (#{area_square_inches} square inches)

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -7,7 +7,8 @@ def room_size
   puts 'Enter the width of the room in feet: '
   width = gets.to_f
   area_square_feet = (length * width).round(2)
-  puts "The area of the room is #{area_square_feet} square feet (#{area_square_feet} square inches)(#{} square centimeters)."
+
+  puts "The area of the room is #{area_square_feet} square feet (#{area_square_inches} square inches)(#{} square centimeters)."
 end
 
 room_size
