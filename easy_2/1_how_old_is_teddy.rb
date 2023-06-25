81ce389a66470e9f49cedce41cb60bdccb7a35ce
Justin Greger <justingreger@gmail.com>
2023-06-24 21:10:02 -0400

    name = gets.chomp

--- easy_2/1_how_old_is_teddy.rb
+++ easy_2/1_how_old_is_teddy.rb
@@ -2,6 +2,7 @@
 
 def teddy_age
   puts "enter a name: "
+  name = gets.chomp
   random_age = rand(20..200)
   puts "Teddy is #{random_age} years old!"
 end
@@ -11,5 +12,5 @@ teddy_age
 Further Exploration
 Modify this program to ask for a name, and then print the age for that person. 
 
-name =
+
 #{name}
