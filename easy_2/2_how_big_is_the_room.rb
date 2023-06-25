a2f680a6377f73dc66e72870b7290c49dbbb0b6b
Justin Greger <justingreger@gmail.com>
2023-06-24 21:48:50 -0400

    removed variables and consolidated the round 2 decimals into 1 variable

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -10,10 +10,8 @@ def room_size
   length = gets.to_f
   puts "Enter the width of the room in meters: "
   width = gets.to_f
-  area = length * width
-  area_square_meter = area.round(2)
-  square_feet = area_square_meter * 10.7639 
-  area_square_feet = square_feet.round(2)
+  area_square_meter = (length * width).round(2)
+  area_square_feet  = area_square_meter * 10.7639
   puts "The area of the room is #{area_square_meter} square meters (#{area_square_feet} square feet)."
 end
 
