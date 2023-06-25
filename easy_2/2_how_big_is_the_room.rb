7ad7a68770997e64273ee659f466d59563629650
Justin Greger <justingreger@gmail.com>
2023-06-25 19:00:13 -0400

    puts 'Enter the length of the room in feet: '

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -2,7 +2,7 @@
 
 def room_size
   sqmeters_to_sqfeet = 10.7639
-  puts 'Enter the length of the room in meters: '
+  puts 'Enter the length of the room in feet: '
   length = gets.to_f
   puts 'Enter the width of the room in meters: '
   width = gets.to_f
