bffa81e8f81ec1d3e01b847b5ffbe3230360cc38
Justin Greger <justingreger@gmail.com>
2023-06-25 19:53:16 -0400

    tip_rate = gets.chomp.to_f

--- easy_2/3_tip_calculator.rb
+++ easy_2/3_tip_calculator.rb
@@ -1,6 +1,6 @@
 def tip_calculator
   puts 'What is the bill?' 
-   and a tip rate. 
+  tip_rate = gets.chomp.to_f 
 The program must compute the tip and then display both the tip and the total amount of the bill.
 end
 Example:
