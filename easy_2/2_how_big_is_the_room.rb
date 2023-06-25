3d9d6c67d36d8abe75b6aacc76b7ba4aa92f2bb2
Justin Greger <justingreger@gmail.com>
2023-06-24 21:39:02 -0400

    area_square_feet = area_square_meter * 10.7639

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -1,7 +1,7 @@
 Build a program that asks a user for the length and width of a room in meters 
 and then displays the area of the room in both square meters and square feet.
 
-Note: 1  == 10.7639 square feet
+Note: 1  == 10.7639 
 
 Do not worry about validating the input at this time.
 
@@ -11,6 +11,7 @@ def room_size
   puts "Enter the width of the room in meters: "
   width = gets.to_f
   area_square_meter = length * width
+  area_square_feet = area_square_meter * 10.7639 
   puts "The area of the room is #{area_square_meter} square meters (753.47 square feet)."
 end
 
