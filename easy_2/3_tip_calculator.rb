c24bec5087fa0cd4cb544654ec4b660c32a5018b
Justin Greger <justingreger@gmail.com>
2023-06-25 19:41:57 -0400

    instructions

---
+++ easy_2/3_tip_calculator.rb
@@ -0,0 +1,5 @@
+Create a simple tip calculator. 
+The program should prompt for a bill amount and a tip rate. 
+The program must compute the tip and then display both the tip and the total amount of the bill.
+
+Example:
