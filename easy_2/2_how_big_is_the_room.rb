81bf3a1e53d194e33bd1f4a889f236213cb186bb
Justin Greger <justingreger@gmail.com>
2023-06-24 21:55:56 -0400

    double to single quotes

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 def room_size
-  puts "Enter the length of the room in meters: "
+  puts 'Enter the length of the room in meters: '
   length = gets.to_f
-  puts "Enter the width of the room in meters: "
+  puts 'Enter the width of the room in meters: '
   width = gets.to_f
   area_square_meter = (length * width).round(2)
   area_square_feet  = (area_square_meter * 10.7639).round(2)
