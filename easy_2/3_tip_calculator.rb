5f82e94f52e9f42f984d57ece7d1dce2ea0a6c96
Justin Greger <justingreger@gmail.com>
2023-06-25 21:43:00 -0400

    removed non essential code and format('%.2f', tip)

--- easy_2/3_tip_calculator.rb
+++ easy_2/3_tip_calculator.rb
@@ -7,14 +7,9 @@ def tip_calculator
   tip_rate = gets.chomp.to_f / 100
   tip = (bill * tip_rate).round(2)
   total = (tip + bill).round(2)
-  puts "The tip is $#{tip}"
+  puts "The tip is $#{format('%.2f', tip)}"
   puts "The total is $#{total}"
 end
 
 tip_calculator
 
-Further Exploration
-
-Our solution prints the results as $30.0 and $230.0 instead of the more usual $30.00 and $230.00. Modify your solution so it always prints the results with 2 decimal places.
-
-Hint: You will likely need Kernel#format for this.
