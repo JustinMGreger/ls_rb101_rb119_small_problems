218b1ddef7e85ca194164231cdc0c4dadcb26fb3
Justin Greger <justingreger@gmail.com>
2023-06-25 21:54:48 -0400

    def what_is_the_tip_percentage   puts 'What is the tip percentage?' end

--- easy_2/3_tip_calculator.rb
+++ easy_2/3_tip_calculator.rb
@@ -4,9 +4,13 @@ def what_is_the_bill
   puts 'What is the bill?'
 end
 
+def what_is_the_tip_percentage
+  puts 'What is the tip percentage?'
+end
+
 def tip_calculator()
   bill = gets.chomp.to_f
-  puts 'What is the tip percentage?'
+
   tip_rate = gets.chomp.to_f / 100
   tip = (bill * tip_rate).round(2)
   total = (tip + bill).round(2)
