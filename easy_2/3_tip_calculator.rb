7a45611ac3d83acf9207263130a7f121e7a61223
Justin Greger <justingreger@gmail.com>
2023-06-25 21:31:49 -0400

    removed a space

--- easy_2/3_tip_calculator.rb
+++ easy_2/3_tip_calculator.rb
@@ -6,7 +6,7 @@ def tip_calculator
   puts 'What is the tip percentage?'
   tip_rate = gets.chomp.to_f / 100
   tip = (bill * tip_rate).round(2)
-  total = tip +  bill
+  total = tip + bill
   puts "The tip is $#{tip}"
   puts "The total is $#{total}"
 end
