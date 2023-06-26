c1fac399235156a083ff4452dada832b3f3fd419
Justin Greger <justingreger@gmail.com>
2023-06-25 21:30:41 -0400

    removed trailing whitespaces

--- easy_2/3_tip_calculator.rb
+++ easy_2/3_tip_calculator.rb
@@ -1,8 +1,8 @@
-# frozen_string_literal: true 
+# frozen_string_literal: true
 
 def tip_calculator
   puts 'What is the bill?'
-  bill = gets.chomp.to_f 
+  bill = gets.chomp.to_f
   puts 'What is the tip percentage?'
   tip_rate = gets.chomp.to_f / 100
   tip =  (bill * tip_rate).round(2)
