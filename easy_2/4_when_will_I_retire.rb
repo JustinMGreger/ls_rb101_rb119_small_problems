952eb905a5ad3d519218a8312ae641c4ef890875
Justin Greger <justingreger@gmail.com>
2023-06-26 21:10:41 -0400

    def retirement

--- easy_2/4_when_will_I_retire.rb
+++ easy_2/4_when_will_I_retire.rb
@@ -10,11 +10,11 @@ def input
   gets.chomp.to_i
 end
 
+def retirement
 prompt 'What is your age?'
 age = input
 prompt 'At what age would you like to retire?' 
 retire = input 
-
 prompt "It's 2016. You will retire in 2056."
 prompt "You have only 40 years of work to go!"
 end
