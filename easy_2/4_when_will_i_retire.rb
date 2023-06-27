b2874348ddd996615bf1be170547c764c47cea61
Justin Greger <justingreger@gmail.com>
2023-06-27 17:30:18 -0400

    current_year = Time.now.year

--- easy_2/4_when_will_i_retire.rb
+++ easy_2/4_when_will_i_retire.rb
@@ -21,6 +21,7 @@ def retirement
   age = input
   prompt 'At what age would you like to retire?'
   retirement_age = input
+  current_year = Time.now.year
   work_years = years_of_work(retirement_age, age)
   retirement_year = retirement_year(work_years)
   prompt "It's 2016. You will retire in #{retirement_year}."
