2da4c1b590d4314fca3c95f3a9fec1e480ce09e6
Justin Greger <justingreger@gmail.com>
2023-06-26 21:04:44 -0400

    prompt

--- easy_2/4_when_will_I_retire.rb
+++ easy_2/4_when_will_I_retire.rb
@@ -11,6 +11,6 @@ prompt 'What is your age?'
 prompt 'At what age would you like to retire?' 
 70
 
-"It's 2016. You will retire in 2056."
-"You have only 40 years of work to go!"
+prompt "It's 2016. You will retire in 2056."
+prompt "You have only 40 years of work to go!"
 end
