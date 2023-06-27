37dddd064d4a637ecea79a822203fab2862a6169
Justin Greger <justingreger@gmail.com>
2023-06-27 18:26:24 -0400

    user_input = Bob

--- easy_2/5_greeting_a_user.rb
+++ easy_2/5_greeting_a_user.rb
@@ -3,7 +3,8 @@ The program will then greet the user.
 If the user writes "name!" then the computer yells back to the user.
 
 Examples
-What is your name? Bob
+What is your name? 
+user_input = Bob
 Hello Bob.
 What is your name? Bob!
 HELLO BOB. WHY ARE WE SCREAMING?
