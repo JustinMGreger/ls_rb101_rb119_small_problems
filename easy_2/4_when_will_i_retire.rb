105566b8913b97fd29f968c0c68044bb92bc3ebd
Justin Greger <justingreger@gmail.com>
2023-06-26 21:55:24 -0400

    code from file that was deleted

---
+++ easy_2/4_when_will_i_retire.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+def prompt(message)
+  puts message
+end
+
+def input
+  gets.chomp.to_i
+end
+
+def years_of_work(retirement_age, age)
+  retirement_age - age
+end
+
+def retirement_year(years_of_work)
+  years_of_work + 2016
+end
+
+def retirement
+  prompt 'What is your age?'
+  age = input
+  prompt 'At what age would you like to retire?'
+  retirement_age = input 
+  work_years = years_of_work(retirement_age, age)
+  retirement_year = retirement_year(work_years)
+  prompt "It's 2016. You will retire in #{retirement_year}."
+  prompt "You have only #{work_years} years of work to go!"
+end
+
+retirement
