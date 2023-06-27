0670d3ad3260e330eee6c12e8a3e44cd0ad956f2
Justin Greger <justingreger@gmail.com>
2023-06-27 19:54:13 -0400

    removed trailing whitespaces

--- easy_2/5_greeting_a_user.rb
+++ easy_2/5_greeting_a_user.rb
@@ -5,12 +5,12 @@ def prompt(message)
 end
 
 def user_name
-  prompt 'What is your name?' 
+  prompt 'What is your name?'
   name = gets.chomp
   if name.end_with?('!')
     name = name.chop
     prompt "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
-  else 
+  else
     prompt "Hello #{name}."
   end
 end
