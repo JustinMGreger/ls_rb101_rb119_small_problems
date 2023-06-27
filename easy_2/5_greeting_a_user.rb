4737166a0517e48772b77035adbd270f7ea0f696
Justin Greger <justingreger@gmail.com>
2023-06-27 19:48:11 -0400

    else prompt Hello #{name}.

--- easy_2/5_greeting_a_user.rb
+++ easy_2/5_greeting_a_user.rb
@@ -8,8 +8,7 @@ def user_name
   prompt 'What is your name?' 
   name = gets.chomp
   if name.end_with?('!')
-  else 
-  prompt "Hello #{name}."
+  else prompt "Hello #{name}."
   name = name.chop
   name = Bob!
   prompt "HELLO BOB. WHY ARE WE SCREAMING?"
