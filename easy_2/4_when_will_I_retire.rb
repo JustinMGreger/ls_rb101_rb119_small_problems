a5b0b669cf3c932b255c28d0ad896ca49c6e40e2
Justin Greger <justingreger@gmail.com>
2023-06-26 20:57:55 -0400

    double quotes

--- easy_2/4_when_will_I_retire.rb
+++ easy_2/4_when_will_I_retire.rb
@@ -7,5 +7,6 @@ Example:
 'At what age would you like to retire?' 
 70
 
-It's 2016. You will retire in 2056.
-You have only 40 years of work to go!
+"It's 2016. You will retire in 2056."
+"You have only 40 years of work to go!"
+end
