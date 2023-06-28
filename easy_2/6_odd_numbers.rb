835cf379cb624ba18e02718bb9e577668b48d1e6
Justin Greger <justingreger@gmail.com>
2023-06-28 18:00:52 -0400

    removed puts

--- easy_2/6_odd_numbers.rb
+++ easy_2/6_odd_numbers.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def odd_numbers
-  (1..99).to_a.select { |value| puts value.odd? }.each { |value| puts value }
+  (1..99).to_a.select { |value| value.odd? }.each { |value| puts value }
 end
 
 odd_numbers
