e37f95973de74d1140aa4105e2584ca464696eff
Justin Greger <justingreger@gmail.com>
2023-06-27 21:33:17 -0400

    LS solution

--- easy_2/6_odd_numbers.rb
+++ easy_2/6_odd_numbers.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 def odd_numbers
-  (1..99).each do |number|
-    puts number if number.odd?
+  value = 1
+  while value <= 99
+    puts value
+    value += 2
   end
 end
 
@@ -11,4 +13,4 @@ odd_numbers
 Further Exploration
 
 Repeat this exercise with a technique different from the one you just used, and different from the solution shown above. 
-You may want to explore the use Integer#upto or Array#select methods, or maybe use Integer#odd?
+You may want to explore the use Integer#upto or Array#select methods 
