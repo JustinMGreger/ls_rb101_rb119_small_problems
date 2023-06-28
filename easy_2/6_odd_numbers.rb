56981cab3704f671b55a31e75704b8a7b6ccc29a
Justin Greger <justingreger@gmail.com>
2023-06-27 21:26:52 -0400

    Further Exploration
    Repeat this exercise with a technique different from the one you just used, and different from the solution shown above.
    You may want to explore the use Integer#upto or Array#select methods, or maybe use Integer#odd?

--- easy_2/6_odd_numbers.rb
+++ easy_2/6_odd_numbers.rb
@@ -7,3 +7,8 @@ def odd_numbers
 end
 
 odd_numbers
+
+Further Exploration
+
+Repeat this exercise with a technique different from the one you just used, and different from the solution shown above. 
+You may want to explore the use Integer#upto or Array#select methods, or maybe use Integer#odd?
