0859eb02a4bd41526750915d7a6a425cf6f68c35
Justin Greger <justingreger@gmail.com>
2023-06-27 20:16:18 -0400

    removed further exploration and modified chomp and chop

--- easy_2/5_greeting_a_user.rb
+++ easy_2/5_greeting_a_user.rb
@@ -6,9 +6,9 @@ end
 
 def user_name
   prompt 'What is your name?'
-  name = gets.chomp
+  name = gets.chomp!
   if name.end_with?('!')
-    name = name.chop
+    name = name.chop!
     prompt "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
   else
     prompt "Hello #{name}."
@@ -16,6 +16,3 @@ def user_name
 end
 
 user_name
-
-Further Exploration
-Try modifying our solution to use String#chomp! and String#chop!, respectively.
