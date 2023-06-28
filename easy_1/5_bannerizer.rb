0667993468d2766278bcc45666f24d6c8b1d79e1
Justin Greger <justingreger@gmail.com>
2023-06-28 16:29:01 -0400

    prompt empty_line

--- easy_1/5_bannerizer.rb
+++ easy_1/5_bannerizer.rb
@@ -27,7 +27,7 @@ def print_in_box(message)
   empty_line = "|#{' ' * (max_line_length + 2)}|"
 
   prompt horizontal_rule
-  puts empty_line
+  prompt empty_line
   message.split("\n").each { |line| puts "| #{line.ljust(max_line_length)} |" }
   puts empty_line
   puts horizontal_rule
