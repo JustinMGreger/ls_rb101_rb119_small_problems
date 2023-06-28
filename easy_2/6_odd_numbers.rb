fe4621cc8666edea4a943db9a343f543b7af887a
Justin Greger <justingreger@gmail.com>
2023-06-27 21:45:37 -0400

    removed end

--- easy_2/6_odd_numbers.rb
+++ easy_2/6_odd_numbers.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 def odd_numbers
-  1.upto
-  while value <= 99
+  value = 1.upto 99
     puts value
     value += 2
-  end
 end
 
 odd_numbers
 
 Further Exploration
 
-Repeat this exercise You may want to explore the use  or Array#select methods 
+Repeat this exercise You may want to explore the use  Array#select methods 
