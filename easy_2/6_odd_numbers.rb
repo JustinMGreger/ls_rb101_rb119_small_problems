bc8525d78c72c563317b69f046d0dd43b75ab16b
Justin Greger <justingreger@gmail.com>
2023-06-27 21:55:04 -0400

    rubocop demanded .odd?

--- easy_2/6_odd_numbers.rb
+++ easy_2/6_odd_numbers.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 def odd_numbers
-  1.upto(99) {|value| puts value if value % 2 != 0 }
+  1.upto(99) { |value| puts value if value.odd? }
 end
 
 odd_numbers
-
-Further Exploration
-
-Repeat this exercise You may want to explore the use  Array#select methods 
