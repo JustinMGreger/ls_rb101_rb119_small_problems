5b8d633e045cb17b9d2bb18f65a44f041ec2cfd7
Justin Greger <justingreger@gmail.com>
2023-06-28 21:14:35 -0400

    if input == s

--- easy_2/8_sum_or_product_of_consecutive_integers.rb
+++ easy_2/8_sum_or_product_of_consecutive_integers.rb
@@ -10,10 +10,10 @@ end
 
 def sum_or_product_of_consecutive_integers
 prompt '>> Please enter an integer greater than 0:'
-5
+input
 prompt '>> Enter 's' to compute the sum, 'p' to compute the product.'
-if 's'
-  prompt "The sum of the integers between 1 and #{5} is #{15}."
+if input == 's'
+  prompt "The sum of the integers between 1 and #{input} is #{15}."
 else
   prompt The product of the integers between 1 and #{6} is #{720}.
 
