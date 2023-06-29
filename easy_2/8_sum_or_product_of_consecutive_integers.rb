9b5a5c96e0d1405517da6b1cd254a6c67b10fa6c
Justin Greger <justingreger@gmail.com>
2023-06-28 21:15:39 -0400

    The sum of the integers between 1 and #{input} is #{sum}.

--- easy_2/8_sum_or_product_of_consecutive_integers.rb
+++ easy_2/8_sum_or_product_of_consecutive_integers.rb
@@ -11,6 +11,12 @@ end
 def sum_or_product_of_consecutive_integers
 prompt '>> Please enter an integer greater than 0:'
 input
+
+The sum of the integers between 1 and #{input} is #{sum}.
+1
+
+
+
 prompt '>> Enter 's' to compute the sum, 'p' to compute the product.'
 if input == 's'
   prompt "The sum of the integers between 1 and #{input} is #{15}."
