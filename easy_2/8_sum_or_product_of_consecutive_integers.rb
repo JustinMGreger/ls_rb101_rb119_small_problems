4efe6fa6545efb7f2970cb4738e5b0b11743882d
Justin Greger <justingreger@gmail.com>
2023-06-28 21:41:07 -0400

    single quotes to double quotes

--- easy_2/8_sum_or_product_of_consecutive_integers.rb
+++ easy_2/8_sum_or_product_of_consecutive_integers.rb
@@ -19,7 +19,7 @@ end
 def sum_or_product_of_consecutive_integers
   prompt '>> Please enter an integer greater than 0:'
   number = input
-  prompt '>> Enter 's' to compute the sum, 'p' to compute the product.'
+  prompt ">> Enter 's' to compute the sum, 'p' to compute the product."
   operation = input
   if operation == 's'
     sum = sum_of_integers(number)
