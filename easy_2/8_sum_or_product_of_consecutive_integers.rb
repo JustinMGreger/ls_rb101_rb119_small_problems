e5b9bbd8dc5492b366958c16345fc750f2cb990d
Justin Greger <justingreger@gmail.com>
2023-06-29 19:53:52 -0400

    examples

--- easy_2/8_sum_or_product_of_consecutive_integers.rb
+++ easy_2/8_sum_or_product_of_consecutive_integers.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+Write a program that asks the user to enter an integer greater than 0, then asks if the user wants to determine the sum or product of all numbers between 1 and the entered integer.
+
+Examples:
+
 >> Please enter an integer greater than 0:
 5
 >> Enter 's' to compute the sum, 'p' to compute the product.
