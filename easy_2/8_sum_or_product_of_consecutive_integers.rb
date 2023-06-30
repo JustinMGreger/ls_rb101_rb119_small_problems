68aa5d0ab1eec80bf3e807cd6b47b74bb8418b9d
Justin Greger <justingreger@gmail.com>
2023-06-29 20:02:21 -0400

    moved further exploration into the instructions

--- easy_2/8_sum_or_product_of_consecutive_integers.rb
+++ easy_2/8_sum_or_product_of_consecutive_integers.rb
@@ -4,6 +4,7 @@ Write a program that asks the user to enter an integer greater than 0, then asks
 product of all numbers between 1 and the entered integer.
 
 Your own solution probably should try to validate input and issue error messages as needed.
+Try to use #inject in your solution to this problem.
 
 Examples:
 
@@ -19,7 +20,3 @@ The sum of the integers between 1 and 5 is 15.
 >> Enter 's' to compute the sum, 'p' to compute the product.
 p
 The product of the integers between 1 and 6 is 720.
-
-Further Exploration
-
-Try to use #inject in your solution to this problem.
