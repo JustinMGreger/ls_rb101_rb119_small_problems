3fffd71dfd914eecfc985f11f1926d97d0e73ab7
Justin Greger <justingreger@gmail.com>
2023-06-29 20:27:23 -0400

    if input is != s or p then put 'error' and go back to asking the question in a loop until it is correct.

--- easy_2/8_sum_or_product_of_consecutive_integers.rb
+++ easy_2/8_sum_or_product_of_consecutive_integers.rb
@@ -16,9 +16,10 @@ prompt '>> Please enter an integer greater than 0:'
 input = 5
 if input is <= 0 then put 'error' and go back to asking the question in a loop until it is correct.
 
-
 prompt ">> Enter 's' to compute the sum, 'p' to compute the product."
 input = s
 prompt "The sum of the integers between 1 and #{5} is #{15}."
 input = p
 prompt "The product of the integers between 1 and #{6} is #{720}."
+
+if input is != s or p then put 'error' and go back to asking the question in a loop until it is correct.
