1de52373c64c0f1e533abd9225dd450c55c2b1c8
Justin Greger <justingreger@gmail.com>
2023-06-29 20:07:57 -0400

    deleted >> Please enter an integer greater than 0:

--- easy_2/8_sum_or_product_of_consecutive_integers.rb
+++ easy_2/8_sum_or_product_of_consecutive_integers.rb
@@ -14,9 +14,6 @@ Examples:
 s
 The sum of the integers between 1 and 5 is 15.
 
-
->> Please enter an integer greater than 0:
-6
 >> Enter 's' to compute the sum, 'p' to compute the product.
 p
 The product of the integers between 1 and 6 is 720.
