e4fb2fe0668fddb29039c45af309f44880c1e910
Justin Greger <justingreger@gmail.com>
2023-06-29 21:18:13 -0400

    shifted text in if elsif else to the right

--- easy_2/8_sum_or_product_of_consecutive_integers.rb
+++ easy_2/8_sum_or_product_of_consecutive_integers.rb
@@ -18,13 +18,11 @@ prompt '>> Please enter an integer greater than 0:'
 input = 5
 if input is <= 0 then prompt 'error' and go back to asking the question in a loop until it is correct.
 
-prompt ">> Enter 's' to compute the sum, 'p' to compute the product."
-if
-operation = 's'
-prompt "The sum of the integers between 1 and #{5} is #{15}."
-elsif
-operation = 'p'
-prompt "The product of the integers between 1 and #{6} is #{720}."
+  prompt ">> Enter 's' to compute the sum, 'p' to compute the product."
+if operation = 's'
+  prompt "The sum of the integers between 1 and #{5} is #{15}."
+elsif operation = 'p'
+  prompt "The product of the integers between 1 and #{6} is #{720}."
 else
-prompt 'error' and go back to asking the question in a loop until it is correct.
+  prompt 'error' and go back to asking the question in a loop until it is correct.
 end
