53c52c49ac002e2b86ae48dad46e7af7b73994fc
Justin Greger <justingreger@gmail.com>
2023-06-30 21:41:06 -0400

    save_name = name

--- easy_2/9_string_assignment.rb
+++ easy_2/9_string_assignment.rb
@@ -13,7 +13,7 @@ What does this code print out? Think about it for a moment before continuing.
 # Bob
 
 you are 100% correct, and the answer should come as no surprise. Now, let's look at something a bit different:
-name = 'Bob'
-save_name = name
+name = 'Bob'                 # the name variable is assigned the 'Bob' string.
+save_name = name            # the save_name variable is assigned the name variable which is assigned the 'Bob' string.
 name.upcase!
 puts name, save_name
