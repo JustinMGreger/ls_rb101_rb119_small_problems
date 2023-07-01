efaedc30f530da2235051b0a4316d68af378e860
Justin Greger <justingreger@gmail.com>
2023-07-01 13:23:55 -0400

    removed   end

--- easy_1/5_bannerizer.rb
+++ easy_1/5_bannerizer.rb
@@ -14,7 +14,6 @@ end
 
 def add_word_to_line(lines, word, max_message_width)
   lines << '' if (lines[-1] + word + ' ').length > max_message_width
-  end
   lines[-1] += word + ' ' unless word.empty?
   lines
 end
