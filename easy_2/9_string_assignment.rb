156e1fa9872cbe3096d37a08824ba41018605868
Justin Greger <justingreger@gmail.com>
2023-07-01 14:24:09 -0400

    # the save_name variable is assigned the name variable which is assigned the 'BOB' string.

--- easy_2/9_string_assignment.rb
+++ easy_2/9_string_assignment.rb
@@ -22,3 +22,4 @@ save_name = name #  the save_name variable is assigned the name variable which i
 
 puts name, save_name #puts is put s which is put string which means print to the screen the string.
 # the name variable is assigned the 'BOB' string.
+# the save_name variable is assigned the name variable which is assigned the 'BOB' string.
