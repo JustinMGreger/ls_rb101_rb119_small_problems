156889efbe1b4aa784d3ac11af88de0ca971206f
Justin Greger <justingreger@gmail.com>
2023-06-30 21:24:20 -0400

    name.upcase!

--- easy_2/9_string_assignment.rb
+++ easy_2/9_string_assignment.rb
@@ -10,6 +10,6 @@ What does this print out?
 
 Can you explain these results?
 name = 'Bob'    name is assigned 'Bob'
-save_name = name
-name.upcase!
-puts name, save_name
+save_name = name    save_name is assigned name which is assigned 'Bob'
+name.upcase!            name which is assigned 'Bob' is upcased which is 'BOB'
+puts name, save_name   
