40fa201cf319a01af6c435a5d637931c4bb190c5
Justin Greger <justingreger@gmail.com>
2023-07-01 14:34:58 -0400

    5 -3 0

--- easy_2/10_always_return_negative.rb
+++ easy_2/10_always_return_negative.rb
@@ -1,6 +1,14 @@
 If the argument is a positive number, return the negative of that number. 
 If the number is 0 or negative, return the original number.
 
+5
+-3
+0
+
+
+
+
+
 Examples:
 negative(5) == -5
 negative(-3) == -3
