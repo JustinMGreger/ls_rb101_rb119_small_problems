8033fdfafdbdbbb173d72c32614350d32375dae3
Justin Greger <justingreger@gmail.com>
2023-07-01 14:27:56 -0400

    instructions

---
+++ easy_2/10_always_return_negative.rb
@@ -0,0 +1,5 @@
+Write a method that takes a number as an argument. 
+If the argument is a positive number, return the negative of that number. 
+If the number is 0 or negative, return the original number.
+
+Examples:
