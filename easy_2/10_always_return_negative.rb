dabb21457babd24ba1813ca56c48f6b161d709be
Justin Greger <justingreger@gmail.com>
2023-07-01 14:28:18 -0400

    examples

--- easy_2/10_always_return_negative.rb
+++ easy_2/10_always_return_negative.rb
@@ -3,3 +3,6 @@ If the argument is a positive number, return the negative of that number.
 If the number is 0 or negative, return the original number.
 
 Examples:
+negative(5) == -5
+negative(-3) == -3
+negative(0) == 0      # There's no such thing as -0 in ruby
