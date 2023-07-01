61b433c139b180f9ec626768728bf17ce657ac4a
Justin Greger <justingreger@gmail.com>
2023-07-01 11:55:59 -0400

    if (lines[-1] + word + ' ').length > max_message_width

--- easy_1/5_bannerizer.rb
+++ easy_1/5_bannerizer.rb
@@ -12,6 +12,7 @@ def break_into_lines(message, max_message_width)
       lines[-1] += "#{word.slice!(0..max_message_width - 1)} "
       lines << '' if word.empty?
     end
+    if (lines[-1] + word + ' ').length > max_message_width
 end
 
 
@@ -20,7 +21,6 @@ def print_in_box(message)
   max_width = 76
   max_message_width = max_width - 4
 
-    if (lines[-1] + word + ' ').length > max_message_width
       lines << ''
     end
     lines[-1] += word + ' ' unless word.empty?
