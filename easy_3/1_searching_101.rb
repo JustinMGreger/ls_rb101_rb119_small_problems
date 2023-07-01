9b3514edbaef0baf56f8932738d4545a46365c0a
Justin Greger <justingreger@gmail.com>
2023-07-01 17:59:32 -0400

    solicit numbers

--- easy_3/1_searching_101.rb
+++ easy_3/1_searching_101.rb
@@ -8,17 +8,31 @@ def integer_input
   gets.chomp.to_i
 end
 
-def solicit_numbers
+def solicit_first_number
   prompt '==> Enter the 1st number:'
   first_number = integer_input
+end
+
+def solicit_second_number
   prompt '==> Enter the 2nd number:'
   second_number = integer_input
+end
+
+def solicit_third_number
   prompt '==> Enter the 3rd number:'
   third_number = integer_input
+end
+
+def solicit_fourth_number
   prompt '==> Enter the 4th number:'
   fourth_number = integer_input
+end
+
+def solicit_fifth_number
   prompt '==> Enter the 5th number:'
   fifth_number = integer_input
+
+def solicit_last_number
   prompt '==> Enter the last number:'
   last_number = integer_input
 end
