0be3d25aea8adfee6d2dca855c046ef4e91756cf
Justin Greger <justingreger@gmail.com>
2023-06-30 21:38:57 -0400

    what does this code print out?

--- easy_2/9_string_assignment.rb
+++ easy_2/9_string_assignment.rb
@@ -1,5 +1,13 @@
 Take a look at the following code:
-name = 'Bob'
-save_name = name
-name = 'Alice'
-puts name, save_name
+name = 'Bob'                                 # the name variable is assigned the string 'Bob'
+save_name = name # the save_name variable is assigned the name variable which is assigned the string 'Bob'
+name = 'Alice'                               # the name variable is assigned the string 'Alice'
+puts name, save_name     
+# print to the screen the name varaible which is assinged the string 'Alice', the save_name variable which is assigned the
+# name variable which is assigned the string 'Bob'. resulting in:          
+# Alice
+# Bob
+
+What does this code print out? Think about it for a moment before continuing.        
+# Alice
+# Bob
