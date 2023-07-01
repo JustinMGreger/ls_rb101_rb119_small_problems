fd138b3a29806e6b88412d2d2605a2c43c28b067
Justin Greger <justingreger@gmail.com>
2023-07-01 14:56:37 -0400

    puts number and test negative 5 was successful

--- easy_2/10_always_return_negative.rb
+++ easy_2/10_always_return_negative.rb
@@ -5,11 +5,13 @@ def negative(number)
     negative_number = number * -1
     puts negative_number
   else
-    number
+    puts number
   end
 end
 
-Tests:
-negative(5) == -5
-negative(-3) == -3
-negative(0) == 0      # There's no such thing as -0 in ruby
+negative(5) 
+
+result -5
+
+Test
+negative(5) == -5 success.
