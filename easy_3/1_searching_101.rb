ba116f051d0c5adec80f81b3deb3c3e334d2d25e
Justin Greger <justingreger@gmail.com>
2023-07-01 16:28:57 -0400

    frozen string literal

--- easy_3/1_searching_101.rb
+++ easy_3/1_searching_101.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+
+
+
 Write a program that solicits 6 numbers from the user, 
 then prints a message that describes whether or not the 6th number appears amongst the first 5 numbers.
 
