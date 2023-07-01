79b2a3bd3a91ea3b7d34d3c8b0b333efcdf292a2
Justin Greger <justingreger@gmail.com>
2023-06-30 21:23:13 -0400

    what does this print out

--- easy_2/9_string_assignment.rb
+++ easy_2/9_string_assignment.rb
@@ -4,4 +4,12 @@ name.upcase!
 puts name, save_name
 
 What does this print out? 
+
+'BOB', 'Bob'
+
+
 Can you explain these results?
+name = 'Bob'    name is assigned 'Bob'
+save_name = name
+name.upcase!
+puts name, save_name
