1eeb912525db4b0896b20042f1c9e4d76d833b26
Justin Greger <justingreger@gmail.com>
2023-07-01 14:42:02 -0400

    def negative(number)   negative numbers are less than 0   positive numbers are greater than 0   0 is equal to 0 end

--- easy_2/10_always_return_negative.rb
+++ easy_2/10_always_return_negative.rb
@@ -5,9 +5,7 @@ if -3  is negative then the original number.
 if 0 is 0 then the original number.
 
 
-negative numbers are less than 0
-positive numbers are greater than 0
-0 is equal to 0
+
 
 
 Examples:
@@ -16,4 +14,7 @@ negative(-3) == -3
 negative(0) == 0      # There's no such thing as -0 in ruby
 
 def negative(number)
+  negative numbers are less than 0
+  positive numbers are greater than 0
+  0 is equal to 0
 end
