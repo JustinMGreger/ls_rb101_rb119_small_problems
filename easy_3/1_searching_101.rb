fdc4ef74af0e973b4f1862bf2fdbd1a5a8991636
Justin Greger <justingreger@gmail.com>
2023-07-01 16:29:36 -0400

    def solicit_numbers end

--- easy_3/1_searching_101.rb
+++ easy_3/1_searching_101.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-
-
+def solicit_numbers
+end
 
 Write a program that solicits 6 numbers from the user, 
 then prints a message that describes whether or not the 6th number appears amongst the first 5 numbers.
