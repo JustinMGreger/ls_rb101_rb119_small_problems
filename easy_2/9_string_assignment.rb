1828b65e46f5e85f743f1466f98ed6a2d3141577
Justin Greger <justingreger@gmail.com>
2023-06-30 21:28:56 -0400

    the name.upcase! mutated the original which is the basis for the assingment to name & save_name.

--- easy_2/9_string_assignment.rb
+++ easy_2/9_string_assignment.rb
@@ -3,6 +3,8 @@ save_name = name
 name.upcase!
 puts name, save_name
 
+
+
 What does this print out? 
 
 'BOB', 'Bob'
@@ -13,3 +15,9 @@ name = 'Bob'    name is assigned 'Bob'
 save_name = name    save_name is assigned name which is assigned 'Bob'
 name.upcase!            name which is assigned 'Bob' is upcased which is 'BOB'
 puts name, save_name   print to the screen 'BOB', 'Bob'
+
+the answer is 
+BOB 
+BOB
+
+the name.upcase! mutated the original which is the basis for the assingment to name & save_name.
