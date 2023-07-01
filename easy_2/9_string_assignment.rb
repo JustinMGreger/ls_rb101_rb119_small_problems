aefbf93a0bb7959e39468eb9ea3e2313946e315b
Justin Greger <justingreger@gmail.com>
2023-06-30 21:43:43 -0400

    # upcase resulting in:

--- easy_2/9_string_assignment.rb
+++ easy_2/9_string_assignment.rb
@@ -15,5 +15,12 @@ What does this code print out? Think about it for a moment before continuing.
 you are 100% correct, and the answer should come as no surprise. Now, let's look at something a bit different:
 name = 'Bob'                 # the name variable is assigned the 'Bob' string.
 save_name = name            # the save_name variable is assigned the name variable which is assigned the 'Bob' string.
-name.upcase!
+name.upcase! # this means: mutate the name variable which is assigned the 'Bob' string so that all the characters are 
+# upcase resulting in:
+name = 'Bob'
+save_name = name
+
+
+
+
 puts name, save_name
