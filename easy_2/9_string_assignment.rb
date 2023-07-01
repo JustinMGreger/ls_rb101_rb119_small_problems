6ac58556f777872953638ecb35bdaab2f4ee39e4
Justin Greger <justingreger@gmail.com>
2023-06-30 21:39:48 -0400

    Now, let's look at something a bit different:

--- easy_2/9_string_assignment.rb
+++ easy_2/9_string_assignment.rb
@@ -11,3 +11,9 @@ puts name, save_name
 What does this code print out? Think about it for a moment before continuing.        
 # Alice
 # Bob
+
+you are 100% correct, and the answer should come as no surprise. Now, let's look at something a bit different:
+name = 'Bob'
+save_name = name
+name.upcase!
+puts name, save_name
