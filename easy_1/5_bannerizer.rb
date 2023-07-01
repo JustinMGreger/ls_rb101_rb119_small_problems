fc8ec7019f859cd12dfcb7abe4a0c0d9fbab284d
Justin Greger <justingreger@gmail.com>
2023-07-01 12:15:32 -0400

    lines[-1] +=

--- easy_1/5_bannerizer.rb
+++ easy_1/5_bannerizer.rb
@@ -6,13 +6,13 @@ end
 
 def split_word(lines, word, max_message_width)
   while word.length > max_message_width
+    lines[-1] += "#{word.slice!(0..max_message_width - 1)} "
 end
 
 def break_into_lines(message, max_message_width)
   words = message.split(' ')
   lines = ['']
   words.each do |word|
-      lines[-1] += "#{word.slice!(0..max_message_width - 1)} "
       lines << '' if word.empty?
     end
     if (lines[-1] + word + ' ').length > max_message_width
