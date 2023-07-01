2c94bfaa5ea49ade9df646576d9b6a2b5bf7e27e
Justin Greger <justingreger@gmail.com>
2023-07-01 14:54:03 -0400

    tests

--- easy_2/10_always_return_negative.rb
+++ easy_2/10_always_return_negative.rb
@@ -5,14 +5,6 @@ if -3  is negative then the original number.
 if 0 is 0 then the original number.
 
 
-
-
-
-Examples:
-negative(5) == -5
-negative(-3) == -3
-negative(0) == 0      # There's no such thing as -0 in ruby
-
 def negative(number)
   if number > 0
     negative_number = number * -1
@@ -21,3 +13,8 @@ def negative(number)
     number
   end
 end
+
+Tests:
+negative(5) == -5
+negative(-3) == -3
+negative(0) == 0      # There's no such thing as -0 in ruby
