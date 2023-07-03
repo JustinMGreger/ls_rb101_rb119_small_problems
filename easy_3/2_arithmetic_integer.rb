c26bc99cfd51a180585e5cb2160fbac2bc7aa486
Justin Greger <justingreger@gmail.com>
2023-07-02 21:27:02 -0400

    #{first_number}

--- easy_3/2_arithmetic_integer.rb
+++ easy_3/2_arithmetic_integer.rb
@@ -21,9 +21,9 @@ end
 
 
 
-prompt addition "==> first_number + second_number = 40"
-prompt subtraction "==> first_number - second_number = 6"
-prompt product "==> first_number * second_number = 391"
-prompt quotient "==> first_number / second_number = 1"
-prompt remainder "==> first_number % second_number = 6"
-prompt power "==> first_number ** second_number = 141050039560662968926103"
+prompt addition "==> #{first_number} + #{second_number} = 40"
+prompt subtraction "==> #{first_number} - second_number = 6"
+prompt product "==> #{first_number} * second_number = 391"
+prompt quotient "==> #{first_number} / second_number = 1"
+prompt remainder "==> #{first_number} % second_number = 6"
+prompt power "==> #{first_number} ** second_number = 141050039560662968926103"
