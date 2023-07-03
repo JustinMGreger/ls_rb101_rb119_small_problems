1934a218b47d556bac8b07cd543caaf76daab985
Justin Greger <justingreger@gmail.com>
2023-07-02 21:30:36 -0400

    (first_number, second_number)

--- easy_3/2_arithmetic_integer.rb
+++ easy_3/2_arithmetic_integer.rb
@@ -21,9 +21,9 @@ end
 
 
 
-prompt addition_method "==> #{first_number} + #{second_number} = #{addition}"
-prompt subtraction_method "==> #{first_number} - #{second_number} = 6"
-prompt product_method "==> #{first_number} * #{second_number} = 391"
-prompt quotient_method "==> #{first_number} / #{second_number} = 1"
-prompt remainder_method "==> #{first_number} % #{second_number} = 6"
-prompt power_method "==> #{first_number} ** #{second_number} = 141050039560662968926103"
+prompt addition_method(first_number, second_number) "==> #{first_number} + #{second_number} = #{addition}"
+prompt subtraction_method(first_number, second_number) "==> #{first_number} - #{second_number} = 6"
+prompt product_method(first_number, second_number) "==> #{first_number} * #{second_number} = 391"
+prompt quotient_method(first_number, second_number) "==> #{first_number} / #{second_number} = 1"
+prompt remainder_method(first_number, second_number) "==> #{first_number} % #{second_number} = 6"
+prompt power_method(first_number, second_number) "==> #{first_number} ** #{second_number} = 141050039560662968926103"
