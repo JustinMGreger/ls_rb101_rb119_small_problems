6dc31e1654c4f5fea7eda4f83e3fae9bf27d5ba9
Justin Greger <justingreger@gmail.com>
2023-07-02 21:16:26 -0400

    product

--- easy_3/2_arithmetic_integer.rb
+++ easy_3/2_arithmetic_integer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 prints the results of the following operations on those two numbers: 
- product, quotient, remainder, and power. 
+, quotient, remainder, and power. 
 Do not worry about validating the input.
 
 
@@ -18,7 +18,7 @@ prompt ==> Enter the second number:
 second_number = 17
 prompt addition ==> 23 + 17 = 40
 prompt subtraction ==> 23 - 17 = 6
-prompt ==> 23 * 17 = 391
+prompt product ==> 23 * 17 = 391
 prompt ==> 23 / 17 = 1
 prompt ==> 23 % 17 = 6
 prompt ==> 23 ** 17 = 141050039560662968926103
