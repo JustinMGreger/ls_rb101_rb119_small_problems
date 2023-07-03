70d114147f26ff57b0809cbef6e111899fb960c9
Justin Greger <justingreger@gmail.com>
2023-07-02 21:11:48 -0400

    def prompt(message)   puts message end

--- easy_3/2_arithmetic_integer.rb
+++ easy_3/2_arithmetic_integer.rb
@@ -3,6 +3,13 @@ and then prints the results of the following operations on those two numbers:
 addition, subtraction, product, quotient, remainder, and power. 
 Do not worry about validating the input.
 
+
+
+def prompt(message) 
+  puts message
+end
+
+
 Example
 ==> Enter the first number:
 23
