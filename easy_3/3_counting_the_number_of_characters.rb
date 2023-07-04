657f412ec54f44c0c8ddba1d96cf2c5853c7bfc4
Justin Greger <justingreger@gmail.com>
2023-07-03 21:26:17 -0400

    def prompt(message)

--- easy_3/3_counting_the_number_of_characters.rb
+++ easy_3/3_counting_the_number_of_characters.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-
+def prompt(message)
+  puts message
+end
 Write a program that will ask a user for an input of a word or multiple words and give back the number of characters. 
 Spaces should not be counted as a character.
 
