bfa885cbbf4afa356a3dcf325c2b0353d281de9d
Justin Greger <justingreger@gmail.com>
2023-07-03 21:44:38 -0400

    deleted Spaces should not be counted as a character.

--- easy_3/3_counting_the_number_of_characters.rb
+++ easy_3/3_counting_the_number_of_characters.rb
@@ -9,7 +9,6 @@ def input
 end
 
 Write a program that will ask a user for an input of a word or multiple words and give back the number of characters. 
-Spaces should not be counted as a character.
 
 input:
 prompt 'Please write word or multiple words:'
