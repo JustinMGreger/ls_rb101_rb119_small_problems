092b960661314a4382f34d9c831754ec412f59d4
Justin Greger <justingreger@gmail.com>
2023-07-03 21:26:56 -0400

    prompt

--- easy_3/3_counting_the_number_of_characters.rb
+++ easy_3/3_counting_the_number_of_characters.rb
@@ -3,14 +3,15 @@
 def prompt(message)
   puts message
 end
+
 Write a program that will ask a user for an input of a word or multiple words and give back the number of characters. 
 Spaces should not be counted as a character.
 
 input:
-Please write word or multiple words: walk
+prompt Please write word or multiple words: walk
 output:
-There are 4 characters in "walk".
+prompt There are 4 characters in "walk".
 input:
-Please write word or multiple words: walk, don't run
+prompt Please write word or multiple words: walk, don't run
 output:
-There are 13 characters in "walk, don't run".
+prompt There are 13 characters in "walk, don't run".
