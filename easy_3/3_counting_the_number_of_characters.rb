d746158435a65ae7616ae0fc513b0f294dc8dd0e
Justin Greger <justingreger@gmail.com>
2023-07-03 21:59:18 -0400

    character_count = input.

--- easy_3/3_counting_the_number_of_characters.rb
+++ easy_3/3_counting_the_number_of_characters.rb
@@ -18,8 +18,7 @@ if word_or_words false = word
 else word_or_words true = words
 
 
-character_count = input determine number of characters and do not count spaces as a character in the variable input
-remove (' ') then .length 
+character_count = input.gsub(" ", "").length
 
   if input is word then prompt "There are #{character_count} characters in "#{word}"."
   else input is words then prompt "There are #{character_count} characters in "#{words}"."
