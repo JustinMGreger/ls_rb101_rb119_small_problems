3f33be494b16c0289a8c1bbab0c981bbd61c0d56
Justin Greger <justingreger@gmail.com>
2023-07-03 21:28:15 -0400

    moved text to the right of : down to a next level

--- easy_3/3_counting_the_number_of_characters.rb
+++ easy_3/3_counting_the_number_of_characters.rb
@@ -8,10 +8,12 @@ Write a program that will ask a user for an input of a word or multiple words an
 Spaces should not be counted as a character.
 
 input:
-prompt Please write word or multiple words: walk
+prompt Please write word or multiple words: 
+walk
 output:
 prompt There are 4 characters in "walk".
 input:
-prompt Please write word or multiple words: walk, don't run
+prompt Please write word or multiple words: 
+walk, don't run
 output:
 prompt There are 13 characters in "walk, don't run".
