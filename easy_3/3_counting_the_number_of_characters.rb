faec46548ef9f8e0727492e225d1a4f6cd8ba357
Justin Greger <justingreger@gmail.com>
2023-07-04 16:07:27 -0400

    words and word are string

--- easy_3/3_counting_the_number_of_characters.rb
+++ easy_3/3_counting_the_number_of_characters.rb
@@ -4,22 +4,22 @@ def prompt(message)
   puts message
 end
 
-def input
-  gets.chomp
-end
-
 prompt 'Please write word or multiple words:'
 
-def word_or_words
-  input.include?(' ')
+input = gets.chomp
+
+def word_or_words(input)
+  if input.include?(' ')
+    'words'
+  else
+    'word'
+  end
 end 
-
-if word_or_words false = word
-else word_or_words true = words
-
 
 character_count = input.gsub(" ", "").length
 
-  if input is word then prompt "There are #{character_count} characters in "#{word}"."
-  else input is words then prompt "There are #{character_count} characters in "#{words}"."
-  end
+if word_or_words(input) ==  'word'
+  prompt "There are #{character_count} characters in \"#{input}\"."
+else
+  prompt "There are #{character_count} characters in \"#{input}\"."
+end
