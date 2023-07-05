d035dcac41a4bb3974ddc36c936da1bd2490451b
Justin Greger <justingreger@gmail.com>
2023-07-04 21:17:11 -0400

    deleted def word_or_words(input)

--- easy_3/3_counting_the_number_of_characters.rb
+++ easy_3/3_counting_the_number_of_characters.rb
@@ -8,14 +8,6 @@ prompt 'Please write word or multiple words:'
 
 input = gets.chomp
 
-def word_or_words(input)
-  if input.include?(' ')
-    'words'
-  else
-    'word'
-  end
-end
-
 character_count = input.gsub(' ', '').length
 
 prompt "There are #{character_count} characters in \"#{input}\"."
