69725d1bbb787c4b11b3a6518e49d9c7e5f271f4
Justin Greger <justingreger@gmail.com>
2023-07-05 16:15:01 -0400

    instructions

--- easy_3/5_squaring_an_argument.rb
+++ easy_3/5_squaring_an_argument.rb
@@ -1,11 +1,4 @@
+Using the multiply method from the "Multiplying Two Numbers" problem, write a method that computes the square of its 
+argument (the square is the result of multiplying a number by itself).
 
-def multiply(number, number)
-  number * number
-end
-
-def square(number)
-  multiply(number, number)
-end
-
-p square(5) == 25
-p square(-8) == 64
+Example:
