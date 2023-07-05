4277b4161f67655e9f2dd08a988dbc47a43613fc
Justin Greger <justingreger@gmail.com>
2023-07-04 21:34:09 -0400

    deleted code

--- easy_3/4_multiplying_two_numbers.rb
+++ easy_3/4_multiplying_two_numbers.rb
@@ -1,29 +1,5 @@
-# frozen_string_literal: true
 
-def prompt(message)
-  puts message
-end
 
-def input
-  gets.chomp.to_i
-end
-
-prompt '==> Enter the first number:'
-first_number = input
-
-prompt '==> Enter the second number:'
-second_number = input
-until second_number != 0
-  prompt '==> Enter the second number:'
-  second_number = input
-end
-
-def product_method(first_number, second_number)
-  product = first_number * second_number
-  prompt "==> #{first_number} * #{second_number} = #{product}"
-end
-
-product_method(first_number, second_number)
 
 Further Exploration
 
