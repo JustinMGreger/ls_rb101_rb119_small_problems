ccec616909f7a0f67438a8c4dc1090fc651d2a35
Justin Greger <justingreger@gmail.com>
2023-07-05 16:55:54 -0400

    further exploration

--- easy_3/5_squaring_an_argument.rb
+++ easy_3/5_squaring_an_argument.rb
@@ -8,5 +8,10 @@ def square(number)
   multiply(number, number)
 end
 
-p square(5) == 25
-p square(-8) == 64
+p square(5) == 25 # result is true.
+p square(-8) == 64 # result is true.
+
+Further Exploration
+
+What if we wanted to generalize this method to a "power to the n" type method: cubed, to the 4th power, to the 5th, etc. 
+How would we go about doing so while still using the multiply method?
