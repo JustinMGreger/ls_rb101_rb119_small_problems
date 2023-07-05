129aa60caa949a9cb3628c76b7d5bf2158661431
Justin Greger <justingreger@gmail.com>
2023-07-05 15:44:25 -0400

    end

--- easy_3/5_squaring_an_argument.rb
+++ easy_3/5_squaring_an_argument.rb
@@ -1,12 +1,9 @@
 write a method that computes the square of its 
 argument (the square is the result of multiplying a number by itself).
 
-Example:
-
-
-
 def multiply(arg_1, arg_2)
   arg_1 * arg_2
 end
 
 def square(number)
+end
