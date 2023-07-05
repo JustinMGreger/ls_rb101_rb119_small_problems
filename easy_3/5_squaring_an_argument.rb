52119dd3b7dec5c82bffce270ae9e1dec2542649
Justin Greger <justingreger@gmail.com>
2023-07-05 17:43:27 -0400

    deleted trailing white space and changed double quotes to single quotes

--- easy_3/5_squaring_an_argument.rb
+++ easy_3/5_squaring_an_argument.rb
@@ -17,8 +17,8 @@ def power_to_the_n(number, power)
   power.to_i.times { result = multiply(result, number) }
   result
 end
- 
-prompt "==> What number do you want to raise to the power?"
+
+prompt '==> What number do you want to raise to the power?'
 number = input
 prompt '==> What power do you want?'
 power = input
