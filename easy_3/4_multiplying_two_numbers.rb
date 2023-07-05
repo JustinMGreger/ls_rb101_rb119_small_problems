d85891620b3dd14160d93b88e82568301700855d
Justin Greger <justingreger@gmail.com>
2023-07-04 21:22:35 -0400

    def multiply(parameter_1, parameter_2)

--- easy_3/4_multiplying_two_numbers.rb
+++ easy_3/4_multiplying_two_numbers.rb
@@ -1,4 +1,12 @@
 Create a method that takes two arguments, multiplies them together, and returns the result.
 
 Example:
+
+def multiply(parameter_1, parameter_2)
+
+end
+
+
+
 multiply(5, 3) == 15
+
