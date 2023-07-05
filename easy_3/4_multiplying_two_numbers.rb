fd703f13b2d01262c487e9533b3f076a6f09782a
Justin Greger <justingreger@gmail.com>
2023-07-04 21:40:29 -0400

    What do you think is happening here?

--- easy_3/4_multiplying_two_numbers.rb
+++ easy_3/4_multiplying_two_numbers.rb
@@ -12,3 +12,6 @@ For fun: what happens if the first argument is an Array?
 Answer: [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
 
 What do you think is happening here?
+the [1, 2] array is being expanded by 5 times the amount of the same elements.
+there are 5 elements of the 1 integer & 5 elements of the 2 integer.
+the integer elements 1, 2 are in order in the array of elements which is why they are repeated together.
