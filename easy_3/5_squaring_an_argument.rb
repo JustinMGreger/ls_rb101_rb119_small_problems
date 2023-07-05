0da6dd9e4617ee5151d546db20957c041b24efb6
Justin Greger <justingreger@gmail.com>
2023-07-05 17:30:45 -0400

    deleted def square(number)   multiply(number, number) end

--- easy_3/5_squaring_an_argument.rb
+++ easy_3/5_squaring_an_argument.rb
@@ -4,28 +4,22 @@ def prompt(message)
   puts message
 end
 
-input = gets.chomp.to_f
+def input
+  gets.chomp.to_f
+end
 
 def multiply(arg_one, arg_two)
   arg_one * arg_two
 end
 
-def square(number)
-  multiply(number, number)
-end
-
-prompt square(5) == 25 # result is true.
-prompt square(-8) == 64 # result is true.
-
-Further Exploration
-
-What if we wanted to generalize this : cubed, to the 4th power, to the 5th, etc. 
-How would we go about doing so while still using the multiply method?
-
-def power_to_the_n(n)
-end
 
 prompt "==> What number do you want to raise to the power?"
 number = input
 prompt '==> What power do you want?'
 power = input
+
+def power_to_the_n(multiply, power)
+  multiply(number, number) * power
+end
+ 
+prompt  "==> The result is: #{power_to_the_n(number, power)}"
