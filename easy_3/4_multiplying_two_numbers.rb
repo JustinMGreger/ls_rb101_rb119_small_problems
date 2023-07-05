5abe9e8dc8e4da57afa5d59cdec217c3529c6040
Justin Greger <justingreger@gmail.com>
2023-07-04 21:38:11 -0400

    Answer: [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]

--- easy_3/4_multiplying_two_numbers.rb
+++ easy_3/4_multiplying_two_numbers.rb
@@ -1,9 +1,14 @@
-Create a method that takes two arguments, multiplies them together, and returns the result.
 
 def multiply(arg_1, arg_2)
   arg_1 * arg_2
 end
 
+
+p multiply([1, 2], 5)
+
 Further Exploration
 
-For fun: what happens if the first argument is an Array? What do you think is happening here?
+For fun: what happens if the first argument is an Array? 
+Answer: [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+
+What do you think is happening here?
