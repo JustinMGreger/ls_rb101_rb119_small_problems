488295a07064c10bb1374bda63aa25a61cb19d51
Justin Greger <justingreger@gmail.com>
2023-07-04 21:31:46 -0400

    refactored the code

--- easy_3/4_multiplying_two_numbers.rb
+++ easy_3/4_multiplying_two_numbers.rb
@@ -4,13 +4,23 @@ def prompt(message)
   puts message
 end
 
-prompt 'what is the first argument you want to multiply by?'
-input = gets.chomp.to_i
-first_argument = input
-prompt 'what is the first argument you want to multiply by?'
-second_argument = input
-
-def multiply(first_argument, second_argument)
-  product = first_argument * second_argument
-  prompt product
+def input
+  gets.chomp.to_i
 end
+
+prompt '==> Enter the first number:'
+first_number = input
+
+prompt '==> Enter the second number:'
+second_number = input
+until second_number != 0
+  prompt '==> Enter the second number:'
+  second_number = input
+end
+
+def product_method(first_number, second_number)
+  product = first_number * second_number
+  prompt "==> #{first_number} * #{second_number} = #{product}"
+end
+
+product_method(first_number, second_number)
