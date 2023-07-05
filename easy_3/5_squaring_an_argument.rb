0ba8e9ff93ebc03d35c3c9aa4ccb2ac3a286b50e
Justin Greger <justingreger@gmail.com>
2023-07-05 16:19:44 -0400

    def square(number)

--- easy_3/5_squaring_an_argument.rb
+++ easy_3/5_squaring_an_argument.rb
@@ -3,5 +3,12 @@ argument (the square is the result of multiplying a number by itself).
 
 Example:
 
+def multiply(arg_1, arg_2)
+  arg_1 * arg_2
+end
+
+def square(number)
+end
+
 square(5) == 25
 square(-8) == 64
