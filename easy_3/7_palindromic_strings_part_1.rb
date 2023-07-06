3e3d8ffe01e1c48bde7384fc6bdf8263b78e45ec
Justin Greger <justingreger@gmail.com>
2023-07-06 19:05:25 -0400

    #

--- easy_3/7_palindromic_strings_part_1.rb
+++ easy_3/7_palindromic_strings_part_1.rb
@@ -12,9 +12,9 @@ p palindrome?('Madam') == false          # (case matters)          # result: tru
 p palindrome?("madam i'm adam") == false # (all characters matter) # result: true
 p palindrome?('356653') == true                                    # result: true
 
-Further Exploration
+# Further Exploration
 
-Now write a method that determines whether an array or a string is palindromic; 
-that is, write a method that can take either an array or a string argument, 
-and determines whether that argument is a palindrome. 
-You may not use an if, unless, or case statement or modifier.
+# Now write a method that determines whether an array or a string is palindromic; 
+# that is, write a method that can take either an array or a string argument, 
+# and determines whether that argument is a palindrome. 
+# You may not use an if, unless, or case statement or modifier.
