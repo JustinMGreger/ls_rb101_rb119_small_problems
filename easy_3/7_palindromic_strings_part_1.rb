17968510cd61189ae3c634fa4a12300418a23cd3
Justin Greger <justingreger@gmail.com>
2023-07-06 17:34:17 -0400

    moved tests to the bottom of the file

--- easy_3/7_palindromic_strings_part_1.rb
+++ easy_3/7_palindromic_strings_part_1.rb
@@ -1,9 +1,5 @@
 
 
-palindrome?('madam') == true
-palindrome?('Madam') == false          # (case matters)
-palindrome?("madam i'm adam") == false # (all characters matter)
-palindrome?('356653') == true
 
 def palindrome?(string_parameter)
   if string_parameter == palindrome A palindrome reads the same forward and backward. For this exercise, case matters as does punctuation and spaces.
@@ -11,3 +7,8 @@ def palindrome?(string_parameter)
   else
     puts false
 end
+
+palindrome?('madam') == true
+palindrome?('Madam') == false          # (case matters)
+palindrome?("madam i'm adam") == false # (all characters matter)
+palindrome?('356653') == true
