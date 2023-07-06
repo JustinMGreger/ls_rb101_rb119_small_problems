0a584877bdaf3725fe33c72a52ab84322d160c17
Justin Greger <justingreger@gmail.com>
2023-07-06 14:26:18 -0400

    p

--- easy_3/6_exclusive_or.rb
+++ easy_3/6_exclusive_or.rb
@@ -10,7 +10,7 @@ def xor?(arg_one, arg_two)
   end
 end
 
-xor?(5.even?, 4.even?) == true
-xor?(5.odd?, 4.odd?) == true
-xor?(5.odd?, 4.even?) == false
-xor?(5.even?, 4.odd?) == false
+p xor?(5.even?, 4.even?) == true
+p xor?(5.odd?, 4.odd?) == true
+p xor?(5.odd?, 4.even?) == false
+p xor?(5.even?, 4.odd?) == false
