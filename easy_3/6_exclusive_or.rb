b181bc00deea2cb349f9c4d0442131878912b7d5
Justin Greger <justingreger@gmail.com>
2023-07-06 14:21:44 -0400

    if elsif else end

--- easy_3/6_exclusive_or.rb
+++ easy_3/6_exclusive_or.rb
@@ -16,8 +16,11 @@ xor?(5.odd?, 4.even?) == false
 xor?(5.even?, 4.odd?) == false
 
 def xor?(arg_one, arg_two)
-  arg_one && arg_two
-
-  arg_one && arg_two
-
+  if arg_one && !arg_two
+    true
+  elsif !arg_one && arg_two
+    true
+  else
+    false
+  end
 end
