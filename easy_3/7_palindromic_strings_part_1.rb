22bacb99602a13429b151c3afd523bcdea82923d
Justin Greger <justingreger@gmail.com>
2023-07-06 19:27:22 -0400

    renamed palindrome and parameter'

--- easy_3/7_palindromic_strings_part_1.rb
+++ easy_3/7_palindromic_strings_part_1.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
-def palindrome_array?(array)
-  array == array.reverse
-end
 
-# Test for array:
-p palindrome_array?([1, true, nil, 'string', 'string', nil, true, 1]) == true  # result is true.
-p palindrome_array?([1, false]) == false # result is true.
- 
 # Further Exploration
 
 # Now write a method that determines whether an array or a string is palindromic; 
 # that is, write a method that can take either an array or a string argument, 
 # and determines whether that argument is a palindrome. 
 # You may not use an if, unless, or case statement or modifier.
+
+def palindrome?(parameter)
+  parameter == parameter.reverse
+end
+
+# Test for array:
+p palindrome?('madam') == true                                     # result: true
+p palindrome?('Madam') == false          # (case matters)          # result: true
+p palindrome?("madam i'm adam") == false # (all characters matter) # result: true
+p palindrome?('356653') == true                                    # result: true
+p palindrome?([1, true, nil, 'string', 'string', nil, true, 1]) == true  # result: true.
+p palindrome?([1, false]) == false # result: true.
