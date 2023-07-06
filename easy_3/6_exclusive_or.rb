79044dbb057abe3b703a61dbcd4a00e45d6332c7
Justin Greger <justingreger@gmail.com>
2023-07-06 14:25:53 -0400

    removed the tests to the bottom of the file

--- easy_3/6_exclusive_or.rb
+++ easy_3/6_exclusive_or.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-xor?(5.even?, 4.even?) == true
-xor?(5.odd?, 4.odd?) == true
-xor?(5.odd?, 4.even?) == false
-xor?(5.even?, 4.odd?) == false
-
 def xor?(arg_one, arg_two)
   if arg_one && !arg_two
     true
@@ -14,3 +9,8 @@ def xor?(arg_one, arg_two)
     false
   end
 end
+
+xor?(5.even?, 4.even?) == true
+xor?(5.odd?, 4.odd?) == true
+xor?(5.odd?, 4.even?) == false
+xor?(5.even?, 4.odd?) == false
