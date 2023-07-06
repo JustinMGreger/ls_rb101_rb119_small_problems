49a1da102f2e0a779b52023c643767b6fb9afefb
Justin Greger <justingreger@gmail.com>
2023-07-06 17:54:10 -0400

    result: true

--- easy_3/7_palindromic_strings_part_1.rb
+++ easy_3/7_palindromic_strings_part_1.rb
@@ -4,7 +4,7 @@ def palindrome?(string_parameter)
   string_parameter == string_parameter.reverse
 end
 
-p palindrome?('madam') == true
-p palindrome?('Madam') == false          # (case matters)
-p palindrome?("madam i'm adam") == false # (all characters matter)
-p palindrome?('356653') == true
+p palindrome?('madam') == true                                     # result: true
+p palindrome?('Madam') == false          # (case matters)          # result: true
+p palindrome?("madam i'm adam") == false # (all characters matter) # result: true
+p palindrome?('356653') == true                                    # result: true
