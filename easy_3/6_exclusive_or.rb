5d0d06e494ed15328699d4a30c892c1b957ad3b9
Justin Greger <justingreger@gmail.com>
2023-07-06 16:08:57 -0400

    # Think of some additional examples.

--- easy_3/6_exclusive_or.rb
+++ easy_3/6_exclusive_or.rb
@@ -20,10 +20,11 @@ p xor?(5.even?, 4.odd?) == false # result: true
 # Can you think of a situation in which a boolean xor method would be useful? 
 # Answer: game theory analysis.
 
-Suppose you were modeling a light at the top of a flight of stairs wired in such a way that the light can be turned on or
-off using either the switch at the bottom of the stairs or the switch at the top of the stairs. 
-This is an xor configuration, and it can be modeled in ruby using the xor method. 
-Think of some additional examples.
+# Suppose you were modeling a light at the top of a flight of stairs wired in such a way that the light can be turned on 
+# or off using either the switch at the bottom of the stairs or the switch at the top of the stairs. 
+
+# This is an xor configuration, and it can be modeled in ruby using the xor method. 
+# Think of some additional examples.
 
 || and && are so-called short circuit operators in that the second operand is not evaluated if its value is not needed. 
 Does the xor method perform short-circuit evaluation of its operands? Why or why not? 
