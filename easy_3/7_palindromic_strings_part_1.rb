c3fc7bdfc321e6af7e276e05486787c011c2e0de
Justin Greger <justingreger@gmail.com>
2023-07-06 17:14:21 -0400

    deleted Write a method that returns true if the string passed as an argument is a palindrome, false otherwise.

--- easy_3/7_palindromic_strings_part_1.rb
+++ easy_3/7_palindromic_strings_part_1.rb
@@ -1,4 +1,3 @@
-Write a method that returns true if the string passed as an argument is a palindrome, false otherwise. 
 A palindrome reads the same forward and backward. For this exercise, case matters as does punctuation and spaces.
 
 Examples:
