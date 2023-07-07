744fc0613c091767a15b8089a5ef83ad2132b244
Justin Greger <justingreger@gmail.com>
2023-07-07 19:58:34 -0400

    results

--- easy_3/9_palindromic_numbers.rb
+++ easy_3/9_palindromic_numbers.rb
@@ -5,14 +5,18 @@ def palindromic_number?(integer)
 end
 
 # Tests:
-p palindromic_number?(34_543) == true # result: true
-p palindromic_number?(123_210) == false # result: true
-p palindromic_number?(22) == true # result: true
-p palindromic_number?(5) == true # result: true
+# p palindromic_number?(34_543) == true # result: true
+# p palindromic_number?(123_210) == false # result: true
+# p palindromic_number?(22) == true # result: true
+# p palindromic_number?(5) == true # result: true
 
 # Further Exploration
 
 # Suppose your number begins with one or more 0s. 
+p palindromic_number?(0110) == true #result: false
+p palindromic_number?(001100) == true #result: false
+p palindromic_number?(011) == true # result: true
+p palindromic_number?(0011) == true # result: true
 # Will your method still work? 
 # Why or why not? 
 # Is there any way to address this?
