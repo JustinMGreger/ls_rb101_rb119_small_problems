4fe8b392b9b42ce5e48b8138b3cfbf086bda48e3
Justin Greger <justingreger@gmail.com>
2023-07-06 20:57:07 -0400

    spacing

--- easy_3/7_palindromic_strings_part_1.rb
+++ easy_3/7_palindromic_strings_part_1.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 def palindrome?(sequence)
-  sequence.size.times.all? { |i| sequence[i] == sequence[-i-1] }
+  sequence.size.times.all? { |i| sequence[i] == sequence[-i - 1] }
 end
 
 # Test for array:
-p palindrome?('madam') == true                                     # result: true
+p palindrome?('madam') == true # result: true
 p palindrome?('Madam') == false          # (case matters)          # result: true
 p palindrome?("madam i'm adam") == false # (all characters matter) # result: true
 p palindrome?('356653') == true                                    # result: true
