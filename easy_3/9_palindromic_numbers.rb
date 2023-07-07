eb2826f325c18ffa600d0872b240525f0bb6d94c
Justin Greger <justingreger@gmail.com>
2023-07-07 19:33:54 -0400

    p

--- easy_3/9_palindromic_numbers.rb
+++ easy_3/9_palindromic_numbers.rb
@@ -8,7 +8,7 @@ def palindromic_number?(integer)
 end
 
 # Tests:
-palindromic_number?(34543) == true
-palindromic_number?(123210) == false
-palindromic_number?(22) == true
-palindromic_number?(5) == true
+p palindromic_number?(34543) == true
+p palindromic_number?(123210) == false
+p palindromic_number?(22) == true
+p palindromic_number?(5) == true
