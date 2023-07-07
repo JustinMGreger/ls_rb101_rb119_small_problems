bc8786c6fe8b2e1540f6a62824941e93e0647d1c
Justin Greger <justingreger@gmail.com>
2023-07-07 16:53:10 -0400

    deleted should be case-insensitive, and it should ignore all non-alphanumeric characters
    # Examples:

--- easy_3/8_palindromic_strings_part_2.rb
+++ easy_3/8_palindromic_strings_part_2.rb
@@ -4,16 +4,13 @@ def palindrome?(sequence)
   sequence.size.times.all? { |i| sequence[i] == sequence[-i - 1] }
 end
 
-should be case-insensitive, and it should ignore all non-alphanumeric characters. 
+def real_palindrome?(sequence)
+  palindrome?(sequence)
+end
 
-# Examples:
 real_palindrome?('madam') == true
 real_palindrome?('Madam') == true           # (case does not matter)
 real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
 real_palindrome?('356653') == true
 real_palindrome?('356a653') == true
 real_palindrome?('123ab321') == false
-
-def real_palindrome?(sequence)
-  palindrome?(sequence)
-end
