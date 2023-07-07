69775183bf0e4461e3bf05ced0bdd92a7aeaab80
Justin Greger <justingreger@gmail.com>
2023-07-07 19:54:56 -0400

    Further Exploration

--- easy_3/9_palindromic_numbers.rb
+++ easy_3/9_palindromic_numbers.rb
@@ -9,3 +9,10 @@ p palindromic_number?(34_543) == true # result: true
 p palindromic_number?(123_210) == false # result: true
 p palindromic_number?(22) == true # result: true
 p palindromic_number?(5) == true # result: true
+
+Further Exploration
+
+Suppose your number begins with one or more 0s. 
+Will your method still work? 
+Why or why not? 
+Is there any way to address this?
