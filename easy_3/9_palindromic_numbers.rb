06a32cae31a63178a613614c27e06be934cd647a
Justin Greger <justingreger@gmail.com>
2023-07-07 19:29:52 -0400

    if its integer argument is palindromic A palindromic number reads the same forwards and backwards.

--- easy_3/9_palindromic_numbers.rb
+++ easy_3/9_palindromic_numbers.rb
@@ -7,9 +7,8 @@ palindromic_number?(22) == true
 palindromic_number?(5) == true
 
 def palindromic_number?(integer)
-  if its integer argument is palindromic
+  if its integer argument is palindromic A palindromic number reads the same forwards and backwards.
     returns true  
   else 
     return false  
-A palindromic number reads the same forwards and backwards.
 end
