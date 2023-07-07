60c44f149784976427e823a4f3dac3a183298887
Justin Greger <justingreger@gmail.com>
2023-07-06 21:56:05 -0400

    examples

--- easy_3/8_palindromic_strings_part_2.rb
+++ easy_3/8_palindromic_strings_part_2.rb
@@ -10,3 +10,9 @@ This time, however, your method should be case-insensitive, and it should ignore
 If you wish, you may simplify things by calling the palindrome? method you wrote in the previous exercise.
 
 Examples:
+real_palindrome?('madam') == true
+real_palindrome?('Madam') == true           # (case does not matter)
+real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+real_palindrome?('356653') == true
+real_palindrome?('356a653') == true
+real_palindrome?('123ab321') == false
