27de9bc8f72e7019be04fc825372401fa765e61d
Justin Greger <justingreger@gmail.com>
2023-07-07 19:32:05 -0400

    moved the placement of the method

--- easy_3/9_palindromic_numbers.rb
+++ easy_3/9_palindromic_numbers.rb
@@ -1,14 +1,12 @@
-
-
-Examples:
-palindromic_number?(34543) == true
-palindromic_number?(123210) == false
-palindromic_number?(22) == true
-palindromic_number?(5) == true
-
 def palindromic_number?(integer)
   if integer == integer.reverse
     true  
   else 
     false  
 end
+
+Examples:
+palindromic_number?(34543) == true
+palindromic_number?(123210) == false
+palindromic_number?(22) == true
+palindromic_number?(5) == true
