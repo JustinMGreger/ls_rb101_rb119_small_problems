cfb1cdc95b78e1a57a435c1ce8fde74354f32390
Justin Greger <justingreger@gmail.com>
2023-07-06 20:58:13 -0400

    removed spacing'

--- easy_3/7_palindromic_strings_part_1.rb
+++ easy_3/7_palindromic_strings_part_1.rb
@@ -8,6 +8,6 @@ end
 p palindrome?('madam') == true # result: true
 p palindrome?('Madam') == false          # (case matters)          # result: true
 p palindrome?("madam i'm adam") == false # (all characters matter) # result: true
-p palindrome?('356653') == true                                    # result: true
-p palindrome?([1, true, nil, 'string', 'string', nil, true, 1]) == true  # result: true.
+p palindrome?('356653') == true # result: true
+p palindrome?([1, true, nil, 'string', 'string', nil, true, 1]) == true # result: true.
 p palindrome?([1, false]) == false # result: true.
