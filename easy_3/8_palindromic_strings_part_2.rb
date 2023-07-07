9d0a081253a3e2d380fc790275997f593a6c64b0
Justin Greger <justingreger@gmail.com>
2023-07-06 21:56:34 -0400

    #

--- easy_3/8_palindromic_strings_part_2.rb
+++ easy_3/8_palindromic_strings_part_2.rb
@@ -4,12 +4,12 @@ def palindrome?(sequence)
   sequence.size.times.all? { |i| sequence[i] == sequence[-i - 1] }
 end
 
-Write another method that returns true if the string passed as an argument is a palindrome, 
-false otherwise. 
-This time, however, your method should be case-insensitive, and it should ignore all non-alphanumeric characters. 
-If you wish, you may simplify things by calling the palindrome? method you wrote in the previous exercise.
+# Write another method that returns true if the string passed as an argument is a palindrome, 
+# false otherwise. 
+# This time, however, your method should be case-insensitive, and it should ignore all non-alphanumeric characters. 
+# If you wish, you may simplify things by calling the palindrome? method you wrote in the previous exercise.
 
-Examples:
+# Examples:
 real_palindrome?('madam') == true
 real_palindrome?('Madam') == true           # (case does not matter)
 real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
