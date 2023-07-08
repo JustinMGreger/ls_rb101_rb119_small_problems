4586832475451dad75a4d15e0868f1a6e1493de2
Justin Greger <justingreger@gmail.com>
2023-07-08 19:21:53 -0400

    #

--- easy_4/1_short_long_short.rb
+++ easy_4/1_short_long_short.rb
@@ -1,19 +1,19 @@
-Write a method that takes two strings as arguments, determines the longest of the two strings, and then returns the result
-of concatenating the shorter string, the longer string, and the shorter string once again. 
-You may assume that the strings are of different lengths.
+# Write a method that takes two strings as arguments, determines the longest of the two strings, and then returns the 
+# result of concatenating the shorter string, the longer string, and the shorter string once again. 
+# You may assume that the strings are of different lengths.
 
-Examples:
+# Examples:
 short_long_short('abc', 'defgh') == "abcdefghabc"
 short_long_short('abcde', 'fgh') == "fghabcdefgh"
 short_long_short('', 'xyz') == "xyz"
 
-PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
-PEDAC stands for “[Understand the] Problem, 
-Identify expected input: 
-and output:
-Make the requirements explicit Identify rules:
-Mental model of the problem (optional):
-Examples / Test Cases, Validate understanding of the problem:
-Data Structure, How we represent data that we will work with when converting the input to output:
-Algorithm, Steps for converting input to output:
-and Code.” Implementation of Algorithm:
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for “[Understand the] Problem, 
+# Identify expected input: 
+# and output:
+# Make the requirements explicit Identify rules:
+# Mental model of the problem (optional):
+# Examples / Test Cases, Validate understanding of the problem:
+# Data Structure, How we represent data that we will work with when converting the input to output:
+# Algorithm, Steps for converting input to output:
+# and Code.” Implementation of Algorithm:
