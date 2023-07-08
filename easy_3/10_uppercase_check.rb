cc91b3a3a926e02a5e1f9a84215491a0fe42c74a
Justin Greger <justingreger@gmail.com>
2023-07-08 17:38:57 -0400

    # It would not make sense.

--- easy_3/10_uppercase_check.rb
+++ easy_3/10_uppercase_check.rb
@@ -13,7 +13,10 @@ p uppercase?('FOUR SCORE') == true # result: true
 p uppercase?('4SCORE!') == true # result: true
 p uppercase?('') == true # result: true
 
-Further Exploration
+# Further Exploration
 
-Food for thought: in our examples, we show that uppercase? should return true if the argument is an empty string. 
-Would it make sense to return false instead? Why or why not?
+# Food for thought: in our examples, we show that uppercase? should return true if the argument is an empty string. 
+# Would it make sense to return false instead? 
+# Why or why not?
+# It would not make sense.
+# There are no characters that are not uppercase as there are no characters.
