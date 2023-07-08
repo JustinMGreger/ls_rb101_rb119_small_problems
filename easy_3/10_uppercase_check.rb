f65296d2db35c4eb42ae60c3e9d4cf3bf444016a
Justin Greger <justingreger@gmail.com>
2023-07-08 16:37:38 -0400

    instructions

---
+++ easy_3/10_uppercase_check.rb
@@ -0,0 +1,5 @@
+Write a method that takes a string argument, 
+and returns true if all of the alphabetic characters inside the string are uppercase, false otherwise. 
+Characters that are not alphabetic should be ignored.
+
+Examples:
