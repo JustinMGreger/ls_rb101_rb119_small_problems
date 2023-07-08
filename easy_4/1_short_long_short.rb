f6148fd1cb6940129b06fac5f1303be7fc620345
Justin Greger <justingreger@gmail.com>
2023-07-08 19:22:12 -0400

    examples to tests

--- easy_4/1_short_long_short.rb
+++ easy_4/1_short_long_short.rb
@@ -2,7 +2,7 @@
 # result of concatenating the shorter string, the longer string, and the shorter string once again. 
 # You may assume that the strings are of different lengths.
 
-# Examples:
+# Tests:
 short_long_short('abc', 'defgh') == "abcdefghabc"
 short_long_short('abcde', 'fgh') == "fghabcdefgh"
 short_long_short('', 'xyz') == "xyz"
