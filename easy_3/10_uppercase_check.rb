f2d533b10c151c20fffbc16d54475d41d24911c2
Justin Greger <justingreger@gmail.com>
2023-07-08 16:43:28 -0400

    p

--- easy_3/10_uppercase_check.rb
+++ easy_3/10_uppercase_check.rb
@@ -3,10 +3,10 @@ and returns true if all of the alphabetic characters inside the string are upper
 Characters that are not alphabetic should be ignored.
 
 # Tests:
-uppercase?('t') == false
-uppercase?('T') == true
-uppercase?('Four Score') == false
-uppercase?('FOUR SCORE') == true
-uppercase?('4SCORE!') == true
-uppercase?('') == true
+p uppercase?('t') == false
+p uppercase?('T') == true
+p uppercase?('Four Score') == false
+p uppercase?('FOUR SCORE') == true
+p uppercase?('4SCORE!') == true
+p uppercase?('') == true
 
