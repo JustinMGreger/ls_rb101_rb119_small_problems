0b3c08720080e314a4e4305005328076ad154f0d
Justin Greger <justingreger@gmail.com>
2023-07-08 19:28:00 -0400

    # two strings

--- easy_4/1_short_long_short.rb
+++ easy_4/1_short_long_short.rb
@@ -9,7 +9,10 @@ short_long_short('', 'xyz') == "xyz"
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem, 
+
 # Identify expected input: 
+# two strings
+
 # and output:
 # Make the requirements explicit Identify rules:
 # Mental model of the problem (optional):
