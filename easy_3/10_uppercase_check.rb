37ff8ec11cc88b581ea7f2a843348cf57c3db47b
Justin Greger <justingreger@gmail.com>
2023-07-08 17:17:21 -0400

    deleted Characters that are not  should be ignored.

--- easy_3/10_uppercase_check.rb
+++ easy_3/10_uppercase_check.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def uppercase?(string_argument)
-  alphabetic = Characters that are not  should be ignored. string_argument
+  alphabetic = string_argument
   alphabetic == alphabetic.upcase
 end
 
