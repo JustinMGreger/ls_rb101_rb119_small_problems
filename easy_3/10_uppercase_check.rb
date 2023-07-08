14812ccad29ff69ef22894ce191bb8d1b6597509
Justin Greger <justingreger@gmail.com>
2023-07-08 16:54:47 -0400

    Write a method that takes a string argument,

--- easy_3/10_uppercase_check.rb
+++ easy_3/10_uppercase_check.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 def uppercase?(string_argument)
-Write a method that takes a string argument, 
 and returns true if all of the alphabetic characters inside the string are uppercase, false otherwise. 
 Characters that are not alphabetic should be ignored.
 
