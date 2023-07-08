c68ce842fe8795ea52ad2396e1e5798f3290972b
Justin Greger <justingreger@gmail.com>
2023-07-07 20:48:34 -0400

    # Why or why not?

--- easy_3/9_palindromic_numbers.rb
+++ easy_3/9_palindromic_numbers.rb
@@ -23,5 +23,31 @@ Yes it will for examples: (011) & (0011)
 No it will not for examples: (0110) & (001100)
 
 # Why or why not? 
+In Ruby integers do not have leading zeros. 
+leading zeros do not change the value of the number. 
+In Ruby when reverse is used the trailing zeros are not stored in mememory.
+
+When you convert an integer to a string, any leading zeros in the integer are dropped, because integers do not have 
+leading zeros. 
+
+In the code the integer is converted to a string. for example: 0110 becomes '110'.
+Then the string is reversed. '110' becomes '011'.
+Then check if the string and the reversed string are equal. '110' == '011' returns false.
+
+(001100)
+In the code the integer is converted to a string. for example: 001100 becomes '1100'.
+Then the string is reversed. '1100' becomes '0011'.
+Then check if the string and the reversed string are equal. '1100' == '0011' returns false.
+
+(011) 
+In the code the integer is converted to a string. for example: 011 becomes '11'.
+Then the string is reversed. '11' becomes '11'.
+Then check if the string and the reversed string are equal. '11' == '11' returns true.
+
+(0011)
+
+In the code the integer is converted to a string. for example:  0011 becomes '11'.
+Then the string is reversed. '11' becomes '11'.
+Then check if the string and the reversed string are equal. '11' == '11' returns true.
 
 # Is there any way to address this?
