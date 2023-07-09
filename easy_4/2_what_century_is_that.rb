9097b6546755305bebb549d48210d91541656bb2
Justin Greger <justingreger@gmail.com>
2023-07-09 19:48:10 -0400

    examples

--- easy_4/2_what_century_is_that.rb
+++ easy_4/2_what_century_is_that.rb
@@ -5,3 +5,13 @@
 # New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
 
 # Examples:
+
+century(2000) == '20th'
+century(2001) == '21st'
+century(1965) == '20th'
+century(256) == '3rd'
+century(5) == '1st'
+century(10103) == '102nd'
+century(1052) == '11th'
+century(1127) == '12th'
+century(11201) == '113th'
