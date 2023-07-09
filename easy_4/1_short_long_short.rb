8c686084d2d5cdc871fafe06ec5a7906d3c743da
Justin Greger <justingreger@gmail.com>
2023-07-09 17:54:40 -0400

    deleted determines the longest & shortest of the two strings.

--- easy_4/1_short_long_short.rb
+++ easy_4/1_short_long_short.rb
@@ -2,7 +2,6 @@
 
 # and Code.” Implementation of Algorithm:
 def short_long_short(string_argument_one, string_argument_two)
-determines the longest & shortest of the two strings.
   if string_argument_one.length > string_argument_two.length
     longest_string = string_argument_one
     shortest_string = string_argument_two
