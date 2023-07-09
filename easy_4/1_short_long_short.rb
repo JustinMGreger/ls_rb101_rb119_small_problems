de6dff920132262d33f2f08db11a554911b5a586
Justin Greger <justingreger@gmail.com>
2023-07-09 17:29:28 -0400

    # and Code.” Implementation of Algorithm:

--- easy_4/1_short_long_short.rb
+++ easy_4/1_short_long_short.rb
@@ -43,3 +43,12 @@
 # 6. assign this to a variable.
 
 # and Code.” Implementation of Algorithm:
+# 1. the short_long_short method accepts 2 string arguments.
+# 2. the short_long_short method determines the longest & shortest of the two strings.
+# 3. that information is stored in variables.
+# 4. organize the variables so that "the shorter_string", "the longer_string", "the shorter_string".
+# 5. concatenate these variables into 1 string.
+# 6. assign this to a variable.
+
+
+
