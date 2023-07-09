2722b59168b2d14c7a18307c2f7e4ccd8ae36c4e
Justin Greger <justingreger@gmail.com>
2023-07-09 17:36:25 -0400

    deleted empty lines

--- easy_4/1_short_long_short.rb
+++ easy_4/1_short_long_short.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-
 # Tests:
 # p short_long_short('abc', 'defgh') == "abcdefghabc"
 # p short_long_short('abcde', 'fgh') == "fghabcdefgh"
 # p short_long_short('', 'xyz') == "xyz"
-
 
 # and Code.” Implementation of Algorithm:
 # 1. the short_long_short method accepts 2 string arguments.
