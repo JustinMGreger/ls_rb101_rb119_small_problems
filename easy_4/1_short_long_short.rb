e62cc47804346d97a425e15006055a21d15df42a
Justin Greger <justingreger@gmail.com>
2023-07-09 17:35:01 -0400

    deleted # Write a method that takes two strings as arguments, determines the longest of the two strings, and then returns the # result of concatenating the shorter string, the longer string, and the shorter string once again. # You may assume that the strings are of different lengths. :

--- easy_4/1_short_long_short.rb
+++ easy_4/1_short_long_short.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# Write a method that takes two strings as arguments, determines the longest of the two strings, and then returns the 
-# result of concatenating the shorter string, the longer string, and the shorter string once again. 
-# You may assume that the strings are of different lengths.
 
 # Tests:
 # p short_long_short('abc', 'defgh') == "abcdefghabc"
