4b466a0705f148eda6afaf2d523aa4aca4cc97d8
Justin Greger <justingreger@gmail.com>
2023-07-09 19:49:12 -0400

    p

--- easy_4/2_what_century_is_that.rb
+++ easy_4/2_what_century_is_that.rb
@@ -6,12 +6,12 @@
 
 # Examples:
 
-century(2000) == '20th'
-century(2001) == '21st'
-century(1965) == '20th'
-century(256) == '3rd'
-century(5) == '1st'
-century(10103) == '102nd'
-century(1052) == '11th'
-century(1127) == '12th'
-century(11201) == '113th'
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
