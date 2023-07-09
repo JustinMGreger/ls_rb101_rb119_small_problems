ea019328797119fe05ac415792b26d1f774cab1f
Justin Greger <justingreger@gmail.com>
2023-07-09 17:40:56 -0400

    determines the longest & shortest of the two strings.

--- easy_4/1_short_long_short.rb
+++ easy_4/1_short_long_short.rb
@@ -7,7 +7,7 @@
 
 # and Code.” Implementation of Algorithm:
 def short_long_short(string_argument_one, string_argument_two)
-# 2. the short_long_short method determines the longest & shortest of the two strings.
+determines the longest & shortest of the two strings.
 # 3. that information is stored in variables.
 # 4. organize the variables so that "the shorter_string", "the longer_string", "the shorter_string".
 # 5. concatenate these variables into 1 string.
