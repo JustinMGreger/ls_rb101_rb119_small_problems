7de1fb5f90a4ef385c584ff3053f93e3bf61d2f4
Justin Greger <justingreger@gmail.com>
2023-07-09 17:48:14 -0400

    else     longest_string = string_argument_two     shortest_string = string_argument_one

--- easy_4/1_short_long_short.rb
+++ easy_4/1_short_long_short.rb
@@ -11,6 +11,9 @@ determines the longest & shortest of the two strings.
   if string_argument_one.length > string_argument_two.length
     longest_string = string_argument_one
     shortest_string = string_argument_two
+  else
+    longest_string = string_argument_two
+    shortest_string = string_argument_one
 # 3. that information is stored in variables.
 # 4. organize the variables so that "the shorter_string", "the longer_string", "the shorter_string".
 # 5. concatenate these variables into 1 string.
