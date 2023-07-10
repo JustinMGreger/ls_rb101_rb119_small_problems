02de3f4ad0ec83e8d70fecd96163bcfdfe32b8fe
Justin Greger <justingreger@gmail.com>
2023-07-09 20:43:38 -0400

    # Make the requirements explicit Identify rules:

--- easy_4/2_what_century_is_that.rb
+++ easy_4/2_what_century_is_that.rb
@@ -27,6 +27,11 @@ p century(11201) == '113th'
 # New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
 
 # Make the requirements explicit Identify rules:
+# Write a method that takes a year as input and returns the century. 
+# The return value should be a string that begins with the century number, 
+# and ends with st, nd, rd, or th as appropriate for that number.
+# New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
+
 # Mental model of the problem (optional):
 # Examples / Test Cases, Validate understanding of the problem:
 # Data Structure, How we represent data that we will work with when converting the input to output:
