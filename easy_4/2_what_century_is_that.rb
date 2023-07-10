2aff324e444351505ebfde6400d39c868f6e73bf
Justin Greger <justingreger@gmail.com>
2023-07-09 21:22:30 -0400

    deleted # and output: # a string that begins with the century number, and ends with st, nd, rd, or th as appropriate for that number. # New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
    # Make the requirements explicit Identify rules:
    # Write a method that takes a year as input and returns the century.
    # The return value should be a string that begins with the century number,
    # and ends with st, nd, rd, or th as appropriate for that number.
    # New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
    
    # Mental model of the problem (optional):
    1. the century method accepts a year integer input.
    2. and returns the century.
    3. The return value should be a string that begins with the century number.
    4. and ends with st, nd, rd, or th as appropriate for that number.
    5. New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.

--- easy_4/2_what_century_is_that.rb
+++ easy_4/2_what_century_is_that.rb
@@ -22,22 +22,6 @@ p century(11201) == '113th'
 # Identify expected input: 
 # a year
 
-# and output:
-# a string that begins with the century number, and ends with st, nd, rd, or th as appropriate for that number.
-# New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
-
-# Make the requirements explicit Identify rules:
-# Write a method that takes a year as input and returns the century. 
-# The return value should be a string that begins with the century number, 
-# and ends with st, nd, rd, or th as appropriate for that number.
-# New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
-
-# Mental model of the problem (optional):
-1. the century method accepts a year integer input.
-2. and returns the century. 
-3. The return value should be a string that begins with the century number.
-4. and ends with st, nd, rd, or th as appropriate for that number.
-5. New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
 
 # Examples / Test Cases, Validate understanding of the problem:
 p century(2000) == '20th'
