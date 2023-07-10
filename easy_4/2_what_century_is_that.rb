25115acb5289efae2e87bdb7d4cba1c2a89aee10
Justin Greger <justingreger@gmail.com>
2023-07-09 20:27:22 -0400

    PEDAC

--- easy_4/2_what_century_is_that.rb
+++ easy_4/2_what_century_is_that.rb
@@ -16,3 +16,14 @@ p century(10103) == '102nd'
 p century(1052) == '11th'
 p century(1127) == '12th'
 p century(11201) == '113th'
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+and output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
