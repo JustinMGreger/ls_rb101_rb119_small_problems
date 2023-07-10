c864bf5e4a97cb4c4c98b2104842b7ea96293b94
Justin Greger <justingreger@gmail.com>
2023-07-10 19:16:20 -0400

    moved tests

--- easy_4/2_what_century_is_that.rb
+++ easy_4/2_what_century_is_that.rb
@@ -5,16 +5,6 @@
 # and ends with st, nd, rd, or th as appropriate for that number.
 # New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
 
-# Tests:
-p century(2000) == '20th'
-p century(2001) == '21st'
-p century(1965) == '20th'
-p century(256) == '3rd'
-p century(5) == '1st'
-p century(10103) == '102nd'
-p century(1052) == '11th'
-p century(1127) == '12th'
-p century(11201) == '113th'
 
 def century(year)
   century_number = calculate_century_number(year)
@@ -44,3 +34,14 @@ def calculate_century_suffix(century)
 
   suffix
 end
+
+# Tests:
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
