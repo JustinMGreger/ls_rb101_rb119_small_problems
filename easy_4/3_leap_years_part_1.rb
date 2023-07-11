48050e25080e7366fd965a2a0e21b08fec6406d3
Justin Greger <justingreger@gmail.com>
2023-07-11 19:40:44 -0400

    #Tests:

--- easy_4/3_leap_years_part_1.rb
+++ easy_4/3_leap_years_part_1.rb
@@ -5,3 +5,18 @@ If the year is evenly divisible by 100, then it is not a leap year unless the ye
 Assume this rule is good for any year greater than year 0. 
 Write a method that takes any year greater than 0 as input, and returns true if the year is a leap year, 
 or false if it is not a leap year.
+
+#Tests:
+leap_year?(2016) == true
+leap_year?(2015) == false
+leap_year?(2100) == false
+leap_year?(2400) == true
+leap_year?(240000) == true
+leap_year?(240001) == false
+leap_year?(2000) == true
+leap_year?(1900) == false
+leap_year?(1752) == true
+leap_year?(1700) == false
+leap_year?(1) == false
+leap_year?(100) == false
+leap_year?(400) == true
