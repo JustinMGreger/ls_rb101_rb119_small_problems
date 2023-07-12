ad670a5856185051a81318f8e16c68d9b768ccbc
Justin Greger <justingreger@gmail.com>
2023-07-12 16:29:41 -0400

    p

--- easy_4/3_leap_years_part_1.rb
+++ easy_4/3_leap_years_part_1.rb
@@ -24,19 +24,19 @@
 # if year % 100 == 0 then year != leap_year unless year % 400 == 0
 
 # Examples / Test Cases, Validate understanding of the problem:
-# leap_year?(2016) == true
-# leap_year?(2015) == false
-# leap_year?(2100) == false
-# leap_year?(2400) == true
-# leap_year?(240000) == true
-# leap_year?(240001) == false
-# leap_year?(2000) == true
-# leap_year?(1900) == false
-# leap_year?(1752) == true
-# leap_year?(1700) == false
-# leap_year?(1) == false
-# leap_year?(100) == false
-# leap_year?(400) == true
+# p leap_year?(2016) == true
+# p leap_year?(2015) == false
+# p leap_year?(2100) == false
+# p leap_year?(2400) == true
+# p leap_year?(240000) == true
+# p leap_year?(240001) == false
+# p leap_year?(2000) == true
+# p leap_year?(1900) == false
+# p leap_year?(1752) == true
+# p leap_year?(1700) == false
+# p leap_year?(1) == false
+# p leap_year?(100) == false
+# p leap_year?(400) == true
 
 
 
