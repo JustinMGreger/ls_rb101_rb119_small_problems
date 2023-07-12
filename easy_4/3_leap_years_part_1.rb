23ae12345e931e86951024b2463e75d2529d68ab
Justin Greger <justingreger@gmail.com>
2023-07-12 17:42:20 -0400

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for “[Understand the] Problem,
    # Identify expected input:
    # year greater than 0
    
    # and output:
    # true if the year is a leap year, or false if it is not a leap year.
    
    # Make the requirements explicit Identify rules:
    # Write a method that takes any year greater than 0 as input,
    # and returns true if the year is a leap year, or false if it is not a leap year.
    # leap years occur in every year that is evenly divisible by 4, unless the year is also divisible by 100.
    # If the year is evenly divisible by 100, then it is not a leap year unless the year is evenly divisible by 400.
    
    # Mental model of the problem (optional):
    # year = input any year greater than 0
    # leap_year?(year)
    # if the year is a leap year return true
    # else return false
    # leap years == year % 4 == 0 unless year % 100 == 0
    # if year % 100 == 0 then year != leap_year unless year % 400 == 0

--- easy_4/3_leap_years_part_1.rb
+++ easy_4/3_leap_years_part_1.rb
@@ -1,30 +1,5 @@
 # frozen_string_literal: true
 
-# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
-# PEDAC stands for “[Understand the] Problem, 
-
-# Identify expected input: 
-# year greater than 0
-
-# and output:
-# true if the year is a leap year, or false if it is not a leap year.
-
-# Make the requirements explicit Identify rules:
-# Write a method that takes any year greater than 0 as input, 
-# and returns true if the year is a leap year, or false if it is not a leap year.
-# leap years occur in every year that is evenly divisible by 4, unless the year is also divisible by 100. 
-# If the year is evenly divisible by 100, then it is not a leap year unless the year is evenly divisible by 400.
-
-# Mental model of the problem (optional):
-# year = input any year greater than 0
-# leap_year?(year) 
-# if the year is a leap year return true 
-# else return false 
-# leap years == year % 4 == 0 unless year % 100 == 0 
-# if year % 100 == 0 then year != leap_year unless year % 400 == 0
-
-
-
 def leap_year?(year)  
   (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
 end
