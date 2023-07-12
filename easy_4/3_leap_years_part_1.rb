24c05ef821698d4835a4fde0f9b642a82bc6afae
Justin Greger <justingreger@gmail.com>
2023-07-12 16:18:50 -0400

    if year % 100 == 0 then year != leap_year unless year % 400 == 0

--- easy_4/3_leap_years_part_1.rb
+++ easy_4/3_leap_years_part_1.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
  then it is a leap_year unless the 
- then year != leap_year unless year % 400 == 0
+ 
 
 Assume this rule is good for any year greater than year 0. 
 Write a method that takes any year greater than 0 as input, 
@@ -44,7 +44,7 @@ if the year is a leap year return true
 else return false 
 
 leap years == year % 4 == 0 unless year % 100 == 0 
-if year % 100 == 0 , then it is not a leap year unless the year is evenly divisible by 400.
+if year % 100 == 0 then year != leap_year unless year % 400 == 0
 
 
 
