fbd4f5566055d27b538eb8f5ff8223a8dd1d837d
Justin Greger <justingreger@gmail.com>
2023-07-11 20:25:00 -0400

    PEDAC

--- easy_4/3_leap_years_part_1.rb
+++ easy_4/3_leap_years_part_1.rb
@@ -20,3 +20,14 @@ leap_year?(1700) == false
 leap_year?(1) == false
 leap_year?(100) == false
 leap_year?(400) == true
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+and output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
