16cc65f714af9832ae6389ba944f59b5eddd7c3c
Justin Greger <justingreger@gmail.com>
2023-07-13 17:53:21 -0400

    # result: true.

--- easy_1/4_whats_my_bonus.rb
+++ easy_1/4_whats_my_bonus.rb
@@ -5,6 +5,6 @@ def calculate_bonus(integer, boolean)
 end
 
 # Tests:
-p calculate_bonus(2800, true) == 1400
-p calculate_bonus(1000, false).zero?
-p calculate_bonus(50_000, true) == 25_000
+p calculate_bonus(2800, true) == 1400 # result: true.
+p calculate_bonus(1000, false).zero? # result: true.
+p calculate_bonus(50_000, true) == 25_000 # result: true.
