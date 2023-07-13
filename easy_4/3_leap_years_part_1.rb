205e656fbbf3ceb944e42f3a726a9562b0feb806
Justin Greger <justingreger@gmail.com>
2023-07-12 20:05:15 -0400

    # def leap_year?(year)   #(year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) end deleted

--- easy_4/3_leap_years_part_1.rb
+++ easy_4/3_leap_years_part_1.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-def leap_year?(year)  
-  (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
-end
 
 # Tests:
 p leap_year?(2016) == true # result: true
@@ -19,10 +16,10 @@ p leap_year?(1) == false # result: true
 p leap_year?(100) == false # result: true
 p leap_year?(400) == true # result: true
 
-Further Exploration
+# Further Exploration
 
-The order in which you perform tests for a leap year calculation is important. For what years will leap_year? 
-fail if you rewrite it as:
+# The order in which you perform tests for a leap year calculation is important. 
+# For what years will leap_year? fail if you rewrite it as:
 
 def leap_year?(year)
   if year % 100 == 0
