920b9626f2e023d28d909669c7bb5a03c024ec11
Justin Greger <justingreger@gmail.com>
2023-07-13 18:59:25 -0400

    deleted   if [11, 12, 13].include?(century % 100)     suffix = 'th'   end

--- easy_4/2_what_century_is_that.rb
+++ easy_4/2_what_century_is_that.rb
@@ -18,9 +18,6 @@ def calculate_century_suffix(century)
            else 'th'
            end
 
-  if [11, 12, 13].include?(century % 100)
-    suffix = 'th'
-  end
 
   suffix
 end
@@ -35,3 +32,7 @@ p century(10_103) == '102nd Century' # result: true
 p century(1052) == '11th Century' # result: true
 p century(1127) == '12th Century' # result: true
 p century(11_201) == '113th Century' # result: true
+
+C: [Correctable] Style/IfUnlessModifier: Favor modifier if usage when having a single-line body. Another good alternative is the usage of control flow &&/||.
+if [11, 12, 13].include?(century % 100)
+^^
