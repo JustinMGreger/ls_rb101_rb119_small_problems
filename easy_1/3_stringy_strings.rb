4ed4f8f73dab7b64b0bdf1b87e9224a5314781df
Justin Greger <justingreger@gmail.com>
2023-07-13 17:45:38 -0400

    puts to p

--- easy_1/3_stringy_strings.rb
+++ easy_1/3_stringy_strings.rb
@@ -9,10 +9,10 @@ def stringy(parameter, optional_parameter = 1)
 end
 
 # Tests:
-puts stringy(6) == '101010'
-puts stringy(9) == '101010101'
-puts stringy(4) == '1010'
-puts stringy(7) == '1010101'
+p stringy(6) == '101010'
+p stringy(9) == '101010101'
+p stringy(4) == '1010'
+p stringy(7) == '1010101'
 
 the output of which is:
 true
