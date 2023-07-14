f654febd9916bf30128dd06b6cf9ea772f5a5f00
Justin Greger <justingreger@gmail.com>
2023-07-14 14:33:54 -0400

    deleted The output of which are: puts is_odd?(2) == false same output of the code using integer % 2 == 1 puts is_odd?(5) == true same output of the code using integer % 2 == 1 puts is_odd?(-17) == false difference detected the original output of the code using integer % 2 == 1 was true. puts is_odd?(-8) == false same output of the code using integer % 2 == 1 puts is_odd?(0) == false same output of the code using integer % 2 == 1 puts is_odd?(7) == true same output of the code using integer % 2 == 1
    Explantion:
    the remainder(2) method being called on -17 has a remainder of -1.
    -1 is not equal to 1 which is why it returns false.
    the % method returns 1 on -17 % 2.
    1 is equal to 1 which is why it returns true.
    the way the % method and the remainder method are programmed are why they return different answers.

--- easy_1/2_odd.rb
+++ easy_1/2_odd.rb
@@ -11,17 +11,3 @@ p is_odd?(-8)   # => false
 p is_odd?(0)    # => false
 p is_odd?(7)    # => true
 
-The output of which are:
-puts is_odd?(2) == false same output of the code using integer % 2 == 1
-puts is_odd?(5) == true same output of the code using integer % 2 == 1 
-puts is_odd?(-17) == false difference detected the original output of the code using integer % 2 == 1 was true.
-puts is_odd?(-8) == false same output of the code using integer % 2 == 1 
-puts is_odd?(0) == false same output of the code using integer % 2 == 1 
-puts is_odd?(7) == true same output of the code using integer % 2 == 1 
-
-Explantion:
-the remainder(2) method being called on -17 has a remainder of -1.
--1 is not equal to 1 which is why it returns false.
-the % method returns 1 on -17 % 2.
-1 is equal to 1 which is why it returns true.
-the way the % method and the remainder method are programmed are why they return different answers.
