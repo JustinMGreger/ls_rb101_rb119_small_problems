6ab998270e161a5059722dfc509feb9492cf5053
Justin Greger <justingreger@gmail.com>
2023-07-14 14:37:14 -0400

    # Tests:

--- easy_1/2_odd.rb
+++ easy_1/2_odd.rb
@@ -4,7 +4,7 @@ def is_odd?(integer)
   integer.remainder(2) == 1
 end
 
-tests:             original output of the code using integer % 2 == 1 
+# Tests:
 p is_odd?(2)    # => false
 p is_odd?(5)    # => true
 p is_odd?(-17)  # => true
