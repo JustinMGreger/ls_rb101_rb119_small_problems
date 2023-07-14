23282e3e8142c3f8df972e9919a213c078cee913
Justin Greger <justingreger@gmail.com>
2023-07-13 21:35:51 -0400

    deleted Under the Julian Calendar,

--- easy_4/4_leap_years_part_2.rb
+++ easy_4/4_leap_years_part_2.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 
-Prior to 1752, the Julian Calendar was used. 
-Under the Julian Calendar, leap years occur in any year that is evenly divisible by 4.
+Prior to 1752, the Julian Calendar was used leap years occur in any year that is evenly divisible by 4.
 
 Using this information, update the method from the previous exercise to determine leap years both before and after 1752.
 
