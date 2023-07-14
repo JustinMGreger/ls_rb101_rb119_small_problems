b9e8d03c21007dde4b3bd531e9fee7c30948479a
Justin Greger <justingreger@gmail.com>
2023-07-13 21:26:15 -0400

    # Tests:

--- easy_4/4_leap_years_part_2.rb
+++ easy_4/4_leap_years_part_2.rb
@@ -14,3 +14,18 @@ def leap_year?(year)
     false
   end
 end
+
+# Tests:
+leap_year?(2016) == true
+leap_year?(2015) == false
+leap_year?(2100) == false
+leap_year?(2400) == true
+leap_year?(240000) == true
+leap_year?(240001) == false
+leap_year?(2000) == true
+leap_year?(1900) == false
+leap_year?(1752) == true
+leap_year?(1700) == true
+leap_year?(1) == false
+leap_year?(100) == true
+leap_year?(400) == true
