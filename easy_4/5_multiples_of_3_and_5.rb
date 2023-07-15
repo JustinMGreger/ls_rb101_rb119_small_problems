4f7c8aaefc41cc48d32f36da259c667427bf08f4
Justin Greger <justingreger@gmail.com>
2023-07-15 19:38:27 -0400

    PEDAC

--- easy_4/5_multiples_of_3_and_5.rb
+++ easy_4/5_multiples_of_3_and_5.rb
@@ -11,3 +11,14 @@ multisum(3) == 3
 multisum(5) == 8
 multisum(10) == 33
 multisum(1_000) == 234_168
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+and output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
