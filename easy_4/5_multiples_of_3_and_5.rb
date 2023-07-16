0a59fca95ae2c73566b12d0470a0f7b23645f32c
Justin Greger <justingreger@gmail.com>
2023-07-15 20:44:12 -0400

    deleted text

--- easy_4/5_multiples_of_3_and_5.rb
+++ easy_4/5_multiples_of_3_and_5.rb
@@ -6,7 +6,6 @@ For instance, if the supplied number is 20, the result should be 98 (3 + 5 + 6 +
 
 You may assume that the number passed in is an integer greater than 1.
 
-
 Mental model of the problem (optional):
 def multisum(parameter)
   parameter > 1
@@ -14,17 +13,8 @@ def multisum(parameter)
   sum_of_multiples = 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + parameter)
 end
 
-Examples / Test Cases, Validate understanding of the problem:
 # Tests:
 multisum(3) == 3
 multisum(5) == 8
 multisum(10) == 33
 multisum(1_000) == 234_168
-
-
-
-
-
-Data Structure, How we represent data that we will work with when converting the input to output:
-Algorithm, Steps for converting input to output:
-and Code.” Implementation of Algorithm:
