28f3023d0c0bfa7bc4bb7487bc7e361c1147671c
Justin Greger <justingreger@gmail.com>
2023-07-15 20:38:35 -0400

    deleted # Identify expected input: # an integer greater than 1.
    # and output:
    # the sum of all multiples of 3 or 5 that lie between 1 and the integer greater than 1.
    
    Make the requirements explicit Identify rules:
    def multisum(parameter)
      parameter > 1
      searches for all multiples of 3 or 5 that lie between 1 and the parameter.
      sum_of_multiples = 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + parameter)
    end

--- easy_4/5_multiples_of_3_and_5.rb
+++ easy_4/5_multiples_of_3_and_5.rb
@@ -15,19 +15,14 @@ multisum(1_000) == 234_168
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem, 
 
-# Identify expected input: 
-# an integer greater than 1.
 
-# and output:
-# the sum of all multiples of 3 or 5 that lie between 1 and the integer greater than 1.
-
-Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
 def multisum(parameter)
   parameter > 1
-  searches for all multiples of 3 or 5 that lie between 1 and the parameter.
+  searches for all multiples of 3 or 5 that lie between 1..parameter.
   sum_of_multiples = 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + parameter)
 end
-Mental model of the problem (optional):
+
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
