831ac800ff19e827a831b8bb95626c8806b6acf3
Justin Greger <justingreger@gmail.com>
2023-07-16 19:44:07 -0400

    # Investigate Enumerable.inject (also known as Enumerable.reduce),

--- easy_4/5_multiples_of_3_and_5.rb
+++ easy_4/5_multiples_of_3_and_5.rb
@@ -21,7 +21,7 @@ p multisum(1_000) == 234_168
 
 # Further Exploration
 
-Investigate Enumerable.inject (also known as Enumerable.reduce), 
+# Investigate Enumerable.inject (also known as Enumerable.reduce), 
 How might this method be useful in solving this problem? 
 (Note that Enumerable methods are available when working with Arrays.) 
 Try writing such a solution. 
