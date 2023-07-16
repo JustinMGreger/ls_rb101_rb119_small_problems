f1ab33ce5a64199e9ccd4d1afb481da1d9f1dc14
Justin Greger <justingreger@gmail.com>
2023-07-15 20:52:56 -0400

    Mental model of the problem (optional):

--- easy_4/5_multiples_of_3_and_5.rb
+++ easy_4/5_multiples_of_3_and_5.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-Mental model of the problem (optional):
 def multisum(parameter)
   searches for all multiples of 3 or 5 that lie between 2..parameter.
   sum_of_multiples = 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + parameter)
