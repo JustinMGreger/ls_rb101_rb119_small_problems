a0a9815f191ee910ef7e7cf1f515916a262c810f
Justin Greger <justingreger@gmail.com>
2023-07-17 19:56:32 -0400

    # Tests:

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -3,7 +3,7 @@
 Write a method that takes an Array of numbers, and returns an Array with the same number of elements, 
 and each element has the running total from the original Array.
 
-Examples:
+# Tests:
 
 running_total([2, 5, 13]) == [2, 7, 20]
 running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
