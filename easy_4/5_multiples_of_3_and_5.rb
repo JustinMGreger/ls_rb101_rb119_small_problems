1b6776428eb2cc3c682ae350e7f0a43819ba60cf
Justin Greger <justingreger@gmail.com>
2023-07-16 20:40:38 -0400

    p

--- easy_4/5_multiples_of_3_and_5.rb
+++ easy_4/5_multiples_of_3_and_5.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 # ls solution modified to by rubocop compliant:
-def multiple?(number, divisor)
-  (number % divisor).zero?
-end
+# def multiple?(number, divisor)
+#  (number % divisor).zero?
+# end
 
-def multisum(max_value)
-  sum = 0
-  1.upto(max_value) do |number|
-    sum += number if multiple?(number, 3) || multiple?(number, 5)
-  end
-  sum
-end
+# def multisum(max_value)
+#  sum = 0
+#  1.upto(max_value) do |number|
+#    sum += number if multiple?(number, 3) || multiple?(number, 5)
+#  end
+#  sum
+# end
 
 # Tests:
-p multisum(3) == 3
-p multisum(5) == 8
-p multisum(10) == 33
-p multisum(1_000) == 234_168
+# p multisum(3) == 3
+# p multisum(5) == 8
+# p multisum(10) == 33
+# p multisum(1_000) == 234_168
 
 # Further Exploration
 
@@ -34,7 +34,17 @@ p multisum(1_000) == 234_168
 # Try writing such a solution. 
 # Answer:
 
+def multiple?(number, divisor)
+  (number % divisor).zero?
+end
 
+def multisum(max_value)
+  sum = 0
+  1.upto(max_value) do |number|
+    sum = [3..number].inject(:+) if multiple?(number, 3) || multiple?(number, 5)
+  end
+  sum
+end
 
 
 
