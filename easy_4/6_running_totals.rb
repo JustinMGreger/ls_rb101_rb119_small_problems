ce5c6e3895cf5c20bdc2e3f3ac78972dabab5daa
Justin Greger <justingreger@gmail.com>
2023-07-17 19:59:34 -0400

    deleted Examples / Test Cases, Validate understanding of the problem:

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -12,7 +12,6 @@ Identify expected input:
 and output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
-Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
