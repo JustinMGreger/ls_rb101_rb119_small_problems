5d9483fd0f92212dc3e6eeec1f4f2c582d6fd279
Justin Greger <justingreger@gmail.com>
2023-07-17 19:56:02 -0400

    frozen string literal true

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Write a method that takes an Array of numbers, and returns an Array with the same number of elements, 
 and each element has the running total from the original Array.
 
