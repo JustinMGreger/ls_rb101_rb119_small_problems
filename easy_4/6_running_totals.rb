77b3c1c98b0b96422b8b847da299332ed6cffae2
Justin Greger <justingreger@gmail.com>
2023-07-17 19:54:36 -0400

    examples

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -3,3 +3,7 @@ and each element has the running total from the original Array.
 
 Examples:
 
+running_total([2, 5, 13]) == [2, 7, 20]
+running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+running_total([3]) == [3]
+running_total([]) == []
