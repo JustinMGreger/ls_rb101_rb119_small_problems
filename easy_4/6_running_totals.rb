403c16278eae8b423676b26dc5e195d6147567be
Justin Greger <justingreger@gmail.com>
2023-07-17 19:55:30 -0400

    instructions

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -7,3 +7,8 @@ running_total([2, 5, 13]) == [2, 7, 20]
 running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
 running_total([3]) == [3]
 running_total([]) == []
+
+A running total is the sum of all values in a list up to and including the current element. 
+Thus, the running total when looking at index 2 of the array [14, 11, 7, 15, 20] is 32 (14 + 11 + 7), 
+while the running total at index 3 is 47 (14 + 11 + 7 + 15).
+
