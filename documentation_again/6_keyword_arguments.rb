d4299f10bee6bdc37501e345cde2ed26f314f381
Justin Greger <justingreger@gmail.com>
2023-07-18 17:30:27 -0400

    deleted Explanation: 5 is a integer. .step method is used to iterate over a range of numbers with a specific step value which follows the syntax: start_value.step(end_value, step_value) { |current_value| # code to be executed } to: means the exclusive limit of the range. 10 is a integer. by: specifies the step increment. 3 is a integer. { |value| puts value } is a block of code that means for value print value to the screen. this means: print to the screen the integer 5 up to and excluding the integer 10 by 3. the output of which is:

--- documentation_again/6_keyword_arguments.rb
+++ documentation_again/6_keyword_arguments.rb
@@ -6,17 +6,6 @@ What does this code print?
 Syntax:
 5.step(to: 10, by: 3) { |value| puts value }
 
-Explanation:
-5 is a integer.
-.step method is used to iterate over a range of numbers with a specific step value which follows the syntax:
-start_value.step(end_value, step_value) { |current_value| # code to be executed } 
-to: means the exclusive limit of the range.
-10 is a integer.
-by: specifies the step increment.
-3 is a integer.
-{ |value| puts value } is a block of code that means for value print value to the screen.
-this means:
-print to the screen the integer 5 up to and excluding the integer 10 by 3.
-the output of which is:
+
 5
 8
