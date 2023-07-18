1b4afd9469c85ffd431868807314767f2f04609c
Justin Greger <justingreger@gmail.com>
2023-07-17 21:13:23 -0400

    deleted examples

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -4,20 +4,6 @@ def running_total([array])
   [array].modify the array so that each element is the sum of the current element and all previous elements.
   return [new_array] with the same number of elements 
 end
-# 2. 
-# 3. return the transformed Array 
-# example: 
-# [14, 11, 7, 15, 20][2] == 32 (14 + 11 + 7)
-# [14, 11, 7, 15, 20][3] == 47 (14 + 11 + 7 + 15)
-
-
-
-
-
-
-
-
-
 
 # Tests:
 running_total([2, 5, 13]) == [2, 7, 20]
