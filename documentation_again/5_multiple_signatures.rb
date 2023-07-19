b01decf3d25eb49c348f6eb25a937aa321d887e8
Justin Greger <justingreger@gmail.com>
2023-07-19 16:45:59 -0400

    a = %w[a b c d e]

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 # What do each of these puts statements output?
-a = %w(a b c d e)
-# puts a.fetch(7) 
-# puts a.fetch(7, 'beats me')
- puts a.fetch(7) { |index| index**2 }
+a = %w[a b c d e]
+puts a.fetch(7) 
+puts a.fetch(7, 'beats me')
+puts a.fetch(7) { |index| index**2 }
 
 # output:
 # index 7 outside of array bounds: -5...5 (IndexError)
