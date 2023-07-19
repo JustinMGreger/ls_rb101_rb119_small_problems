e83d3aecf9b9db315580c87fd849d7ce8ae46438
Justin Greger <justingreger@gmail.com>
2023-07-19 19:48:07 -0400

    deleted empty lines and # LS solution: def running_total(array)   sum = 0   array.map { |value| sum += value } end
    # Enumerable#inject
    def running_total(array)
      array.inject([[], 0]) do |(totals, sum), value|
        new_sum = sum + value
        [totals + [new_sum], new_sum]
      end.first
    end

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
-# LS solution:
-def running_total(array)
-  sum = 0
-  array.map { |value| sum += value }
-end
-
-# Enumerable#inject 
-def running_total(array)
-  array.inject([[], 0]) do |(totals, sum), value|
-    new_sum = sum + value
-    [totals + [new_sum], new_sum]
-  end.first
-end
-
 # Further Exploration
 # Enumerable#each_with_object
 
