eb36f99a62f466e3786ee16822f45cf379b4370c
Justin Greger <justingreger@gmail.com>
2023-07-19 16:42:42 -0400

    # 49

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -4,8 +4,9 @@
 a = %w(a b c d e)
 # puts a.fetch(7) 
 # puts a.fetch(7, 'beats me')
-# puts a.fetch(7) { |index| index**2 }
+ puts a.fetch(7) { |index| index**2 }
 
 # output:
 # index 7 outside of array bounds: -5...5 (IndexError)
 # beats me
+# 49
