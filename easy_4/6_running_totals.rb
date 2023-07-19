f844c5911d13c1f4d8361c67089727b27ae97b5c
Justin Greger <justingreger@gmail.com>
2023-07-18 20:00:54 -0400

    # Further Exploration

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -11,3 +11,8 @@ p running_total([2, 5, 13]) == [2, 7, 20] # result: true.
 p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67] # result: true.
 p running_total([3]) == [3] # result: true.
 p running_total([]) == [] # result: true.
+
+# Further Exploration
+
+# Try solving this problem using Enumerable#each_with_object or Enumerable#inject (note that Enumerable methods 
+# can be applied to Arrays).
