510cd8b19bcbf836188119a090d162bfb0f2b8ac
Justin Greger <justingreger@gmail.com>
2023-07-19 16:38:25 -0400

    # puts a.fetch(7, 'beats me')

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -3,8 +3,9 @@
 # What do each of these puts statements output?
 a = %w(a b c d e)
 # puts a.fetch(7) 
- puts a.fetch(7, 'beats me')
+# puts a.fetch(7, 'beats me')
 # puts a.fetch(7) { |index| index**2 }
 
 # output:
 # index 7 outside of array bounds: -5...5 (IndexError)
+# beats me
