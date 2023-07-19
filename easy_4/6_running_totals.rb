f96fad5b42b7480f0349a005866609a6daf36146
Justin Greger <justingreger@gmail.com>
2023-07-18 20:52:11 -0400

    moved the test

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -6,12 +6,6 @@
 #   array.map { |value| sum += value }
 # end
 
-# Tests:
-p running_total([2, 5, 13]) == [2, 7, 20] # result: true.
-p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67] # result: true.
-p running_total([3]) == [3] # result: true.
-p running_total([]) == [] # result: true.
-
 # Further Exploration
 
 # Try solving this problem using 
@@ -23,3 +17,9 @@ p running_total([]) == [] # result: true.
 # end
 
 # Enumerable#inject 
+
+# Tests:
+p running_total([2, 5, 13]) == [2, 7, 20] # result: true.
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67] # result: true.
+p running_total([3]) == [3] # result: true.
+p running_total([]) == [] # result: true.
