ab0f255e941b48c5cbb8fe280644cf0b32442eb5
Justin Greger <justingreger@gmail.com>
2023-07-19 16:26:55 -0400

    deleted Explanation of Initial Given Syntax:
    Syntax:
    a = %w(a b c d e)
    
    Explanation:
    a is a variable.
    = means assignment.
    %w is used to create an array of string literals, where each string is separated by whitespace.
    (a b c d e) are string elements.
    this means:
    the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace.
    the result of which is:
    [a, b, c, d, e]

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -9,20 +9,7 @@ puts a.fetch(7)
 puts a.fetch(7, 'beats me')
 puts a.fetch(7) { |index| index**2 }
 
-Explanation of Initial Given Syntax:
 
-Syntax:
-a = %w(a b c d e) 
-
-Explanation:
-a is a variable.
-= means assignment.
-%w is used to create an array of string literals, where each string is separated by whitespace.
-(a b c d e) are string elements.
-this means: 
-the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace. 
-the result of which is:
-["a", "b", "c", "d", "e"]
 
 Syntax:
 puts a.fetch(7)
