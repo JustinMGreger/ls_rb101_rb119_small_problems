907859ab43ec275e7f5953ce8b1bf571944ff6d3
Justin Greger <justingreger@gmail.com>
2023-07-19 19:57:19 -0400

    # Description of Exercise:

--- documentation_again/1_class_and_instance_methods.rb
+++ documentation_again/1_class_and_instance_methods.rb
@@ -1,7 +1,7 @@
 
-Description of Exercise:
-Locate the ruby documentation for methods File::path and File#path. 
-How are they different?
+# Description of Exercise:
+# Locate the ruby documentation for methods File::path and File#path. 
+# How are they different?
 
 Answer:
 File::path is a class method.
