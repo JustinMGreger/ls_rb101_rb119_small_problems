34bb02fd514d0751bebf75d8392128c760a0c159
Justin Greger <justingreger@gmail.com>
2023-07-19 16:27:53 -0400

    deleted a lot of text

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -1,81 +1,9 @@
 # frozen_string_literal: true
 
-Description of Exercise:
 What do each of these puts statements output?
-
-Initial Given Syntax:
 a = %w(a b c d e)
 puts a.fetch(7)
 puts a.fetch(7, 'beats me')
 puts a.fetch(7) { |index| index**2 }
 
 
-
-Syntax:
-puts a.fetch(7)
-
-Explanation:
-puts is put s which is put string which is print string to the screen.
-a is a variable.
-= means assignment.
-%w is used to create an array of string literals, where each string is separated by whitespace.
-(a b c d e) are string elements.
-this means: 
-the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace.
-the result of which is:
-["a", "b", "c", "d", "e"]
-.fetch method is used to retrieve the element at the specified index.
-(7) is a integer argument to the fetch method.
-this means:
-print to the screen the result of retrieving the element at index 7 from the 
-variable a which is assigned to the ["a", "b", "c", "d", "e"] array of string literals with each element seperated by 
-whitespace.
-the output of which is:
-`fetch': index 7 outside of array bounds: -5...5 (IndexError)
-	from ruby.rb:2:in `<main>'
-
-Syntax:
-puts a.fetch(7, 'beats me')
-
-Explanation:
-puts is put s which is put string which is print string to the screen.
-a is a variable.
-= means assignment.
-%w is used to create an array of string literals, where each string is separated by whitespace.
-(a b c d e) are string elements.
-this means: 
-the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace.
-the result of which is:
-["a", "b", "c", "d", "e"]
-.fetch method is used to retrieve the element at the specified index.
-(7, 'beats me') are arguments to the fetch method where 7 is a integer and 'beats me' is a string.
-this means:
-print to the screen the result of retrieving the element at index 7 from the 
-variable a which is assigned to the ["a", "b", "c", "d", "e"] array of string literals with each element seperated by 
-whitespace and if this fails then print to the screen the string 'beats me'.
-the output of which is:
-beats me
-
-Syntax:
-puts a.fetch(7) { |index| index**2 }
-
-Explanation:
-puts is put s which is put string which is print string to the screen.
-a is a variable.
-= means assignment.
-%w is used to create an array of string literals, where each string is separated by whitespace.
-(a b c d e) are string elements.
-this means: 
-the variable a is assigned to the (a b c d e) array of string literals, where each string is separated by whitespace.
-the result of which is:
-["a", "b", "c", "d", "e"]
-.fetch method is used to retrieve the element at the specified index.
-(7) is a integer argument to the fetch method.
-{ |index| index**2 } is a block of code passed to the fetch method where index is the index of the elements which are
-then raised to the power of 2 that is multiplied by itself.
-this means:
-print to the screen the result of retrieving the element at index 7 from the variable a which is assigned to the 
-["a", "b", "c", "d", "e"] array of string literals with each element seperated by whitespace and if this fails then print 
-to the screen the result of the index 7 raised to the power of 2 that is multiplied by itself which is the integer 49.
-the output of which is:
-49
