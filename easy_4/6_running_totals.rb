ccc7e5b219fb255ceeadd24632c10df458f918a5
Justin Greger <justingreger@gmail.com>
2023-07-19 19:54:24 -0400

    #

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
+#LS Solution:
+# def running_total(array)
+#   sum = 0
+#   array.map { |value| sum += value }
+# end
+
+# Enumerable#inject
+# def running_total(array)
+#  array.inject([[], 0]) do |(totals, sum), value|
+#    new_sum = sum + value
+#    [totals + [new_sum], new_sum]
+#  end.first
+# end
+
 # Further Exploration
 # Enumerable#each_with_object
 
+def running_total(array)
+
+
+
+end
 # Tests:
 p running_total([2, 5, 13]) == [2, 7, 20] # result: true.
 p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67] # result: true.
