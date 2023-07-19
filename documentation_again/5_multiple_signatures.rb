8a27a76370f244d91ae755dad1726dc15b5c7c1f
Justin Greger <justingreger@gmail.com>
2023-07-19 16:36:34 -0400

    # puts a.fetch(7)

--- documentation_again/5_multiple_signatures.rb
+++ documentation_again/5_multiple_signatures.rb
@@ -2,8 +2,8 @@
 
 # What do each of these puts statements output?
 a = %w(a b c d e)
-puts a.fetch(7) 
-# puts a.fetch(7, 'beats me')
+# puts a.fetch(7) 
+ puts a.fetch(7, 'beats me')
 # puts a.fetch(7) { |index| index**2 }
 
 # output:
