6934bf89dde41be3bda458f9819b3538fae01cb0
Justin Greger <justingreger@gmail.com>
2023-07-19 20:00:21 -0400

    # Answer:

--- documentation_again/1_class_and_instance_methods.rb
+++ documentation_again/1_class_and_instance_methods.rb
@@ -4,10 +4,10 @@
 # Locate the ruby documentation for methods File::path and File#path. 
 # How are they different?
 
-Answer:
-File::path is a class method.
-File::path is used to get the path to a file by giving the file's name.
+# Answer:
+# File::path is a class method.
+# File::path is used to get the path to a file by giving the file's name.
 
-File#path is an instance method.
-File#path is used to get the path of a file object that we have already created.
+# File#path is an instance method.
+# File#path is used to get the path of a file object that we have already created.
 
