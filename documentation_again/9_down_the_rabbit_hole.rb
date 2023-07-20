e9cac2a44bf200927603494db4dbdf41b8f2d824
Justin Greger <justingreger@gmail.com>
2023-07-20 18:05:51 -0400

    # Sometimes, the documentation is going to leave you scratching your head. # In a very early assignment at Launch School, you are tasked with writing a program that loads some text messages # from a YAML file. We do this with YAML::load_file:

--- documentation_again/9_down_the_rabbit_hole.rb
+++ documentation_again/9_down_the_rabbit_hole.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 # Description of Exercise:
+# Sometimes, the documentation is going to leave you scratching your head.
+# In a very early assignment at Launch School, you are tasked with writing a program that loads some text messages
+# from a YAML file. We do this with YAML::load_file:
 # Find the documentation for YAML::load_file.
 
 # Answer:
