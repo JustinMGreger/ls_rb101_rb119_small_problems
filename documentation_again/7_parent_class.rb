4afb15c80b325b022c496fb4980a641ebbb6a00b
Justin Greger <justingreger@gmail.com>
2023-07-19 21:45:39 -0400

    deleted empty lines

--- documentation_again/7_parent_class.rb
+++ documentation_again/7_parent_class.rb
@@ -11,11 +11,6 @@
 # How would you modify this code to print just the public methods that are defined or overridden by the String class? 
 # That is, the list should exclude all members that are only defined in Object, BasicObject, and Kernel.
 
-
-
-
-
-
 # LS solution:
 s = 'abc'
 puts s.public_methods(false).inspect
