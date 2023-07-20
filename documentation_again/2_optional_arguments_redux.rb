3472b2e2eadee2908b355a1847fdfb4ea8164514
Justin Greger <justingreger@gmail.com>
2023-07-19 20:29:18 -0400

    deleted text

--- documentation_again/2_optional_arguments_redux.rb
+++ documentation_again/2_optional_arguments_redux.rb
@@ -10,30 +10,6 @@ puts Date.civil(2016, 5)
 puts Date.civil(2016, 5, 13)
 
 # What will each of the 4 puts statements print?
-
-Explanaiton of Initial Given Syntax:
-
-Syntax:
-require 'date'
-
-Explanation:
-require means search for a file and if found then read and execute the file.
-'' is a string representing the name of the file.
-date is the name of a file.
-this means:
-search for the file named date and if found then read and execute the date file.
-
-Syntax:
-puts Date.civil
-
-Explanation:
-puts means put s which is put string which means print to the screen the string.
-Date is a class that provides methods for storing and manipulating calendar dates.
-.civil is a Date class method that creates a new Date object and with no arguments given it will return the date for 
-January 1, -4712.
-this means:
-print to the screen the result of creating a new Date object which is -4712-01-01.
-the result of which is:
 -4712-01-01
 
 Syntax:
