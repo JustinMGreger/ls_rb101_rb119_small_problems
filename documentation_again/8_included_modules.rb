0f8349319d27c73d93326aaae31d55e1be5823b8
Justin Greger <justingreger@gmail.com>
2023-07-20 17:33:26 -0400

    # LS solution:

--- documentation_again/8_included_modules.rb
+++ documentation_again/8_included_modules.rb
@@ -5,6 +5,7 @@
 # puts a.min
 # Find the documentation for the #min method and change the above code to print the two smallest values in the Array.
 
+# LS solution:
 a = [5, 9, 3, 11]
 puts a.min(2)
 
