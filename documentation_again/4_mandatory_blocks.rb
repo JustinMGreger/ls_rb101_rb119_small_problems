103a0745ea8709ced1bb7d8c1f8276569f9a369a
Justin Greger <justingreger@gmail.com>
2023-07-19 21:18:14 -0400

    deleted empty lines

--- documentation_again/4_mandatory_blocks.rb
+++ documentation_again/4_mandatory_blocks.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-
-
 # The Array#bsearch method is used to search ordered Arrays more quickly than #find and #select can. 
 # Assume you have the following code:
 a = [1, 4, 8, 11, 15, 19]
