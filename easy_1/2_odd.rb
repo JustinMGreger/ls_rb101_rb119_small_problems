9e33ff552fc9befd34f261239e139f39f79affa1
Justin Greger <justingreger@gmail.com>
2023-07-20 20:47:19 -0400

    # Examples:

--- easy_1/2_odd.rb
+++ easy_1/2_odd.rb
@@ -4,7 +4,13 @@
 # This method returns true if the number's absolute value is odd. 
 # You may assume that the argument is a valid integer value.
 
-Examples:
+# Examples:
+# puts is_odd?(2)    # => false
+# puts is_odd?(5)    # => true
+# puts is_odd?(-17)  # => true
+# puts is_odd?(-8)   # => false
+# puts is_odd?(0)    # => false
+# puts is_odd?(7)    # => true
 
 def is_odd?(integer)
   integer.remainder(2) == 1
