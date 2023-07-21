2bc92b5bca3a3c6975930f593757efad9ee69f72
Justin Greger <justingreger@gmail.com>
2023-07-21 15:48:36 -0400

    # Write a method that takes a positive integer as an argument and returns that number # with its digits reversed. # Examples:

--- easy_1/8_reverse_the_digits_in_a_number.rb
+++ easy_1/8_reverse_the_digits_in_a_number.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Write a method that takes a positive integer as an argument and returns that number
+# with its digits reversed.
+# Examples:
+
 def reversed_number(num)
   puts num.to_s.reverse.to_i
 end
