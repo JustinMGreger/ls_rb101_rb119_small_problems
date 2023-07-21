0891850c31b1e35185e4d8d43fc4e754226388c0
Justin Greger <justingreger@gmail.com>
2023-07-20 21:28:41 -0400

    # Examples:

--- easy_1/4_whats_my_bonus.rb
+++ easy_1/4_whats_my_bonus.rb
@@ -4,6 +4,9 @@
 # If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
 
 # Examples:
+# puts calculate_bonus(2800, true) == 1400
+# puts calculate_bonus(1000, false) == 0
+# puts calculate_bonus(50000, true) == 25000
 
 def calculate_bonus(integer, boolean)
   boolean ? (integer / 2) : 0
