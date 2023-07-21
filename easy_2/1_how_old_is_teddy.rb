2c6f83aec395b0e28fceaaf4aa1ebf8bc72c3d96
Justin Greger <justingreger@gmail.com>
2023-07-21 19:42:23 -0400

    # Build a program that randomly generates and prints Teddy's age. # To get the age, you should generate a random number between 20 and 200.
    # Example Output:

--- easy_2/1_how_old_is_teddy.rb
+++ easy_2/1_how_old_is_teddy.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Build a program that randomly generates and prints Teddy's age.
+# To get the age, you should generate a random number between 20 and 200.
+
+# Example Output:
+
 def teddy_age
   puts 'enter a name: '
   name = gets.chomp
