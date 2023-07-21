da2b78fe1d57797f2059acfdc8d86fda3f866776
Justin Greger <justingreger@gmail.com>
2023-07-20 20:12:24 -0400

    # Write a method that takes two arguments, a string and a positive integer, and prints the string as many times # as the integer indicates.
    # Example:

--- easy_1/1_repeat_yourself.rb
+++ easy_1/1_repeat_yourself.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Write a method that takes two arguments, a string and a positive integer, and prints the string as many times
+# as the integer indicates.
+
+# Example:
+
 def repeat(string, integer)
   integer.times do
     puts string
