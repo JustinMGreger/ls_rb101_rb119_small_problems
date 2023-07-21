a6dc0bd91cbbc37ee5e9f4d9c8aa8e0103195912
Justin Greger <justingreger@gmail.com>
2023-07-20 20:17:39 -0400

    # Write a method that takes one integer argument, which may be positive, negative, or zero. # This method returns true if the number's absolute value is odd. # You may assume that the argument is a valid integer value.

--- easy_1/2_odd.rb
+++ easy_1/2_odd.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Write a method that takes one integer argument, which may be positive, negative, or zero. 
+# This method returns true if the number's absolute value is odd. 
+# You may assume that the argument is a valid integer value.
+
+Examples:
+
 def is_odd?(integer)
   integer.remainder(2) == 1
 end
