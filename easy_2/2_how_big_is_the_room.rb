cf232c7eceae91c6eede1d8b6a609d4ea569058e
Justin Greger <justingreger@gmail.com>
2023-07-21 19:57:32 -0400

    # Build a program that asks a user for the length and width of a room in meters and then displays the area # of the room in both square meters and square feet.
    # Note: 1 square meter == 10.7639 square feet
    
    # Do not worry about validating the input at this time.
    
    # Example Run:

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# Build a program that asks a user for the length and width of a room in meters and then displays the area
+# of the room in both square meters and square feet.
+
+# Note: 1 square meter == 10.7639 square feet
+
+# Do not worry about validating the input at this time.
+
+# Example Run:
+
 def room_size
   puts 'Enter the length of the room in feet: '
   length = gets.to_f
