43858fa251d22edd3aa25bc03ef92aa934fab631
Justin Greger <justingreger@gmail.com>
2023-07-21 17:50:31 -0400

    p

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -33,8 +33,8 @@ def center_of(str)
 end
 
 # Tests:
-center_of('I love ruby') == 'e'
-center_of('Launch School') == ' '
-center_of('Launch') == 'un'
-center_of('Launchschool') == 'hs'
-center_of('x') == 'x'
+p center_of('I love ruby') == 'e'
+p center_of('Launch School') == ' '
+p center_of('Launch') == 'un'
+p center_of('Launchschool') == 'hs'
+p center_of('x') == 'x'
