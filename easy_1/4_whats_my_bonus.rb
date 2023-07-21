0f94b1c40c4fb1949c0606c76901869ac88f6a0b
Justin Greger <justingreger@gmail.com>
2023-07-20 21:28:08 -0400

    # Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. # If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
    # Examples:

--- easy_1/4_whats_my_bonus.rb
+++ easy_1/4_whats_my_bonus.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary.
+# If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
+
+# Examples:
+
 def calculate_bonus(integer, boolean)
   boolean ? (integer / 2) : 0
 end
