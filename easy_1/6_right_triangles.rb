01ff34ed0dcdd1c2e76858c6ad0c8241b975b245
Justin Greger <justingreger@gmail.com>
2023-07-20 21:40:20 -0400

    # Write a method that takes a positive integer, n, as an argument, and displays a right triangle # whose sides each have n stars. # The hypotenuse of the triangle (the diagonal side in the images below) should have one end # at the lower-left of the triangle, and the other end at the upper-right.
    # Examples:

--- easy_1/6_right_triangles.rb
+++ easy_1/6_right_triangles.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# Write a method that takes a positive integer, n, as an argument, and displays a right triangle
+# whose sides each have n stars.
+# The hypotenuse of the triangle (the diagonal side in the images below) should have one end
+# at the lower-left of the triangle, and the other end at the upper-right.
+
+# Examples:
+
 def ask_for_stars
   puts 'How many stars do you want?'
   gets.to_i
