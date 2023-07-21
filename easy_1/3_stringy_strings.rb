8e77d22efd8a432b00669e85463ff747d3f4c9d2
Justin Greger <justingreger@gmail.com>
2023-07-20 21:20:09 -0400

    # Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, # always starting with 1. The length of the string should match the given integer.
    # Examples:

--- easy_1/3_stringy_strings.rb
+++ easy_1/3_stringy_strings.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, 
+# always starting with 1. The length of the string should match the given integer.
+
+# Examples:
+
 def stringy(parameter, optional_parameter = 1)
   integers_array = []
   parameter.times do |index|
