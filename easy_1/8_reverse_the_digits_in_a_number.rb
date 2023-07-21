afe8693c8ae42b9628b28401d8fcaa9286e96cbe
Justin Greger <justingreger@gmail.com>
2023-07-21 17:04:29 -0400

    _ thousands

--- easy_1/8_reverse_the_digits_in_a_number.rb
+++ easy_1/8_reverse_the_digits_in_a_number.rb
@@ -27,9 +27,9 @@ def reversed_number(num)
 end
 
 # Tests:
-reversed_number(12345) == 54321
-reversed_number(12213) == 31221
+reversed_number(12_345) == 54_321
+reversed_number(12_213) == 31_221
 reversed_number(456) == 654
-reversed_number(12000) == 21 # No leading zeros in return value!
-reversed_number(12003) == 30021
+reversed_number(12_000) == 21 # No leading zeros in return value!
+reversed_number(12_003) == 30_021
 reversed_number(1) == 1
