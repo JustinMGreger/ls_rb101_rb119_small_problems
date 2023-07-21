ab30594165c0eb542fd0c81d13dd2062c91d526b
Justin Greger <justingreger@gmail.com>
2023-07-21 17:49:50 -0400

    deleted puts and moved examples

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -29,5 +29,11 @@ def center_of(str)
                      else
                        "'#{str[middle_index - 1..middle_index]}'"
                      end
-  puts middle_character
+  middle_character
 end
+
+center_of('I love ruby') == 'e'
+center_of('Launch School') == ' '
+center_of('Launch') == 'un'
+center_of('Launchschool') == 'hs'
+center_of('x') == 'x'
