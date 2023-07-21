7f0fa6dd6c75361c746f87a33311af1446d905bc
Justin Greger <justingreger@gmail.com>
2023-07-21 17:10:33 -0400

    # Write a method that takes a non-empty string argument, and returns the middle character or # characters of the argument. # If the argument has an odd length, you should return exactly one character. # If the argument has an even length, you should return exactly two characters.
    # Examples:

--- easy_1/9_get_middle_character.rb
+++ easy_1/9_get_middle_character.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# Write a method that takes a non-empty string argument, and returns the middle character or
+# characters of the argument.
+# If the argument has an odd length, you should return exactly one character.
+# If the argument has an even length, you should return exactly two characters.
+
+# Examples:
+
 def center_of(str)
   middle_index = str.length / 2
   middle_character = if str.length.odd?
