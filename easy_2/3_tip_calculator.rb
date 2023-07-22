b1f75834e8d4f498dc682aa7567972291b638a27
Justin Greger <justingreger@gmail.com>
2023-07-21 21:07:12 -0400

    # What is the bill? 200 # What is the tip percentage? 15
    # The tip is .0
    # The total is .0

--- easy_2/3_tip_calculator.rb
+++ easy_2/3_tip_calculator.rb
@@ -5,6 +5,11 @@
 # The program must compute the tip and then display both the tip and the total amount of the bill.
 
 # Example:
+# What is the bill? 200
+# What is the tip percentage? 15
+
+# The tip is $30.0
+# The total is $230.0
 
 def prompt(message)
   puts message
