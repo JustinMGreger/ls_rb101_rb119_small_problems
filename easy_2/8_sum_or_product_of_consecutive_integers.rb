08e1ddd768473d607f076a244b2405a5f3b67f0a
Justin Greger <justingreger@gmail.com>
2023-07-22 17:56:40 -0400

    # Examples:

--- easy_2/8_sum_or_product_of_consecutive_integers.rb
+++ easy_2/8_sum_or_product_of_consecutive_integers.rb
@@ -4,6 +4,18 @@
 # determine the sum or product of all numbers between 1 and the entered integer.
 
 # Examples:
+# >> Please enter an integer greater than 0:
+# 5
+# >> Enter 's' to compute the sum, 'p' to compute the product.
+# s
+# The sum of the integers between 1 and 5 is 15.
+
+
+# >> Please enter an integer greater than 0:
+# 6
+# >> Enter 's' to compute the sum, 'p' to compute the product.
+# p
+# The product of the integers between 1 and 6 is 720.
 
 def prompt(message)
   puts ">> #{message}"
