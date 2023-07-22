808b2b468a0dbb203d3405e6aba4aee3837a78c3
Justin Greger <justingreger@gmail.com>
2023-07-21 21:26:12 -0400

    # LS solution:

--- easy_2/6_odd_numbers.rb
+++ easy_2/6_odd_numbers.rb
@@ -2,6 +2,13 @@
 
 # Print all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
 
+# LS solution:
+# value = 1
+# while value <= 99
+#   puts value
+#   value += 2
+# end
+
 def odd_numbers
   puts (1..99).to_a.select(&:odd?)
 end
