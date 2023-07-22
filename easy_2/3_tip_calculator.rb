186f8598ff5ac49cfbd34bb285d3bd3914b5cb5c
Justin Greger <justingreger@gmail.com>
2023-07-21 21:06:41 -0400

    # Create a simple tip calculator. # The program should prompt for a bill amount and a tip rate. # The program must compute the tip and then display both the tip and the total amount of the bill.
    # Example:

--- easy_2/3_tip_calculator.rb
+++ easy_2/3_tip_calculator.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Create a simple tip calculator. 
+# The program should prompt for a bill amount and a tip rate. 
+# The program must compute the tip and then display both the tip and the total amount of the bill.
+
+# Example:
+
 def prompt(message)
   puts message
 end
