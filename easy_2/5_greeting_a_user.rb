7653d1469c5f69009e4f7e1ec22ff65ff275e1e1
Justin Greger <justingreger@gmail.com>
2023-07-21 21:18:14 -0400

    # LS solution:

--- easy_2/5_greeting_a_user.rb
+++ easy_2/5_greeting_a_user.rb
@@ -10,6 +10,17 @@
 # What is your name? Bob!
 # HELLO BOB. WHY ARE WE SCREAMING?
 
+# LS solution:
+# print 'What is your name? '
+# name = gets.chomp
+
+# if name[-1] == '!'
+#   name = name.chop
+#   puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+# else
+#   puts "Hello #{name}."
+# end
+
 def prompt(message)
   puts message
 end
