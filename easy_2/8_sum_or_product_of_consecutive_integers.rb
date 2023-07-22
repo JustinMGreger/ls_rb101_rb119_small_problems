82582b49b246581265510937135bc126046ebaee
Justin Greger <justingreger@gmail.com>
2023-07-22 17:50:18 -0400

    # Write a program that asks the user to enter an integer greater than 0, then asks if the user wants to # determine the sum or product of all numbers between 1 and the entered integer.
    # Examples:

--- easy_2/8_sum_or_product_of_consecutive_integers.rb
+++ easy_2/8_sum_or_product_of_consecutive_integers.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Write a program that asks the user to enter an integer greater than 0, then asks if the user wants to
+# determine the sum or product of all numbers between 1 and the entered integer.
+
+# Examples:
+
 def prompt(message)
   puts ">> #{message}"
 end
