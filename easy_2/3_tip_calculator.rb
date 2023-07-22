357a47b37df87e15edb87b29c9b8a75087f70748
Justin Greger <justingreger@gmail.com>
2023-07-21 21:09:59 -0400

    deleted trailing white spaces

--- easy_2/3_tip_calculator.rb
+++ easy_2/3_tip_calculator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# Create a simple tip calculator. 
-# The program should prompt for a bill amount and a tip rate. 
+# Create a simple tip calculator.
+# The program should prompt for a bill amount and a tip rate.
 # The program must compute the tip and then display both the tip and the total amount of the bill.
 
 # Example:
