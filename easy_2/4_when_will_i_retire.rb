8c486966dd5ef1c7e436eb220cc3b27d9f19e8ff
Justin Greger <justingreger@gmail.com>
2023-07-21 21:11:22 -0400

    # Build a program that displays when the user will retire and how many years she has to work till retirement.
    # Example:

--- easy_2/4_when_will_i_retire.rb
+++ easy_2/4_when_will_i_retire.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Build a program that displays when the user will retire and how many years she has to work till retirement.
+
+# Example:
+
 def prompt(message)
   puts message
 end
