23c8a27c8ab1a49cf99eb87f9e2a1d3ac832d856
Justin Greger <justingreger@gmail.com>
2023-07-22 17:40:44 -0400

    # LS solution:

--- easy_2/7_even_numbers.rb
+++ easy_2/7_even_numbers.rb
@@ -2,6 +2,13 @@
 
 # Print all even numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
 
+# LS solution:
+# value = 1
+# while value <= 99
+#   puts value if value.even?
+#   value += 1
+# end
+
 def even_numbers
   puts (1..99).to_a.select(&:even?)
 end
