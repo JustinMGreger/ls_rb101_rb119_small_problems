e54245d9774e530a7565ed29505da17572887041
Justin Greger <justingreger@gmail.com>
2023-07-21 21:25:31 -0400

    # Print all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.

--- easy_2/6_odd_numbers.rb
+++ easy_2/6_odd_numbers.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Print all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
 def odd_numbers
   puts (1..99).to_a.select(&:odd?)
 end
