1ee21dca2d600a766c0fe8ceae038fe3b6d26ecd
Justin Greger <justingreger@gmail.com>
2023-07-21 21:08:11 -0400

    # LS solution:

--- easy_2/3_tip_calculator.rb
+++ easy_2/3_tip_calculator.rb
@@ -11,6 +11,21 @@
 # The tip is $30.0
 # The total is $230.0
 
+# LS solution:
+# print 'What is the bill? '
+# bill = gets.chomp
+# bill = bill.to_f
+
+# print 'What is the tip percentage? '
+# percentage = gets.chomp
+# percentage = percentage.to_f
+
+# tip   = (bill * (percentage / 100)).round(2)
+# total = (bill + tip).round(2)
+
+# puts "The tip is $#{tip}"
+# puts "The total is $#{total}"
+
 def prompt(message)
   puts message
 end
