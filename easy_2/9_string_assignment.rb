d7f9eb87cc09bcb0801df310b165ccfa25c1dae7
Justin Greger <justingreger@gmail.com>
2023-07-22 19:42:39 -0400

    deleted trailing white lines

--- easy_2/9_string_assignment.rb
+++ easy_2/9_string_assignment.rb
@@ -2,28 +2,28 @@
 # name = 'Bob'
 # save_name = name
 # name = 'Alice'
-# puts name, save_name     
+# puts name, save_name
 
-# What does this code print out? 
+# What does this code print out?
 # Think about it for a moment before continuing.
 
-# If you said that code printed    
+# If you said that code printed
 # Alice
 # Bob
 
-# you are 100% correct, and the answer should come as no surprise. 
+# you are 100% correct, and the answer should come as no surprise.
 # Now, let's look at something a bit different:
 name = 'Bob'
 save_name = name
 name.upcase!
 puts name, save_name
 
-# What does this print out? 
+# What does this print out?
 # BOB
 # BOB
 # Can you explain these results?
 # the name variable is assigned the Bob string value.
-# the save_name variable is assinged the name variable. 
+# the save_name variable is assinged the name variable.
 # name is permenantly converted to upcase.
 # this changed the value asssigned to the save_name variable.
 # print on a new line the value value assigned to name and save_name.
