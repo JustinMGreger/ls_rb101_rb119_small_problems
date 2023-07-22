048495ad9b738197002c36e4b32ad717a6f68c56
Justin Greger <justingreger@gmail.com>
2023-07-21 21:02:08 -0400

    # NOTE: 1 square meter == 10.7639 square feet

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -3,7 +3,7 @@
 # Build a program that asks a user for the length and width of a room in meters and then displays the area
 # of the room in both square meters and square feet.
 
-# Note: 1 square meter == 10.7639 square feet
+# NOTE: 1 square meter == 10.7639 square feet
 
 # Do not worry about validating the input at this time.
 
