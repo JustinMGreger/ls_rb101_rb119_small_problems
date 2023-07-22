d475ad540b9e4ab9d78671fb2302131237aeda8d
Justin Greger <justingreger@gmail.com>
2023-07-21 21:09:07 -0400

    # Further Exploration

--- easy_2/3_tip_calculator.rb
+++ easy_2/3_tip_calculator.rb
@@ -26,6 +26,13 @@
 # puts "The tip is $#{tip}"
 # puts "The total is $#{total}"
 
+# Further Exploration
+
+# Our solution prints the results as $30.0 and $230.0 instead of the more usual $30.00 and $230.00. 
+# Modify your solution so it always prints the results with 2 decimal places.
+
+# Hint: You will likely need Kernel#format for this.
+
 def prompt(message)
   puts message
 end
