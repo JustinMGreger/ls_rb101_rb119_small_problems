cab573de6710e0203b4e5c1f1c55c0a4656f2a62
Justin Greger <justingreger@gmail.com>
2023-07-22 17:28:57 -0400

    # Print all even numbers from 1 to 99, inclusive, to the console, with each number on a separate line.

--- easy_2/7_even_numbers.rb
+++ easy_2/7_even_numbers.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Print all even numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
 def even_numbers
   puts (1..99).to_a.select(&:even?)
 end
