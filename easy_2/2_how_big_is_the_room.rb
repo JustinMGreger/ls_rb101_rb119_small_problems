71a057e3bba27685a41f44d47cc9d21fad45485f
Justin Greger <justingreger@gmail.com>
2023-07-21 20:49:13 -0400

    # LS solution:

--- easy_2/2_how_big_is_the_room.rb
+++ easy_2/2_how_big_is_the_room.rb
@@ -14,6 +14,21 @@
 # 7
 # The area of the room is 70.0 square meters (753.47 square feet).
 
+# LS solution:
+# SQMETERS_TO_SQFEET = 10.7639
+
+# puts '==> Enter the length of the room in meters: '
+# length = gets.to_f
+
+# puts '==> Enter the width of the room in meters: '
+# width = gets.to_f
+
+# square_meters = (length * width).round(2)
+# square_feet = (square_meters * SQMETERS_TO_SQFEET).round(2)
+
+# puts "The area of the room is #{square_meters} " + \
+#      "square meters (#{square_feet} square feet)."
+
 def room_size
   puts 'Enter the length of the room in feet: '
   length = gets.to_f
