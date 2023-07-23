d23fafe6fd4c201533702dc5821d40454b6050b9
Justin Greger <justingreger@gmail.com>
2023-07-22 20:01:25 -0400

    # Write a method that takes a number as an argument. # If the argument is a positive number, return the negative of that number. # If the number is 0 or negative, return the original number.
    # Examples:

--- easy_2/10_always_return_negative.rb
+++ easy_2/10_always_return_negative.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Write a method that takes a number as an argument.
+# If the argument is a positive number, return the negative of that number.
+# If the number is 0 or negative, return the original number.
+
+# Examples:
+
 def negative(number)
   if number.positive?
     negative_number = number * -1
