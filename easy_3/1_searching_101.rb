29f15737cc5049b3a42284e708a28d93490593ab
Justin Greger <justingreger@gmail.com>
2023-07-22 20:38:21 -0400

    # LS solution:

--- easy_3/1_searching_101.rb
+++ easy_3/1_searching_101.rb
@@ -33,6 +33,28 @@
 # 18
 # The number 18 does not appear in [25, 15, 20, 17, 23].
 
+# LS solution:
+# numbers = []
+
+# puts "Enter the 1st number:"
+# numbers << gets.chomp.to_i
+# puts "Enter the 2nd number:"
+# numbers << gets.chomp.to_i
+# puts "Enter the 3rd number:"
+# numbers << gets.chomp.to_i
+# puts "Enter the 4th number:"
+# numbers << gets.chomp.to_i
+# puts "Enter the 5th number:"
+# numbers << gets.chomp.to_i
+# puts "Enter the last number:"
+# last_number = gets.chomp.to_i
+
+# if numbers.include? last_number
+#   puts "The number #{last_number} appears in #{numbers}."
+# else
+#   puts "The number #{last_number} does not appear in #{numbers}."
+# end
+
 def prompt(message)
   puts message
 end
