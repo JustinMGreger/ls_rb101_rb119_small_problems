5a554a01d6a8f1dd26eec0fc5050d1867454dadc
Justin Greger <justingreger@gmail.com>
2023-07-22 20:35:56 -0400

    # Write a program that solicits 6 numbers from the user, # then prints a message that describes whether or not the 6th number appears amongst the first 5 numbers.
    # Examples:

--- easy_3/1_searching_101.rb
+++ easy_3/1_searching_101.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Write a program that solicits 6 numbers from the user,
+# then prints a message that describes whether or not the 6th number appears amongst the first 5 numbers.
+
+# Examples:
+
 def prompt(message)
   puts message
 end
