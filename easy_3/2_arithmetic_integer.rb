f18e2cdf6e15fd55df3b2d3bf3ee94621935f4b7
Justin Greger <justingreger@gmail.com>
2023-07-22 20:50:53 -0400

    # my solution:

--- easy_3/2_arithmetic_integer.rb
+++ easy_3/2_arithmetic_integer.rb
@@ -34,60 +34,61 @@
 # prompt("#{first_number} % #{second_number} = #{first_number % second_number}")
 # prompt("#{first_number} ** #{second_number} = #{first_number**second_number}")
 
-def prompt(message)
-  puts message
-end
+# my solution:
+# def prompt(message)
+#   puts message
+# end
 
-def input
-  gets.chomp.to_i
-end
+# def input
+#   gets.chomp.to_i
+# end
 
-prompt '==> Enter the first number:'
-first_number = input
+# prompt '==> Enter the first number:'
+# first_number = input
 
-prompt '==> Enter the second number:'
-second_number = input
-until second_number != 0
-  prompt '==> Enter the second number:'
-  second_number = input
-end
+# prompt '==> Enter the second number:'
+# second_number = input
+# until second_number != 0
+#   prompt '==> Enter the second number:'
+#   second_number = input
+# end
 
-def addition_method(first_number, second_number)
-  addition = first_number + second_number
-  prompt "==> #{first_number} + #{second_number} = #{addition}"
-end
+# def addition_method(first_number, second_number)
+#   addition = first_number + second_number
+#   prompt "==> #{first_number} + #{second_number} = #{addition}"
+# end
 
-def subtraction_method(first_number, second_number)
-  subtraction = first_number - second_number
-  prompt "==> #{first_number} - #{second_number} = #{subtraction}"
-end
+# def subtraction_method(first_number, second_number)
+#   subtraction = first_number - second_number
+#   prompt "==> #{first_number} - #{second_number} = #{subtraction}"
+# end
 
-def product_method(first_number, second_number)
-  product = first_number * second_number
-  prompt "==> #{first_number} * #{second_number} = #{product}"
-end
+# def product_method(first_number, second_number)
+#   product = first_number * second_number
+#   prompt "==> #{first_number} * #{second_number} = #{product}"
+# end
 
-def quotient_method(first_number, second_number)
-  quotient = first_number / second_number
-  prompt "==> #{first_number} / #{second_number} = #{quotient}"
-end
+# def quotient_method(first_number, second_number)
+#   quotient = first_number / second_number
+#   prompt "==> #{first_number} / #{second_number} = #{quotient}"
+# end
 
-def remainder_method(first_number, second_number)
-  remainder = first_number % second_number
-  prompt "==> #{first_number} % #{second_number} = #{remainder}"
-end
+# def remainder_method(first_number, second_number)
+#   remainder = first_number % second_number
+#   prompt "==> #{first_number} % #{second_number} = #{remainder}"
+# end
 
-def power_method(first_number, second_number)
-  power = first_number**second_number
-  prompt "==> #{first_number} ** #{second_number} = #{power}"
-end
+# def power_method(first_number, second_number)
+#   power = first_number**second_number
+#   prompt "==> #{first_number} ** #{second_number} = #{power}"
+# end
 
-addition_method(first_number, second_number)
-subtraction_method(first_number, second_number)
-product_method(first_number, second_number)
-quotient_method(first_number, second_number)
-remainder_method(first_number, second_number)
-power_method(first_number, second_number)
+# addition_method(first_number, second_number)
+# subtraction_method(first_number, second_number)
+# product_method(first_number, second_number)
+# quotient_method(first_number, second_number)
+# remainder_method(first_number, second_number)
+# power_method(first_number, second_number)
 
 # Discussion
 
@@ -97,3 +98,24 @@ power_method(first_number, second_number)
 # When floats are used instead of integers the concept of floating-point arithmetic and precision limitations comes up.
 # Ruby uses a system called "binary floating point" to represent and manipulate decimal numbers.
 # This is a way of approximating real numbers using a finite amount of memory.
+
+# LS solution that has been modified in response to this:
+def prompt(message)
+  puts "==> #{message}"
+end
+
+prompt("Enter the first number:")
+first_number = gets.chomp.to_f
+prompt("Enter the second number:")
+second_number = gets.chomp.to_f
+
+if second_number == 0.0
+  prompt("The second number can't be zero!")
+else
+  prompt("#{first_number} + #{second_number} = #{first_number + second_number}")
+  prompt("#{first_number} - #{second_number} = #{first_number - second_number}")
+  prompt("#{first_number} * #{second_number} = #{first_number * second_number}")
+  prompt("#{first_number} / #{second_number} = #{first_number / second_number}")
+  prompt("#{first_number} % #{second_number} = #{first_number % second_number}")
+  prompt("#{first_number} ** #{second_number} = #{first_number**second_number}")
+end
