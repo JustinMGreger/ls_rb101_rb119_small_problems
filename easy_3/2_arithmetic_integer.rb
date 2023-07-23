46a44cd720f66de041efa7a1db3ef3cd30fd3d7c
Justin Greger <justingreger@gmail.com>
2023-07-22 20:53:59 -0400

    cant to cannot

--- easy_3/2_arithmetic_integer.rb
+++ easy_3/2_arithmetic_integer.rb
@@ -110,7 +110,7 @@ prompt('Enter the second number:')
 second_number = gets.chomp.to_f
 
 if second_number == 0.0
-  prompt('The second number can't be zero!')
+  prompt('The second number cannot be zero!')
 else
   prompt("#{first_number} + #{second_number} = #{first_number + second_number}")
   prompt("#{first_number} - #{second_number} = #{first_number - second_number}")
