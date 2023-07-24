e29af4ad30164cf85bf2aa4d95d19060e913b5fd
Justin Greger <justingreger@gmail.com>
2023-07-24 15:45:50 -0700

    # Examples: # palindromic_number?(34543) == true # palindromic_number?(123210) == false # palindromic_number?(22) == true # palindromic_number?(5) == true

--- easy_3/9_palindromic_numbers.rb
+++ easy_3/9_palindromic_numbers.rb
@@ -4,6 +4,10 @@
 # A palindromic number reads the same forwards and backwards.
 
 # Examples:
+# palindromic_number?(34543) == true
+# palindromic_number?(123210) == false
+# palindromic_number?(22) == true
+# palindromic_number?(5) == true
 
 def palindromic_number?(integer)
   integer.to_s == integer.to_s.reverse
