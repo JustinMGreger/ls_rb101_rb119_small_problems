253d02635c69486c9a7909165aac09d9c02b93fc
Justin Greger <justingreger@gmail.com>
2023-07-24 15:16:18 -0700

    # Write another method that returns true if the string passed as an argument is a palindrome, # false otherwise. # This time, however, your method should be case-insensitive, # and it should ignore all non-alphanumeric characters. # If you wish, you may simplify things by calling the palindrome? method you wrote in the previous exercise.
    # Examples:

--- easy_3/8_palindromic_strings_part_2.rb
+++ easy_3/8_palindromic_strings_part_2.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+# Write another method that returns true if the string passed as an argument is a palindrome,
+# false otherwise.
+# This time, however, your method should be case-insensitive,
+# and it should ignore all non-alphanumeric characters.
+# If you wish, you may simplify things by calling the palindrome? method you wrote in the previous exercise.
+
+# Examples:
+
 def palindrome?(sequence)
   sequence.size.times.all? { |i| sequence[i] == sequence[-i - 1] }
 end
