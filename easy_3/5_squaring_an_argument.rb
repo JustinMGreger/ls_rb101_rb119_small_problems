e736835fa9a62c5bc00c381d0c352065a0032c7e
Justin Greger <justingreger@gmail.com>
2023-07-24 11:58:15 -0700

    # Further Exploration

--- easy_3/5_squaring_an_argument.rb
+++ easy_3/5_squaring_an_argument.rb
@@ -13,6 +13,12 @@
 #   multiply(n, n)
 # end
 
+# Further Exploration
+
+# What if we wanted to generalize this method to a "power to the n" type method:
+# cubed, to the 4th power, to the 5th, etc. 
+# How would we go about doing so while still using the multiply method?
+
 def prompt(message)
   puts message
 end
