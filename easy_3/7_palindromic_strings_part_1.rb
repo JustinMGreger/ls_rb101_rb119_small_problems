d79ccc5e84d05789bf2afaf83308b10fd7d76be4
Justin Greger <justingreger@gmail.com>
2023-07-24 13:58:25 -0700

    # Examples:

--- easy_3/7_palindromic_strings_part_1.rb
+++ easy_3/7_palindromic_strings_part_1.rb
@@ -5,6 +5,10 @@
 # For this exercise, case matters as does punctuation and spaces.
 
 # Examples:
+# palindrome?('madam') == true
+# palindrome?('Madam') == false          # (case matters)
+# palindrome?("madam i'm adam") == false # (all characters matter)
+# palindrome?('356653') == true
 
 def palindrome?(sequence)
   sequence.size.times.all? { |i| sequence[i] == sequence[-i - 1] }
