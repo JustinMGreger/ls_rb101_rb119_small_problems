a8584a50ab4292968096e1ead5b3a83213ba3a35
Justin Greger <justingreger@gmail.com>
2023-07-24 13:57:24 -0700

    # Write a method that returns true if the string passed as an argument is a palindrome, false otherwise. # A palindrome reads the same forward and backward. # For this exercise, case matters as does punctuation and spaces.
    # Examples:

--- easy_3/7_palindromic_strings_part_1.rb
+++ easy_3/7_palindromic_strings_part_1.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Write a method that returns true if the string passed as an argument is a palindrome, false otherwise.
+# A palindrome reads the same forward and backward.
+# For this exercise, case matters as does punctuation and spaces.
+
+# Examples:
+
 def palindrome?(sequence)
   sequence.size.times.all? { |i| sequence[i] == sequence[-i - 1] }
 end
