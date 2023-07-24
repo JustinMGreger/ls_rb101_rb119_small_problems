0b23b290a1a41b209a1b5889215443799fa77038
Justin Greger <justingreger@gmail.com>
2023-07-24 15:17:19 -0700

    # real_palindrome?

--- easy_3/8_palindromic_strings_part_2.rb
+++ easy_3/8_palindromic_strings_part_2.rb
@@ -7,6 +7,12 @@
 # If you wish, you may simplify things by calling the palindrome? method you wrote in the previous exercise.
 
 # Examples:
+# real_palindrome?('madam') == true
+# real_palindrome?('Madam') == true           # (case does not matter)
+# real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+# real_palindrome?('356653') == true
+# real_palindrome?('356a653') == true
+# real_palindrome?('123ab321') == false
 
 def palindrome?(sequence)
   sequence.size.times.all? { |i| sequence[i] == sequence[-i - 1] }
