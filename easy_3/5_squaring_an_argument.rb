dbf831373dd8de11d2be42cbf91698badd489127
Justin Greger <justingreger@gmail.com>
2023-07-24 11:59:11 -0700

    deleted trailing white space

--- easy_3/5_squaring_an_argument.rb
+++ easy_3/5_squaring_an_argument.rb
@@ -16,7 +16,7 @@
 # Further Exploration
 
 # What if we wanted to generalize this method to a "power to the n" type method:
-# cubed, to the 4th power, to the 5th, etc. 
+# cubed, to the 4th power, to the 5th, etc.
 # How would we go about doing so while still using the multiply method?
 
 def prompt(message)
