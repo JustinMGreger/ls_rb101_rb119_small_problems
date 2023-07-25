69a5f6f432ecbde614fa4e5db7425bec3ca42f6d
Justin Greger <justingreger@gmail.com>
2023-07-25 13:44:56 -0700

    deleted trailing white spaces

--- easy_4/8_convert_a_string_to_a_signed_number.rb
+++ easy_4/8_convert_a_string_to_a_signed_number.rb
@@ -11,7 +11,7 @@
 # You may assume the string will always contain a valid number.
 
 # You may not use any of the standard conversion methods available in Ruby, such as String#to_i,
-# Integer(), etc. 
+# Integer(), etc.
 # You may, however, use the string_to_integer method from the previous lesson.
 
 # Examples
