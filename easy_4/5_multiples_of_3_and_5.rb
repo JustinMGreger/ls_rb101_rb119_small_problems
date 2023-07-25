17568c540b68a0b22be99c9927d4893994a1849c
Justin Greger <justingreger@gmail.com>
2023-07-24 17:26:05 -0700

    # Write a method that searches for all multiples of 3 or 5 that lie between 1 and some other number, # and then computes the sum of those multiples. # For instance, if the supplied number is 20, the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
    # You may assume that the number passed in is an integer greater than 1.
    
    # Examples

--- easy_4/5_multiples_of_3_and_5.rb
+++ easy_4/5_multiples_of_3_and_5.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+# Write a method that searches for all multiples of 3 or 5 that lie between 1 and some other number,
+# and then computes the sum of those multiples.
+# For instance, if the supplied number is 20, the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
+
+# You may assume that the number passed in is an integer greater than 1.
+
+# Examples
+
 # ls solution modified to by rubocop compliant:
 # def multiple?(number, divisor)
 #  (number % divisor).zero?
