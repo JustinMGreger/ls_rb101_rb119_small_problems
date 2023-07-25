4d918d438183f8bc853de704a50cfe5bbbb08456
Justin Greger <justingreger@gmail.com>
2023-07-24 17:27:39 -0700

    # LS solution: # def multiple?(number, divisor) #   number % divisor == 0 # end

--- easy_4/5_multiples_of_3_and_5.rb
+++ easy_4/5_multiples_of_3_and_5.rb
@@ -12,6 +12,21 @@
 # multisum(10) == 33
 # multisum(1000) == 234168
 
+# LS solution:
+# def multiple?(number, divisor)
+#   number % divisor == 0
+# end
+
+# def multisum(max_value)
+#   sum = 0
+#   1.upto(max_value) do |number|
+#     if multiple?(number, 3) || multiple?(number, 5)
+#       sum += number
+#     end
+#   end
+#   sum
+# end
+
 # ls solution modified to by rubocop compliant:
 # def multiple?(number, divisor)
 #  (number % divisor).zero?
