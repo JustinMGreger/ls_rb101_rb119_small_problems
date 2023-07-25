e73a69ce8cc6c34a47b6edaf3a1408363ec4524d
Justin Greger <justingreger@gmail.com>
2023-07-24 17:10:39 -0700

    # Examples:

--- easy_4/3_leap_years_part_1.rb
+++ easy_4/3_leap_years_part_1.rb
@@ -8,6 +8,21 @@
 # Write a method that takes any year greater than 0 as input, and returns true if the year is a leap year,
 # or false if it is not a leap year.
 
+# Examples:
+# leap_year?(2016) == true
+# leap_year?(2015) == false
+# leap_year?(2100) == false
+# leap_year?(2400) == true
+# leap_year?(240000) == true
+# leap_year?(240001) == false
+# leap_year?(2000) == true
+# leap_year?(1900) == false
+# leap_year?(1752) == true
+# leap_year?(1700) == false
+# leap_year?(1) == false
+# leap_year?(100) == false
+# leap_year?(400) == true
+
 # Further Exploration
 
 # Can you rewrite leap_year? to perform its tests in the opposite order of the above solution?
