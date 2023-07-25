57758d4a4c5e92ac7b22a1f28e92d2f93c5c1469
Justin Greger <justingreger@gmail.com>
2023-07-24 17:45:53 -0700

    #

--- easy_4/6_running_totals.rb
+++ easy_4/6_running_totals.rb
@@ -41,7 +41,7 @@
 # end
 
 # Tests:
-p running_total([2, 5, 13]) == [2, 7, 20] # result: true.
-p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67] # result: true.
-p running_total([3]) == [3] # result: true.
-p running_total([]) == [] # result: true.
+# p running_total([2, 5, 13]) == [2, 7, 20] # result: true.
+# p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67] # result: true.
+# p running_total([3]) == [3] # result: true.
+# p running_total([]) == [] # result: true.
