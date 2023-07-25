da48020c1fc9b2acf758c8e55f6c1215041fe6a4
Justin Greger <justingreger@gmail.com>
2023-07-24 17:20:36 -0700

    # Examples: # leap_year?(2016) == true

--- easy_4/4_leap_years_part_2.rb
+++ easy_4/4_leap_years_part_2.rb
@@ -9,6 +9,21 @@
 # Using this information, update the method from the previous exercise to determine leap years
 # both before and after 1752.
 
+# Examples:
+# leap_year?(2016) == true
+# leap_year?(2015) == false
+# leap_year?(2100) == false
+# leap_year?(2400) == true
+# leap_year?(240000) == true
+# leap_year?(240001) == false
+# leap_year?(2000) == true
+# leap_year?(1900) == false
+# leap_year?(1752) == true
+# leap_year?(1700) == true
+# leap_year?(1) == false
+# leap_year?(100) == true
+# leap_year?(400) == true
+
 def julian_calendar?(year)
   (year % 4).zero?
 end
