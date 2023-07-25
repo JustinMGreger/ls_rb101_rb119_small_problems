9099a2a8baa56ea8ae91c2359cc882c9d03166c8
Justin Greger <justingreger@gmail.com>
2023-07-24 17:30:46 -0700

    deleted trailing white spaces

--- easy_4/5_multiples_of_3_and_5.rb
+++ easy_4/5_multiples_of_3_and_5.rb
@@ -48,17 +48,17 @@
 
 # Further Exploration
 
-# Investigate Enumerable.inject (also known as Enumerable.reduce), 
+# Investigate Enumerable.inject (also known as Enumerable.reduce),
 # Answer:
-# The inject method combines all elements of an enumerable object (like an array or a hash) by applying a 
+# The inject method combines all elements of an enumerable object (like an array or a hash) by applying a
 # binary operation, specified by a block or a symbol that names a method or operator.
 
-# How might this method be useful in solving this problem? 
-# (Note that Enumerable methods are available when working with Arrays.) 
+# How might this method be useful in solving this problem?
+# (Note that Enumerable methods are available when working with Arrays.)
 # Answer:
 # combine all elements of numbers from 3 to the parameter by applying a block of code with the addition assignment.
 
-# Try writing such a solution. 
+# Try writing such a solution.
 # Answer:
 
 def multiple?(number, divisor)
@@ -66,7 +66,7 @@ def multiple?(number, divisor)
 end
 
 def multisum(max_value)
-  (1..max_value).select { |number| multiple?(number, 3) || multiple?(number, 5) }.inject { |sum, n| sum + n } 
+  (1..max_value).select { |number| multiple?(number, 3) || multiple?(number, 5) }.inject { |sum, n| sum + n }
 end
 
 # Tests:
@@ -75,7 +75,7 @@ p multisum(5) == 8
 p multisum(10) == 33
 p multisum(1_000) == 234_168
 
-# Which is clearer? 
+# Which is clearer?
 # Answer:
 # the inject method.
 
