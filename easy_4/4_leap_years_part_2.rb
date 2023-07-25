97eb13f253733e31a2b2456de8dd7d5f7b03470a
Justin Greger <justingreger@gmail.com>
2023-07-24 17:19:41 -0700

    # A continuation of the previous exercise.
    # The British Empire adopted the Gregorian Calendar in 1752, which was a leap year.
    # Prior to 1752, the Julian Calendar was used. Under the Julian Calendar,
    # leap years occur in any year that is evenly divisible by 4.
    
    # Using this information, update the method from the previous exercise to determine leap years
    # both before and after 1752.

--- easy_4/4_leap_years_part_2.rb
+++ easy_4/4_leap_years_part_2.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# A continuation of the previous exercise.
+
+# The British Empire adopted the Gregorian Calendar in 1752, which was a leap year.
+# Prior to 1752, the Julian Calendar was used. Under the Julian Calendar,
+# leap years occur in any year that is evenly divisible by 4.
+
+# Using this information, update the method from the previous exercise to determine leap years
+# both before and after 1752.
+
 def julian_calendar?(year)
   (year % 4).zero?
 end
