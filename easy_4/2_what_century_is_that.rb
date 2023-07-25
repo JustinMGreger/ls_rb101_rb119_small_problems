455d1f8c3c14e5e4fa79d2eaa15bd3886c10e35d
Justin Greger <justingreger@gmail.com>
2023-07-24 17:04:04 -0700

    # LS solution: # def century(year)

--- easy_4/2_what_century_is_that.rb
+++ easy_4/2_what_century_is_that.rb
@@ -17,6 +17,25 @@
 # century(1127) == '12th'
 # century(11201) == '113th'
 
+# LS solution:
+# def century(year)
+#   century = year / 100 + 1
+#   century -= 1 if year % 100 == 0
+#   century.to_s + century_suffix(century)
+# end
+
+# def century_suffix(century)
+#   return 'th' if [11, 12, 13].include?(century % 100)
+#   last_digit = century % 10
+
+#   case last_digit
+#   when 1 then 'st'
+#   when 2 then 'nd'
+#   when 3 then 'rd'
+#   else 'th'
+#   end
+# end
+
 def century(year)
   century_number = calculate_century_number(year)
   suffix = calculate_century_suffix(century_number)
