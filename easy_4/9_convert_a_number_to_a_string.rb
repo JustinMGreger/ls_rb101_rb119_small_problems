efae097b03ca7d991102f59e8d07b296892ac024
Justin Greger <justingreger@gmail.com>
2023-07-26 07:50:38 -0700

    # Does the Array class have any methods that fit this pattern? # No.

--- easy_4/9_convert_a_number_to_a_string.rb
+++ easy_4/9_convert_a_number_to_a_string.rb
@@ -60,6 +60,17 @@ puts integer_to_string(5000) # Should print '5000'
 # end with a ! because there is no non-mutating form of prepend.
 
 # How many mutating String methods can you find that do not end with a !. 
+# concat
+# replace
+# prepend
+# insert
+# clear
+# force_encoding
+# setbyte
+
 # Can you find any that end with a !, but don't have a non-mutating form? 
+# string.delete
+
 # Does the Array class have any methods that fit this pattern? 
+# No.
 # How about the Hash class?
