d80f9be6de9954b8632f7bcbe2aed268c37b2ff0
Justin Greger <justingreger@gmail.com>
2023-07-25 17:13:19 -0700

    freeze ..

--- easy_4/8_convert_a_string_to_a_signed_number.rb
+++ easy_4/8_convert_a_string_to_a_signed_number.rb
@@ -78,7 +78,7 @@
 DIGITS = {
   '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
   '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
-}
+}.freeze
 
 def string_to_integer(string)
   digits = string.chars.map { |char| DIGITS[char] }
@@ -89,11 +89,15 @@ def string_to_integer(string)
 end
 
 def string_to_signed_integer(string)
-  case string[0]
-  when '-' then -string_to_integer(string[1..-1])
-  when '+' then string_to_integer(string[1..-1])
-  else          string_to_integer(string)
+  sign = 1
+  if string.start_with?('-')
+    sign = -1
+    string = string[1..]
+  elsif string.start_with?('+')
+    string = string[1..]
   end
+
+  string_to_integer(string) * sign
 end
 
 # tests:
