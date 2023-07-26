10fa1cb8812bcec0f296c79a095723685ce647b3
Justin Greger <justingreger@gmail.com>
2023-07-26 11:39:45 -0700

    # In the previous exercise, you developed a method that converts non-negative numbers to strings.

--- easy_4/10_convert_a_signed_number_to_a_string.rb
+++ easy_4/10_convert_a_signed_number_to_a_string.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+# In the previous exercise, you developed a method that converts non-negative numbers to strings.
+# In this exercise, you're going to extend that method by adding the ability to represent negative
+# numbers as well.
+
+# Write a method that takes an integer, and converts it to a string representation.
+
+# You may not use any of the standard conversion methods available in Ruby, such as Integer#to_s,
+# String(), Kernel#format, etc. You may, however, use integer_to_string from the previous exercise.
+
+# Examples:
