9d40bb1475f4515af695d170a2894b55a184c83c
Justin Greger <justingreger@gmail.com>
2023-07-26 12:44:51 -0700

    DIGITS = %w[0 1 2 3 4 5 6 7 8 9].freeze

--- easy_4/10_convert_a_signed_number_to_a_string.rb
+++ easy_4/10_convert_a_signed_number_to_a_string.rb
@@ -15,7 +15,7 @@
 # signed_integer_to_string(0) == '0'
 
 # LS solution modified to comply with rubocop:
-DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].freeze
+DIGITS = %w[0 1 2 3 4 5 6 7 8 9].freeze
 
 def integer_to_string(number)
   result = String.new
