7d90b6bb044685143269c8d24b46752a8007142f
Justin Greger <justingreger@gmail.com>
2023-07-25 21:17:51 -0700

    # LS solution:

--- easy_4/9_convert_a_number_to_a_string.rb
+++ easy_4/9_convert_a_number_to_a_string.rb
@@ -30,6 +30,19 @@
 #   result
 # end
 
+# LS solution:
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].freeze
+
+def integer_to_string(number)
+  result = ''
+  loop do
+    number, remainder = number.divmod(10)
+    result.prepend(DIGITS[remainder])
+    break if number == 0
+  end
+  result
+end
+
 # tests:
 puts integer_to_string(4321) # Should print '4321'
 puts integer_to_string(0) # Should print '0'
