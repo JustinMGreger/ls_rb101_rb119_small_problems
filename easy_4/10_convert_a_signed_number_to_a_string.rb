d78012314fb9a616a9e1b27857b57dfe9621b205
Justin Greger <justingreger@gmail.com>
2023-07-28 20:19:13 -0700

    # my solution:

--- easy_4/10_convert_a_signed_number_to_a_string.rb
+++ easy_4/10_convert_a_signed_number_to_a_string.rb
@@ -36,30 +36,30 @@
 # end
 
 # my solution:
-def integer_to_string(number)
-  lookup = '0123456789'
-  return lookup[0] if number.zero?
+# def integer_to_string(number)
+#   lookup = '0123456789'
+#   return lookup[0] if number.zero?
 
-  result = ''
+#   result = ''
 
-  while number.positive?
-    digit = number % 10
-    number /= 10
-    result = lookup[digit] + result
-  end
+#   while number.positive?
+#     digit = number % 10
+#     number /= 10
+#     result = lookup[digit] + result
+#   end
 
-  result
-end
+#   result
+# end
 
-def signed_integer_to_string(number)
-  if number.positive?
-    "+#{integer_to_string(number)}"
-  elsif number < 0
-    "-#{integer_to_string(-number)}"
-  else
-    integer_to_string(number)
-  end
-end
+# def signed_integer_to_string(number)
+#   if number.positive?
+#     "+#{integer_to_string(number)}"
+#   elsif number.negative?
+#     "-#{integer_to_string(-number)}"
+#   else
+#     integer_to_string(number)
+#   end
+# end
 
 # tests:
 p signed_integer_to_string(4321) == '+4321'
