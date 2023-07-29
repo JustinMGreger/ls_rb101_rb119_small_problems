674165ba957194eb420143b6f7d34b0e2f597c06
Justin Greger <justingreger@gmail.com>
2023-07-29 14:42:53 -0700

    str_num to quotes'

--- easy_4/10_convert_a_signed_number_to_a_string.rb
+++ easy_4/10_convert_a_signed_number_to_a_string.rb
@@ -104,15 +104,15 @@ def integer_to_string(number)
 end
 
 def signed_integer_to_string(number)
-  str_num = integer_to_string(number.abs)
-
-  if number.positive?
-    '+' + str_num
-  elsif number.negative?
-    '-' + str_num
-  else
-    str_num
-  end
+  str_num =
+    if number.positive?
+      '+'
+    elsif number.negative?
+      '-'
+    else
+      ''
+    end
+  str_num + integer_to_string(number.abs)
 end
 
 # tests:
