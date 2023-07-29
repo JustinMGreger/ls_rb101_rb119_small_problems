99552c6e033963fc8cfecfc1f117d2a24f7a5134
Justin Greger <justingreger@gmail.com>
2023-07-28 20:27:59 -0700

    deleted #

--- easy_4/10_convert_a_signed_number_to_a_string.rb
+++ easy_4/10_convert_a_signed_number_to_a_string.rb
@@ -66,25 +66,25 @@
 # Refactor our solution to reduce the 3 integer_to_string calls to just one.
 
 # LS solution refactored:
-# DIGITS = %w[0 1 2 3 4 5 6 7 8 9].freeze
+DIGITS = %w[0 1 2 3 4 5 6 7 8 9].freeze
 
-# def integer_to_string(number)
-#   result = String.new
-#   loop do
-#     number, remainder = number.divmod(10)
-#     result.prepend(DIGITS[remainder])
-#     break if number.zero?
-#   end
-#   result
-# end
+def integer_to_string(number)
+  result = String.new
+  loop do
+    number, remainder = number.divmod(10)
+    result.prepend(DIGITS[remainder])
+    break if number.zero?
+  end
+  result
+end
 
-# def signed_integer_to_string(number)
-#   case number <=> 0
-#   when -1 then "-#{integer_to_string(-number)}"
-#   when +1 then "+#{integer_to_string(number)}"
-#   else         integer_to_string(number)
-#   end
-# end
+def signed_integer_to_string(number)
+  case number <=> 0
+  when -1 then "-#{integer_to_string(-number)}"
+  when +1 then "+#{integer_to_string(number)}"
+  else         integer_to_string(number)
+  end
+end
 
 
 
