ae8a6c55f328b8b3b9b4077fe2943ec21ddd501f
Justin Greger <justingreger@gmail.com>
2023-07-28 20:37:24 -0700

    sign =  case number <=> 0

--- easy_4/10_convert_a_signed_number_to_a_string.rb
+++ easy_4/10_convert_a_signed_number_to_a_string.rb
@@ -79,10 +79,10 @@ def integer_to_string(number)
 end
 
 def signed_integer_to_string(number)
-  case number <=> 0
+sign =  case number <=> 0
   when -1 then "-#{integer_to_string(-number)}"
   when +1 then "+#{integer_to_string(number)}"
-  else         integer_to_string(number)
+  else         ""
   end
 end
 
