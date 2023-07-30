03c88c79f42a44260a3e907cd3e1e27f9874eaee
Justin Greger <justingreger@gmail.com>
2023-07-29 19:51:53 -0700

    ascii_value
    end

--- easy_5/1_ascii_string_value.rb
+++ easy_5/1_ascii_string_value.rb
@@ -4,6 +4,12 @@
 # The ASCII string value is the sum of the ASCII values of every character in the string.
 # (You may use String#ord to determine the ASCII value of a character.)
 
+ascii_value
+
+
+
+
+end
 # tests:
 ascii_value('Four score') == 984
 ascii_value('Launch School') == 1251
