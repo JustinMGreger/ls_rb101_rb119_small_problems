f51f7e3347d94b67097c461a9359b3b3441e736c
Justin Greger <justingreger@gmail.com>
2023-07-29 20:14:26 -0700

    def ascii_value(parameter) value of first character = parameter.ord loop through the parameter string and extract the first character break loop when parameter string is empty   ascii_string_value = The is the sum of the ASCII values of every character in the string. end

--- easy_5/1_ascii_string_value.rb
+++ easy_5/1_ascii_string_value.rb
@@ -5,10 +5,12 @@
 # (You may use String#ord to determine the ASCII value of a character.)
 
 def ascii_value(parameter)
-
-parameter.ord
+value of first character = parameter.ord
+loop through the parameter string and extract the first character
+break loop when parameter string is empty
   ascii_string_value = The is the sum of the ASCII values of every character in the string.
 end
+
 # tests:
 ascii_value('Four score') == 984
 ascii_value('Launch School') == 1251
