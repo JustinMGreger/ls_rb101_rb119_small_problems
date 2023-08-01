8c28c67d6a696524e883f45520dc9bbcb6426564
Justin Greger <justingreger@gmail.com>
2023-08-01 14:19:11 -0700

    # Examples:

--- easy_5/2_after_midnight_part_1.rb
+++ easy_5/2_after_midnight_part_1.rb
@@ -10,3 +10,10 @@
 # You may not use ruby's Date and Time classes.
 
 # Examples:
+time_of_day(0) == "00:00"
+time_of_day(-3) == "23:57"
+time_of_day(35) == "00:35"
+time_of_day(-1437) == "00:03"
+time_of_day(3000) == "02:00"
+time_of_day(800) == "13:20"
+time_of_day(-4231) == "01:29"
