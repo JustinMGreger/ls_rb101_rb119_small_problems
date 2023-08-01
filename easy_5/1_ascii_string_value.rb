d1a31eb8466844cd55af7e51b702d43f88b25354
Justin Greger <justingreger@gmail.com>
2023-08-01 14:09:49 -0700

    # tests:

--- easy_5/1_ascii_string_value.rb
+++ easy_5/1_ascii_string_value.rb
@@ -20,6 +20,12 @@
 #   sum
 # end
 
+# tests:
+# p ascii_value('Four score') == 984
+# p ascii_value('Launch School') == 1251
+# p ascii_value('a') == 97
+# p ascii_value('') == 0
+
 # Further Exploration:
 
 # There is an Integer method such that:
