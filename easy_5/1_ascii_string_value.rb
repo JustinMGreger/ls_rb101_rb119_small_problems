cd9a9a081f9ab8dd55bb97a27126e1a0c46ab539
Justin Greger <justingreger@gmail.com>
2023-08-01 10:54:44 -0700

    # Further Exploration:

--- easy_5/1_ascii_string_value.rb
+++ easy_5/1_ascii_string_value.rb
@@ -20,6 +20,17 @@
 #   sum
 # end
 
+# Further Exploration:
+
+# There is an Integer method such that:
+def mystery
+  char.ord.mystery == char
+end
+# where mystery is our mystery method.
+# Can you determine what method name should be used in place of mystery?
+
+# What happens if you try this with a longer string instead of a single character?
+
 # tests:
 p ascii_value('Four score') == 984
 p ascii_value('Launch School') == 1251
