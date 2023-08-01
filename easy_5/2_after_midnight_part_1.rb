032e05aec6a0e0d502fe5dbbe86bd2b3c0044e42
Justin Greger <justingreger@gmail.com>
2023-08-01 14:15:58 -0700

    # The time of day can be represented as the number of minutes before or after midnight.

--- easy_5/2_after_midnight_part_1.rb
+++ easy_5/2_after_midnight_part_1.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+# The time of day can be represented as the number of minutes before or after midnight.
+# If the number of minutes is positive, the time is after midnight.
+# If the number of minutes is negative, the time is before midnight.
+
+# Write a method that takes a time using this minute-based format and returns the time of day in 24 hour format (hh:mm).
+# Your method should work with any integer input.
+
+# You may not use ruby's Date and Time classes.
+
+# Examples:
