c5dde3e6691c082068dec057e149e29c247f2bcd
Justin Greger <justingreger@gmail.com>
2023-08-04 21:13:22 -0400

    double to single quotations

--- easy_5/2_after_midnight_part_1.rb
+++ easy_5/2_after_midnight_part_1.rb
@@ -27,7 +27,7 @@ def time_of_day(minutes)
   total_minutes = ((minutes % 1440) + 1440) % 1440
 
   hours, minutes = total_minutes.divmod(60)
-  Kernel.format("%02d:%02d", hours, minutes)
+  Kernel.format('%02d:%02d', hours, minutes)
 end
 
 # tests:
