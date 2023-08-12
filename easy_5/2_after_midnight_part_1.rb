caef256ef84f30d58c2abd097ac7bfe87f225ea3
Justin Greger <justingreger@gmail.com>
2023-08-12 19:07:33 -0400

    # Problem 3

--- easy_5/2_after_midnight_part_1.rb
+++ easy_5/2_after_midnight_part_1.rb
@@ -99,6 +99,13 @@
 #   time.strftime('%H:%M')
 # end
 
+# Problem 3
+
+# How would you approach this problem if you were allowed to use ruby's Date and Time classes and wanted to consider
+# the day of week in your calculation?
+# (Assume that delta_minutes is the number of minutes before or after midnight between Saturday and Sunday;
+# in such a method, a delta_minutes value of -4231 would need to produce a return value of Thursday 01:29.)
+
 # tests:
 p time_of_day(0) == '00:00' # result: true
 p time_of_day(-3) == '23:57' # result: true
