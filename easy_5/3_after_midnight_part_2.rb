2b248e236da0adc534d06a3234c7435f760ad00c
Justin Greger <justingreger@gmail.com>
2023-08-12 20:41:46 -0400

    # Examples:

--- easy_5/3_after_midnight_part_2.rb
+++ easy_5/3_after_midnight_part_2.rb
@@ -10,3 +10,9 @@
 # You may not use ruby's Date and Time methods.
 
 # Examples:
+after_midnight('00:00') == 0
+before_midnight('00:00') == 0
+after_midnight('12:34') == 754
+before_midnight('12:34') == 686
+after_midnight('24:00') == 0
+before_midnight('24:00') == 0
