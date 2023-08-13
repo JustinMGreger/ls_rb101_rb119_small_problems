ab1a4cfabf536459a8ed03c0d927ff7db071b5da
Justin Greger <justingreger@gmail.com>
2023-08-12 21:48:24 -0400

    deleted PEDAC

--- easy_5/3_after_midnight_part_2.rb
+++ easy_5/3_after_midnight_part_2.rb
@@ -22,55 +22,6 @@ before_midnight('24:00') == 0
 
 # Disregard Daylight Savings and Standard Time and other irregularities.
 
-# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
-# PEDAC stands for “[Understand the] Problem, 
-# Identify expected input: a time of day in 24 hour format
-# and output: return the number of minutes before and after midnight, respectively.
-# Both methods should return a value in the range 0..1439.
-
-# Make the requirements explicit Identify rules:
-# As seen in the previous exercise, the time of day can be represented as the number of minutes before or after midnight.
-# If the number of minutes is positive, the time is after midnight.
-# If the number of minutes is negative, the time is before midnight.
-# Write two methods that each take a time of day in 24 hour format,
-# and return the number of minutes before and after midnight, respectively.
-# Both methods should return a value in the range 0..1439.
-# You may not use ruby's Date and Time methods.
-# Disregard Daylight Savings and Standard Time and other irregularities.
-
-# Mental model of the problem (optional):
-# Write two methods that each take a time of day in 24 hour format,
-# and return the number of minutes before and after midnight, respectively.
-# Both methods should return a value in the range 0..1439.
-# the before_midnight method if the number of minutes is negative.
-# the after_midnight method if the number of minutes is positive.
-# As seen in the previous exercise, the time of day can be represented as the number of minutes before or after midnight.
-# You may not use ruby's Date and Time methods.
-# Disregard Daylight Savings and Standard Time and other irregularities.
-
-# Examples / Test Cases, Validate understanding of the problem:
-# after_midnight('00:00') == 0
-# before_midnight('00:00') == 0
-# after_midnight('12:34') == 754
-# before_midnight('12:34') == 686
-# after_midnight('24:00') == 0
-# before_midnight('24:00') == 0
-
-# Data Structure, How we represent data that we will work with when converting the input to output:
-# answer: possibly array.
-
-# Algorithm, Steps for converting input to output:
-# Write two methods that each take a time of day in 24 hour format,
-# the before_midnight method if the number of minutes is negative.
-# before_midnight('00:00') == 0
-# before_midnight('24:00') == 0
-# the after_midnight method if the number of minutes is positive.
-# after_midnight('00:00') == 0
-# after_midnight('24:00') == 0
-# after_midnight('12:34') == 754
-# and return the number of minutes before and after midnight, respectively.
-# Both methods should return a value in the range 0..1439.
-
 # and Code.” Implementation of Algorithm:
 Write two methods that each take a time of day in 24 hour format,
  the before_midnight method if the number of minutes is negative.
