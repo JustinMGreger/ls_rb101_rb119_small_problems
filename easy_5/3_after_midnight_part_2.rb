1e1eedb26c89a7537559a23c12107410272a56d8
Justin Greger <justingreger@gmail.com>
2023-08-13 16:12:06 -0400

    p

--- easy_5/3_after_midnight_part_2.rb
+++ easy_5/3_after_midnight_part_2.rb
@@ -28,9 +28,9 @@ def after_midnight(time_string)
 end
 
 # tests:
-after_midnight('00:00') == 0
-before_midnight('00:00') == 0
-after_midnight('12:34') == 754
-before_midnight('12:34') == 686
-after_midnight('24:00') == 0
-before_midnight('24:00') == 0
+p after_midnight('00:00') == 0
+p before_midnight('00:00') == 0
+p after_midnight('12:34') == 754
+p before_midnight('12:34') == 686
+p after_midnight('24:00') == 0
+p before_midnight('24:00') == 0
