fb412ad90de8299ce9571916f9adf47f87cb6f71
Justin Greger <justingreger@gmail.com>
2023-08-13 16:31:20 -0400

    p before_midnight('00:00').zero?

--- easy_5/3_after_midnight_part_2.rb
+++ easy_5/3_after_midnight_part_2.rb
@@ -35,7 +35,7 @@ end
 
 # tests:
 p after_midnight('00:00').zero?
-p before_midnight('00:00') == 0
+p before_midnight('00:00').zero?
 p after_midnight('12:34') == 754
 p before_midnight('12:34') == 686
 p after_midnight('24:00') == 0
