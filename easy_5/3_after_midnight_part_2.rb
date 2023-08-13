69b326908fd2d64b97ef42a4870bf46756cd8c75
Justin Greger <justingreger@gmail.com>
2023-08-12 20:42:27 -0400

    # Yes, we know that 24:00 isn't a valid time in 24-hour format.

--- easy_5/3_after_midnight_part_2.rb
+++ easy_5/3_after_midnight_part_2.rb
@@ -16,3 +16,6 @@ after_midnight('12:34') == 754
 before_midnight('12:34') == 686
 after_midnight('24:00') == 0
 before_midnight('24:00') == 0
+
+# Yes, we know that 24:00 isn't a valid time in 24-hour format.
+# Humor us, though; it makes the problem more interesting.
