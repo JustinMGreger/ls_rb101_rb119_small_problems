ad7f362b361c2c2574d47803becce2b3d5bdd06f
Justin Greger <justingreger@gmail.com>
2023-08-15 20:17:00 -0400

    # Given a string of words separated by spaces, # write a method that takes this string of words and returns a string in which the first and last letters of # every word are swapped.
    # You may assume that every word contains at least one letter,
    # and that the string will always contain at least one word.
    # You may also assume that each string contains nothing but words and spaces.
    
    # Examples:

--- easy_5/4_letter_swap.rb
+++ easy_5/4_letter_swap.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+# Given a string of words separated by spaces,
+# write a method that takes this string of words and returns a string in which the first and last letters of
+# every word are swapped.
+
+# You may assume that every word contains at least one letter,
+# and that the string will always contain at least one word.
+# You may also assume that each string contains nothing but words and spaces.
+
+# Examples:
