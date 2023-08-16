b994433b0c2e6742fb6f146efa28311fd3e2e0f9
Justin Greger <justingreger@gmail.com>
2023-08-15 21:34:40 -0400

    # Examples / Test Cases, Validate understanding of the problem:

--- easy_5/4_letter_swap.rb
+++ easy_5/4_letter_swap.rb
@@ -7,26 +7,17 @@
 # and that the string will always contain at least one word.
 # You may also assume that each string contains nothing but words and spaces.
 
-# Examples:
-swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
-swap('Abcde') == 'ebcdA'
-swap('a') == 'a'
-
-# Make the requirements explicit Identify rules:
-# write a method that takes:
-# a string of words where every word contains at least one letter,
-# that the string will always contain at least one word,
-# each string contains nothing but words and spaces, 
-# separated by spaces 
-# and returns a string in which the first and last letters of every word are swapped.
-
 # Mental model of the problem (optional):
 # def swap (a string that will always contain nothing but at least one word where every word contains at least one letter
 # and each word is separated by spaces)
 #   returns a string in which the first and last letters of every word are swapped.
 # end
 
-Examples / Test Cases, Validate understanding of the problem:
+# Examples / Test Cases, Validate understanding of the problem:
+p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
+p swap('Abcde') == 'ebcdA'
+p swap('a') == 'a'
+
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
