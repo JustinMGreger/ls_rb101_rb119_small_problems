71444047d0d4963722ef68457d4c01ef407e6ad3
Justin Greger <justingreger@gmail.com>
2023-08-15 21:51:08 -0400

    # my answer:

--- easy_5/4_letter_swap.rb
+++ easy_5/4_letter_swap.rb
@@ -23,4 +23,18 @@ p swap('a') == 'a'
 # 2. assigned the result of: swapping the first and last letters of every word
 # 3. return this swapped string.
 
-and Code.” Implementation of Algorithm:
+# my answer:
+
+def swap (a string that will always contain nothing but at least one word where every word contains at least one letter
+and each word is separated by spaces)
+  returns a string in which the first and last letters of every word are swapped.
+end
+
+the swap method accepts a string.
+assigned the result of: swapping the first and last letters of every word
+return this swapped string.
+
+# Examples / Test Cases, Validate understanding of the problem:
+p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
+p swap('Abcde') == 'ebcdA'
+p swap('a') == 'a'
