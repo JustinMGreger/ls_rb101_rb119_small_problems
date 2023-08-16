de56e41bb69536ddcc14e823b0649d583681efb2
Justin Greger <justingreger@gmail.com>
2023-08-15 21:51:43 -0400

    deleted lines

--- easy_5/4_letter_swap.rb
+++ easy_5/4_letter_swap.rb
@@ -7,22 +7,6 @@
 # and that the string will always contain at least one word.
 # You may also assume that each string contains nothing but words and spaces.
 
-# Mental model of the problem (optional):
-# def swap (a string that will always contain nothing but at least one word where every word contains at least one letter
-# and each word is separated by spaces)
-#   returns a string in which the first and last letters of every word are swapped.
-# end
-
-# Examples / Test Cases, Validate understanding of the problem:
-p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
-p swap('Abcde') == 'ebcdA'
-p swap('a') == 'a'
-
-# Algorithm, Steps for converting input to output:
-# 1. the swap method accepts a string.
-# 2. assigned the result of: swapping the first and last letters of every word
-# 3. return this swapped string.
-
 # my answer:
 
 def swap (a string that will always contain nothing but at least one word where every word contains at least one letter
