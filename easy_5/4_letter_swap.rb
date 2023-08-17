a1011c96ee784dae711369dff8129539bc53c1be
Justin Greger <justingreger@gmail.com>
2023-08-16 20:19:00 -0400

    # LS answer:

--- easy_5/4_letter_swap.rb
+++ easy_5/4_letter_swap.rb
@@ -19,6 +19,19 @@
 #   end.join(' ')
 # end
 
+# LS answer:
+# def swap_first_last_characters(word)
+#   word[0], word[-1] = word[-1], word[0]
+#   word
+# end
+
+# def swap(words)
+#   result = words.split.map do |word|
+#     swap_first_last_characters(word)
+#   end
+#   result.join(' ')
+# end
+
 # Examples / Test Cases, Validate understanding of the problem:
 p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
 p swap('Abcde') == 'ebcdA'
