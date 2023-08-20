9c86341b11c3dd6febf03c0aa97a8dd7b3fdebb6
Justin Greger <justingreger@gmail.com>
2023-08-19 21:52:17 -0400

    # Mental model of the problem (optional):

--- easy_5/5_clean_up_the_words.rb
+++ easy_5/5_clean_up_the_words.rb
@@ -20,12 +20,12 @@ cleanup("---what's my +*& line?") == ' what s my line '
 # 1. write a method that returns that string with all of the non-alphabetic characters replaced by spaces. 
 # 2. the result should never have consecutive spaces
 
+# Mental model of the problem (optional):
+# input: a string that consists of some words (all lowercased) and an assortment of non-alphabetic characters
+# write a method that returns that string with all of the non-alphabetic characters replaced by spaces. 
+# the result should never have consecutive spaces
+# output: return a string with all of the non-alphabetic characters replaced by spaces.
 
-
-
-
-
-Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
