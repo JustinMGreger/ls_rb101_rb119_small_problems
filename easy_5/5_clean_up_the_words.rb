56574e243aeddc24a652365f40690c8290b15b47
Justin Greger <justingreger@gmail.com>
2023-08-19 21:47:53 -0400

    # Make the requirements explicit Identify rules:

--- easy_5/5_clean_up_the_words.rb
+++ easy_5/5_clean_up_the_words.rb
@@ -16,7 +16,15 @@ cleanup("---what's my +*& line?") == ' what s my line '
 # If one or more non-alphabetic characters occur in a row, you should only have one space in the result
 # (the result should never have consecutive spaces).
 
-Make the requirements explicit Identify rules:
+# Make the requirements explicit Identify rules:
+# 1. write a method that returns that string with all of the non-alphabetic characters replaced by spaces. 
+# 2. the result should never have consecutive spaces
+
+
+
+
+
+
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
