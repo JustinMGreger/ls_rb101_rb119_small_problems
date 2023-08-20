f6ecf2d7791e025c748c4818ca6ff289755cf16f
Justin Greger <justingreger@gmail.com>
2023-08-19 21:21:21 -0400

    # Given a string that consists of some words

--- easy_5/5_clean_up_the_words.rb
+++ easy_5/5_clean_up_the_words.rb
@@ -1,1 +1,8 @@
 # frozen_string_literal: true
+
+# Given a string that consists of some words (all lowercased) and an assortment of non-alphabetic characters,
+# write a method that returns that string with all of the non-alphabetic characters replaced by spaces.
+# If one or more non-alphabetic characters occur in a row,
+# you should only have one space in the result (the result should never have consecutive spaces).
+
+# Examples:
