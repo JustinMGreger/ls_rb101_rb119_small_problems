427555319a414d78d6c0722c56449c025c2c0235
Justin Greger <justingreger@gmail.com>
2023-08-19 20:30:41 -0400

    # def swap_first_last_characters(a, b)

--- easy_5/4_letter_swap.rb
+++ easy_5/4_letter_swap.rb
@@ -36,12 +36,12 @@
 # How come our solution passes word into the swap_first_last_characters method invocation instead of just passing
 # the characters that needed to be swapped?
 # Suppose we had this implementation:
-def swap_first_last_characters(a, b)
-  a, b = b, a
-end
+# def swap_first_last_characters(a, b)
+#   a, b = b, a
+# end
 
 # and called the method like this:
-swap_first_last_characters(word[0], word[-1])
+# swap_first_last_characters(word[0], word[-1])
 
 # Would this method work? Why or why not?
 
