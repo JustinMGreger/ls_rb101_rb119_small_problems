6aeb2bc62e1fb20707210023941d3dc6e44681ef
Justin Greger <justingreger@gmail.com>
2023-08-20 19:59:13 -0400

    deleted lines

--- easy_5/5_clean_up_the_words.rb
+++ easy_5/5_clean_up_the_words.rb
@@ -5,17 +5,6 @@
 # If one or more non-alphabetic characters occur in a row,
 # you should only have one space in the result (the result should never have consecutive spaces).
 
-# Data Structure, How we represent data that we will work with when converting the input to output:
-# Array.
-
-# Algorithm, Steps for converting input to output:
-# 1. the cleanup method accepts the string parameter that consists of some words (all lowercased) and an assortment of
-# non-alphabetic characters
-# 2. split the string parameter based on all non-alphabetic characters
-# 3. replace all non-alphabetic characters with spaces
-# 4. combine the result into a string.
-# 5. return the resulting string
-
 def cleanup(string_parameter)
   replaced_string = string_parameter.gsub(/[^a-zA-Z]/, ' ')
   result_string = replaced_string.gsub(/\s+/, ' ')
