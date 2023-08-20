15b0102caa7120e9fb8afc52fa00dfc0553fa624
Justin Greger <justingreger@gmail.com>
2023-08-20 17:24:14 -0400

    deleted mental model

--- easy_5/5_clean_up_the_words.rb
+++ easy_5/5_clean_up_the_words.rb
@@ -5,12 +5,6 @@
 # If one or more non-alphabetic characters occur in a row,
 # you should only have one space in the result (the result should never have consecutive spaces).
 
-# Mental model of the problem (optional):
-# input: a string that consists of some words (all lowercased) and an assortment of non-alphabetic characters
-# write a method that returns that string with all of the non-alphabetic characters replaced by spaces. 
-# the result should never have consecutive spaces
-# output: return a string with all of the non-alphabetic characters replaced by spaces.
-
 # Examples / Test Cases, Validate understanding of the problem:
 p cleanup("---what's my +*& line?") == ' what s my line '
 
