c097a5588741942fab2384296e521d89393fe169
Justin Greger <justingreger@gmail.com>
2023-08-19 21:39:26 -0400

    # and output:

--- easy_5/5_clean_up_the_words.rb
+++ easy_5/5_clean_up_the_words.rb
@@ -11,7 +11,11 @@ cleanup("---what's my +*& line?") == ' what s my line '
 # Identify expected input: 
 # a string that consists of some words (all lowercased) and an assortment of non-alphabetic characters
 
-and output:
+# and output:
+# string with all of the non-alphabetic characters replaced by spaces.
+# If one or more non-alphabetic characters occur in a row, you should only have one space in the result
+# (the result should never have consecutive spaces).
+
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
