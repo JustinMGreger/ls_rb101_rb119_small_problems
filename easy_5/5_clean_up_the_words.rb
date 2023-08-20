7ce2d42433ab88e153d8082b6ebe7f680350e787
Justin Greger <justingreger@gmail.com>
2023-08-19 21:53:41 -0400

    deleted lines

--- easy_5/5_clean_up_the_words.rb
+++ easy_5/5_clean_up_the_words.rb
@@ -8,18 +8,6 @@
 # Examples:
 cleanup("---what's my +*& line?") == ' what s my line '
 
-# Identify expected input: 
-# a string that consists of some words (all lowercased) and an assortment of non-alphabetic characters
-
-# and output:
-# string with all of the non-alphabetic characters replaced by spaces.
-# If one or more non-alphabetic characters occur in a row, you should only have one space in the result
-# (the result should never have consecutive spaces).
-
-# Make the requirements explicit Identify rules:
-# 1. write a method that returns that string with all of the non-alphabetic characters replaced by spaces. 
-# 2. the result should never have consecutive spaces
-
 # Mental model of the problem (optional):
 # input: a string that consists of some words (all lowercased) and an assortment of non-alphabetic characters
 # write a method that returns that string with all of the non-alphabetic characters replaced by spaces. 
