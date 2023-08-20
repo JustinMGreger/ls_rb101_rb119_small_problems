649d99ac4e9b7f39fbde37ec9fb16506df8a6a30
Justin Greger <justingreger@gmail.com>
2023-08-19 21:26:45 -0400

    PEDAC

--- easy_5/5_clean_up_the_words.rb
+++ easy_5/5_clean_up_the_words.rb
@@ -7,3 +7,14 @@
 
 # Examples:
 cleanup("---what's my +*& line?") == ' what s my line '
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+and output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
