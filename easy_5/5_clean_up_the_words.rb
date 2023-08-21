b848b3026e1ed08dffe3eae50fd9dae5e149116c
Justin Greger <justingreger@gmail.com>
2023-08-21 16:34:46 -0400

    # LS Solution 1

--- easy_5/5_clean_up_the_words.rb
+++ easy_5/5_clean_up_the_words.rb
@@ -12,5 +12,23 @@
 #   result_string
 # end
 
+# LS Solution 1
+
+# ALPHABET = ('a'..'z').to_a
+
+# def cleanup(text)
+#   clean_chars = []
+
+#   text.chars.each do |char|
+#     if ALPHABET.include?(char)
+#       clean_chars << char
+#     else
+#       clean_chars << ' ' unless clean_chars.last == ' '
+#     end
+#   end
+
+#   clean_chars.join
+# end
+
 # test:
 p cleanup("---what's my +*& line?") == ' what s my line '
