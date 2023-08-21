e7504582ffa7c73b24601dab3733797c8da9d043
Justin Greger <justingreger@gmail.com>
2023-08-21 19:39:13 -0400

    # Write a method that takes a

--- easy_5/6_letter_counter_part_1.rb
+++ easy_5/6_letter_counter_part_1.rb
@@ -1,1 +1,8 @@
 # frozen_string_literal: true
+
+# Write a method that takes a string with one or more space separated words and returns a hash that shows the number
+# of words of different sizes.
+
+# Words consist of any string of characters that do not include a space.
+
+# Examples
