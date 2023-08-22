ae1e28daebf50e9ca9ddf12f0e08544da0d67fd4
Justin Greger <justingreger@gmail.com>
2023-08-21 20:22:31 -0400

    Mental model of the problem (optional):

--- easy_5/6_letter_counter_part_1.rb
+++ easy_5/6_letter_counter_part_1.rb
@@ -23,6 +23,16 @@ Write a method that takes a string with one or more space separated words
 and returns a hash that shows the number of words of different sizes.
 
 Mental model of the problem (optional):
+Words = consist of any string of characters that do not include a space.
+Write a method that takes a string with one or more space separated words
+and returns a hash that shows the number of words of different sizes.
+Hash is a data Structure of keys & values.
+def word_sizes(string)
+  key = word lengths
+  value = how many words are that length
+  hash = {key: value, key:value}
+end
+
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
