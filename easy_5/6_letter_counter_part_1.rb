ea619be8e8c4da3b79bffe92c7281d9f365d2eca
Justin Greger <justingreger@gmail.com>
2023-08-21 20:24:03 -0400

    deleted examples

--- easy_5/6_letter_counter_part_1.rb
+++ easy_5/6_letter_counter_part_1.rb
@@ -5,12 +5,6 @@
 
 # Words consist of any string of characters that do not include a space.
 
-# Examples
-word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
-word_sizes('') == {}
-
 Mental model of the problem (optional):
 Words = consist of any string of characters that do not include a space.
 Write a method that takes a string with one or more space separated words
