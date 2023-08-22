4b1a755bcd49bc6857b352b836ffa3974693fa2f
Justin Greger <justingreger@gmail.com>
2023-08-21 20:04:26 -0400

    Identify expected input: a string with one or more space separated words

--- easy_5/6_letter_counter_part_1.rb
+++ easy_5/6_letter_counter_part_1.rb
@@ -11,7 +11,7 @@ word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 
 word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
 word_sizes('') == {}
 
-Identify expected input: 
+Identify expected input: a string with one or more space separated words
 and output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
