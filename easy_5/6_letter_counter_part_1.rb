5d717dc88a45b1b2be926ab4e7155e0753d260ff
Justin Greger <justingreger@gmail.com>
2023-08-21 21:14:24 -0400

    and Code.” Implementation of Algorithm:

--- easy_5/6_letter_counter_part_1.rb
+++ easy_5/6_letter_counter_part_1.rb
@@ -32,3 +32,10 @@ Words = consist of any string of characters that do not include a space.
 6. return this hash data structure.
 
 and Code.” Implementation of Algorithm:
+Words = consist of any string of characters that do not include a space.
+  1. the word_sizes method accepts the string parameter and is defined as the follow:
+  2. extract from the string parameter the words.
+  3. determine the length of the words and assign to symbol keys.
+  4. determine how many words are of each length and assign the integer values to the appropriate symbol keys.
+  5. assign the symbol key value pairs to a hash data structure.
+  6. return this hash data structure.
