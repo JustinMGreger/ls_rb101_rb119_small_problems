d773dd01b5c9202256a399d92f1a234cd946185b
Justin Greger <justingreger@gmail.com>
2023-08-23 17:29:37 -0400

    p

--- easy_5/7_letter_counter_part_2.rb
+++ easy_5/7_letter_counter_part_2.rb
@@ -4,7 +4,7 @@
 # For instance, the length of "it's" is 3, not 4.
 
 # Examples
-word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
-word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
-word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
-word_sizes('') == {}
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
+p word_sizes('') == {}
