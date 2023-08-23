bbc2db7038125503cce537d6aee408c132f896b8
Justin Greger <justingreger@gmail.com>
2023-08-22 20:01:57 -0400

    # LS solution:

--- easy_5/6_letter_counter_part_1.rb
+++ easy_5/6_letter_counter_part_1.rb
@@ -14,6 +14,15 @@
 #   key.transform_values(&:size)
 # end
 
+# LS solution:
+# def word_sizes(words_string)
+#   counts = Hash.new(0)
+#   words_string.split.each do |word|
+#     counts[word.size] += 1
+#   end
+#   counts
+# end
+
 # test:
 p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
 p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
