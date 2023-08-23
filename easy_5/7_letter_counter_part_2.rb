09ac8269c96b3a6c8671a8bbdeb1727ebc0aed3f
Justin Greger <justingreger@gmail.com>
2023-08-23 17:28:43 -0400

    # Modify the word_sizes method

--- easy_5/7_letter_counter_part_2.rb
+++ easy_5/7_letter_counter_part_2.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+# Modify the word_sizes method from the previous exercise to exclude non-letters when determining word size.
+# For instance, the length of "it's" is 3, not 4.
+
+# Examples
