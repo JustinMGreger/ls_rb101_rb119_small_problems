082e9b6ea8eaba5e104c092aaacd7fdfc6942a4b
Justin Greger <justingreger@gmail.com>
2023-08-23 17:32:44 -0400

    # my solution from the previous exercise:

--- easy_5/7_letter_counter_part_2.rb
+++ easy_5/7_letter_counter_part_2.rb
@@ -3,6 +3,16 @@
 # Modify the word_sizes method from the previous exercise to exclude non-letters when determining word size.
 # For instance, the length of "it's" is 3, not 4.
 
+# my solution from the previous exercise:
+
+WORDS = /\S+/.freeze
+
+def word_sizes(string_parameter)
+  words_array = string_parameter.scan(WORDS)
+  key = words_array.group_by(&:length)
+  key.transform_values(&:size)
+end
+
 # tests:
 p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
 p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
