97363236932dd7594a908b0c317744504f91a3bb
Justin Greger <justingreger@gmail.com>
2023-08-23 20:04:37 -0400

    # LS solution:

--- easy_5/7_letter_counter_part_2.rb
+++ easy_5/7_letter_counter_part_2.rb
@@ -14,6 +14,16 @@
 #   word_length_count.transform_values(&:size)
 # end
 
+# LS solution:
+# def word_sizes(words_string)
+#   counts = Hash.new(0)
+#   words_string.split.each do |word|
+#     clean_word = word.delete('^A-Za-z')
+#     counts[clean_word.size] += 1
+#   end
+#   counts
+# end
+
 # tests:
 p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
 p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
