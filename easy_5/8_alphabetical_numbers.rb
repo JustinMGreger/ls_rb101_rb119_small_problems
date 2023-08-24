41f751cef05dd1a0eef1ccf5f3351c53d0ebaa4a
Justin Greger <justingreger@gmail.com>
2023-08-24 19:39:26 -0400

    mental model

--- easy_5/8_alphabetical_numbers.rb
+++ easy_5/8_alphabetical_numbers.rb
@@ -6,13 +6,6 @@
 # zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen,
 # fourteen, fifteen, sixteen, seventeen, eighteen, nineteen
 
-Mental model of the problem (optional):
-1. Write a method.
-2. the method accepts an Array of Integers between 0 and 19 as the parameter.
-3. sort the Integers based on the English words for each number: zero, one, two, three, four, five, six, seven,
-eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen
-4. the method returns an Array of those Integers
-
 # tests:
 p alphabetic_number_sort((0..19).to_a) == [
   8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
