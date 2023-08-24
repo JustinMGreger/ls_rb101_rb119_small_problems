425c380460c7789e81359aace451b0f2fe080c90
Justin Greger <justingreger@gmail.com>
2023-08-24 19:13:56 -0400

    deleted examples

--- easy_5/8_alphabetical_numbers.rb
+++ easy_5/8_alphabetical_numbers.rb
@@ -6,13 +6,6 @@
 # zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen,
 # fourteen, fifteen, sixteen, seventeen, eighteen, nineteen
 
-# Examples:
-alphabetic_number_sort((0..19).to_a) == [
-  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
-  6, 16, 10, 13, 3, 12, 2, 0
-]
-
-
 Mental model of the problem (optional):
 1. Write a method.
 2. the method accepts an Array of Integers between 0 and 19 as the parameter.
