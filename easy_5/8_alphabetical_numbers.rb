3ebb91aa7db45622f1906f7a16151244394e4fa2
Justin Greger <justingreger@gmail.com>
2023-08-24 16:51:47 -0400

    PEDAC

--- easy_5/8_alphabetical_numbers.rb
+++ easy_5/8_alphabetical_numbers.rb
@@ -11,3 +11,14 @@ alphabetic_number_sort((0..19).to_a) == [
   8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
   6, 16, 10, 13, 3, 12, 2, 0
 ]
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+and output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
