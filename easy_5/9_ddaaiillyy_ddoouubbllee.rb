7d502a2237d1f98f58277418c3593e5d913a69fc
Justin Greger <justingreger@gmail.com>
2023-08-27 20:55:40 -0400

    # LS solution:

--- easy_5/9_ddaaiillyy_ddoouubbllee.rb
+++ easy_5/9_ddaaiillyy_ddoouubbllee.rb
@@ -16,6 +16,17 @@
 #   new_string.join
 # end
 
+# LS solution:
+# def crunch(text)
+#   index = 0
+#   crunch_text = ''
+#   while index <= text.length - 1
+#     crunch_text << text[index] unless text[index] == text[index + 1]
+#     index += 1
+#   end
+#   crunch_text
+# end
+
 # test:
 p crunch('ddaaiillyy ddoouubbllee') == 'daily double'
 p crunch('4444abcabccba') == '4abcabcba'
