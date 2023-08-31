35818a5f99f5ed8c3c70568fc3ebebcf8a41199e
Justin Greger <justingreger@gmail.com>
2023-08-30 21:47:30 -0400

    # answer: # a different object. # the .split method returns a new array.

--- easy_5/10_spin_me_around_in_circles.rb
+++ easy_5/10_spin_me_around_in_circles.rb
@@ -5,3 +5,15 @@
 # but with each word's characters reversed.
 # Given the method's implementation,
 # will the returned string be the same object as the one passed in as an argument or a different object?
+
+def spin_me(str)
+  str.split.each do |word|
+    word.reverse!
+  end.join(" ")
+end
+
+p spin_me("hello world") # "olleh dlrow"
+
+# answer:
+# a different object.
+# the .split method returns a new array.
