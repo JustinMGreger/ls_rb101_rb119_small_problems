0391b259d3054ad8bf2beb3e9138beb026681597
Justin Greger <justingreger@gmail.com>
2023-08-31 21:45:57 -0400

    Examples / Test Cases, Validate understanding of the problem: # tests:

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -2,22 +2,18 @@
 
 # Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
 
-# Examples:
-puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
-puts digit_list(7) == [7]                     # => true
-puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
-puts digit_list(444) == [4, 4, 4]             # => true
-
 Mental model of the problem (optional):
 def digit_list(positive_integer)
   and returns a list of the digits in the number.
 end
 
-
-
-
-
 Examples / Test Cases, Validate understanding of the problem:
+# tests:
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
+
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
