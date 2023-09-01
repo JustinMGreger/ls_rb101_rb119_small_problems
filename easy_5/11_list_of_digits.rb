fa64ffbb5120c90f44f15de0eb4bf713c91fcde6
Justin Greger <justingreger@gmail.com>
2023-08-31 21:19:41 -0400

    p

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -3,3 +3,7 @@
 # Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
 
 # Examples:
+p puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+p puts digit_list(7) == [7]                     # => true
+p puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+p puts digit_list(444) == [4, 4, 4]             # => true
