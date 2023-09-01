3410052d47d516fadacd878d1c673c75f32f1d9f
Justin Greger <justingreger@gmail.com>
2023-08-31 21:24:50 -0400

    PEDAC

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -7,3 +7,14 @@ p puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
 p puts digit_list(7) == [7]                     # => true
 p puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
 p puts digit_list(444) == [4, 4, 4]             # => true
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
