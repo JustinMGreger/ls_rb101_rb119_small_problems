12824387fa755c78783e4c734e5a7a56518f703d
Justin Greger <justingreger@gmail.com>
2023-08-31 21:53:02 -0400

    deleted Mental model of the problem (optional):

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -2,15 +2,17 @@
 
 # Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
 
-Mental model of the problem (optional):
-def digit_list(positive_integer)
-  and returns a list of the digits in the number.
-end
+
 
 Data Structure, How we represent data that we will work with when converting the input to output:
 array
 
 Algorithm, Steps for converting input to output:
+def digit_list(positive_integer)
+  and returns a list of the digits in the number.
+end
+
+
 and Code.” Implementation of Algorithm:
 
 # tests:
