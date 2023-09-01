633ff8f7537eff3c3beca73269e3c0694a384a77
Justin Greger <justingreger@gmail.com>
2023-09-01 17:30:17 -0400

    deleted   the digit_list splits the positive_integer into an array of elements.   the elements are turned into integer

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -6,8 +6,6 @@
 def digit_list(positive_integer)
   input = gets.chomp
   numbers = input.split.map(&:to_i)
-  the digit_list splits the positive_integer into an array of elements.
-  the elements are turned into integer
   the array is returned
   and returns a list of the digits in the number.
 end
