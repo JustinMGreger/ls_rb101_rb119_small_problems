10cc2edd08c8ac4b079a2588681e52fc31a5de4b
Justin Greger <justingreger@gmail.com>
2023-09-01 17:42:41 -0400

    12_345 and 375_290

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -2,15 +2,13 @@
 
 # Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
 
-
 def digit_list(positive_integer)
   input = gets.chomp
   numbers = input.split.map(&:to_i)
 end
 
-
 # tests:
-puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(12_345) == [1, 2, 3, 4, 5]     # => true
 puts digit_list(7) == [7]                     # => true
-puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(375_290) == [3, 7, 5, 2, 9, 0] # => true
 puts digit_list(444) == [4, 4, 4]             # => true
