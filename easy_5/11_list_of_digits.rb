69b088c5141c99c35a51537bb9b5028aa8f7bc49
Justin Greger <justingreger@gmail.com>
2023-08-31 21:52:08 -0400

    moved tests

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -7,12 +7,12 @@ def digit_list(positive_integer)
   and returns a list of the digits in the number.
 end
 
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
+
 # tests:
 puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
 puts digit_list(7) == [7]                     # => true
 puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
 puts digit_list(444) == [4, 4, 4]             # => true
-
-Data Structure, How we represent data that we will work with when converting the input to output:
-Algorithm, Steps for converting input to output:
-and Code.” Implementation of Algorithm:
