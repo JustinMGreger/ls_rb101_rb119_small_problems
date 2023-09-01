a1d96e40334445353a904109e225c347c15cee1f
Justin Greger <justingreger@gmail.com>
2023-08-31 21:33:24 -0400

    Make the requirements explicit Identify rules: digit_list (one argument, a positive integer) and returns a list of the digits in the number.

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -3,10 +3,10 @@
 # Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
 
 # Examples:
-p puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
-p puts digit_list(7) == [7]                     # => true
-p puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
-p puts digit_list(444) == [4, 4, 4]             # => true
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
 
 Identify expected input: 
 a positive integer.
@@ -15,6 +15,13 @@ output:
 a list of the digits in the number.
 
 Make the requirements explicit Identify rules:
+digit_list (one argument, a positive integer)
+and returns a list of the digits in the number.
+
+
+
+
+
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
