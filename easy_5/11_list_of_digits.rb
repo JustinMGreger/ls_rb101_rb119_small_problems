2f1a312f8a8c23369577fe2c514c92451672ea2b
Justin Greger <justingreger@gmail.com>
2023-09-01 17:56:15 -0400

    # LS solution:

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -7,6 +7,8 @@ def digit_list(positive_integer)
   positive_integer.to_s.chars.map(&:to_i)
 end
 
+# LS solution:
+
 # tests:
 puts digit_list(12_345) == [1, 2, 3, 4, 5] # => true
 puts digit_list(7) == [7] # => true
