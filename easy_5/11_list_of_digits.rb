0f5d93bfe4a05c5b064c42d78bb1732eb5c7dc9e
Justin Greger <justingreger@gmail.com>
2023-08-31 21:43:32 -0400

    def digit_list(positive_integer)

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -8,11 +8,9 @@ puts digit_list(7) == [7]                     # => true
 puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
 puts digit_list(444) == [4, 4, 4]             # => true
 
-
-
-
 Mental model of the problem (optional):
-digit_list (one argument, a positive integer)
+def digit_list(positive_integer)
+
 and returns a list of the digits in the number.
 
 
