d5645a563142ef9a163e7252248e55998bc90ee9
Justin Greger <justingreger@gmail.com>
2023-08-31 22:00:01 -0400

    code

--- easy_5/11_list_of_digits.rb
+++ easy_5/11_list_of_digits.rb
@@ -3,6 +3,9 @@
 # Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
 
 Algorithm, Steps for converting input to output:
+
+
+and Code.” Implementation of Algorithm:
 the digit_list method accepts the positive_integer
 the digit_list splits the positive_integer into an array of elements.
 the elements are turned into integer
@@ -11,9 +14,6 @@ the array is returned
 def digit_list(positive_integer)
   and returns a list of the digits in the number.
 end
-
-
-and Code.” Implementation of Algorithm:
 
 
 # tests:
