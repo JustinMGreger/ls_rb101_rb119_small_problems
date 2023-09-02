1bc2b8c0e4c4e03868ebb2dced6a62c728d55c37
Justin Greger <justingreger@gmail.com>
2023-09-02 19:20:34 -0400

    Make the requirements explicit Identify rules:

--- easy_6/1_cute_angles.rb
+++ easy_6/1_cute_angles.rb
@@ -30,6 +30,20 @@ output:
 a String that represents that angle in (°) degrees, (') minutes and (") seconds.
 
 Make the requirements explicit Identify rules:
+Write a method that takes a floating point number that represents an angle between 0 and 360 degrees
+and returns a String that represents that angle in degrees, minutes and seconds.
+You should use a degree symbol (°) to represent degrees,
+a single quote (') to represent minutes,
+and a double quote (") to represent seconds.
+A degree has 60 minutes, while a minute has 60 seconds.
+Note: your results may differ slightly depending on how you round values,
+but should be within a second or two of the results shown.
+You should use two digit numbers with leading zeros when formatting the minutes and seconds, e.g., 321°03'07".
+You may use this constant to represent the degree symbol:
+DEGREE = "\xC2\xB0"
+
+
+
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
