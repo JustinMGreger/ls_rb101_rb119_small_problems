537cfb83a25a086039e16008f3f820acc6ba9ee7
Justin Greger <justingreger@gmail.com>
2023-09-02 17:38:06 -0400

    # Write a method

--- easy_6/1_cute_angles.rb
+++ easy_6/1_cute_angles.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+# Write a method that takes a floating point number that represents an angle between 0 and 360 degrees
+# and returns a String that represents that angle in degrees, minutes and seconds.
+# You should use a degree symbol (°) to represent degrees,
+# a single quote (') to represent minutes,
+# and a double quote (") to represent seconds.
+# A degree has 60 minutes, while a minute has 60 seconds.
+
+# Examples:
