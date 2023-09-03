64c66fb32e8ab989c0b726f083d528a7545f62cd
Justin Greger <justingreger@gmail.com>
2023-09-03 19:19:14 -0400

    # my solution:

--- easy_6/1_cute_angles.rb
+++ easy_6/1_cute_angles.rb
@@ -15,42 +15,43 @@
 # You may use this constant to represent the degree symbol:
 # DEGREE = "\xC2\xB0"
 
-DEGREE = "\xC2\xB0"
-MINUTES = "\x27"
-SECONDS = "\x22"
+# my solution:
+# DEGREE = "\xC2\xB0"
+# MINUTES = "\x27"
+# SECONDS = "\x22"
 
-def dms(floating_point)
-  degrees, minutes, seconds = decompose_angle(floating_point)
-  format_angle(degrees, minutes, seconds)
-end
+# def dms(floating_point)
+#   degrees, minutes, seconds = decompose_angle(floating_point)
+#   format_angle(degrees, minutes, seconds)
+# end
 
-private
+# private
 
-def decompose_angle(floating_point)
-  degrees, remainder = floating_point.divmod(1.0)
-  minutes, seconds = (remainder * 60).divmod(1.0)
-  seconds = (seconds * 60).round
+# def decompose_angle(floating_point)
+#   degrees, remainder = floating_point.divmod(1.0)
+#   minutes, seconds = (remainder * 60).divmod(1.0)
+#   seconds = (seconds * 60).round
 
-  adjust_for_overflow(degrees, minutes, seconds)
-end
+#   adjust_for_overflow(degrees, minutes, seconds)
+# end
 
-def adjust_for_overflow(degrees, minutes, seconds)
-  if seconds == 60
-    seconds = 0
-    minutes += 1
-  end
+# def adjust_for_overflow(degrees, minutes, seconds)
+#   if seconds == 60
+#     seconds = 0
+#     minutes += 1
+#   end
 
-  if minutes == 60
-    minutes = 0
-    degrees += 1
-  end
+#   if minutes == 60
+#     minutes = 0
+#     degrees += 1
+#   end
 
-  [degrees, minutes, seconds]
-end
+#   [degrees, minutes, seconds]
+# end
 
-def format_angle(degrees, minutes, seconds)
-  "#{degrees.floor}#{DEGREE}#{minutes.floor.to_s.rjust(2, '0')}#{MINUTES}#{seconds.to_s.rjust(2, '0')}#{SECONDS}"
-end
+# def format_angle(degrees, minutes, seconds)
+#   "#{degrees.floor}#{DEGREE}#{minutes.floor.to_s.rjust(2, '0')}#{MINUTES}#{seconds.to_s.rjust(2, '0')}#{SECONDS}"
+# end
 
 # tests:
 p dms(30) == %(30°00'00")
