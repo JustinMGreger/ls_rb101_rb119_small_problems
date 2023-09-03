7bbde165651ba568ba6ad7d9dd556d8898731ead
Justin Greger <justingreger@gmail.com>
2023-09-03 19:21:30 -0400

    # LS solution:

--- easy_6/1_cute_angles.rb
+++ easy_6/1_cute_angles.rb
@@ -53,6 +53,19 @@
 #   "#{degrees.floor}#{DEGREE}#{minutes.floor.to_s.rjust(2, '0')}#{MINUTES}#{seconds.to_s.rjust(2, '0')}#{SECONDS}"
 # end
 
+# LS solution:
+# DEGREE = "\xC2\xB0"
+# MINUTES_PER_DEGREE = 60
+# SECONDS_PER_MINUTE = 60
+# SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE
+
+# def dms(degrees_float)
+#   total_seconds = (degrees_float * SECONDS_PER_DEGREE).round
+#   degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
+#   minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
+#   format(%(#{degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
+# end
+
 # tests:
 p dms(30) == %(30°00'00")
 p dms(76.73) == %(76°43'48")
