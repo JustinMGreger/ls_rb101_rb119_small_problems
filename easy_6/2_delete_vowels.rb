6c3de06f765278c6f781e7c204d5bad630f6bd80
Justin Greger <justingreger@gmail.com>
2023-09-04 20:06:34 -0400

    # tests:

--- easy_6/2_delete_vowels.rb
+++ easy_6/2_delete_vowels.rb
@@ -4,7 +4,7 @@
 # and returns an array of the same string values,
 # except with the vowels (a, e, i, o, u) removed.
 
-# Example:
+# tests:
 p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
 p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
 p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
