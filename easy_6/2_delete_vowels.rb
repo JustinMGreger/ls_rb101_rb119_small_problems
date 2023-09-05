32bd22e6552460df45d5f1a4e2a970a3a65fb39b
Justin Greger <justingreger@gmail.com>
2023-09-05 17:53:18 -0400

    def remove_vowels(string_array)   new_array = string_array.to_s.gsub(/[aeiou]/, '')   new_array.chars end

--- easy_6/2_delete_vowels.rb
+++ easy_6/2_delete_vowels.rb
@@ -5,12 +5,13 @@
 # except with the vowels (a, e, i, o, u) removed.
 
 # my solution:
-def remove_vowels((string_array))
-  new_array = string_array.map { |str| str.gsub(/[aeiou]/, '') }
-  new_array.to_a
+def remove_vowels(string_array)
+  new_array = string_array.to_s.gsub(/[aeiou]/, '')
+  new_array.chars
 end
 
 # tests:
+p remove_vowels(%w(aardvark))
 p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
 p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
 p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
