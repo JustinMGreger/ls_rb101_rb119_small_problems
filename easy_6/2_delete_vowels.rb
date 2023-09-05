43871707b4f1b3472d37dc46ec52be416c49f07a
Justin Greger <justingreger@gmail.com>
2023-09-04 20:02:24 -0400

    # Write a method that takes an array of strings,

--- easy_6/2_delete_vowels.rb
+++ easy_6/2_delete_vowels.rb
@@ -1,1 +1,7 @@
 # frozen_string_literal: true
+
+# Write a method that takes an array of strings,
+# and returns an array of the same string values,
+# except with the vowels (a, e, i, o, u) removed.
+
+# Example:
