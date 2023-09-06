64c79e34432070e8544df169c20a0879f4987aa8
Justin Greger <justingreger@gmail.com>
2023-09-06 19:18:24 -0400

    # Write a method that

--- easy_6/3_fibonacci_number_location_by_length.rb
+++ easy_6/3_fibonacci_number_location_by_length.rb
@@ -12,6 +12,7 @@
 # the 100th Fibonacci number is 354,224,848,179,261,915,075 -- that's enormous,
 # especially considering that it takes 6 iterations before it generates the first 2 digit number.
 
-Write a method that calculates and returns the index of the first Fibonacci number that has the number of digits specified as an argument. (The first Fibonacci number has index 1.)
+# Write a method that calculates and returns the index of the first Fibonacci number that has the number of
+# digits specified as an argument. (The first Fibonacci number has index 1.)
 
-Examples:
+# Examples:
