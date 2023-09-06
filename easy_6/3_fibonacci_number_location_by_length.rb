559e0ef05fe5d456695f967973928f65e88ff355
Justin Greger <justingreger@gmail.com>
2023-09-06 19:20:43 -0400

    # Examples:'

--- easy_6/3_fibonacci_number_location_by_length.rb
+++ easy_6/3_fibonacci_number_location_by_length.rb
@@ -16,3 +16,9 @@
 # digits specified as an argument. (The first Fibonacci number has index 1.)
 
 # Examples:
+p find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
+p find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
+p find_fibonacci_index_by_length(10) == 45
+p find_fibonacci_index_by_length(100) == 476
+p find_fibonacci_index_by_length(1000) == 4782
+p find_fibonacci_index_by_length(10000) == 47847
