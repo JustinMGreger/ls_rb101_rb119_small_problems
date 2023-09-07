9aaf02ebab706d1dfe1e333595e18a5a40773efb
Justin Greger <justingreger@gmail.com>
2023-09-06 21:44:23 -0400

    deleted   fibonacci_parameter == number of digits specified as an argument

--- easy_6/3_fibonacci_number_location_by_length.rb
+++ easy_6/3_fibonacci_number_location_by_length.rb
@@ -32,7 +32,6 @@ def find_fibonacci_index_by_length(fibonacci_parameter)
 
   run fibonacci sequence loop
   break if block_parameter.length == fibonacci_parameter
-  fibonacci_parameter == number of digits specified as an argument
   What is the index of the fibonacci sequence that has the number of digits as the fibonacci_parameter?
   calculates and returns the index of the first Fibonacci number that has the number of digits specified as an argument.
   index_fibonacci = 
