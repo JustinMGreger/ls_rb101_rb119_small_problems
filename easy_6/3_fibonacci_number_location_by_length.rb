32280d4945b88ee2c9f1b4dfe996a99c0d728758
Justin Greger <justingreger@gmail.com>
2023-09-06 20:06:35 -0400

    PEDAC

--- easy_6/3_fibonacci_number_location_by_length.rb
+++ easy_6/3_fibonacci_number_location_by_length.rb
@@ -24,3 +24,14 @@ p find_fibonacci_index_by_length(1_000) == 4_782
 p find_fibonacci_index_by_length(10_000) == 47_847
 
 # You may assume that the argument is always greater than or equal to 2.
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
