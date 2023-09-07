5dbea09ef2de0a5732e2754bc20540ee6f2f2a70
Justin Greger <justingreger@gmail.com>
2023-09-06 21:16:27 -0400

    def find_fibonacci_index_by_length(fibonacci_parameter)

--- easy_6/3_fibonacci_number_location_by_length.rb
+++ easy_6/3_fibonacci_number_location_by_length.rb
@@ -28,7 +28,7 @@ The first Fibonacci number has index 1.
 the first 2 numbers are 1 by definition and each subsequent number is the sum of the two previous numbers
 You may assume that the argument is always greater than or equal to 2.
 
-def find_fibonacci_index_by_length()
+def find_fibonacci_index_by_length(fibonacci_parameter)
   calculates and returns the index of the first Fibonacci number that has the number of digits specified as an argument. 
 end
 
