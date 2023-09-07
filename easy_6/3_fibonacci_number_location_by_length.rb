e8ffa9140856cdbd3032861461434973bd74f361
Justin Greger <justingreger@gmail.com>
2023-09-06 21:46:18 -0400

    deleted   What is the index of the fibonacci sequence that has the number of digits as the fibonacci_parameter?

--- easy_6/3_fibonacci_number_location_by_length.rb
+++ easy_6/3_fibonacci_number_location_by_length.rb
@@ -34,7 +34,6 @@ def find_fibonacci_index_by_length(fibonacci_parameter)
   keep track of number of iterations of loop
   break if block_parameter.length == fibonacci_parameter
   return the number of iterations of loop
-  What is the index of the fibonacci sequence that has the number of digits as the fibonacci_parameter?
   calculates and returns the index of the first Fibonacci number that has the number of digits specified as an argument.
   index_fibonacci = 
 end
