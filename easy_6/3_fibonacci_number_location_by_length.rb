49d02d701aa68adc22b40c7f2f760b15762cb8d3
Justin Greger <justingreger@gmail.com>
2023-09-07 21:43:26 -0400

    deleted     first = second     second = fibonacci

--- easy_6/3_fibonacci_number_location_by_length.rb
+++ easy_6/3_fibonacci_number_location_by_length.rb
@@ -26,8 +26,6 @@ def find_fibonacci_index_by_length(fibonacci_parameter)
 
   loop do
     first, second = generate_next_fibonacci(first, second)
-    first = second
-    second = fibonacci
     index += 1
     break if fibonacci.to_s.length == fibonacci_parameter
   end
