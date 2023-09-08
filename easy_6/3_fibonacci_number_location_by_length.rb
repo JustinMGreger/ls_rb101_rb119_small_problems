482d8acd79d55c373bba618193fee0f7224b097a
Justin Greger <justingreger@gmail.com>
2023-09-07 21:41:24 -0400

    first = second

--- easy_6/3_fibonacci_number_location_by_length.rb
+++ easy_6/3_fibonacci_number_location_by_length.rb
@@ -26,6 +26,7 @@ def find_fibonacci_index_by_length(fibonacci_parameter)
 
   loop do
     fibonacci = first + second
+    first = second
     index += 1
     break if fibonacci.to_s.length == fibonacci_parameter
   end
