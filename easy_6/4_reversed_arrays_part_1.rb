f47f9b486bb78b9003d3caec41152ed49d394bb6
Justin Greger <justingreger@gmail.com>
2023-09-08 21:46:49 -0400

    # Write a method

--- easy_6/4_reversed_arrays_part_1.rb
+++ easy_6/4_reversed_arrays_part_1.rb
@@ -1,1 +1,9 @@
 # frozen_string_literal: true
+
+# Write a method that takes an Array as an argument, and reverses its elements in place;
+# that is, mutate the Array passed into this method.
+# The return value should be the same Array object.
+
+# You may not use Array#reverse or Array#reverse!.
+
+# Examples:
