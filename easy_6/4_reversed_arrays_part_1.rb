d0e67790dbcc523257c7b44de95b0f6f289c3f3a
Justin Greger <justingreger@gmail.com>
2023-09-08 21:56:56 -0400

    p

--- easy_6/4_reversed_arrays_part_1.rb
+++ easy_6/4_reversed_arrays_part_1.rb
@@ -9,21 +9,21 @@
 # Examples:
 list = [1,2,3,4]
 result = reverse!(list)
-result == [4, 3, 2, 1] # true
-list == [4, 3, 2, 1] # true
-list.object_id == result.object_id # true
+p result == [4, 3, 2, 1] # true
+p list == [4, 3, 2, 1] # true
+p list.object_id == result.object_id # true
 
 list = %w(a b e d c)
-reverse!(list) == ["c", "d", "e", "b", "a"] # true
-list == ["c", "d", "e", "b", "a"] # true
+p reverse!(list) == ["c", "d", "e", "b", "a"] # true
+p list == ["c", "d", "e", "b", "a"] # true
 
 list = ['abc']
-reverse!(list) == ["abc"] # true
-list == ["abc"] # true
+p reverse!(list) == ["abc"] # true
+p list == ["abc"] # true
 
 list = []
-reverse!(list) == [] # true
-list == [] # true
+p reverse!(list) == [] # true
+p list == [] # true
 
 # NOTE: for the test case list = ['abc'], we want to reverse the elements in the array.
 # The array only has one element, a String,
