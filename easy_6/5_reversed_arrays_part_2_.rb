64c43e380f10e8d23109ac591e97de9d94049323
Justin Greger <justingreger@gmail.com>
2023-09-10 19:55:25 -0400

    # Write a method that

--- easy_6/5_reversed_arrays_part_2_.rb
+++ easy_6/5_reversed_arrays_part_2_.rb
@@ -1,1 +1,8 @@
 # frozen_string_literal: true
+
+# Write a method that takes an Array, and returns a new Array with the elements of the original list in reverse order.
+# Do not modify the original list.
+
+# You may not use Array#reverse or Array#reverse!, nor may you use the method you wrote in the previous exercise.
+
+# Examples:
