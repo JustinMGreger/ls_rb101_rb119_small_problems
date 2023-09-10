cca4008fbe075814abcb050f2554ed4a79a929fb
Justin Greger <justingreger@gmail.com>
2023-09-10 19:57:29 -0400

    space after comma

--- easy_6/5_reversed_arrays_part_2_.rb
+++ easy_6/5_reversed_arrays_part_2_.rb
@@ -6,7 +6,7 @@
 # You may not use Array#reverse or Array#reverse!, nor may you use the method you wrote in the previous exercise.
 
 # Examples:
-p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+p reverse([1, 2, 3, 4]) == [4, 3, 2, 1]          # => true
 p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
 p reverse(['abc']) == ['abc']              # => true
 p reverse([]) == []                        # => true
