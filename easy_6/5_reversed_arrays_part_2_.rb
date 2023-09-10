93fe2f2a9c8f614e5a05b349455c29ae1dbbde58
Justin Greger <justingreger@gmail.com>
2023-09-10 19:55:51 -0400

    # Examples:

--- easy_6/5_reversed_arrays_part_2_.rb
+++ easy_6/5_reversed_arrays_part_2_.rb
@@ -6,3 +6,13 @@
 # You may not use Array#reverse or Array#reverse!, nor may you use the method you wrote in the previous exercise.
 
 # Examples:
+reverse([1,2,3,4]) == [4,3,2,1]          # => true
+reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+reverse(['abc']) == ['abc']              # => true
+reverse([]) == []                        # => true
+
+list = [1, 3, 2]                      # => [1, 3, 2]
+new_list = reverse(list)              # => [2, 3, 1]
+list.object_id != new_list.object_id  # => true
+list == [1, 3, 2]                     # => true
+new_list == [2, 3, 1]                 # => true
