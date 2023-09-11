7adc3eff419ad8a838c5eddc8a8c63ce94b404e6
Justin Greger <justingreger@gmail.com>
2023-09-10 21:55:49 -0400

    # Examples:

--- easy_6/5_reversed_arrays_part_2_.rb
+++ easy_6/5_reversed_arrays_part_2_.rb
@@ -6,16 +6,16 @@
 # You may not use Array#reverse or Array#reverse!, nor may you use the method you wrote in the previous exercise.
 
 # Examples:
-p reverse([1, 2, 3, 4]) == [4, 3, 2, 1] # => true
-p reverse(%w[a b e d c]) == %w[c d e b a]  # => true
-p reverse(['abc']) == ['abc']              # => true
-p reverse([]) == []                        # => true
+# p reverse([1, 2, 3, 4]) == [4, 3, 2, 1] # => true
+# p reverse(%w[a b e d c]) == %w[c d e b a]  # => true
+# p reverse(['abc']) == ['abc']              # => true
+# p reverse([]) == []                        # => true
 
-p list = [1, 3, 2]                      # => [1, 3, 2]
-p new_list = reverse(list)              # => [2, 3, 1]
-p list.object_id != new_list.object_id  # => true
-p list == [1, 3, 2]                     # => true
-p new_list == [2, 3, 1]                 # => true
+# p list = [1, 3, 2]                      # => [1, 3, 2]
+# p new_list = reverse(list)              # => [2, 3, 1]
+# p list.object_id != new_list.object_id  # => true
+# p list == [1, 3, 2]                     # => true
+# p new_list == [2, 3, 1]                 # => true
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem, 
