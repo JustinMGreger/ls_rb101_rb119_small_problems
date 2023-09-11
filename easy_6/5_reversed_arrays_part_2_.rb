ad10294213b354817116c6bf520255d7d273dd33
Justin Greger <justingreger@gmail.com>
2023-09-10 21:16:39 -0400

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for “[Understand the] Problem, # Identify expected input: # an Array

--- easy_6/5_reversed_arrays_part_2_.rb
+++ easy_6/5_reversed_arrays_part_2_.rb
@@ -17,9 +17,12 @@ p list.object_id != new_list.object_id  # => true
 p list == [1, 3, 2]                     # => true
 p new_list == [2, 3, 1]                 # => true
 
-PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
-PEDAC stands for “[Understand the] Problem, 
-Identify expected input: 
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for “[Understand the] Problem, 
+
+# Identify expected input: 
+# an Array
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
