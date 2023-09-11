7a22def7957677e9d82c762c96624e7fd691d937
Justin Greger <justingreger@gmail.com>
2023-09-10 20:53:09 -0400

    () to []

--- easy_6/5_reversed_arrays_part_2_.rb
+++ easy_6/5_reversed_arrays_part_2_.rb
@@ -7,7 +7,7 @@
 
 # Examples:
 p reverse([1, 2, 3, 4]) == [4, 3, 2, 1] # => true
-p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+p reverse(%w[a b e d c]) == %w[c d e b a]  # => true
 p reverse(['abc']) == ['abc']              # => true
 p reverse([]) == []                        # => true
 
