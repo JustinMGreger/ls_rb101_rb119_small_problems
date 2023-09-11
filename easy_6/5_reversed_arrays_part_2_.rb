688ee1901633c7372a0cfd745b42c9a3f382954f
Justin Greger <justingreger@gmail.com>
2023-09-10 21:44:06 -0400

    # Mental model of the problem (optional):

--- easy_6/5_reversed_arrays_part_2_.rb
+++ easy_6/5_reversed_arrays_part_2_.rb
@@ -32,7 +32,12 @@ p new_list == [2, 3, 1]                 # => true
 # 3. Nor may you use the method you wrote in the previous exercise.
 # 4. Write a method that takes an Array, and returns a new Array with the elements of the original list in reverse order.
 
-Mental model of the problem (optional):
+# Mental model of the problem (optional):
+# 1. the reverse method accepts an Array parameter.
+# 2. the reverse method reverses the order of the elements of the Array parameter.
+# 3. the reverse method assigns the the result of the reversal to a new Array variable.
+# 4. the reverse method returns the new Array variable.
+
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
