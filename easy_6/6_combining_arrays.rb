78a7fc2e32285facd98377e5685cc6b116b5a007
Justin Greger <justingreger@gmail.com>
2023-09-12 21:59:31 -0400

    Make the requirements explicit Identify rules:

--- easy_6/6_combining_arrays.rb
+++ easy_6/6_combining_arrays.rb
@@ -18,6 +18,11 @@ p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
 # a new Array that contains all of the non duplicate values from the argument Arrays
 
 Make the requirements explicit Identify rules:
+Write a method that takes two Arrays as arguments,
+and returns an Array that contains all of the values from the argument Arrays.
+There should be no duplication of values in the returned Array,
+even if there are duplicates in the original Arrays.
+
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
