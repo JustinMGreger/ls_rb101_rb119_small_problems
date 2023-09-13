97c332a322d4e2827cf51087f8f71fdc57e0e5a2
Justin Greger <justingreger@gmail.com>
2023-09-12 21:47:54 -0400

    PEDAC

--- easy_6/6_combining_arrays.rb
+++ easy_6/6_combining_arrays.rb
@@ -7,3 +7,14 @@
 
 # Example
 p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
