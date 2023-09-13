ee84d7220c0625fac7d4c144fe5fc82de1a8ea06
Justin Greger <justingreger@gmail.com>
2023-09-12 21:28:57 -0400

    # Write a method

--- easy_6/6_combining_arrays.rb
+++ easy_6/6_combining_arrays.rb
@@ -1,1 +1,8 @@
 # frozen_string_literal: true
+
+# Write a method that takes two Arrays as arguments,
+# and returns an Array that contains all of the values from the argument Arrays.
+# There should be no duplication of values in the returned Array,
+# even if there are duplicates in the original Arrays.
+
+# Example
