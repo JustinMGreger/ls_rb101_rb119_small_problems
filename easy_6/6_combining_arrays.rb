b9182b5e2e935d0a8b5529b54712d85425bee618
Justin Greger <justingreger@gmail.com>
2023-09-13 17:54:29 -0400

    deleted trailing white space

--- easy_6/6_combining_arrays.rb
+++ easy_6/6_combining_arrays.rb
@@ -9,7 +9,7 @@
 # p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
-# PEDAC stands for “[Understand the] Problem, 
+# PEDAC stands for “[Understand the] Problem,
 
 # Identify expected input: 
 # two Arrays as arguments
