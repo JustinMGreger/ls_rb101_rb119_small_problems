6cfa61e78312662da86cb0788580d7409844ec4d
Justin Greger <justingreger@gmail.com>
2023-09-14 19:26:02 -0400

    # Data Structure

--- easy_6/7_halvsies.rb
+++ easy_6/7_halvsies.rb
@@ -22,6 +22,9 @@ output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
-Data Structure, How we represent data that we will work with when converting the input to output:
+
+# Data Structure, How we represent data that we will work with when converting the input to output:
+# array.
+
 Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
