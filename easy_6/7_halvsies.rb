217f6c7c67a0e21f239d3d6a770b9aa9ffc9271e
Justin Greger <justingreger@gmail.com>
2023-09-14 19:25:36 -0400

    # Identify expected input:

--- easy_6/7_halvsies.rb
+++ easy_6/7_halvsies.rb
@@ -15,7 +15,9 @@ p halvsies([]) == [[], []]
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem, 
 
-Identify expected input: 
+# Identify expected input: 
+# an Array as an argument
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
