f4c6d1857e907f5f8b47a43abe5cca6d8a5d1e82
Justin Greger <justingreger@gmail.com>
2023-09-14 18:50:02 -0400

    # Write a method that takes an Array as an argument,

--- easy_6/7_halvsies.rb
+++ easy_6/7_halvsies.rb
@@ -1,1 +1,9 @@
 # frozen_string_literal: true
+
+# Write a method that takes an Array as an argument,
+# and returns two Arrays (as a pair of nested Arrays) that contain the first half
+# and second half of the original Array, respectively.
+# If the original array contains an odd number of elements,
+# the middle element should be placed in the first half Array.
+
+# Examples:
