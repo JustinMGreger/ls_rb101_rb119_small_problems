ae2e822ddc6e56addf8f75979e18a4dcf3334729
Justin Greger <justingreger@gmail.com>
2023-09-14 19:17:24 -0400

    PEDAC

--- easy_6/7_halvsies.rb
+++ easy_6/7_halvsies.rb
@@ -11,3 +11,14 @@ p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
 p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
 p halvsies([5]) == [[5], []]
 p halvsies([]) == [[], []]
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem, 
+Identify expected input: 
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
