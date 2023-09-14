d80f38f870c108b1b3396d947fb4f1f9ff12f8be
Justin Greger <justingreger@gmail.com>
2023-09-14 18:59:21 -0400

    p

--- easy_6/7_halvsies.rb
+++ easy_6/7_halvsies.rb
@@ -7,7 +7,7 @@
 # the middle element should be placed in the first half Array.
 
 # Examples:
-halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
-halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
-halvsies([5]) == [[5], []]
-halvsies([]) == [[], []]
+p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
+p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
+p halvsies([5]) == [[5], []]
+p halvsies([]) == [[], []]
