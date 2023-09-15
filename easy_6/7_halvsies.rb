998353dbab4e5ac59cb61991586bb5b3eccaee09
Justin Greger <justingreger@gmail.com>
2023-09-15 19:26:12 -0400

    deleted trailing white space

--- easy_6/7_halvsies.rb
+++ easy_6/7_halvsies.rb
@@ -54,7 +54,7 @@
 # and Code.” Implementation of Algorithm:
 def halvsies(array_one)
   middle_element = (array_one.length / 2.0).ceil
- 
+
   first_half_array = array_one.slice(0, middle_element)
   second_half_array = array_one.slice(middle_element, array_one.length - middle_element)
 
