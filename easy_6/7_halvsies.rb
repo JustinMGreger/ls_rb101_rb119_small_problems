8392ff6fc28dfd6d7f1aa31e31c57c786922418d
Justin Greger <justingreger@gmail.com>
2023-09-16 18:57:41 -0400

    # LS solution:

--- easy_6/7_halvsies.rb
+++ easy_6/7_halvsies.rb
@@ -62,12 +62,12 @@
 # end
 
 # LS solution:
-def halvsies(array)
-  middle = (array.size / 2.0).ceil
-  first_half = array.slice(0, middle)
-  second_half = array.slice(middle, array.size - middle)
-  [first_half, second_half]
-end
+# def halvsies(array)
+#   middle = (array.size / 2.0).ceil
+#   first_half = array.slice(0, middle)
+#   second_half = array.slice(middle, array.size - middle)
+#   [first_half, second_half]
+# end
 
 # Further Exploration
 
