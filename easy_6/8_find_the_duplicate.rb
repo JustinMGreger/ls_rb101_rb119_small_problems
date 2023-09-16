23c441aaf541f2cdb238a3cf79936dc437aa68f9
Justin Greger <justingreger@gmail.com>
2023-09-16 19:03:55 -0400

    # Given an unordered array

--- easy_6/8_find_the_duplicate.rb
+++ easy_6/8_find_the_duplicate.rb
@@ -1,1 +1,8 @@
 # frozen_string_literal: true
+
+# Given an unordered array and the information that exactly one value in the array occurs twice
+# (every other value occurs exactly once),
+# how would you determine which value occurs twice?
+# Write a method that will find and return the duplicate value that is known to be in the array.
+
+# Examples:
