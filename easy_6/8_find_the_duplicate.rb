c6385e93f1f5d0a34a2587bfcdd4d43acea585cc
Justin Greger <justingreger@gmail.com>
2023-09-16 21:02:25 -0400

    # Mental model of the problem (optional):

--- easy_6/8_find_the_duplicate.rb
+++ easy_6/8_find_the_duplicate.rb
@@ -35,16 +35,10 @@ find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
 # determine which value occurs twice and return it.
 
 # Mental model of the problem (optional):
-write a method.
-the method accepts a unordered array.
-exactly one value in the array occurs twice.
-all other values occurs exactly once.
-determine which value occurs twice and return it.
-
-
-
-
-
+# the method accepts a unordered array.
+# 1 value in the array occurs twice.
+# all other values occurs exactly once.
+# determine which value occurs twice and return it.
 
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
