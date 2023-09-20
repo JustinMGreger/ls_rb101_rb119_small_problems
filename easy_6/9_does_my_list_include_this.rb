c6f671cc63b22786ad06febc1b4735abdfe148c0
Justin Greger <justingreger@gmail.com>
2023-09-19 21:45:35 -0400

    , false if it is not.

--- easy_6/9_does_my_list_include_this.rb
+++ easy_6/9_does_my_list_include_this.rb
@@ -27,7 +27,7 @@
 # include?(an Array and a search value)
 # include? returns true if the search value is in the array
 # include? returns false if the search value is not in the array.
-, false if it is not. You may not use the Array#include? method in your solution.
+You may not use the Array#include? method in your solution.
 
 
 
