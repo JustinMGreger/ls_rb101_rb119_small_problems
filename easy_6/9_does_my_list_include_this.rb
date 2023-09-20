476f8e1bbc6183ff5d441888fd66641558c4fcd4
Justin Greger <justingreger@gmail.com>
2023-09-19 21:05:59 -0400

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for “[Understand the] Problem,

--- easy_6/9_does_my_list_include_this.rb
+++ easy_6/9_does_my_list_include_this.rb
@@ -13,8 +13,9 @@ p include?([], 3) == false
 p include?([nil], nil) == true
 p include?([], nil) == false
 
-PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
-PEDAC stands for “[Understand the] Problem, 
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for “[Understand the] Problem, 
+
 Identify expected input: 
 output:
 Make the requirements explicit Identify rules:
