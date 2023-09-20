95888c1883eb40c6d846b8bdfd05690ce05701be
Justin Greger <justingreger@gmail.com>
2023-09-20 19:22:09 -0400

    # 3. if search_value is in array_parameter return true.

--- easy_6/9_does_my_list_include_this.rb
+++ easy_6/9_does_my_list_include_this.rb
@@ -51,9 +51,7 @@
 # Algorithm, Steps for converting input to output:
 # 1. the include? method accepts the array_parameter and the search_value.
 # 2. determine if the search_value is in the array_parameter.
-
-  if search_value is in array_parameter
-    true
+# 3. if search_value is in array_parameter return true.
   else
     false
   end
