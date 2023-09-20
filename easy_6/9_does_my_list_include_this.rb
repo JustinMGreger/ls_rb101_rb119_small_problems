e23f78dfa1a4c5c40abf8198ff51048a6206e500
Justin Greger <justingreger@gmail.com>
2023-09-19 21:44:42 -0400

    # include? returns true if the search value is in the array

--- easy_6/9_does_my_list_include_this.rb
+++ easy_6/9_does_my_list_include_this.rb
@@ -25,9 +25,9 @@
 
 # Make the requirements explicit Identify rules:
 # include?(an Array and a search value)
+# include? returns true if the search value is in the array
 
-
- This method should return true if the search value is in the array, false if it is not. You may not use the Array#include? method in your solution.
+, false if it is not. You may not use the Array#include? method in your solution.
 
 
 
