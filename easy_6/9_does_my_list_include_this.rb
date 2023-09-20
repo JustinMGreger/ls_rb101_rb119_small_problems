9504b4b53a3b959325a8ef3bb9b08758b1095a81
Justin Greger <justingreger@gmail.com>
2023-09-19 21:59:56 -0400

    # 1. the include? method accepts the array_parameter and the search_value.

--- easy_6/9_does_my_list_include_this.rb
+++ easy_6/9_does_my_list_include_this.rb
@@ -49,6 +49,10 @@
 # array.
 
 # Algorithm, Steps for converting input to output:
+# 1. the include? method accepts the array_parameter and the search_value.
+
+
+
 def include?(array_parameter, search_value)
   if search_value is in array_parameter
     true
