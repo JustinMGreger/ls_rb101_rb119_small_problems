ddf26f1ab6ae440c01f05f703822684284d3d537
Justin Greger <justingreger@gmail.com>
2023-09-19 21:08:04 -0400

    # p include?([1, 2, 3, 4, 5], 3) == true

--- easy_6/9_does_my_list_include_this.rb
+++ easy_6/9_does_my_list_include_this.rb
@@ -7,11 +7,11 @@
 # You may not use the Array#include? method in your solution.
 
 # tests:
-p include?([1, 2, 3, 4, 5], 3) == true
-p include?([1, 2, 3, 4, 5], 6) == false
-p include?([], 3) == false
-p include?([nil], nil) == true
-p include?([], nil) == false
+# p include?([1, 2, 3, 4, 5], 3) == true
+# p include?([1, 2, 3, 4, 5], 6) == false
+# p include?([], 3) == false
+# p include?([nil], nil) == true
+# p include?([], nil) == false
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem, 
