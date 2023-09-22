13e3c41df0e77668b3dd19fb62e055363392b3c6
Justin Greger <justingreger@gmail.com>
2023-09-22 17:51:11 -0400

    # Do

--- easy_6/10_mutation.rb
+++ easy_6/10_mutation.rb
@@ -2,6 +2,7 @@
 
 # What will the following code print,
 # and why?
+# Don't run the code until you have tried to answer.
 
 # array1 = %w(Moe Larry Curly Shemp Harpo Chico Groucho Zeppo)
 # this is the same as:
@@ -12,7 +13,7 @@ array2 = []
 # this is the same as:
 # reason:
 
-# Don't run the code until you have tried to answer.
+
 array1.each { |value| array2 << value }
 array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
 puts array2
