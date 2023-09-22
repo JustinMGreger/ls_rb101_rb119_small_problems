2850b5177ba5184b06edbc5c052260015832cae8
Justin Greger <justingreger@gmail.com>
2023-09-22 17:57:08 -0400

    array2 = []

--- easy_6/10_mutation.rb
+++ easy_6/10_mutation.rb
@@ -10,14 +10,10 @@
 # reason: %w() is the same as ('string')
 
 array2 = []
-# this is the same as:
-array2 = []
-# reason:
-array2 = []
 
 array1.each { |value| array2 << value }
-# this is the same as:
-# reason:
+# this is means:
+
 
 array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
 # this is the same as:
