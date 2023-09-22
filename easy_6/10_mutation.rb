cb0459d8656a34a7b7318f10ecabfc723494ea99
Justin Greger <justingreger@gmail.com>
2023-09-22 17:47:44 -0400

    deleted array1 = %w(Moe Larry Curly Shemp Harpo Chico Groucho Zeppo)

--- easy_6/10_mutation.rb
+++ easy_6/10_mutation.rb
@@ -13,7 +13,6 @@
 
 
 # Don't run the code until you have tried to answer.
-array1 = %w(Moe Larry Curly Shemp Harpo Chico Groucho Zeppo)
 array2 = []
 array1.each { |value| array2 << value }
 array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
