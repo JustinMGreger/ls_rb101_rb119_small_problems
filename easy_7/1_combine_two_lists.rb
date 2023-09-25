15dde05a4075e0918cb2106dce9266710f4b25ac
Justin Greger <justingreger@gmail.com>
2023-09-25 19:22:54 -0400

    # Write a method

--- easy_7/1_combine_two_lists.rb
+++ easy_7/1_combine_two_lists.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+# Write a method that combines two Arrays passed in as arguments,
+# and returns a new Array that contains all elements from both Array arguments,
+# with the elements taken in alternation.
+
+# You may assume that both input Arrays are non-empty,
+# and that they have the same number of elements.
+
+# Example:
