bb3303a171d447012e0aaeca03db3bafcd4fec46
Justin Greger <justingreger@gmail.com>
2023-09-25 19:32:18 -0400

    # Examples / Test Cases, Validate understanding of the problem:

--- easy_7/1_combine_two_lists.rb
+++ easy_7/1_combine_two_lists.rb
@@ -17,7 +17,10 @@ Identify expected input:
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
-Examples / Test Cases, Validate understanding of the problem:
+
+# Examples / Test Cases, Validate understanding of the problem:
+interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
+
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
