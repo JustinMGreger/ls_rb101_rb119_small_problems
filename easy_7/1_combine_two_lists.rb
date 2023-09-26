15306438f4608bacd979329f0fbcd5f7c189ec5f
Justin Greger <justingreger@gmail.com>
2023-09-25 21:58:51 -0400

    deleted trailing white space

--- easy_7/1_combine_two_lists.rb
+++ easy_7/1_combine_two_lists.rb
@@ -13,7 +13,7 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem,
 
-# Identify expected input: 
+# Identify expected input:
 # two non-empty Arrays that have the same number of elements.
 
 # output:
