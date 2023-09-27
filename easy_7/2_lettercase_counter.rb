d5baa5805f09e7ecfb6c35596c53c8366740da95
Justin Greger <justingreger@gmail.com>
2023-09-27 19:25:38 -0400

    # p

--- easy_7/2_lettercase_counter.rb
+++ easy_7/2_lettercase_counter.rb
@@ -29,10 +29,10 @@ Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
 
 # Examples / Test Cases, Validate understanding of the problem:
-p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
-p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
-p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
-p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
+# p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+# p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+# p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+# p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
 
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
