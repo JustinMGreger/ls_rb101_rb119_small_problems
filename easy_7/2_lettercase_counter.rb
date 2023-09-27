ae5aaab58ae232d2c2cf780e6930e7c894c8d70c
Justin Greger <justingreger@gmail.com>
2023-09-27 17:38:29 -0400

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for “[Understand the] Problem,

--- easy_7/2_lettercase_counter.rb
+++ easy_7/2_lettercase_counter.rb
@@ -12,8 +12,9 @@ p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
 p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
 p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
 
-PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
-PEDAC stands for “[Understand the] Problem, 
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for “[Understand the] Problem,
+
 Identify expected input: 
 output:
 Make the requirements explicit Identify rules:
