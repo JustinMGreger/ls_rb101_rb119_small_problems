2d569929e9472499c8aa51e6070d151513ffa15e
Justin Greger <justingreger@gmail.com>
2023-09-26 21:01:01 -0400

    # See if you can rewrite interleave to use zip.

--- easy_7/1_combine_two_lists.rb
+++ easy_7/1_combine_two_lists.rb
@@ -75,7 +75,7 @@
 # and more flexible.
 # In fact,
 # interleave can be implemented in terms of zip and one other method from the Array class.
-See if you can rewrite interleave to use zip.
+# See if you can rewrite interleave to use zip.
 
 # tests:
 p interleave([1, 2, 3], %w[a b c]) == [1, 'a', 2, 'b', 3, 'c']
