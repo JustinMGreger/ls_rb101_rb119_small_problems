0b26dd7d9c1f48a78f95c5634f457a1a0b7ac33f
Justin Greger <justingreger@gmail.com>
2023-09-27 19:26:06 -0400

    # p

--- easy_7/2_lettercase_counter.rb
+++ easy_7/2_lettercase_counter.rb
@@ -7,10 +7,10 @@
 # and one represents the number of characters that are neither.
 
 # Examples
-p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
-p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
-p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
-p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
+# p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+# p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+# p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+# p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem,
