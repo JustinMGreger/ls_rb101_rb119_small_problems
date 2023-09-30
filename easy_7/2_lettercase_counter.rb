53ed044bbc805eb3ba32e6639fccb2e0edc096cb
Justin Greger <justingreger@gmail.com>
2023-09-29 20:18:47 -0400

    # LS Solution 1:

--- easy_7/2_lettercase_counter.rb
+++ easy_7/2_lettercase_counter.rb
@@ -68,24 +68,24 @@
 # end
 
 # LS Solution 1:
-UPPERCASE_CHARS = ('A'..'Z').to_a
-LOWERCASE_CHARS = ('a'..'z').to_a
+# UPPERCASE_CHARS = ('A'..'Z').to_a
+# LOWERCASE_CHARS = ('a'..'z').to_a
 
-def letter_case_count(string)
-  counts = { lowercase: 0, uppercase: 0, neither: 0 }
+# def letter_case_count(string)
+#   counts = { lowercase: 0, uppercase: 0, neither: 0 }
 
-  string.chars.each do |char|
-    if UPPERCASE_CHARS.include?(char)
-      counts[:uppercase] += 1
-    elsif LOWERCASE_CHARS.include?(char)
-      counts[:lowercase] += 1
-    else
-      counts[:neither] += 1
-    end
-  end
+#   string.chars.each do |char|
+#     if UPPERCASE_CHARS.include?(char)
+#       counts[:uppercase] += 1
+#     elsif LOWERCASE_CHARS.include?(char)
+#       counts[:lowercase] += 1
+#     else
+#       counts[:neither] += 1
+#     end
+#   end
 
-  counts
-end
+#   counts
+# end
 
 # tests:
 p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
