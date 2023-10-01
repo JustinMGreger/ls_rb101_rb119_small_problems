02b3e7af9c8fdde217188388eccd24a2eda1a4c9
Justin Greger <justingreger@gmail.com>
2023-09-30 21:38:53 -0400

    p word_cap

--- easy_7/3_capitalize_words.rb
+++ easy_7/3_capitalize_words.rb
@@ -8,3 +8,6 @@
 # You may assume that words are any sequence of non-blank characters.
 
 # Examples
+p word_cap('four score and seven') == 'Four Score And Seven'
+p word_cap('the javaScript language') == 'The Javascript Language'
+p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
