9d5014048e7e2d85f839d0fa8330297bec4a7d4c
Justin Greger <justingreger@gmail.com>
2023-10-01 20:30:30 -0400

    # You may assume that words are any sequence of non-blank characters.

--- easy_7/3_capitalize_words.rb
+++ easy_7/3_capitalize_words.rb
@@ -25,13 +25,7 @@
 # Make the requirements explicit Identify rules:
 def word_cap(a single String argument)
  returns a new single String with the first character of every word capitalized and all other letters lowercase.
-
-You may assume that words are any sequence of non-blank characters.
-
-
-
-
-
+# You may assume that words are any sequence of non-blank characters.
 
 Mental model of the problem (optional):
 
