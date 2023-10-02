2ab43a028ceddc5f593ad28e159d983e4edcc4b1
Justin Greger <justingreger@gmail.com>
2023-10-01 21:48:33 -0400

    deleted empty lines

--- easy_7/3_capitalize_words.rb
+++ easy_7/3_capitalize_words.rb
@@ -58,12 +58,6 @@ def word_cap(string_parameter)
   combined_string
 end
 
-
-
-
-
-
-
 # tests:
 p word_cap('four score and seven') == 'Four Score And Seven'
 p word_cap('the javaScript language') == 'The Javascript Language'
