4ca8adc9734b92784936689b75a690f58a9a2aaf
Justin Greger <justingreger@gmail.com>
2023-10-03 21:32:25 -0400

    # 2. return a new string in which every uppercase letter is replaced by its lowercase version # and every lowercase letter by its uppercase version.

--- easy_7/4_swap_case.rb
+++ easy_7/4_swap_case.rb
@@ -26,8 +26,10 @@ p swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
 
 # Make the requirements explicit Identify rules:
 # 1. Write a method that takes a string as an argument.
+# 2. return a new string in which every uppercase letter is replaced by its lowercase version
+# and every lowercase letter by its uppercase version.
 
-and returns a new string in which every uppercase letter is replaced by its lowercase version, and every lowercase letter by its uppercase version. All other characters should be unchanged.
+All other characters should be unchanged.
 
 You may not use String#swapcase; write your own version of this method.
 
