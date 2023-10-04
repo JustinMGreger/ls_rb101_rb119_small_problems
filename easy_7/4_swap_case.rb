358d7bb52b22f1224c7858cbd0e3a4fade5d9ffd
Justin Greger <justingreger@gmail.com>
2023-10-04 19:34:16 -0400

    # LS Solution:

--- easy_7/4_swap_case.rb
+++ easy_7/4_swap_case.rb
@@ -58,6 +58,20 @@
 #   end
 # end
 
+# LS Solution:
+def swapcase(string)
+  characters = string.chars.map do |char|
+    if char =~ /[a-z]/
+      char.upcase
+    elsif char =~ /[A-Z]/
+      char.downcase
+    else
+      char
+    end
+  end
+  characters.join
+end
+
 # test:
 p swapcase('CamelCase') == 'cAMELcASE'
 p swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
