f8e220efa64cce45226d9a1b1c74ef63ebc535db
Justin Greger <justingreger@gmail.com>
2023-10-04 17:57:02 -0400

    def swapcase(string_parameter)

--- easy_7/4_swap_case.rb
+++ easy_7/4_swap_case.rb
@@ -52,7 +52,10 @@
 # the result of the modified string_parameter is assigned to the new_string variable which is returned.
 
 # and Code.” Implementation of Algorithm:
-
+def swapcase(string_parameter)
+in the string_parameter /[A-Z]/ is replaced by /[a-z]/
+in the string_parameter /[a-z]/ is replaced by /[A-Z]/ 
+the result of the modified string_parameter is assigned to the new_string variable which is returned.
 
 
 
