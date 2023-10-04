7b98ebc027f9217ac81cc8e2f144672c283152c6
Justin Greger <justingreger@gmail.com>
2023-10-04 18:11:08 -0400

    hilow_string = string_parameter.gsub(/[A-Z]/, /[a-z]/)

--- easy_7/4_swap_case.rb
+++ easy_7/4_swap_case.rb
@@ -53,7 +53,7 @@
 
 # and Code.” Implementation of Algorithm:
 def swapcase(string_parameter)
-  string_parameter.gsub(/[A-Z]/, /[a-z]/)
+  hilow_string = string_parameter.gsub(/[A-Z]/, /[a-z]/)
   string_parameter.gsub(/[a-z]/, /[A-Z]/)
   the result of the modified string_parameter is assigned to the new_string variable which is returned.
 
