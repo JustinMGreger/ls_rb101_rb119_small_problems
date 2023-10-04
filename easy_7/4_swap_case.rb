a04a47e3142e2ab8975009bef776bd7544ed5bbb
Justin Greger <justingreger@gmail.com>
2023-10-03 20:02:20 -0400

    # Swap Case

--- easy_7/4_swap_case.rb
+++ easy_7/4_swap_case.rb
@@ -1,1 +1,9 @@
 # frozen_string_literal: true
+
+# Swap Case
+
+Write a method that takes a string as an argument and returns a new string in which every uppercase letter is replaced by its lowercase version, and every lowercase letter by its uppercase version. All other characters should be unchanged.
+
+You may not use String#swapcase; write your own version of this method.
+
+Example:
