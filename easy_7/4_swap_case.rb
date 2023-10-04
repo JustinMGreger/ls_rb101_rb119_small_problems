959856a87df526009d5ddebb29894992de50f478
Justin Greger <justingreger@gmail.com>
2023-10-03 20:04:08 -0400

    # and returns a new string in which every uppercase letter is replaced by its lowercase version,

--- easy_7/4_swap_case.rb
+++ easy_7/4_swap_case.rb
@@ -3,7 +3,8 @@
 # Swap Case
 
 # Write a method that takes a string as an argument
-and returns a new string in which every uppercase letter is replaced by its lowercase version, and every lowercase letter by its uppercase version. All other characters should be unchanged.
+# and returns a new string in which every uppercase letter is replaced by its lowercase version,
+ and every lowercase letter by its uppercase version. All other characters should be unchanged.
 
 You may not use String#swapcase; write your own version of this method.
 
