ab7301eed7400c3b945cf7515cdec5c951dc3162
Justin Greger <justingreger@gmail.com>
2023-10-04 21:50:36 -0400

    # Make the requirements explicit Identify rules:

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -25,7 +25,26 @@
 # Characters that are not letters should not be changed,
 # but count as characters when switching between upper and lowercase.
 
-Make the requirements explicit Identify rules:
+# Make the requirements explicit Identify rules:
+# 1. Write a method that takes a String as an argument
+
+,
+ and returns a new String that contains the original value using a staggered capitalization scheme
+ in which every other character is capitalized, and the remaining characters are lowercase.
+   Characters that are not letters should not be changed, but count as characters when
+    switching between upper and lowercase.
+
+
+
+
+
+
+
+
+
+
+
+
 Mental model of the problem (optional):
 
 # Examples / Test Cases, Validate understanding of the problem:
