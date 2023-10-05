2c1571e1b98c05d6d68233d97de4f8eac722f149
Justin Greger <justingreger@gmail.com>
2023-10-04 21:09:00 -0400

    p staggered_case

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -8,6 +8,6 @@
 # but count as characters when switching between upper and lowercase.
 
 # Example:
-staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
-staggered_case('ALL_CAPS') == 'AlL_CaPs'
-staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
