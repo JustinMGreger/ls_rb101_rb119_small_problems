018e2d4f1222189207e22eb2c8aeafcbf9dec227
Justin Greger <justingreger@gmail.com>
2023-10-04 21:51:12 -0400

    # 1. Write a method that takes a String as an argument. # 2. return a new String.

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -26,10 +26,11 @@
 # but count as characters when switching between upper and lowercase.
 
 # Make the requirements explicit Identify rules:
-# 1. Write a method that takes a String as an argument
-
-,
- and returns a new String that contains the original value using a staggered capitalization scheme
+# 1. Write a method that takes a String as an argument.
+# 2. return a new String.
+ 
+ 
+ that contains the original value using a staggered capitalization scheme
  in which every other character is capitalized, and the remaining characters are lowercase.
    Characters that are not letters should not be changed, but count as characters when
     switching between upper and lowercase.
