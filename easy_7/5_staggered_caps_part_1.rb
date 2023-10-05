2c5fd915d2b9bbdc599e23e1ba9b2e80538041b9
Justin Greger <justingreger@gmail.com>
2023-10-04 21:52:05 -0400

    # 3. the new string contains the original value.

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -28,9 +28,10 @@
 # Make the requirements explicit Identify rules:
 # 1. Write a method that takes a String as an argument.
 # 2. return a new String.
- 
- 
- that contains the original value using a staggered capitalization scheme
+# 3. the new string contains the original value.
+
+
+using a staggered capitalization scheme
  in which every other character is capitalized, and the remaining characters are lowercase.
    Characters that are not letters should not be changed, but count as characters when
     switching between upper and lowercase.
