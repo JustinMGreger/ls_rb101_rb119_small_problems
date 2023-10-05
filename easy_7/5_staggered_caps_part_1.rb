1b45ee7acaad9c69fe1428fce486e4745b781f42
Justin Greger <justingreger@gmail.com>
2023-10-04 21:37:07 -0400

    # Identify expected input: # a String parameter.

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -15,7 +15,9 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# a String parameter.
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
