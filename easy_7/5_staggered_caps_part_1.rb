8b7d85c07681fbe2462723413fab6348ec66279b
Justin Greger <justingreger@gmail.com>
2023-10-04 21:01:04 -0400

    # Write a method that takes a String as an argument,

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+# Write a method that takes a String as an argument,
+# and returns a new String that contains the original value
+# using a staggered capitalization scheme in which every other character is capitalized,
+# and the remaining characters are lowercase.
+# Characters that are not letters should not be changed,
+# but count as characters when switching between upper and lowercase.
+
+# Example:
