865fe6803953ab80f67a0c72449c0caf256b363b
Justin Greger <justingreger@gmail.com>
2023-10-06 19:09:31 -0400

    new_string.push(new_char)

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -61,6 +61,7 @@ def staggered_case(string_parameter)
   
   string_parameter.chars.each_with_index do |char, index|
     new_char = index.even? ? char.upcase : char.downcase
+    new_string.push(new_char)
     else
       new_string << char
     end
