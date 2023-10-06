3f1839f6cf08ff0efd13eda9c9d27b26f5a972fe
Justin Greger <justingreger@gmail.com>
2023-10-06 16:42:09 -0400

    string_array = string_parameter.split

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -57,6 +57,7 @@
 
 # and Code.” Implementation of Algorithm:
 def staggered_case(string_parameter)
+  string_array = string_parameter.split
   sort through each character in the string_parameter
   if every other character is a /[A-Za-z]/ character then replace with a /[A-Z]/ character.
   combine this into a new_string varible.
