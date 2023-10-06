3792c9d052d24da40165a10797c10b2adb2dade1
Justin Greger <justingreger@gmail.com>
2023-10-06 16:44:45 -0400

    if |block| ==

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -58,8 +58,11 @@
 # and Code.” Implementation of Algorithm:
 def staggered_case(string_parameter)
   string_array = string_parameter.split
+
   sort through each character in the string_parameter
-  if every other character is a /[A-Za-z]/ character then replace with a /[A-Z]/ character.
+
+  if |block| ==
+     every other character is a /[A-Za-z]/ character then replace with a /[A-Z]/ character.
   combine this into a new_string varible.
   return new_string 
 end
