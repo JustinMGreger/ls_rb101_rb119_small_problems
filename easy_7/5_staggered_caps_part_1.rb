277f4fece8f50f38ac5197f677a079725065418c
Justin Greger <justingreger@gmail.com>
2023-10-07 18:54:50 -0400

    # Further Exploration

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -82,6 +82,13 @@
 #   result
 # end
 
+# Further Exploration
+
+# Can you modify this method so the caller can request that the first character be downcased rather than upcased?
+# If the first character is downcased, then the second character should be upcased, and so on.
+
+# Hint: Use a keyword argument.
+
 # tests:
 p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
 p staggered_case('ALL_CAPS') == 'AlL_CaPs'
