486bc651715aaf6c22fb80f01a8b3093e8ef342a
Justin Greger <justingreger@gmail.com>
2023-10-07 18:53:48 -0400

    # LS Solution:

--- easy_7/5_staggered_caps_part_1.rb
+++ easy_7/5_staggered_caps_part_1.rb
@@ -68,19 +68,19 @@
 # end
 
 # LS Solution:
-def staggered_case(string)
-  result = ''
-  need_upper = true
-  string.chars.each do |char|
-    if need_upper
-      result += char.upcase
-    else
-      result += char.downcase
-    end
-    need_upper = !need_upper
-  end
-  result
-end
+# def staggered_case(string)
+#   result = ''
+#   need_upper = true
+#   string.chars.each do |char|
+#     if need_upper
+#       result += char.upcase
+#     else
+#       result += char.downcase
+#     end
+#     need_upper = !need_upper
+#   end
+#   result
+# end
 
 # tests:
 p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
