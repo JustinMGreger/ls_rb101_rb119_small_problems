a40148824459574b1dd3ee27e108fd85de734381
Justin Greger <justingreger@gmail.com>
2023-10-07 21:10:18 -0400

    # Modify the method

--- easy_7/6_staggered_caps_part_2.rb
+++ easy_7/6_staggered_caps_part_2.rb
@@ -1,1 +1,8 @@
 # frozen_string_literal: true
+
+# Modify the method from the previous exercise so it ignores non-alphabetic characters
+# when determining whether it should uppercase or lowercase each letter.
+# The non-alphabetic characters should still be included in the return value;
+# they just don't count when toggling the desired case.
+
+# Example:
