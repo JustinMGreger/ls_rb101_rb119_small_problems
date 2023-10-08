05ef27b0a9f932f40d7259e875bc2f28507ba163
Justin Greger <justingreger@gmail.com>
2023-10-07 21:15:04 -0400

    # LS Solution from previous exercise:

--- easy_7/6_staggered_caps_part_2.rb
+++ easy_7/6_staggered_caps_part_2.rb
@@ -10,6 +10,7 @@ p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
 p staggered_case('ALL CAPS') == 'AlL cApS'
 p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
 
+# LS Solution from previous exercise:
 def staggered_case(string)
   result = ''
   need_upper = true
