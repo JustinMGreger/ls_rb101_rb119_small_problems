b336b2d32183779a7f64c63e0377e12431cfe732
Justin Greger <justingreger@gmail.com>
2023-10-07 21:10:53 -0400

    p staggered_case

--- easy_7/6_staggered_caps_part_2.rb
+++ easy_7/6_staggered_caps_part_2.rb
@@ -6,3 +6,6 @@
 # they just don't count when toggling the desired case.
 
 # Example:
+p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
+p staggered_case('ALL CAPS') == 'AlL cApS'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
