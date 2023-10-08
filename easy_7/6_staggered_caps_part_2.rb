a1c5fdf3f6c7161fb7293429fd08b2f1f77d653c
Justin Greger <justingreger@gmail.com>
2023-10-07 21:16:13 -0400

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for “[Understand the] Problem,

--- easy_7/6_staggered_caps_part_2.rb
+++ easy_7/6_staggered_caps_part_2.rb
@@ -25,8 +25,9 @@ def staggered_case(string)
   result
 end
 
-PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
-PEDAC stands for “[Understand the] Problem,
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for “[Understand the] Problem,
+
 Identify expected input:
 output:
 Make the requirements explicit Identify rules:
