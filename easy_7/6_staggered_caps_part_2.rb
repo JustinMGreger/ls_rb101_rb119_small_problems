f0d3a46c7d72299ca5149e12a3483e1dc71bd9c4
Justin Greger <justingreger@gmail.com>
2023-10-09 21:43:57 -0400

    # LS Solution:

--- easy_7/6_staggered_caps_part_2.rb
+++ easy_7/6_staggered_caps_part_2.rb
@@ -89,6 +89,25 @@
 #   char =~ /[A-Za-z]/ ? !need_upper : need_upper
 # end
 
+# LS Solution:
+def staggered_case(string)
+  result = ''
+  need_upper = true
+  string.chars.each do |char|
+    if char =~ /[a-z]/i
+      if need_upper
+        result += char.upcase
+      else
+        result += char.downcase
+      end
+      need_upper = !need_upper
+    else
+      result += char
+    end
+  end
+  result
+end
+
 # tests:
 p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
 p staggered_case('ALL CAPS') == 'AlL cApS'
