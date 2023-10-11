8e280c3508a2dd10fcce1310f478b09085f06692
Justin Greger <justingreger@gmail.com>
2023-10-11 19:58:12 -0400

    # modified solution:

--- easy_7/6_staggered_caps_part_2.rb
+++ easy_7/6_staggered_caps_part_2.rb
@@ -144,7 +144,7 @@ def toggle_case_for_all(string)
   end.join
 end
 
-# Provided tests:
+# tests:
 p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
 p staggered_case('ALL CAPS') == 'AlL cApS'
 p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
