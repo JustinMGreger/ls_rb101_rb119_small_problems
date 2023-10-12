2564a229f53e612d103c1c9fb80883446e2dc39e
Justin Greger <justingreger@gmail.com>
2023-10-12 19:30:15 -0400

    # Write a method that takes an Array of integers as input,

--- easy_7/7_multiplicative_average.rb
+++ easy_7/7_multiplicative_average.rb
@@ -2,6 +2,10 @@
 
 # Multiplicative Average
 
-Write a method that takes an Array of integers as input, multiplies all the numbers together, divides the result by the number of entries in the Array, and then prints the result rounded to 3 decimal places. Assume the array is non-empty.
+# Write a method that takes an Array of integers as input,
+# multiplies all the numbers together,
+# divides the result by the number of entries in the Array,
+# and then prints the result rounded to 3 decimal places.
+# Assume the array is non-empty.
 
-Examples:
+# Examples:
