bd961f970f59de24cc72dcffdd53ceed31285623
Justin Greger <justingreger@gmail.com>
2023-10-12 19:25:49 -0400

    # Multiplicative Average

--- easy_7/7_multiplicative_average.rb
+++ easy_7/7_multiplicative_average.rb
@@ -1,1 +1,7 @@
 # frozen_string_literal: true
+
+# Multiplicative Average
+
+Write a method that takes an Array of integers as input, multiplies all the numbers together, divides the result by the number of entries in the Array, and then prints the result rounded to 3 decimal places. Assume the array is non-empty.
+
+Examples:
