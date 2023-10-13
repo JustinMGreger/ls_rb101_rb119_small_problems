fcbbd8cdbd23be98f121bf231d487156e9a5ae17
Justin Greger <justingreger@gmail.com>
2023-10-12 20:34:04 -0400

    # Identify expected input: # a non-empty Array of integers.

--- easy_7/7_multiplicative_average.rb
+++ easy_7/7_multiplicative_average.rb
@@ -16,7 +16,9 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# a non-empty Array of integers.
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
