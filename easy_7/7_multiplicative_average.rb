7109503bb94b9e1fd80bd27df75e71e49bdf95f4
Justin Greger <justingreger@gmail.com>
2023-10-12 21:11:21 -0400

    # 2. multiplies all the numbers together.

--- easy_7/7_multiplicative_average.rb
+++ easy_7/7_multiplicative_average.rb
@@ -24,8 +24,8 @@
 
 # Make the requirements explicit Identify rules:
 # 1. Write a method that takes an Array of integers as input.
-
-, multiplies all the numbers together, divides the result by the number of entries in the Array, and then prints the result rounded to 3 decimal places. Assume the array is non-empty.
+# 2. multiplies all the numbers together.
+divides the result by the number of entries in the Array, and then prints the result rounded to 3 decimal places. Assume the array is non-empty.
 
 
 
