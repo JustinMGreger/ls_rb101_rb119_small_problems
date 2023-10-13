74fa3caf08dba11930ae197a395e1af8a156746f
Justin Greger <justingreger@gmail.com>
2023-10-12 20:17:32 -0400

    # p show_multiplicative_average

--- easy_7/7_multiplicative_average.rb
+++ easy_7/7_multiplicative_average.rb
@@ -9,9 +9,9 @@
 # Assume the array is non-empty.
 
 # Examples:
-p show_multiplicative_average([3, 5]) == The result is 7.500
-p show_multiplicative_average([6]) == The result is 6.000
-p show_multiplicative_average([2, 5, 7, 11, 13, 17]) == The result is 28361.667
+# p show_multiplicative_average([3, 5]) == The result is 7.500
+# p show_multiplicative_average([6]) == The result is 6.000
+# p show_multiplicative_average([2, 5, 7, 11, 13, 17]) == The result is 28361.667
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem,
