6ef40ad6a7e1e8bfa5e903dc8017913425e5baf6
Justin Greger <justingreger@gmail.com>
2023-10-12 20:15:59 -0400

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for “[Understand the] Problem,

--- easy_7/7_multiplicative_average.rb
+++ easy_7/7_multiplicative_average.rb
@@ -12,3 +12,15 @@
 p show_multiplicative_average([3, 5]) == The result is 7.500
 p show_multiplicative_average([6]) == The result is 6.000
 p show_multiplicative_average([2, 5, 7, 11, 13, 17]) == The result is 28361.667
+
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for “[Understand the] Problem,
+
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
