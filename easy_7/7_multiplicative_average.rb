e73a0f9055b76866a67760c8452413ad3ef73eee
Justin Greger <justingreger@gmail.com>
2023-10-13 21:42:43 -0400

    rounded_quotient variable = quotient_result.round(3)

--- easy_7/7_multiplicative_average.rb
+++ easy_7/7_multiplicative_average.rb
@@ -57,7 +57,7 @@ def show_multiplicative_average(array_parameter)
   number_elements = array_parameter.count
   product_result variable =   array_parameter.inject(1) { |accumulator, n| accumulator * n }
   quotient_result = product_result.to_f /number_elements
-  rounded_quotient variable = the result of rounding the quotient_result to 3 decimal places.
+  rounded_quotient variable = quotient_result.round(3)
   print "The result is #{rounded_quotient}"
 
 
