0067861a87b8d22a515e7d91ad286695172693ec
Justin Greger <justingreger@gmail.com>
2023-10-13 21:47:18 -0400

    quotient_result = product_result.to_f / number_elements

--- easy_7/7_multiplicative_average.rb
+++ easy_7/7_multiplicative_average.rb
@@ -56,7 +56,7 @@
 def show_multiplicative_average(array_parameter)
   number_elements = array_parameter.count
   product_result variable = array_parameter.inject(1) { |accumulator, n| accumulator * n }
-  quotient_result = product_result.to_f /number_elements
+  quotient_result = product_result.to_f / number_elements
   rounded_quotient variable = quotient_result.round(3)
   "The result is #{rounded_quotient}"
 end
