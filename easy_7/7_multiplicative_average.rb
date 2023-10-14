19ce1274da42657b3c785ad6dd2001a459d5aa13
Justin Greger <justingreger@gmail.com>
2023-10-14 19:36:57 -0400

    def show_multiplicative_average(numbers)

--- easy_7/7_multiplicative_average.rb
+++ easy_7/7_multiplicative_average.rb
@@ -62,6 +62,12 @@
 # end
 
 # LS Solution:
+def show_multiplicative_average(numbers)
+  product = 1.to_f
+  numbers.each { |number| product *= number }
+  average = product / numbers.size
+  puts "The result is #{format('%.3f', average)}"
+end
 
 # tests:
 p show_multiplicative_average([3, 5]) == 'The result is 7.500'
