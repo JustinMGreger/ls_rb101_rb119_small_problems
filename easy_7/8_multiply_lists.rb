2df272db92949b1b795a3f07873b9260c5c0a80e
Justin Greger <justingreger@gmail.com>
2023-10-14 21:07:43 -0400

    # Write a method that takes two Array arguments in which each Array contains a list of numbers,

--- easy_7/8_multiply_lists.rb
+++ easy_7/8_multiply_lists.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+# Write a method that takes two Array arguments in which each Array contains a list of numbers,
+and returns a new Array that contains the product of each pair of numbers from the arguments that have the same index. You may assume that the arguments contain the same number of elements.
+
+Examples:
