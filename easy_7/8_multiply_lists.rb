ece5bac84125b8682e4f965f5a6b3ea687eec1a5
Justin Greger <justingreger@gmail.com>
2023-10-15 19:41:53 -0400

    deleted Write a method that takes two Array arguments

--- easy_7/8_multiply_lists.rb
+++ easy_7/8_multiply_lists.rb
@@ -27,7 +27,6 @@
 # Mental model of the problem (optional):
 # def multiply_list(array_one, array_two)
 You may assume that the arguments contain the same number of elements.
-Write a method that takes two Array arguments
 each Array contains a list of numbers.
 return a new Array that contains the product of each pair of numbers from the arguments that have the same index.
 
