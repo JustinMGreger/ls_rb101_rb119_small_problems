7847839f8d9e97cbaaa2478f4e0032d3c0c49245
Justin Greger <justingreger@gmail.com>
2023-10-14 21:09:26 -0400

    # Examples:

--- easy_7/8_multiply_lists.rb
+++ easy_7/8_multiply_lists.rb
@@ -5,4 +5,4 @@
 # that contains the product of each pair of numbers from the arguments that have the same index.
 # You may assume that the arguments contain the same number of elements.
 
-Examples:
+# Examples:
