225dafe1fad45a25599e72cab292d602e66546fe
Justin Greger <justingreger@gmail.com>
2023-10-14 21:08:50 -0400

    # that contains the product of each pair of numbers from the arguments that have the same index.

--- easy_7/8_multiply_lists.rb
+++ easy_7/8_multiply_lists.rb
@@ -2,6 +2,7 @@
 
 # Write a method that takes two Array arguments in which each Array contains a list of numbers,
 # and returns a new Array
-that contains the product of each pair of numbers from the arguments that have the same index. You may assume that the arguments contain the same number of elements.
+# that contains the product of each pair of numbers from the arguments that have the same index.
+ You may assume that the arguments contain the same number of elements.
 
 Examples:
