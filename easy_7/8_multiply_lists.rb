c422bddd2a54cc4c9e9ba701f77475b556840c42
Justin Greger <justingreger@gmail.com>
2023-10-14 21:26:06 -0400

    PEDAC

--- easy_7/8_multiply_lists.rb
+++ easy_7/8_multiply_lists.rb
@@ -8,3 +8,14 @@
 # Examples:
 
 p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem,
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
