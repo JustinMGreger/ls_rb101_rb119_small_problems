16f2b48db4230efe17b7096119576d33ce63643d
Justin Greger <justingreger@gmail.com>
2023-10-15 21:48:23 -0400

    # return the new_array varible.

--- easy_7/8_multiply_lists.rb
+++ easy_7/8_multiply_lists.rb
@@ -39,6 +39,7 @@
 # the multiply_list method accepts the (array_one, array_two) parameters.
 # the new_array varible is assigned the product of each pair of numbers from the array_one and array_two parameters
 # that have the same index.
+# return the new_array varible.
 
 # and Code.” Implementation of Algorithm:
 def multiply_list(array_one, array_two)
