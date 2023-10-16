3dc1f464ec164b97ae0a500b26db89d0912df5c7
Justin Greger <justingreger@gmail.com>
2023-10-16 17:38:56 -0400

    # LS Solution:

--- easy_7/8_multiply_lists.rb
+++ easy_7/8_multiply_lists.rb
@@ -46,5 +46,14 @@
 #   array_one.zip(array_two).map { |a, b| a * b }
 # end
 
+# LS Solution:
+def multiply_list(list_1, list_2)
+  products = []
+  list_1.each_with_index do |item, index|
+    products << item * list_2[index]
+  end
+  products
+end
+
 # tests:
 p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
