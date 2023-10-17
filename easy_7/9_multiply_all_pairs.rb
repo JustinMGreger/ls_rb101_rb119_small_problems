139679ac8124e2a0482743df12bd951ae45fd529
Justin Greger <justingreger@gmail.com>
2023-10-17 18:52:38 -0400

    PEDAC

--- easy_7/9_multiply_all_pairs.rb
+++ easy_7/9_multiply_all_pairs.rb
@@ -9,3 +9,14 @@
 
 # Examples:
 p multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem,
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
