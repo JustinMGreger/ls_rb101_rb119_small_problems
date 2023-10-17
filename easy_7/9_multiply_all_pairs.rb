da2230140d795a965e7c955eb2ba5f6dc73e8afb
Justin Greger <justingreger@gmail.com>
2023-10-17 17:53:36 -0400

    # between the elements of the two Arrays. # The results should be sorted by increasing value.
    # You may assume that neither argument is an empty Array.
    
    # Examples:

--- easy_7/9_multiply_all_pairs.rb
+++ easy_7/9_multiply_all_pairs.rb
@@ -2,8 +2,9 @@
 
 # Write a method that takes two Array arguments in which each Array contains a list of numbers,
 # and returns a new Array that contains the product of every pair of numbers that can be formed
-between the elements of the two Arrays. The results should be sorted by increasing value.
+# between the elements of the two Arrays.
+# The results should be sorted by increasing value.
 
-You may assume that neither argument is an empty Array.
+# You may assume that neither argument is an empty Array.
 
-Examples:
+# Examples:
