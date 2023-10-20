c0fd97d4d52bb9d62783cafd106a109182cd913d
Justin Greger <justingreger@gmail.com>
2023-10-19 21:54:39 -0400

    # Algorithm, Steps for converting input to output: # the penultimate method accepts the (string) parameter and is defined as the following: # split the string parameter into an array of words.

--- easy_7/10_the_end_is_near_but_not_here.rb
+++ easy_7/10_the_end_is_near_but_not_here.rb
@@ -35,8 +35,9 @@
 # Data Structure, How we represent data that we will work with when converting the input to output:
 # array.
 
-Algorithm, Steps for converting input to output:
+# Algorithm, Steps for converting input to output:
 # the penultimate method accepts the (string) parameter and is defined as the following:
+# split the string parameter into an array of words.
  the penultimate method returns the next to last word in the string parameter.
 
 
