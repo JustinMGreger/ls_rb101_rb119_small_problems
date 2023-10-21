8d7335ab14902301702ebaa3a963eead687a2ace
Justin Greger <justingreger@gmail.com>
2023-10-20 21:58:39 -0400

    # Identify expected input: # a string of a phrase or sentence.

--- easy_7/10_the_end_is_near_but_not_here.rb
+++ easy_7/10_the_end_is_near_but_not_here.rb
@@ -70,7 +70,9 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# a string of a phrase or sentence.
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
