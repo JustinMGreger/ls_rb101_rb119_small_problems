1a15ef236dbf4d3a990686663dbad5ff7bf44a25
Justin Greger <justingreger@gmail.com>
2023-10-21 19:10:44 -0400

    # check if array is 1 string in total return the string.

--- easy_7/10_the_end_is_near_but_not_here.rb
+++ easy_7/10_the_end_is_near_but_not_here.rb
@@ -84,7 +84,7 @@
 # the middle method accepts the string_parameter and is defined as the following:
 # break the string_parameter into an array of string elements.
 # check if the array is empty and handle that by printing nil.
-# check if array is 1 string in total if so handle that by printing the string 'enter a string with more words'
+# check if array is 1 string in total return the string.
 # check if the array is odd if odd then return the middle string.
 # check if the array is even if even then handle that by returning the 2 middle numbers.
 
