b66c9704a8ac5ca681af0909229587013166452c
Justin Greger <justingreger@gmail.com>
2023-10-20 21:51:04 -0400

    PEDAC

--- easy_7/10_the_end_is_near_but_not_here.rb
+++ easy_7/10_the_end_is_near_but_not_here.rb
@@ -66,3 +66,14 @@
 # How would you handle those edge cases without ignoring them?
 # Write a method that returns the middle word of a phrase or sentence.
 # It should handle all of the edge cases you thought of.
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem,
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
