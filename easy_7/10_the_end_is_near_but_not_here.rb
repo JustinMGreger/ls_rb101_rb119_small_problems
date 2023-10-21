99743a3a79e5868b5a916146ff6a8f4f8ba99e5c
Justin Greger <justingreger@gmail.com>
2023-10-21 19:26:50 -0400

    # Data Structure, How we represent data that we will work with when converting the input to output: # array.

--- easy_7/10_the_end_is_near_but_not_here.rb
+++ easy_7/10_the_end_is_near_but_not_here.rb
@@ -91,7 +91,9 @@
 # Examples / Test Cases, Validate understanding of the problem:
 # p middle('apple orange banana') == 'orange'
 
-Data Structure, How we represent data that we will work with when converting the input to output:
+# Data Structure, How we represent data that we will work with when converting the input to output:
+# array.
+
 Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
 
