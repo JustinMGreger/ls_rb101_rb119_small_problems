eb1f77c68dd8749dfd9131260b4ddc7601b45838
Justin Greger <justingreger@gmail.com>
2023-10-21 21:55:39 -0400

    words = string_parameter.split

--- easy_7/10_the_end_is_near_but_not_here.rb
+++ easy_7/10_the_end_is_near_but_not_here.rb
@@ -108,6 +108,15 @@
 
 # and Code.” Implementation of Algorithm:
 def middle(string_parameter)
+  words = string_parameter.split
+the middle_word variable is assigned to case expression that follows the number of elements in the words variable.
+if the array is empty then nil.
+if array is 1 string in total return the string.
+if the array contains 2 strings then return both strings.
+the middle_index variable is assigned to the number of elements in the words variable divided by 2.
+if the array is odd if odd then return the middle_index variable.
+check if the array is even if even then handle that by return the value located at the middle_index variable - 1 index
+location of the words variable and the middle_index variable index locaiton of the words variable.
 
 
 
