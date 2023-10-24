7e06edcf61b33f8cc3893568c142bb41f41e44b0
Justin Greger <justingreger@gmail.com>
2023-10-23 20:36:34 -0400

    # vehicles = [ #   car, car, truck, car, SUV, truck, #   motorcycle, motorcycle, car, truck # ]
    # count_occurrences(vehicles)

--- easy_7/11_how_many.rb
+++ easy_7/11_how_many.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
 # Write a method that counts the number of occurrences of each element in a given array.
+
+# vehicles = [
+#   'car', 'car', 'truck', 'car', 'SUV', 'truck',
+#   'motorcycle', 'motorcycle', 'car', 'truck'
+# ]
+
+# count_occurrences(vehicles)
