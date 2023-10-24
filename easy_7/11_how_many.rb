bac0bd096705da9fc81773a283055d792afc6db9
Justin Greger <justingreger@gmail.com>
2023-10-23 21:59:22 -0400

    # Identify expected input: # vehicles = [

--- easy_7/11_how_many.rb
+++ easy_7/11_how_many.rb
@@ -21,7 +21,12 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# vehicles = [
+#   'car', 'car', 'truck', 'car', 'SUV', 'truck',
+#   'motorcycle', 'motorcycle', 'car', 'truck'
+# ]
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
