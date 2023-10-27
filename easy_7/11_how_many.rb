2f139937f2447bb6381db60121801a23386a87f3
Justin Greger <justingreger@gmail.com>
2023-10-26 20:49:02 -0400

    # LS solution:

--- easy_7/11_how_many.rb
+++ easy_7/11_how_many.rb
@@ -68,17 +68,17 @@
 # end
 
 # LS solution:
-def count_occurrences(array)
-  occurrences = {}
+# def count_occurrences(array)
+#   occurrences = {}
 
-  array.uniq.each do |element|
-    occurrences[element] = array.count(element)
-  end
+#   array.uniq.each do |element|
+#     occurrences[element] = array.count(element)
+#   end
 
-  occurrences.each do |element, count|
-    puts "#{element} => #{count}"
-  end
-end
+#   occurrences.each do |element, count|
+#     puts "#{element} => #{count}"
+#   end
+# end
 
 # test:
 vehicles = %w[
