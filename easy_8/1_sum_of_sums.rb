67fed72d624fecce33ace53120a244f20a048cb7
Justin Greger <justingreger@gmail.com>
2023-10-28 21:56:53 -0400

    # and then returns the sum of the sums of each leading subsequence for that Array.

--- easy_8/1_sum_of_sums.rb
+++ easy_8/1_sum_of_sums.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # Write a method that takes an Array of numbers
- and then returns the sum of the sums of each leading subsequence for that Array. You may assume that the Array always contains at least one number.
+# and then returns the sum of the sums of each leading subsequence for that Array.
+ You may assume that the Array always contains at least one number.
 
 Examples:
