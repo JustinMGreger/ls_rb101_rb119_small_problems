4c0f08740c9bd4f4188d22cb719989d94a29e838
Justin Greger <justingreger@gmail.com>
2023-10-28 21:58:18 -0400

    # You may assume that the Array always contains at least one number.

--- easy_8/1_sum_of_sums.rb
+++ easy_8/1_sum_of_sums.rb
@@ -2,6 +2,6 @@
 
 # Write a method that takes an Array of numbers
 # and then returns the sum of the sums of each leading subsequence for that Array.
- You may assume that the Array always contains at least one number.
+# You may assume that the Array always contains at least one number.
 
 Examples:
