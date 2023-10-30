dab86e63a7ac962ca3d1de241aa824d9ffa486e5
Justin Greger <justingreger@gmail.com>
2023-10-29 21:04:57 -0400

    # return the sum of the sums of each leading subsequence for that Array.

--- easy_8/1_sum_of_sums.rb
+++ easy_8/1_sum_of_sums.rb
@@ -21,8 +21,8 @@
 
 # Make the requirements explicit Identify rules:
 # Write a method that takes an Array of numbers.
-
-and then returns the sum of the sums of each leading subsequence for that Array. You may assume that the Array always contains at least one number.
+# return the sum of the sums of each leading subsequence for that Array.
+ You may assume that the Array always contains at least one number.
 
 
 
