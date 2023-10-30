9a6151035183b7509a06371d9293443236763bd8
Justin Greger <justingreger@gmail.com>
2023-10-29 22:00:15 -0400

    # Algorithm, Steps for converting input to output:

--- easy_8/1_sum_of_sums.rb
+++ easy_8/1_sum_of_sums.rb
@@ -39,7 +39,11 @@
 # Data Structure, How we represent data that we will work with when converting the input to output:
 # array.
 
-Algorithm, Steps for converting input to output:
+# Algorithm, Steps for converting input to output:
+
+
+
+
 and Code.” Implementation of Algorithm:
 
 # tests:
