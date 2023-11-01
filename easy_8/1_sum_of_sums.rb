ef3882a8b04612e844cb5191e5dc87cfea58ce1c
Justin Greger <justingreger@gmail.com>
2023-10-31 21:59:40 -0400

    # Can you see how these two solutions model the problem slightly differently?

--- easy_8/1_sum_of_sums.rb
+++ easy_8/1_sum_of_sums.rb
@@ -81,6 +81,9 @@ def sum_of_sums(numbers)
   sum_total
 end
 
+# Can you see how these two solutions model the problem slightly differently?
+Which model did you use in your own solution?
+
 # tests:
 p sum_of_sums([3, 5, 2]) == 3 + (3 + 5) + (3 + 5 + 2) # -> (21)
 p sum_of_sums([1, 5, 7, 3]) == 1 + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
