35105c58b1e09fcce88a63d2a0b0ab31d537d709
Justin Greger <justingreger@gmail.com>
2023-10-31 20:06:50 -0400

    # LS Solution 1:

--- easy_8/1_sum_of_sums.rb
+++ easy_8/1_sum_of_sums.rb
@@ -59,6 +59,19 @@
 #   sum
 # end
 
+# LS Solution 1:
+def sum_of_sums(numbers)
+  sum_total = 0
+  accumulator = 0
+
+  numbers.each do |num|
+    accumulator += num
+    sum_total += accumulator
+  end
+
+  sum_total
+end
+
 # tests:
 p sum_of_sums([3, 5, 2]) == 3 + (3 + 5) + (3 + 5 + 2) # -> (21)
 p sum_of_sums([1, 5, 7, 3]) == 1 + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
