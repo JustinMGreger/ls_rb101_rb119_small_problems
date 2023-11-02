193061ea12c003c2902745e949121b44638a1a84
Justin Greger <justingreger@gmail.com>
2023-11-01 20:42:06 -0400

    # tests:

--- easy_8/2_leading_substrings.rb
+++ easy_8/2_leading_substrings.rb
@@ -24,3 +24,8 @@ p leading_substrings('xyzzy') == %w[x xy xyz xyzz xyzzy]
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
+
+# tests:
+p leading_substrings('abc') == %w[a ab abc]
+p leading_substrings('a') == ['a']
+p leading_substrings('xyzzy') == %w[x xy xyz xyzz xyzzy]
