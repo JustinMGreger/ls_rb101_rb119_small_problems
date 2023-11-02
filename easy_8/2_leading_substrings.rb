e93bfddd5eef776b35d7b51abf62971171f40b12
Justin Greger <justingreger@gmail.com>
2023-11-01 20:36:04 -0400

    p leading_substrings(abc) == %w[a ab abc]

--- easy_8/2_leading_substrings.rb
+++ easy_8/2_leading_substrings.rb
@@ -4,6 +4,6 @@
 # The return value should be arranged in order from shortest to longest substring.
 
 # Examples:
-p leading_substrings('abc') == ['a', 'ab', 'abc']
+p leading_substrings('abc') == %w[a ab abc]
 p leading_substrings('a') == ['a']
 p leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
