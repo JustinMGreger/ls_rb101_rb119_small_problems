1b8bdc50f9650e7f33a101dae614d6bd36f3adbb
Justin Greger <justingreger@gmail.com>
2023-11-02 19:56:06 -0400

    # the leading_substrings method accepts a string parameter.

--- easy_8/2_leading_substrings.rb
+++ easy_8/2_leading_substrings.rb
@@ -38,7 +38,7 @@
 # array.
 
 # Algorithm, Steps for converting input to output:
-the leading_substrings method accepts a string parameter.
+# the leading_substrings method accepts a string parameter.
 the string parameter is broken into substrings.
 the sub strings are assigned to a return_array varible.
 substrings are concatenated into a substring based on the previous substrings.
