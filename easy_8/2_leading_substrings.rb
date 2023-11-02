6ab84e069d3f32008c6110fb2826a411a7d5d404
Justin Greger <justingreger@gmail.com>
2023-11-01 21:38:37 -0400

    # Make the requirements explicit Identify rules: # Write a method that returns a list of all substrings of a string that start at the beginning of the original string. # The return value should be arranged in order from shortest to longest substring.

--- easy_8/2_leading_substrings.rb
+++ easy_8/2_leading_substrings.rb
@@ -18,7 +18,10 @@
 # a list of all substrings of a string that start at the beginning of the original string
 # and arranged in order from shortest to longest substring.
 
-Make the requirements explicit Identify rules:
+# Make the requirements explicit Identify rules:
+# Write a method that returns a list of all substrings of a string that start at the beginning of the original string.
+# The return value should be arranged in order from shortest to longest substring.
+
 Mental model of the problem (optional):
 
 # Examples / Test Cases, Validate understanding of the problem:
