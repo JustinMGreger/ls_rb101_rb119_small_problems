9cdc93e3a9fec2593346f987e59d37a326d5c350
Justin Greger <justingreger@gmail.com>
2023-11-01 20:31:01 -0400

    # Write a method that returns a list of all substrings of a string that start at the beginning of the original string. # The return value should be arranged in order from shortest to longest substring.
    # Examples:

--- easy_8/2_leading_substrings.rb
+++ easy_8/2_leading_substrings.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+# Write a method that returns a list of all substrings of a string that start at the beginning of the original string.
+# The return value should be arranged in order from shortest to longest substring.
+
+# Examples:
