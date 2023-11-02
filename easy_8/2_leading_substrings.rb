d7b5015369698b6573d7bde063423a573a366d75
Justin Greger <justingreger@gmail.com>
2023-11-01 20:37:57 -0400

    PEDAC

--- easy_8/2_leading_substrings.rb
+++ easy_8/2_leading_substrings.rb
@@ -7,3 +7,14 @@
 p leading_substrings('abc') == %w[a ab abc]
 p leading_substrings('a') == ['a']
 p leading_substrings('xyzzy') == %w[x xy xyz xyzz xyzzy]
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem,
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
