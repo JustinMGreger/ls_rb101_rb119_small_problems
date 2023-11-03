2f3ff15bfe6bea858c52eb118dfa0c312fc47496
Justin Greger <justingreger@gmail.com>
2023-11-02 20:01:58 -0400

    deleted the sub strings are assigned to a return_array varible.

--- easy_8/2_leading_substrings.rb
+++ easy_8/2_leading_substrings.rb
@@ -40,7 +40,6 @@
 # Algorithm, Steps for converting input to output:
 # the leading_substrings method accepts a string parameter.
 # the return_array varible is assigned to the result of splitting the string parameter into an array of substrings.
-the sub strings are assigned to a return_array varible.
 substrings are concatenated into a substring based on the previous substrings.
 return the return_array.
 
