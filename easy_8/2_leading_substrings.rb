35ffd765c29998a3078425d0e8ed8f4778eecbd9
Justin Greger <justingreger@gmail.com>
2023-11-02 21:57:40 -0400

    # substrings.

--- easy_8/2_leading_substrings.rb
+++ easy_8/2_leading_substrings.rb
@@ -39,7 +39,8 @@
 
 # Algorithm, Steps for converting input to output:
 # the leading_substrings method accepts a string parameter.
-# the intermediate_array varible is assigned to the result of splitting the string parameter into an array of substrings.
+# the intermediate_array varible is assigned to the result of splitting the string parameter into an array of
+# substrings.
 # the return_array variable is assigned to the result of iterating through the return_array concatenating the substrings
 # into a substring based on the previous substrings.
 # return the return_array.
