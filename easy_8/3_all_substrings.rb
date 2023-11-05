d08e870ffa7c8b1e27e9bb142b3d1801fa251fa9
Justin Greger <justingreger@gmail.com>
2023-11-04 21:19:18 -0400

    # Identify expected input: # a string parameter.

--- easy_8/3_all_substrings.rb
+++ easy_8/3_all_substrings.rb
@@ -21,7 +21,9 @@ p substrings('abcde') == %w[
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# a string parameter.
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
