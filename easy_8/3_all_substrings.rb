351ba5e404def75cd7841c6bfa0413a61f71f662
Justin Greger <justingreger@gmail.com>
2023-11-04 20:52:52 -0400

    # Since multiple substrings will occur at each position,

--- easy_8/3_all_substrings.rb
+++ easy_8/3_all_substrings.rb
@@ -4,7 +4,8 @@
 # The returned list should be ordered by where in the string the substring begins.
 # This means that all substrings that start at position 0 should come first,
 # then all substrings that start at position 1, and so on.
- Since multiple substrings will occur at each position, the substrings at a given position should be returned in order from shortest to longest.
+# Since multiple substrings will occur at each position,
+ the substrings at a given position should be returned in order from shortest to longest.
 
 You may (and should) use the leading_substrings method you wrote in the previous exercise:
 
