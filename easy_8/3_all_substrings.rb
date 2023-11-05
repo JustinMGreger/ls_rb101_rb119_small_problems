9e9e104c75b0998cb78df49c6fd704f93026d4a4
Justin Greger <justingreger@gmail.com>
2023-11-04 21:57:10 -0400

    # Write a method that accepts a string parameter and returns an array of strings.

--- easy_8/3_all_substrings.rb
+++ easy_8/3_all_substrings.rb
@@ -29,13 +29,13 @@ p substrings('abcde') == %w[
 
 # Make the requirements explicit Identify rules:
 # use the leading_substrings method you wrote in the previous exercise.
+# Write a method that accepts a string parameter and returns an array of strings.
 
 
 
+list of all substrings of a string.
 
-
-
-Write a method that returns a list of all substrings of a string. The returned list should be ordered by where in the string the substring begins. This means that all substrings that start at position 0 should come first, then all substrings that start at position 1, and so on. Since multiple substrings will occur at each position, the substrings at a given position should be returned in order from shortest to longest.
+The returned list should be ordered by where in the string the substring begins. This means that all substrings that start at position 0 should come first, then all substrings that start at position 1, and so on. Since multiple substrings will occur at each position, the substrings at a given position should be returned in order from shortest to longest.
 
 
 
