ee4b3263ff1ce1aa701294a43881e07a16b86f4a
Justin Greger <justingreger@gmail.com>
2023-11-04 20:07:01 -0400

    # This means that all substrings that start at position 0 should come first,

--- easy_8/3_all_substrings.rb
+++ easy_8/3_all_substrings.rb
@@ -2,7 +2,8 @@
 
 # Write a method that returns a list of all substrings of a string.
 # The returned list should be ordered by where in the string the substring begins.
- This means that all substrings that start at position 0 should come first, then all substrings that start at position 1, and so on. Since multiple substrings will occur at each position, the substrings at a given position should be returned in order from shortest to longest.
+# This means that all substrings that start at position 0 should come first,
+ then all substrings that start at position 1, and so on. Since multiple substrings will occur at each position, the substrings at a given position should be returned in order from shortest to longest.
 
 You may (and should) use the leading_substrings method you wrote in the previous exercise:
 
