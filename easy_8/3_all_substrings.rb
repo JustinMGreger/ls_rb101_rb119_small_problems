1b6f29e2baa2e6cba1a9130c683c5dedf0ffdce2
Justin Greger <justingreger@gmail.com>
2023-11-04 22:00:03 -0400

    # p substrings(abcde) == %w[

--- easy_8/3_all_substrings.rb
+++ easy_8/3_all_substrings.rb
@@ -56,13 +56,13 @@ Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
 
 # Data Structure, How we represent data that we will work with when converting the input to output:
-p substrings('abcde') == %w[
-  a ab abc abcd abcde
-  b bc bcd bcde
-  c cd cde
-  d de
-  e
-]
+# p substrings('abcde') == %w[
+#   a ab abc abcd abcde
+#   b bc bcd bcde
+#   c cd cde
+#   d de
+#   e
+# ]
 
 Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
