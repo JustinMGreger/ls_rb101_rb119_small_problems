979d46da982bd6a30c40a7d845f444446769335e
Justin Greger <justingreger@gmail.com>
2023-11-04 20:02:53 -0400

    # Write a method that returns a list of all substrings of a string.

--- easy_8/3_all_substrings.rb
+++ easy_8/3_all_substrings.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-Write a method that returns a list of all substrings of a string. The returned list should be ordered by where in the string the substring begins. This means that all substrings that start at position 0 should come first, then all substrings that start at position 1, and so on. Since multiple substrings will occur at each position, the substrings at a given position should be returned in order from shortest to longest.
+# Write a method that returns a list of all substrings of a string.
+The returned list should be ordered by where in the string the substring begins. This means that all substrings that start at position 0 should come first, then all substrings that start at position 1, and so on. Since multiple substrings will occur at each position, the substrings at a given position should be returned in order from shortest to longest.
 
 You may (and should) use the leading_substrings method you wrote in the previous exercise:
 
