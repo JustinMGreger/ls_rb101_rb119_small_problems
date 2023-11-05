b3d3f7a9f6ef4bba273a9e20e113453e9fff717c
Justin Greger <justingreger@gmail.com>
2023-11-04 21:09:37 -0400

    PEDAC

--- easy_8/3_all_substrings.rb
+++ easy_8/3_all_substrings.rb
@@ -17,3 +17,14 @@ p substrings('abcde') == %w[
   d de
   e
 ]
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for “[Understand the] Problem,
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
