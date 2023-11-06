93c2a75825fa402d2eb9afbc30b093cf9cc1bbfb
Justin Greger <justingreger@gmail.com>
2023-11-05 20:55:12 -0500

    The returned list of

--- easy_8/3_all_substrings.rb
+++ easy_8/3_all_substrings.rb
@@ -33,9 +33,11 @@
 
 
 
-list of all substrings of a string.
-
-The returned list should be ordered by where in the string the substring begins. This means that all substrings that start at position 0 should come first, then all substrings that start at position 1, and so on. Since multiple substrings will occur at each position, the substrings at a given position should be returned in order from shortest to longest.
+The returned list of all substrings of the string parameter should be ordered by where in the string the substring begins.
+This means that all substrings that start at position 0 should come first,
+then all substrings that start at position 1, and so on.
+Since multiple substrings will occur at each position,
+the substrings at a given position should be returned in order from shortest to longest.
 
 
 
