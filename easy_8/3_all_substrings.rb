d733f2c50987cac923680d379f15500be25032c5
Justin Greger <justingreger@gmail.com>
2023-11-05 20:56:24 -0500

    # The returned list of all substrings of the string parameter should be ordered # by where in the string the substring begins. # This means that all substrings that start at position 0 should come first, # then all substrings that start at position 1, and so on. # Since multiple substrings will occur at each position, # the substrings at a given position should be returned in order from shortest to longest.

--- easy_8/3_all_substrings.rb
+++ easy_8/3_all_substrings.rb
@@ -30,29 +30,12 @@
 # Make the requirements explicit Identify rules:
 # use the leading_substrings method you wrote in the previous exercise.
 # Write a method that accepts a string parameter and returns an array of strings.
-
-
-
-The returned list of all substrings of the string parameter should be ordered by where in the string the substring begins.
-This means that all substrings that start at position 0 should come first,
-then all substrings that start at position 1, and so on.
-Since multiple substrings will occur at each position,
-the substrings at a given position should be returned in order from shortest to longest.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# The returned list of all substrings of the string parameter should be ordered
+# by where in the string the substring begins.
+# This means that all substrings that start at position 0 should come first,
+# then all substrings that start at position 1, and so on.
+# Since multiple substrings will occur at each position,
+# the substrings at a given position should be returned in order from shortest to longest.
 
 Mental model of the problem (optional):
 
