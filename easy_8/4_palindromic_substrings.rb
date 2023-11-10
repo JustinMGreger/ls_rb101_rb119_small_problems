fa278021302c42c3752dd28f46c02c7e0ef8a351
Justin Greger <justingreger@gmail.com>
2023-11-09 21:56:06 -0500

    # Examples:

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -13,16 +13,16 @@
 # In addition, assume that single characters are not palindromes.
 
 # Examples:
-p palindromes('abcd') == []
-p palindromes('madam') == %w[madam ada]
-p palindromes('hello-madam-did-madam-goodbye') == [
-  'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
-  'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
-  '-madam-', 'madam', 'ada', 'oo'
-]
-p palindromes('knitting cassettes') == %w[
-  nittin itti tt ss settes ette tt
-]
+# p palindromes('abcd') == []
+# p palindromes('madam') == %w[madam ada]
+# p palindromes('hello-madam-did-madam-goodbye') == [
+#   'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+#   'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+#   '-madam-', 'madam', 'ada', 'oo'
+# ]
+# p palindromes('knitting cassettes') == %w[
+#   nittin itti tt ss settes ette tt
+# ]
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem,
