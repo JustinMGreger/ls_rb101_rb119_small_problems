d554dc73496fa0fbefade9a16958a4cd77e81052
Justin Greger <justingreger@gmail.com>
2023-11-09 19:50:01 -0500

    # The return value should be arranged in the same sequence as the substrings appear in the string. # Duplicate palindromes should be included multiple times.
    # You may (and should) use the substrings method you wrote in the previous exercise.

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -2,9 +2,10 @@
 
 # Write a method that returns a list of all substrings of a string that are palindromic.
 # That is, each substring must consist of the same sequence of characters forwards as it does backwards.
-The return value should be arranged in the same sequence as the substrings appear in the string. Duplicate palindromes should be included multiple times.
+# The return value should be arranged in the same sequence as the substrings appear in the string.
+# Duplicate palindromes should be included multiple times.
 
-You may (and should) use the substrings method you wrote in the previous exercise.
+# You may (and should) use the substrings method you wrote in the previous exercise.
 
 For the purposes of this exercise, you should consider all characters and pay attention to case; that is, "AbcbA" is a palindrome, but neither "Abcba" nor "Abc-bA" are. In addition, assume that single characters are not palindromes.
 
