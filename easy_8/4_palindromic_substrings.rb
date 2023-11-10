4d96a1748ee411665863e86932e35386f3f8a738
Justin Greger <justingreger@gmail.com>
2023-11-09 19:16:33 -0500

    # That is, each substring must consist of the same sequence of characters forwards as it does backwards.

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 # Write a method that returns a list of all substrings of a string that are palindromic.
-That is, each substring must consist of the same sequence of characters forwards as it does backwards. The return value should be arranged in the same sequence as the substrings appear in the string. Duplicate palindromes should be included multiple times.
+# That is, each substring must consist of the same sequence of characters forwards as it does backwards.
+The return value should be arranged in the same sequence as the substrings appear in the string. Duplicate palindromes should be included multiple times.
 
 You may (and should) use the substrings method you wrote in the previous exercise.
 
