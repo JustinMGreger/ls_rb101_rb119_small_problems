ea25be89ee73208a3c650289e182f47b9139c12b
Justin Greger <justingreger@gmail.com>
2023-11-09 20:33:11 -0500

    # Examples:

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -13,3 +13,13 @@
 # In addition, assume that single characters are not palindromes.
 
 # Examples:
+palindromes('abcd') == []
+palindromes('madam') == ['madam', 'ada']
+palindromes('hello-madam-did-madam-goodbye') == [
+  'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+  'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+  '-madam-', 'madam', 'ada', 'oo'
+]
+palindromes('knitting cassettes') == [
+  'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
+]
