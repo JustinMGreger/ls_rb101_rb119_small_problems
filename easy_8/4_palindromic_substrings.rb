29c4dea72d07bd9ce7b99d74f705e1660213aba0
Justin Greger <justingreger@gmail.com>
2023-11-09 20:43:27 -0500

    %w[madam ada]

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -14,7 +14,7 @@
 
 # Examples:
 p palindromes('abcd') == []
-p palindromes('madam') == ['madam', 'ada']
+p palindromes('madam') == %w[madam ada]
 p palindromes('hello-madam-did-madam-goodbye') == [
   'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
   'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
