db5b65b7a5e8ff485f447c1bcf5ae755549824df
Justin Greger <justingreger@gmail.com>
2023-11-09 19:11:26 -0500

    created ruby file

--- easy_8/3_all_substrings.rb
+++ easy_8/3_all_substrings.rb
@@ -96,6 +96,16 @@
 #   all_substrings
 # end
 
+# LS Solution:
+def substrings(string)
+  results = []
+  (0...string.size).each do |start_index|
+    this_substring = string[start_index..-1]
+    results.concat(leading_substrings(this_substring))
+  end
+  results
+end
+
 # tests:
 p substrings('abcde') == %w[
   a ab abc abcd abcde
