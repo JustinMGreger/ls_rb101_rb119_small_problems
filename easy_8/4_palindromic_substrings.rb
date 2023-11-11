5114b0e5d74c59476aeea45dedcc1b46c5461edf
Justin Greger <justingreger@gmail.com>
2023-11-10 19:18:16 -0500

    # Make the requirements explicit Identify rules:

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -33,7 +33,26 @@
 # output:
 # a list of all substrings of a string that are palindromic. 
 
-Make the requirements explicit Identify rules:
+# Make the requirements explicit Identify rules:
+
+
+
+Write a method that returns a list of all substrings of a string that are palindromic. That is, each substring must consist of the same sequence of characters forwards as it does backwards. The return value should be arranged in the same sequence as the substrings appear in the string. Duplicate palindromes should be included multiple times.
+
+You may (and should) use the substrings method you wrote in the previous exercise.
+
+For the purposes of this exercise, you should consider all characters and pay attention to case; that is, "AbcbA" is a palindrome, but neither "Abcba" nor "Abc-bA" are. In addition, assume that single characters are not palindromes.
+
+Examples:
+
+
+
+
+
+
+
+
+
 Mental model of the problem (optional):
 
 # Examples / Test Cases, Validate understanding of the problem:
