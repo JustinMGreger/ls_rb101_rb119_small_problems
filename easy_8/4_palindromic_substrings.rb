cbd02d16bae123b1d684a31e3aa3e1c3fd27d3a3
Justin Greger <justingreger@gmail.com>
2023-11-10 19:00:26 -0500

    # tests:

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -47,3 +47,15 @@ p palindromes('knitting cassettes') == %w[
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
+
+# tests:
+p palindromes('abcd') == []
+p palindromes('madam') == %w[madam ada]
+p palindromes('hello-madam-did-madam-goodbye') == [
+  'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+  'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+  '-madam-', 'madam', 'ada', 'oo'
+]
+p palindromes('knitting cassettes') == %w[
+  nittin itti tt ss settes ette tt
+]
