c085e7614c286678af664e50f8b248e9c7631511
Justin Greger <justingreger@gmail.com>
2023-11-14 17:22:33 -0500

    # LS Solution:

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -151,18 +151,18 @@
 # end
 
 # LS Solution:
-def palindromes(string)
-  all_substrings = substrings(string)
-  results = []
-  all_substrings.each do |substring|
-    results << substring if palindrome?(substring)
-  end
-  results
-end
+# def palindromes(string)
+#   all_substrings = substrings(string)
+#   results = []
+#   all_substrings.each do |substring|
+#     results << substring if palindrome?(substring)
+#   end
+#   results
+# end
 
-def palindrome?(string)
-  string == string.reverse && string.size > 1
-end
+# def palindrome?(string)
+#   string == string.reverse && string.size > 1
+# end
 
 # tests:
 # p palindromes('abcd') == []
