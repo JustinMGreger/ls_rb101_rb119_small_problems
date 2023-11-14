316431f97fb51dfa627ed7f4e14b6786dfe45291
Justin Greger <justingreger@gmail.com>
2023-11-14 17:29:44 -0500

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for “[Understand the] Problem,

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -181,6 +181,25 @@
 # Can you modify this method (and/or its predecessors) to ignore non-alphanumeric characters and case?
 # Alphanumeric characters are alphabetic characters(upper and lowercase) and digits.
 
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for “[Understand the] Problem,
+
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code.” Implementation of Algorithm:
+
+
+
+
+
+
+
+
 # tests:
 p palindromes('abcd') == []
 p palindromes('madam') == %w[madam ada]
