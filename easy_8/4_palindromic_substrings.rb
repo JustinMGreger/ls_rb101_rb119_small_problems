c647f9b9bbb8b75f9d957d2a3c6e2fc79ec70ff2
Justin Greger <justingreger@gmail.com>
2023-11-14 17:57:01 -0500

    # Identify expected input: # string parameter.

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -184,7 +184,11 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for “[Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# string parameter.
+
+
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
