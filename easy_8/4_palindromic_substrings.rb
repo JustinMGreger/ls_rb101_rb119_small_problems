781946bb92829d4968c70afb7916d93faa8c67aa
Justin Greger <justingreger@gmail.com>
2023-11-14 21:04:00 -0500

    p palindromes

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -220,6 +220,21 @@
 # Alphanumeric characters are alphabetic characters(upper and lowercase) and digits.
 
 Examples / Test Cases, Validate understanding of the problem:
+p palindromes('abcd') == []
+p palindromes('madam') == %w[madam ada]
+p palindromes('hello-madam-did-madam-goodbye') == [
+  'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+  'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+  '-madam-', 'madam', 'ada', 'oo'
+]
+p palindromes('knitting cassettes') == %w[
+  nittin itti tt ss settes ette tt
+]
+
+
+
+
+
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code.” Implementation of Algorithm:
