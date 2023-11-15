a8558a96eaba5920e44af939c8db78736de603f2
Justin Greger <justingreger@gmail.com>
2023-11-14 21:13:45 -0500

    deleted ignore non-alphanumeric characters and case

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -230,8 +230,8 @@ p palindromes('hello-madam-did-madam-goodbye') == [
 p palindromes('knitting cassettes') == %w[
   nittin itti tt ss settes ette tt
 ]
-p palandrome ignore non-alphanumeric characters and case
-
+p palandrome 
+aa11bB$$
 
 
 
