4486f7b87ff4889eed09d9dd615a34bde7d3bc9c
Justin Greger <justingreger@gmail.com>
2023-11-14 19:25:58 -0500

    # Write a method that returns a list of all substrings of a string that are palindromic.

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -191,8 +191,8 @@
 # an array of string palindromes.
 
 # Make the requirements explicit Identify rules:
+# Write a method that returns a list of all substrings of a string that are palindromic.
 
-Write a method that returns a list of all substrings of a string that are palindromic.
 That is, each substring must consist of the same sequence of characters forwards as it does backwards.
 The return value should be arranged in the same sequence as the substrings appear in the string.
 Duplicate palindromes should be included multiple times.
