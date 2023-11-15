4431a5636cf93d8a367bc9ad31977120c3b00367
Justin Greger <justingreger@gmail.com>
2023-11-14 20:42:07 -0500

    deleted You may (and should) use the substrings method you wrote in the previous exercise.

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -201,13 +201,13 @@
 # In addition, assume that single characters are not palindromes.
 
 # Mental model of the problem (optional):
-
-
 Write a method that returns a list of all substrings of a string that are palindromic.
 each substring must consist of the same sequence of characters forwards as it does backwards.
 The return value should be arranged in the same sequence as the substrings appear in the string.
 Duplicate palindromes should be included multiple times.
-You may (and should) use the substrings method you wrote in the previous exercise.
+
+
+
 
 Can you modify this method (and/or its predecessors) to ignore non-alphanumeric characters and case?
 Alphanumeric characters are alphabetic characters(upper and lowercase) and digits.
