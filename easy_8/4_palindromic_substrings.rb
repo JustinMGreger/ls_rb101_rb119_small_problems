23f13c6f4d088c1daa1a1c1d3be8be982c9c2056
Justin Greger <justingreger@gmail.com>
2023-11-14 21:26:28 -0500

    # Mental model of the problem (optional):

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -221,6 +221,22 @@
 # each substring must consist of the same sequence of characters forwards as it does backwards.
 # The return value should be arranged in the same sequence as the substrings appear in the string.
 # Duplicate palindromes should be included multiple times.
+# def leading_substrings(string)
+#   return_array = []
+#   intermediate_array = string.chars
+#   intermediate_array.each do |x|
+#     return_array << (return_array.last.to_s + x)
+#   end
+#   return_array
+# end
+# def substrings(string)
+#   results = []
+#   (0...string.size).each do |start_index|
+#     this_substring = string[start_index..-1]
+#     results.concat(leading_substrings(this_substring))
+#   end
+#   results
+# end
 # def palindromes(string)
 #   all_substrings = substrings(string)
 #   results = []
