1ea6fbf2e18476ae7ea52837437ef3770c790709
Justin Greger <justingreger@gmail.com>
2023-11-16 21:52:29 -0500

    # remove non alphanumeric characters from the string parameter.

--- easy_8/4_palindromic_substrings.rb
+++ easy_8/4_palindromic_substrings.rb
@@ -269,8 +269,7 @@
 
 # Algorithm, Steps for converting input to output:
 # the clean_string method accepts the (string) parameter and is defined as the following:
-
-
+# remove non alphanumeric characters from the string parameter.
 
 
 
