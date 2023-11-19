2b1e9b2f25dfbf4dc951600959582d74e7af8416
Justin Greger <justingreger@gmail.com>
2023-11-18 21:08:08 -0500

    # Make the requirements explicit Identify rules: # Write a method that takes two arguments: # the first is the starting number, # and the second is the ending number.

--- easy_8/5_fizzbuzz.rb
+++ easy_8/5_fizzbuzz.rb
@@ -22,8 +22,19 @@
 # if a number is divisible by 5, print "Buzz",
 # and finally if a number is divisible by 3 and 5, print "FizzBuzz".
 
+# Make the requirements explicit Identify rules:
+# Write a method that takes two arguments:
+# the first is the starting number,
+# and the second is the ending number.
 
-Make the requirements explicit Identify rules:
+Print out all numbers from the starting number to the ending number, except if a number is divisible by 3, print "Fizz", if a number is divisible by 5, print "Buzz", and finally if a number is divisible by 3 and 5, print "FizzBuzz".
+
+
+
+
+
+
+
 Mental model of the problem (optional):
 
 # Examples / Test Cases, Validate understanding of the problem:
