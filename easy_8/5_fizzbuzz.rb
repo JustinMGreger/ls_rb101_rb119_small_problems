6ea93acabaa26c36e10b7dd454ca28c72b198078
Justin Greger <justingreger@gmail.com>
2023-11-18 21:22:33 -0500

    # if a number is divisible by 3,

--- easy_8/5_fizzbuzz.rb
+++ easy_8/5_fizzbuzz.rb
@@ -34,7 +34,7 @@
 # Mental model of the problem (optional):
 # the fizzbuzz method accepts the (starting_number, ending_number) parameters and is defined as:
 # Print out all numbers from the starting number to the ending number
- if a number is divisible by 3, print "Fizz",
+# if a number is divisible by 3, print "Fizz"
  if a number is divisible by 5, print "Buzz",
  and finally if a number is divisible by 3 and 5, print "FizzBuzz".
 
