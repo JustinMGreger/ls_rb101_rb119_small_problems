0ba53e5b708f8f552573021e033d5f7efa733f84
Justin Greger <justingreger@gmail.com>
2023-11-18 21:18:52 -0500

    deleted  the first is the starting number,  and the second is the ending number.

--- easy_8/5_fizzbuzz.rb
+++ easy_8/5_fizzbuzz.rb
@@ -33,8 +33,7 @@
 
 # Mental model of the problem (optional):
 # the fizzbuzz method accepts the (starting_number, ending_number) parameters and is defined as:
- the first is the starting number,
- and the second is the ending number.
+
 Print out all numbers from the starting number to the ending number,
  except if a number is divisible by 3, print "Fizz",
  if a number is divisible by 5, print "Buzz",
