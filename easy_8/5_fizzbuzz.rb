78b6a16eea4194cca9942e329dcca95422115d8e
Justin Greger <justingreger@gmail.com>
2023-11-18 21:16:24 -0500

    # Mental model of the problem (optional): # the fizzbuzz method accepts the (starting_number, ending_number) parameters and is defined as:

--- easy_8/5_fizzbuzz.rb
+++ easy_8/5_fizzbuzz.rb
@@ -31,7 +31,22 @@
 # if a number is divisible by 5, print "Buzz",
 # and finally if a number is divisible by 3 and 5, print "FizzBuzz".
 
-Mental model of the problem (optional):
+# Mental model of the problem (optional):
+# the fizzbuzz method accepts the (starting_number, ending_number) parameters and is defined as:
+ the first is the starting number,
+ and the second is the ending number.
+Print out all numbers from the starting number to the ending number,
+ except if a number is divisible by 3, print "Fizz",
+ if a number is divisible by 5, print "Buzz",
+ and finally if a number is divisible by 3 and 5, print "FizzBuzz".
+
+
+
+
+
+
+
+
 
 # Examples / Test Cases, Validate understanding of the problem:
 # p fizzbuzz(1, 15) # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
