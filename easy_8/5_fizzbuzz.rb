3bad591314ef5c6d7aa42b381851a6af57a29531
Justin Greger <justingreger@gmail.com>
2023-11-18 21:04:23 -0500

    # output:

--- easy_8/5_fizzbuzz.rb
+++ easy_8/5_fizzbuzz.rb
@@ -16,9 +16,11 @@
 # Identify expected input:
 # two arguments: the first is the starting number, and the second is the ending number. 
 
-output:
-
-
+# output:
+# Print out all numbers from the starting number to the ending number,
+# except if a number is divisible by 3, print "Fizz",
+# if a number is divisible by 5, print "Buzz",
+# and finally if a number is divisible by 3 and 5, print "FizzBuzz".
 
 
 Make the requirements explicit Identify rules:
