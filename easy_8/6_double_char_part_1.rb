7cd7aa1ee0df21c5183c6beb07d6690a285ee00f
Justin Greger <justingreger@gmail.com>
2023-11-21 20:40:41 -0500

    # Examples:

--- easy_8/6_double_char_part_1.rb
+++ easy_8/6_double_char_part_1.rb
@@ -3,3 +3,6 @@
 # Write a method that takes a string, and returns a new string in which every character is doubled.
 
 # Examples:
+p repeater('Hello') == "HHeelllloo"
+p repeater("Good job!") == "GGoooodd  jjoobb!!"
+p repeater('') == ''
