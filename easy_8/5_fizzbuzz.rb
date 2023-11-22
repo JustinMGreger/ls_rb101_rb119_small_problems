43b0afe6c9cc900969631135a9d824079f936319
Justin Greger <justingreger@gmail.com>
2023-11-21 20:31:38 -0500

    deleted trailing white space

--- easy_8/5_fizzbuzz.rb
+++ easy_8/5_fizzbuzz.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Write a method that takes two arguments: 
+# Write a method that takes two arguments:
 # the first is the starting number, and the second is the ending number.
 # Print out all numbers from the starting number to the ending number,
 # except if a number is divisible by 3, print "Fizz",
