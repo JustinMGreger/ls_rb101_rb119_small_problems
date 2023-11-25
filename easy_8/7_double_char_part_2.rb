4c82683b05ee921f8aadddcc17e8ae7d6414fd61
Justin Greger <justingreger@gmail.com>
2023-11-24 19:58:40 -0500

    double to single quotes

--- easy_8/7_double_char_part_2.rb
+++ easy_8/7_double_char_part_2.rb
@@ -5,7 +5,7 @@
 # Vowels (a,e,i,o,u), digits, punctuation, and whitespace should not be doubled.
 
 # Examples:
-double_consonants('String') == "SSttrrinngg"
-double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
-double_consonants("July 4th") == "JJullyy 4tthh"
-double_consonants('') == ""
+double_consonants('String') == 'SSttrrinngg'
+double_consonants('Hello-World!') == 'HHellllo-WWorrlldd!'
+double_consonants('July 4th') == 'JJullyy 4tthh'
+double_consonants('') == ''
