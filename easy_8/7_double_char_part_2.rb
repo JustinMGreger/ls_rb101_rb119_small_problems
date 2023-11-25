c4f7358ca77e36f753264c75f18d5d4eb55df92c
Justin Greger <justingreger@gmail.com>
2023-11-24 20:08:43 -0500

    p double_consonants

--- easy_8/7_double_char_part_2.rb
+++ easy_8/7_double_char_part_2.rb
@@ -5,7 +5,7 @@
 # Vowels (a,e,i,o,u), digits, punctuation, and whitespace should not be doubled.
 
 # Examples:
-double_consonants('String') == 'SSttrrinngg'
-double_consonants('Hello-World!') == 'HHellllo-WWorrlldd!'
-double_consonants('July 4th') == 'JJullyy 4tthh'
-double_consonants('') == ''
+p double_consonants('String') == 'SSttrrinngg'
+p double_consonants('Hello-World!') == 'HHellllo-WWorrlldd!'
+p double_consonants('July 4th') == 'JJullyy 4tthh'
+p double_consonants('') == ''
