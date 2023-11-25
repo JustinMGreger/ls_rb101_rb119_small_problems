b16e619fccf2f40f17e3489a8ea021b0d86420eb
Justin Greger <justingreger@gmail.com>
2023-11-24 21:10:10 -0500

    # output: # a new string in which every consonant character is doubled.

--- easy_8/7_double_char_part_2.rb
+++ easy_8/7_double_char_part_2.rb
@@ -16,7 +16,9 @@
 # Identify expected input:
 # a string
 
-output:
+# output:
+# a new string in which every consonant character is doubled.
+
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
 
