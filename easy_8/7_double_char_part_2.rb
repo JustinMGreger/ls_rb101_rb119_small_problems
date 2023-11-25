0323064b3e79704a88b50e97133c4cc9b5a7307c
Justin Greger <justingreger@gmail.com>
2023-11-24 19:40:41 -0500

    # Write a method that takes a string,

--- easy_8/7_double_char_part_2.rb
+++ easy_8/7_double_char_part_2.rb
@@ -1,1 +1,7 @@
 # frozen_string_literal: true
+
+# Write a method that takes a string,
+and returns a new string in which every consonant character is doubled. Vowels (a,e,i,o,u), digits, punctuation, and whitespace should not be doubled.
+
+Examples:
+
