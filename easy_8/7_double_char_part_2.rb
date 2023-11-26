718d2477defd854f2aa9dc746a7d078214dd4b31
Justin Greger <justingreger@gmail.com>
2023-11-25 21:29:32 -0500

    # LS Solution:

--- easy_8/7_double_char_part_2.rb
+++ easy_8/7_double_char_part_2.rb
@@ -57,6 +57,18 @@
 #   end.join
 # end
 
+# LS Solution:
+CONSONANTS = %w(b c d f g h j k l m n p q r s t v w x y z)
+
+def double_consonants(string)
+  result = ''
+  string.each_char do |char|
+    result << char
+    result << char if CONSONANTS.include?(char.downcase)
+  end
+  result
+end
+
 # tests:
 p double_consonants('String') == 'SSttrrinngg'
 p double_consonants('Hello-World!') == 'HHellllo-WWorrlldd!'
