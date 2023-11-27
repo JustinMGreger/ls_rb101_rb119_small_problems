af04a14a698f09e591117a37e23b651578f45114
Justin Greger <justingreger@gmail.com>
2023-11-26 21:05:44 -0500

    PEDAC

--- easy_8/8_reverse_it_part_1.rb
+++ easy_8/8_reverse_it_part_1.rb
@@ -9,3 +9,14 @@ puts reverse_sentence('Hello World') == 'World Hello'
 puts reverse_sentence('Reverse these words') == 'words these Reverse'
 puts reverse_sentence('') == ''
 puts reverse_sentence('    ') == '' # Any number of spaces results in ''
+
+PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+PEDAC stands for [Understand the] Problem,
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code. Implementation of Algorithm:
