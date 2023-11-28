8a1ae5a34486760d12c2844eea6a2ee2d4a402e4
Justin Greger <justingreger@gmail.com>
2023-11-27 21:56:11 -0500

    # and returns the given string with words that contain five or more characters reversed.

--- easy_8/9_reverse_it_part_2.rb
+++ easy_8/9_reverse_it_part_2.rb
@@ -2,6 +2,7 @@
 
 # Write a method that takes one argument,
 # a string containing one or more words,
-and returns the given string with words that contain five or more characters reversed. Each string will consist of only letters and spaces. Spaces should be included only when more than one word is present.
+# and returns the given string with words that contain five or more characters reversed.
+Each string will consist of only letters and spaces. Spaces should be included only when more than one word is present.
 
 Examples:
