c0af03187ea8449c1fd6fa1697416aadbf344e7c
Justin Greger <justingreger@gmail.com>
2023-11-27 21:50:31 -0500

    # a string containing one or more words,

--- easy_8/9_reverse_it_part_2.rb
+++ easy_8/9_reverse_it_part_2.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Write a method that takes one argument,
-a string containing one or more words,
+# a string containing one or more words,
 and returns the given string with words that contain five or more characters reversed. Each string will consist of only letters and spaces. Spaces should be included only when more than one word is present.
 
 Examples:
