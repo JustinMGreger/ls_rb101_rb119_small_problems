16dd651815bb8ea15d122fed130e5d260650794b
Justin Greger <justingreger@gmail.com>
2023-11-27 19:45:54 -0500

    # Data Structure, How we represent data that we will work with when converting the input to output: # array.

--- easy_8/8_reverse_it_part_1.rb
+++ easy_8/8_reverse_it_part_1.rb
@@ -30,15 +30,15 @@
 # new_string = with the words from string parameter in reverse order.
 # return new_string
 
-
-
 # Examples / Test Cases, Validate understanding of the problem:
 # puts reverse_sentence('Hello World') == 'World Hello'
 # puts reverse_sentence('Reverse these words') == 'words these Reverse'
 # puts reverse_sentence('') == ''
 # puts reverse_sentence('    ') == '' # Any number of spaces results in ''
 
-Data Structure, How we represent data that we will work with when converting the input to output:
+# Data Structure, How we represent data that we will work with when converting the input to output:
+# array.
+
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
 
