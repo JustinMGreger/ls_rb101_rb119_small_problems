6eb66d48833afcd8339011e01b9190891d5ef4d6
Justin Greger <justingreger@gmail.com>
2023-11-27 21:59:19 -0500

    # Spaces should be included only when more than one word is present.

--- easy_8/9_reverse_it_part_2.rb
+++ easy_8/9_reverse_it_part_2.rb
@@ -4,6 +4,6 @@
 # a string containing one or more words,
 # and returns the given string with words that contain five or more characters reversed.
 # Each string will consist of only letters and spaces.
-Spaces should be included only when more than one word is present.
+# Spaces should be included only when more than one word is present.
 
 Examples:
