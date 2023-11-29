ac767a5fb27551d18bc71c2812f220a88d84397f
Justin Greger <justingreger@gmail.com>
2023-11-28 19:57:59 -0500

    deleted the argument is a string containing one or more words.

--- easy_8/9_reverse_it_part_2.rb
+++ easy_8/9_reverse_it_part_2.rb
@@ -29,7 +29,6 @@
 
 # Mental model of the problem (optional):
 # def reverse_words(string)
-the argument is a string containing one or more words.
 return the given string with words that contain five or more characters reversed.
 Each string will consist of only letters and spaces.
 Spaces should be included only when more than one word is present.
