0b83fa15141ef3530840c05d9cc28588c4219d38
Justin Greger <justingreger@gmail.com>
2023-11-28 19:19:18 -0500

    # tests:

--- easy_8/9_reverse_it_part_2.rb
+++ easy_8/9_reverse_it_part_2.rb
@@ -27,3 +27,8 @@ puts reverse_words('Launch School')         # => hcnuaL loohcS
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
+
+# tests:
+puts reverse_words('Professional')          # => lanoisseforP
+puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
+puts reverse_words('Launch School')         # => hcnuaL loohcS
