90bc3dae910c24fa192d0606cdd75236e2b49a13
Justin Greger <justingreger@gmail.com>
2023-11-28 21:51:29 -0500

    # Algorithm, Steps for converting input to output:

--- easy_8/9_reverse_it_part_2.rb
+++ easy_8/9_reverse_it_part_2.rb
@@ -42,7 +42,13 @@
 # Data Structure, How we represent data that we will work with when converting the input to output:
 # array.
 
-Algorithm, Steps for converting input to output:
+# Algorithm, Steps for converting input to output:
+
+
+
+
+
+
 and Code. Implementation of Algorithm:
 
 # tests:
