1f798bec1e102f62efa9a2fd56a948271ef36ff9
Justin Greger <justingreger@gmail.com>
2023-11-28 19:35:54 -0500

    # Write a method that takes one argument.

--- easy_8/9_reverse_it_part_2.rb
+++ easy_8/9_reverse_it_part_2.rb
@@ -21,7 +21,7 @@
 # the given string with words that contain five or more characters reversed.
 
 # Make the requirements explicit Identify rules:
-Write a method that takes one argument,
+# Write a method that takes one argument.
 a string containing one or more words,
 and returns the given string with words that contain five or more characters reversed. Each string will consist of only letters and spaces. Spaces should be included only when more than one word is present.
 
