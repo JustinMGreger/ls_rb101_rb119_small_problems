1361a18447b39d00ab1d5de2cc2ee7fb895aa531
Justin Greger <justingreger@gmail.com>
2023-11-29 20:07:56 -0500

    # and if it does not then return the word block parameter.

--- easy_8/9_reverse_it_part_2.rb
+++ easy_8/9_reverse_it_part_2.rb
@@ -47,7 +47,7 @@
 # the words varible is assigned to the result of splitting the string parameter into an array of substrings
 # and then passing the following block of code to each element:
 # if the word block parameter has a number of characters greater than 5 then reverse the word block parameter
-
+# and if it does not then return the word block parameter.
 
 
 
