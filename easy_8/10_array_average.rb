3a9773818b179c69d12f7d0fff4dc558969c07d6
Justin Greger <justingreger@gmail.com>
2023-12-01 18:52:45 -0500

    # Algorithm, Steps for converting input to output:

--- easy_8/10_array_average.rb
+++ easy_8/10_array_average.rb
@@ -42,7 +42,14 @@
 # Data Structure, How we represent data that we will work with when converting the input to output:
 # array.
 
-Algorithm, Steps for converting input to output:
+# Algorithm, Steps for converting input to output:
+def average(array_parameter)
+return the average of all numbers in the array.
+solve this problem using iteration more directly.
+
+
+
+
 and Code. Implementation of Algorithm:
 
 # tests:
