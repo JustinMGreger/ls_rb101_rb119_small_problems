228bcede0ae0538d08bfc742059952f2ab46c09f
Justin Greger <justingreger@gmail.com>
2023-11-30 19:45:53 -0500

    # Write a method that takes one argument, an array containing integers,

--- easy_8/10_array_average.rb
+++ easy_8/10_array_average.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# Write a method that takes one argument,
- an array containing integers, and returns the average of all numbers in the array. The array will never be empty and the numbers will always be positive integers. Your result should also be an integer.
+# Write a method that takes one argument, an array containing integers,
+  and returns the average of all numbers in the array. The array will never be empty and the numbers will always be positive integers. Your result should also be an integer.
 
 Don't use the Array#sum method for your solution - see if you can solve this problem using iteration more directly.
 
