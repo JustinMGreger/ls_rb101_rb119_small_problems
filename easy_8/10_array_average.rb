1bb117892ff82a67c33cb6c41141e209467e7534
Justin Greger <justingreger@gmail.com>
2023-11-30 21:25:34 -0500

    # return the average of all numbers in the array.

--- easy_8/10_array_average.rb
+++ easy_8/10_array_average.rb
@@ -23,7 +23,9 @@
 
 # Make the requirements explicit Identify rules:
 # Write a method that takes one argument, an array containing integers,
-and returns the average of all numbers in the array. The array will never be empty and the numbers will always be positive integers. Your result should also be an integer.
+# return the average of all numbers in the array.
+
+The array will never be empty and the numbers will always be positive integers. Your result should also be an integer.
 
 Don't use the Array#sum method for your solution - see if you can solve this problem using iteration more directly.
 
