7a0c4ee616c90deb784e0642305954b2836ce7ca
Justin Greger <justingreger@gmail.com>
2023-11-30 20:30:17 -0500

    # Examples:

--- easy_8/10_array_average.rb
+++ easy_8/10_array_average.rb
@@ -8,3 +8,6 @@
 # Don't use the Array#sum method for your solution - see if you can solve this problem using iteration more directly.
 
 # Examples:
+puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
