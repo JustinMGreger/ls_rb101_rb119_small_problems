b5a06b78ce656f44306c797d81d1465f29e0a1e9
Justin Greger <justingreger@gmail.com>
2023-11-30 21:42:27 -0500

    deleted - see if you can

--- easy_8/10_array_average.rb
+++ easy_8/10_array_average.rb
@@ -35,7 +35,7 @@ return the average of all numbers in the array.
 The array will never be empty and the numbers will always be positive integers.
 Your result should also be an integer.
 Don't use the Array#sum method for your solution
-- see if you can solve this problem using iteration more directly.
+solve this problem using iteration more directly.
 
 
 
