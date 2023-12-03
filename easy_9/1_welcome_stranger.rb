07b182e45e53c32e6f88d7dfb16362752f0ffe8a
Justin Greger <justingreger@gmail.com>
2023-12-02 20:51:08 -0500

    # when combined with adjoining spaces,

--- easy_9/1_welcome_stranger.rb
+++ easy_9/1_welcome_stranger.rb
@@ -2,6 +2,7 @@
 
 # Create a method that takes 2 arguments, an array and a hash.
 # The array will contain 2 or more elements that,
-when combined with adjoining spaces, will produce a person's name. The hash will contain two keys, :title and :occupation, and the appropriate values. Your method should return a greeting that uses the person's full name, and mentions the person's title and occupation.
+# when combined with adjoining spaces, will produce a person's name.
+The hash will contain two keys, :title and :occupation, and the appropriate values. Your method should return a greeting that uses the person's full name, and mentions the person's title and occupation.
 
 Example:
