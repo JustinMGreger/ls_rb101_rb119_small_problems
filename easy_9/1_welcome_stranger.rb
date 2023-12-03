66151c1512413971f1d598e3c65eadd2959c2658
Justin Greger <justingreger@gmail.com>
2023-12-02 21:03:53 -0500

    # Your method should return a greeting that uses the person

--- easy_9/1_welcome_stranger.rb
+++ easy_9/1_welcome_stranger.rb
@@ -4,6 +4,7 @@
 # The array will contain 2 or more elements that,
 # when combined with adjoining spaces, will produce a person's name.
 # The hash will contain two keys, :title and :occupation, and the appropriate values.
-Your method should return a greeting that uses the person's full name, and mentions the person's title and occupation.
+# Your method should return a greeting that uses the person's full name,
+and mentions the person's title and occupation.
 
 Example:
