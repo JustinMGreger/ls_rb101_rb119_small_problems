dc6aa35c8f6feb6b50018f910d830763e090358a
Justin Greger <justingreger@gmail.com>
2023-12-02 21:41:57 -0500

    # Make the requirements explicit Identify rules: # Create a method that takes 2 arguments, an array and a hash.

--- easy_9/1_welcome_stranger.rb
+++ easy_9/1_welcome_stranger.rb
@@ -22,7 +22,15 @@ p greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
 # output:
 # a greeting that uses the person's full name, and mentions the person's title and occupation.
 
-Make the requirements explicit Identify rules:
+# Make the requirements explicit Identify rules:
+# Create a method that takes 2 arguments, an array and a hash.
+The array will contain 2 or more elements that, when combined with adjoining spaces, will produce a person's name. The hash will contain two keys, :title and :occupation, and the appropriate values. Your method should return a greeting that uses the person's full name, and mentions the person's title and occupation.
+
+
+
+
+
+
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
