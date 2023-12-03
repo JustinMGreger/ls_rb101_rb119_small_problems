177dc829b3e36223ce6cf081be6e08ea92dbe2bd
Justin Greger <justingreger@gmail.com>
2023-12-02 21:47:03 -0500

    # Mental model of the problem (optional):

--- easy_9/1_welcome_stranger.rb
+++ easy_9/1_welcome_stranger.rb
@@ -29,7 +29,17 @@ p greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
 # The hash will contain two keys, :title and :occupation, and the appropriate values.
 # Your method should return a greeting that uses the person's full name, and mentions the person's title and occupation.
 
-Mental model of the problem (optional):
+# Mental model of the problem (optional):
+Create a method that takes 2 arguments, an array and a hash.
+The array will contain 2 or more elements that,
+when combined with adjoining spaces, will produce a person's name.
+The hash will contain two keys, :title and :occupation, and the appropriate values.
+Your method should return a greeting that uses the person's full name, and mentions the person's title and occupation.
+
+
+
+
+
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
