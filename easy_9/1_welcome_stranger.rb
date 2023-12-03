e315110c72c92a47e407eb43f5a08c38b56c3bd5
Justin Greger <justingreger@gmail.com>
2023-12-02 20:43:30 -0500

    # Create a method that takes 2 arguments, an array and a hash.

--- easy_9/1_welcome_stranger.rb
+++ easy_9/1_welcome_stranger.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+# Create a method that takes 2 arguments, an array and a hash.
+The array will contain 2 or more elements that, when combined with adjoining spaces, will produce a person's name. The hash will contain two keys, :title and :occupation, and the appropriate values. Your method should return a greeting that uses the person's full name, and mentions the person's title and occupation.
+
+Example:
