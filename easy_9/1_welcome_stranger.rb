53ce6c4b8947037c876733a210c7952fa762566f
Justin Greger <justingreger@gmail.com>
2023-12-02 21:42:50 -0500

    # The array will contain 2 or more elements that,

--- easy_9/1_welcome_stranger.rb
+++ easy_9/1_welcome_stranger.rb
@@ -24,7 +24,8 @@ p greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
 
 # Make the requirements explicit Identify rules:
 # Create a method that takes 2 arguments, an array and a hash.
-The array will contain 2 or more elements that, when combined with adjoining spaces, will produce a person's name. The hash will contain two keys, :title and :occupation, and the appropriate values. Your method should return a greeting that uses the person's full name, and mentions the person's title and occupation.
+# The array will contain 2 or more elements that,
+when combined with adjoining spaces, will produce a person's name. The hash will contain two keys, :title and :occupation, and the appropriate values. Your method should return a greeting that uses the person's full name, and mentions the person's title and occupation.
 
 
 
