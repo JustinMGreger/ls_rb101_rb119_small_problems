1c55c5b28e302ea0a042d33e37fc5795318dff46
Justin Greger <justingreger@gmail.com>
2023-12-03 19:20:33 -0500

    # title = hash_parameter[:title]

--- easy_9/1_welcome_stranger.rb
+++ easy_9/1_welcome_stranger.rb
@@ -32,6 +32,8 @@
 # Mental model of the problem (optional):
 # def greetings(array_parameter, hash_parameter)
 # name = array_parameter combined with adjoining spaces into a single string.
+# title = hash_parameter[:title]
+
 
 The hash will contain two keys, :title and :occupation, and the appropriate values.
 Your method should return a greeting that uses the person's full name, and mentions the person's title and occupation.
