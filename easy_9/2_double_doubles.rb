fa8d6b68ff5c7cc338ba129a8a89dea09003bf62
Justin Greger <justingreger@gmail.com>
2023-12-04 18:33:55 -0500

    # Write a method that returns 2 times the number provided as an argument,

--- easy_9/2_double_doubles.rb
+++ easy_9/2_double_doubles.rb
@@ -5,6 +5,7 @@
 # For example, 44, 3333, 103103, 7676 are all double numbers.
 # 444, 334433, and 107 are not.
 
-Write a method that returns 2 times the number provided as an argument, unless the argument is a double number; double numbers should be returned as-is.
+# Write a method that returns 2 times the number provided as an argument,
+unless the argument is a double number; double numbers should be returned as-is.
 
 Examples:
