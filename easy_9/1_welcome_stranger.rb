b84f2a3ea22feb5c0fb73cee328127c16eed8f56
Justin Greger <justingreger@gmail.com>
2023-12-03 20:27:36 -0500

    deleted white spaces

--- easy_9/1_welcome_stranger.rb
+++ easy_9/1_welcome_stranger.rb
@@ -15,9 +15,9 @@
 # PEDAC stands for [Understand the] Problem,
 
 # Identify expected input:
-# 2 arguments, an array and a hash. 
+# 2 arguments, an array and a hash.
 # The array will contain 2 or more elements that, when combined with adjoining spaces, will produce a person's name.
-# The hash will contain two keys, :title and :occupation, and the appropriate values. 
+# The hash will contain two keys, :title and :occupation, and the appropriate values.
 
 # output:
 # a greeting that uses the person's full name, and mentions the person's title and occupation.
