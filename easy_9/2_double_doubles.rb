88ae9aec5e9032ca23c7fad773d068e4f32003ee
Justin Greger <justingreger@gmail.com>
2023-12-04 19:51:38 -0500

    # double_number = number even number of digits && whose left-side digits are exactly the same as # its right-side digits.

--- easy_9/2_double_doubles.rb
+++ easy_9/2_double_doubles.rb
@@ -43,9 +43,9 @@
 
 # Mental model of the problem (optional):
 # def twice(number)
+# double_number = number even number of digits && whose left-side digits are exactly the same as
+# its right-side digits.
 
-A double number is a number with an even number of digits whose left-side digits are exactly the same as
-its right-side digits.
 For example, 44, 3333, 103103, 7676 are all double numbers. 444, 334433, and 107 are not.
 Write a method that returns 2 times the number provided as an argument,
 unless the argument is a double number; double numbers should be returned as-is.
