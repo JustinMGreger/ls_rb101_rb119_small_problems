2e22ce56bd6e3891d2263528ea1897abf5a3f54c
Justin Greger <justingreger@gmail.com>
2023-12-04 19:48:36 -0500

    # def twice(number)

--- easy_9/2_double_doubles.rb
+++ easy_9/2_double_doubles.rb
@@ -42,6 +42,8 @@
 # unless the argument is a double number; double numbers should be returned as-is.
 
 # Mental model of the problem (optional):
+# def twice(number)
+
 A double number is a number with an even number of digits whose left-side digits are exactly the same as
 its right-side digits.
 For example, 44, 3333, 103103, 7676 are all double numbers. 444, 334433, and 107 are not.
