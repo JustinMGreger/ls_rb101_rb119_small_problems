d69b54e723b7ec28970732ffa2ec2baabd8d2c59
Justin Greger <justingreger@gmail.com>
2023-12-04 19:31:20 -0500

    # p twice

--- easy_9/2_double_doubles.rb
+++ easy_9/2_double_doubles.rb
@@ -9,16 +9,16 @@
 # unless the argument is a double number; double numbers should be returned as-is.
 
 # Examples:
-p twice(37) == 74
-p twice(44) == 44
-p twice(334_433) == 668_866
-p twice(444) == 888
-p twice(107) == 214
-p twice(103_103) == 103_103
-p twice(3333) == 3333
-p twice(7676) == 7676
-p twice(123_456_789_123_456_789) == 123_456_789_123_456_789
-p twice(5) == 10
+# p twice(37) == 74
+# p twice(44) == 44
+# p twice(334_433) == 668_866
+# p twice(444) == 888
+# p twice(107) == 214
+# p twice(103_103) == 103_103
+# p twice(3333) == 3333
+# p twice(7676) == 7676
+# p twice(123_456_789_123_456_789) == 123_456_789_123_456_789
+# p twice(5) == 10
 
 # NOTE: underscores are used for clarity above.
 # Ruby lets you use underscores when writing long numbers;
