52b39a67a2384a0f5003e8e9f6157dc5db8fa46e
Justin Greger <justingreger@gmail.com>
2023-12-04 19:28:03 -0500

    # Examples / Test Cases, Validate understanding of the problem:

--- easy_9/2_double_doubles.rb
+++ easy_9/2_double_doubles.rb
@@ -36,7 +36,19 @@ p twice(5) == 10
 
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
-Examples / Test Cases, Validate understanding of the problem:
+
+# Examples / Test Cases, Validate understanding of the problem:
+p twice(37) == 74
+p twice(44) == 44
+p twice(334_433) == 668_866
+p twice(444) == 888
+p twice(107) == 214
+p twice(103_103) == 103_103
+p twice(3333) == 3333
+p twice(7676) == 7676
+p twice(123_456_789_123_456_789) == 123_456_789_123_456_789
+p twice(5) == 10
+
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
