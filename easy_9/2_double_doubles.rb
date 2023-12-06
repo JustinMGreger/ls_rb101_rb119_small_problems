ab364ff76a95c2485ed9948d864afb1697c4e098
Justin Greger <justingreger@gmail.com>
2023-12-05 20:11:55 -0500

    # and Code. Implementation of Algorithm:

--- easy_9/2_double_doubles.rb
+++ easy_9/2_double_doubles.rb
@@ -82,21 +82,24 @@
 
 # and Code. Implementation of Algorithm:
 def double_number?(number)
-the num_str varible is assigned to the result of transforming the number parameter into string form.
-the len varible is assigned to the result of returning the number of characters in the num_str varible.
-return false if len is not even.
-the left_side varible is assigned to the result of returning the value located from the index location
-of the 0 integer to the result of dividing the len varible by the integer 2 in the num_str varible.
-the right_side varible is assigned the result of returning the value located from the index location of
-dividing the len varible by the integer 2 to the len varible in the num_str varible.
-return the boolean result of determining if the left_side varible is equal to the right_side varible.
-this is the last line of the double_number? method.
-the twice method accepts the number parameter and is defined as the following:
-if the result of passing the number parameter to the double_number? method returns true then return the
-number parameter.
-else return the result of multiplying the number variable by the integer 2.
-this is the last line of the twice method.
+  num_str = number.to_s
+  len = num_str.length
 
+  return false unless len.even?
+
+  left_side = num_str[0, len / 2]
+  right_side = num_str[len / 2, len]
+
+  left_side == right_side
+end
+
+def twice(number)
+  if double_number?(number)
+    number
+  else
+    2 * number
+  end
+end
 
 # tests:
 p twice(37) == 74
