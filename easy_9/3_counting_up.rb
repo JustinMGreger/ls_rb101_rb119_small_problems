999c2999a1cb9560ed86a8f54ad8fca1d9654b3b
Justin Greger <justingreger@gmail.com>
2023-12-06 19:35:25 -0500

    # Examples / Test Cases, Validate understanding of the problem:

--- easy_9/3_counting_up.rb
+++ easy_9/3_counting_up.rb
@@ -29,7 +29,11 @@ Write a method that takes an integer argument,
 and returns an Array of all integers, in sequence, between 1 and the argument.
 You may assume that the argument will always be a valid integer that is greater than 0.
 
-Examples / Test Cases, Validate understanding of the problem:
+# Examples / Test Cases, Validate understanding of the problem:
+p sequence(5) == [1, 2, 3, 4, 5]
+p sequence(3) == [1, 2, 3]
+p sequence(1) == [1]
+
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
