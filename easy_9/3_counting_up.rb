8c8ee6e1e3e93649bd5282df6cada0f3c5faa317
Justin Greger <justingreger@gmail.com>
2023-12-06 20:55:41 -0500

    # Further Exploration

--- easy_9/3_counting_up.rb
+++ easy_9/3_counting_up.rb
@@ -51,6 +51,10 @@ def sequence(number)
   (1..number).to_a
 end
 
+# Further Exploration
+
+Food for thought: what do you think sequence should return if the argument is not greater than 0. For instance, what should you do if the argument is -1? Can you alter your method to handle this case?
+
 # tests:
 p sequence(5) == [1, 2, 3, 4, 5]
 p sequence(3) == [1, 2, 3]
