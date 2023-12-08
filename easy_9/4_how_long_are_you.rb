1b36586585b634ee1b309e7832602e22e080ade7
Justin Greger <justingreger@gmail.com>
2023-12-07 21:42:13 -0500

    double to single quotes

--- easy_9/4_how_long_are_you.rb
+++ easy_9/4_how_long_are_you.rb
@@ -8,10 +8,10 @@
 # and that any substring of non-space characters is a word.
 
 # Examples
-p word_lengths('cow sheep chicken') == ["cow 3", "sheep 5", "chicken 7"]
+p word_lengths('cow sheep chicken') == ['cow 3', 'sheep 5', 'chicken 7']
 
 p word_lengths('baseball hot dogs and apple pie') ==
-  ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+  ['baseball 8', 'hot 3', 'dogs 4', 'and 3', 'apple 5', 'pie 3']
 
 p word_lengths('It ain\'t easy, is it?') == ['It 2', 'ain\'t 5', 'easy, 5', 'is 2', 'it? 3']
 
