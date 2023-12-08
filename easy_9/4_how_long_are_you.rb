a0a5f0d68313f01f1528301a8f1480ea6913d48d
Justin Greger <justingreger@gmail.com>
2023-12-07 20:44:20 -0500

    # to which you have appended a space and the word length.

--- easy_9/4_how_long_are_you.rb
+++ easy_9/4_how_long_are_you.rb
@@ -2,7 +2,7 @@
 
 # Write a method that takes a string as an argument,
 # and returns an Array that contains every word from the string,
-to which you have appended a space and the word length.
+# to which you have appended a space and the word length.
 
 You may assume that words in the string are separated by exactly one space, and that any substring of non-space characters is a word.
 
