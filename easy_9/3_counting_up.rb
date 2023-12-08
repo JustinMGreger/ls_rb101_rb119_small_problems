9f16cf09c35b13e9f7a95bb9dc8ebaea5ac6a921
Justin Greger <justingreger@gmail.com>
2023-12-07 19:15:23 -0500

    # For instance, what should you do if the argument is -1? # Can you alter your method to handle this case?

--- easy_9/3_counting_up.rb
+++ easy_9/3_counting_up.rb
@@ -54,7 +54,8 @@ end
 # Further Exploration
 
 # Food for thought: what do you think sequence should return if the argument is not greater than 0.
-For instance, what should you do if the argument is -1? Can you alter your method to handle this case?
+# For instance, what should you do if the argument is -1?
+# Can you alter your method to handle this case?
 
 # tests:
 p sequence(5) == [1, 2, 3, 4, 5]
