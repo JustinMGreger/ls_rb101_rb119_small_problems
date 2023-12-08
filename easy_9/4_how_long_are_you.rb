ac14564df57987fa74b0e795e7b01bc137994cc9
Justin Greger <justingreger@gmail.com>
2023-12-07 21:58:58 -0500

    # Data Structure, How we represent data that we will work with when converting the input to output: # array.

--- easy_9/4_how_long_are_you.rb
+++ easy_9/4_how_long_are_you.rb
@@ -45,7 +45,9 @@ Mental model of the problem (optional):
 
 # p word_lengths('') == []
 
-Data Structure, How we represent data that we will work with when converting the input to output:
+# Data Structure, How we represent data that we will work with when converting the input to output:
+# array.
+
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
 
