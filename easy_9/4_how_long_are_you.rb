7cf3cc247133536a489da23d26db2e7cb53a6678
Justin Greger <justingreger@gmail.com>
2023-12-07 21:52:33 -0500

    # Identify expected input: # a string argument.

--- easy_9/4_how_long_are_you.rb
+++ easy_9/4_how_long_are_you.rb
@@ -23,7 +23,9 @@ p word_lengths('') == []
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# a string argument.
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
