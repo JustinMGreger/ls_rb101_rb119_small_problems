227473c6e299cd49d167251639de79ef3b0cc7c3
Justin Greger <justingreger@gmail.com>
2023-12-07 21:56:19 -0500

    # Examples / Test Cases, Validate understanding of the problem:

--- easy_9/4_how_long_are_you.rb
+++ easy_9/4_how_long_are_you.rb
@@ -31,7 +31,20 @@ p word_lengths('') == []
 
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
-Examples / Test Cases, Validate understanding of the problem:
+
+# Examples / Test Cases, Validate understanding of the problem:
+p word_lengths('cow sheep chicken') == ['cow 3', 'sheep 5', 'chicken 7']
+
+p word_lengths('baseball hot dogs and apple pie') ==
+  ['baseball 8', 'hot 3', 'dogs 4', 'and 3', 'apple 5', 'pie 3']
+
+p word_lengths('It ain\'t easy, is it?') == ['It 2', 'ain\'t 5', 'easy, 5', 'is 2', 'it? 3']
+
+p word_lengths('Supercalifragilisticexpialidocious') ==
+  ['Supercalifragilisticexpialidocious 34']
+
+p word_lengths('') == []
+
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
