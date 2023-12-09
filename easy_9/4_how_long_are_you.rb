c540990dcf4728366e2a1a607877e275234f2964
Justin Greger <justingreger@gmail.com>
2023-12-08 19:06:27 -0500

    # and returns an Array that contains every word from the string,

--- easy_9/4_how_long_are_you.rb
+++ easy_9/4_how_long_are_you.rb
@@ -31,7 +31,8 @@
 
 # Make the requirements explicit Identify rules:
 # Write a method that takes a string as an argument,
-and returns an Array that contains every word from the string, to which you have appended a space and the word length.
+# and returns an Array that contains every word from the string,
+to which you have appended a space and the word length.
 
 You may assume that words in the string are separated by exactly one space, and that any substring of non-space characters is a word.
 
