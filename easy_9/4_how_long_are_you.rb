0b965b3876720f6015c4242bba10ecd5061c046b
Justin Greger <justingreger@gmail.com>
2023-12-08 19:07:37 -0500

    deleted You may assume that words in the string are separated by exactly one space, and that any substring of non-space characters is a word.

--- easy_9/4_how_long_are_you.rb
+++ easy_9/4_how_long_are_you.rb
@@ -34,11 +34,6 @@
 # and returns an Array that contains every word from the string,
 # to which you have appended a space and the word length.
 
-You may assume that words in the string are separated by exactly one space, and that any substring of non-space characters is a word.
-
-
-
-
 Mental model of the problem (optional):
 
 # Examples / Test Cases, Validate understanding of the problem:
