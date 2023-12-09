4094029d20fdd2aa8e6a0038f86cd34c20009363
Justin Greger <justingreger@gmail.com>
2023-12-08 19:04:23 -0500

    # Make the requirements explicit Identify rules:

--- easy_9/4_how_long_are_you.rb
+++ easy_9/4_how_long_are_you.rb
@@ -29,7 +29,14 @@
 # output:
 # returns an Array that contains every word from the string, to which you have appended a space and the word length.
 
-Make the requirements explicit Identify rules:
+# Make the requirements explicit Identify rules:
+Write a method that takes a string as an argument, and returns an Array that contains every word from the string, to which you have appended a space and the word length.
+
+You may assume that words in the string are separated by exactly one space, and that any substring of non-space characters is a word.
+
+
+
+
 Mental model of the problem (optional):
 
 # Examples / Test Cases, Validate understanding of the problem:
