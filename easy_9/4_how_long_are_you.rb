f17a88b0b3030510c6867f7d0b53b4f8021d1420
Justin Greger <justingreger@gmail.com>
2023-12-08 19:05:23 -0500

    # Write a method that takes a string as an argument,

--- easy_9/4_how_long_are_you.rb
+++ easy_9/4_how_long_are_you.rb
@@ -30,7 +30,8 @@
 # returns an Array that contains every word from the string, to which you have appended a space and the word length.
 
 # Make the requirements explicit Identify rules:
-Write a method that takes a string as an argument, and returns an Array that contains every word from the string, to which you have appended a space and the word length.
+# Write a method that takes a string as an argument,
+and returns an Array that contains every word from the string, to which you have appended a space and the word length.
 
 You may assume that words in the string are separated by exactly one space, and that any substring of non-space characters is a word.
 
