b59edd721b43c2dff4cdbfd31e9f99050673a998
Justin Greger <justingreger@gmail.com>
2023-12-09 21:48:45 -0500

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for [Understand the] Problem,

--- easy_9/5_name_swapping.rb
+++ easy_9/5_name_swapping.rb
@@ -5,3 +5,15 @@
 
 # Examples
 p swap_name('Joe Roberts') == 'Roberts, Joe'
+
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for [Understand the] Problem,
+
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code. Implementation of Algorithm:
