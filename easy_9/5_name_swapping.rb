d1595f97de39a7cb301e5a409d666b1a7addc9a9
Justin Greger <justingreger@gmail.com>
2023-12-09 21:44:25 -0500

    # and returns a string that contains the last name, a comma, a space, and the first name.

--- easy_9/5_name_swapping.rb
+++ easy_9/5_name_swapping.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 # Write a method that takes a first name, a space, and a last name passed as a single String argument,
-and returns a string that contains the last name, a comma, a space, and the first name.
+# and returns a string that contains the last name, a comma, a space, and the first name.
 
 Examples
