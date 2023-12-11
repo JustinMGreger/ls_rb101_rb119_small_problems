648be910970cc8115b789fcbf3806e40879d41cb
Justin Greger <justingreger@gmail.com>
2023-12-10 21:25:45 -0500

    # The first argument is a count, and the second is the first number of a sequence that your method will create.

--- easy_9/6_sequence_count.rb
+++ easy_9/6_sequence_count.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 # Create a method that takes two integers as arguments.
-The first argument is a count, and the second is the first number of a sequence that your method will create. The method should return an Array that contains the same number of elements as the count argument, while the values of each element will be multiples of the starting number.
+# The first argument is a count, and the second is the first number of a sequence that your method will create.
+The method should return an Array that contains the same number of elements as the count argument, while the values of each element will be multiples of the starting number.
 
 You may assume that the count argument will always have a value of 0 or greater, while the starting number can be any integer value. If the count is 0, an empty list should be returned.
 
