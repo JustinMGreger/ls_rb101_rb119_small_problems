d3468bfa8f560c1ccf6ddf3e0cb10eb6361dbd00
Justin Greger <justingreger@gmail.com>
2023-12-10 19:00:54 -0500

    # Write a method that takes a first name, a space, and a last name passed as a single String argument, # and returns a string that contains the last name, a comma, a space, and the first name.

--- easy_9/5_name_swapping.rb
+++ easy_9/5_name_swapping.rb
@@ -16,8 +16,8 @@
 # a string that contains the last name, a comma, a space, and the first name.
 
 # Make the requirements explicit Identify rules:
-Write a method that takes a first name, a space, and a last name passed as a single String argument,
-and returns a string that contains the last name, a comma, a space, and the first name.
+# Write a method that takes a first name, a space, and a last name passed as a single String argument,
+# and returns a string that contains the last name, a comma, a space, and the first name.
 
 Mental model of the problem (optional):
 
