ca1019b8bd7b852598b004902649d5f25f4219eb
Justin Greger <justingreger@gmail.com>
2023-12-10 21:46:52 -0500

    # p sequence

--- easy_9/6_sequence_count.rb
+++ easy_9/6_sequence_count.rb
@@ -10,10 +10,10 @@
 # If the count is 0, an empty list should be returned.
 
 # Examples:
-p sequence(5, 1) == [1, 2, 3, 4, 5]
-p sequence(4, -7) == [-7, -14, -21, -28]
-p sequence(3, 0) == [0, 0, 0]
-p sequence(0, 1000000) == []
+# p sequence(5, 1) == [1, 2, 3, 4, 5]
+# p sequence(4, -7) == [-7, -14, -21, -28]
+# p sequence(3, 0) == [0, 0, 0]
+# p sequence(0, 1000000) == []
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
