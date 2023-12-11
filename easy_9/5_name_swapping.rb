c36e8081a61727c08857e11121ff0e8623ed2112
Justin Greger <justingreger@gmail.com>
2023-12-10 19:02:13 -0500

    # Mental model of the problem (optional):

--- easy_9/5_name_swapping.rb
+++ easy_9/5_name_swapping.rb
@@ -19,7 +19,12 @@
 # Write a method that takes a first name, a space, and a last name passed as a single String argument,
 # and returns a string that contains the last name, a comma, a space, and the first name.
 
-Mental model of the problem (optional):
+# Mental model of the problem (optional):
+Write a method that takes a first name, a space, and a last name passed as a single String argument,
+and returns a string that contains the last name, a comma, a space, and the first name.
+
+
+
 
 # Examples / Test Cases, Validate understanding of the problem:
 # p swap_name('Joe Roberts') == 'Roberts, Joe'
