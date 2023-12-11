5ff3cdc8e7b56fe8257a0f80371d312a5787f701
Justin Greger <justingreger@gmail.com>
2023-12-10 19:05:13 -0500

    # def swap_name(first name, a space, and a last name passed as a single String argument)

--- easy_9/5_name_swapping.rb
+++ easy_9/5_name_swapping.rb
@@ -20,7 +20,7 @@
 # and returns a string that contains the last name, a comma, a space, and the first name.
 
 # Mental model of the problem (optional):
-Write a method that takes a first name, a space, and a last name passed as a single String argument,
+# def swap_name(first name, a space, and a last name passed as a single String argument)
 and returns a string that contains the last name, a comma, a space, and the first name.
 
 
