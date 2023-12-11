650a9c7461a8f9571d7b612518fa4c543ebfb935
Justin Greger <justingreger@gmail.com>
2023-12-10 21:35:10 -0500

    p sequence

--- easy_9/6_sequence_count.rb
+++ easy_9/6_sequence_count.rb
@@ -10,3 +10,7 @@
 # If the count is 0, an empty list should be returned.
 
 # Examples:
+p sequence(5, 1) == [1, 2, 3, 4, 5]
+p sequence(4, -7) == [-7, -14, -21, -28]
+p sequence(3, 0) == [0, 0, 0]
+p sequence(0, 1000000) == []
