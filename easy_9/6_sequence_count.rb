1c50b73a3fd904c16e8bb7b702d1019d3893ec5e
Justin Greger <justingreger@gmail.com>
2023-12-11 20:13:25 -0500

    # You may assume that the count argument will always have a value of 0 or greater,

--- easy_9/6_sequence_count.rb
+++ easy_9/6_sequence_count.rb
@@ -31,8 +31,8 @@
 # The first argument is a count, and the second is the first number of a sequence that your method will create.
 # The method should return an Array that contains the same number of elements as the count argument,
 # while the values of each element will be multiples of the starting number.
-
-You may assume that the count argument will always have a value of 0 or greater, while the starting number can be any integer value. If the count is 0, an empty list should be returned.
+# You may assume that the count argument will always have a value of 0 or greater,
+ while the starting number can be any integer value. If the count is 0, an empty list should be returned.
 
 Mental model of the problem (optional):
 
