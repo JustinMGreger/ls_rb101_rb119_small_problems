8f24b4c7158e4d39710aea76a2247db6799b13a0
Justin Greger <justingreger@gmail.com>
2023-12-13 17:28:08 -0500

    # Algorithm, Steps for converting input to output:

--- easy_9/7_grade_book.rb
+++ easy_9/7_grade_book.rb
@@ -49,6 +49,16 @@
 # array.
 
 # Algorithm, Steps for converting input to output:
+def get_grade(three_scores)
+score = the (average) of the three_scores
+Numerical Score Letter	Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+return the letter value associated with that grade.
+
 # and Code. Implementation of Algorithm:
 
 # tests:
