a7f79cd4e7d475299cd5370f930deb296cb5754a
Justin Greger <justingreger@gmail.com>
2023-12-12 21:57:08 -0500

    # Numerical Score Letter	Grade

--- easy_9/7_grade_book.rb
+++ easy_9/7_grade_book.rb
@@ -33,6 +33,13 @@
 # Mental model of the problem (optional):
 # def get_grade(three_scores)
 # determine the mean (average) of the three_scores.
+
+# Numerical Score Letter	Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
 and returns the letter value associated with that grade.
 
 
