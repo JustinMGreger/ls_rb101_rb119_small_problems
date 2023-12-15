3fa18307d50947d5e3dfb963a1acee2e7d02a944
Justin Greger <justingreger@gmail.com>
2023-12-15 17:45:10 -0500

    double to single quotes

--- easy_9/8_grocery_list.rb
+++ easy_9/8_grocery_list.rb
@@ -4,5 +4,5 @@
 # and converts it into an array of the correct number of each fruit.
 
 # Example:
-p buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) ==
-  ["apples", "apples", "apples", "orange", "bananas","bananas"]
+p buy_fruit([['apples', 3], ['orange', 1], ['bananas', 2]]) ==
+  ['apples', 'apples', 'apples', 'orange', 'bananas','bananas']
