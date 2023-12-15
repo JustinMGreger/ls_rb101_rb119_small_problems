b2e8e92b11ec7e8c3d37295365d6ad1c85b9df13
Justin Greger <justingreger@gmail.com>
2023-12-14 21:38:45 -0500

    # Further exploration Solution:

--- easy_9/7_grade_book.rb
+++ easy_9/7_grade_book.rb
@@ -85,6 +85,19 @@
 
 # How would you handle this if there was a possibility of extra credit grades causing it to exceed 100 points?
 
+# Further exploration Solution:
+def get_grade(s1, s2, s3)
+  result = (s1 + s2 + s3)/3
+
+  case result
+  when 90.. then 'A'
+  when 80..89  then 'B'
+  when 70..79  then 'C'
+  when 60..69  then 'D'
+  else              'F'
+  end
+end
+
 # tests:
 p get_grade(95, 90, 93) == 'A'
 p get_grade(50, 50, 95) == 'D'
