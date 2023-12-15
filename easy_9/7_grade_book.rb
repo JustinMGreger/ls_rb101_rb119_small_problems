188d5913d015cff8f4849685fa0832c3ff53b023
Justin Greger <justingreger@gmail.com>
2023-12-14 21:19:55 -0500

    # LS Solution:

--- easy_9/7_grade_book.rb
+++ easy_9/7_grade_book.rb
@@ -69,17 +69,17 @@
 # end
 
 # LS Solution:
-def get_grade(s1, s2, s3)
-  result = (s1 + s2 + s3)/3
+# def get_grade(s1, s2, s3)
+#   result = (s1 + s2 + s3)/3
 
-  case result
-  when 90..100 then 'A'
-  when 80..89  then 'B'
-  when 70..79  then 'C'
-  when 60..69  then 'D'
-  else              'F'
-  end
-end
+#   case result
+#   when 90..100 then 'A'
+#   when 80..89  then 'B'
+#   when 70..79  then 'C'
+#   when 60..69  then 'D'
+#   else              'F'
+#   end
+# end
 
 # tests:
 p get_grade(95, 90, 93) == 'A'
