17e7d2f5ac83c0fa5e5ce82fed0d329a9ee89288
Justin Greger <justingreger@gmail.com>
2023-12-15 17:41:06 -0500

    # Write a method which takes a grocery list (array) of fruits with quantities

--- easy_9/8_grocery_list.rb
+++ easy_9/8_grocery_list.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+# Write a method which takes a grocery list (array) of fruits with quantities
+ and converts it into an array of the correct number of each fruit.
+
+Example:
