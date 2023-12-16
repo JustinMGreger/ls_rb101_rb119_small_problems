84d93ccd88c43fd094afe9c424630b89e11eb9fc
Justin Greger <justingreger@gmail.com>
2023-12-15 19:38:41 -0500

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for [Understand the] Problem,

--- easy_9/8_grocery_list.rb
+++ easy_9/8_grocery_list.rb
@@ -6,3 +6,15 @@
 # Example:
 p buy_fruit([['apples', 3], ['orange', 1], ['bananas', 2]]) ==
   %w[apples apples apples orange bananas bananas]
+
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for [Understand the] Problem,
+
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code. Implementation of Algorithm:
