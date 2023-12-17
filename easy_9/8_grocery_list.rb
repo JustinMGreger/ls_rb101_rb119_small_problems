8d9d2d7dfba9d2a3598e0cd87578cf9674368a36
Justin Greger <justingreger@gmail.com>
2023-12-16 21:36:08 -0500

    # pass the following block of code to each element in the grocery_list parameter:

--- easy_9/8_grocery_list.rb
+++ easy_9/8_grocery_list.rb
@@ -34,8 +34,7 @@
 # Algorithm, Steps for converting input to output:
 # the buy_fruit method accepts the (grocery_list) parameter and is defined as the following:
 # the new_array varible is assigned the [] array.
-
-
+# pass the following block of code to each element in the grocery_list parameter:
 
 
 and Code. Implementation of Algorithm:
