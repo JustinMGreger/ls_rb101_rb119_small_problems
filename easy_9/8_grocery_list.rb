c70d2f14a0cd4a6869a7b4dd7b4eeb4255b7a109
Justin Greger <justingreger@gmail.com>
2023-12-16 21:51:54 -0500

    # Solution 1:

--- easy_9/8_grocery_list.rb
+++ easy_9/8_grocery_list.rb
@@ -49,5 +49,17 @@
 #   new_array
 # end
 
+# Solution 1:
+def buy_fruit(list)
+  expanded_list = []
+
+  list.each do |item|
+    fruit, quantity = item[0], item[1]
+    quantity.times { expanded_list << fruit }
+  end
+
+  expanded_list
+end
+
 # tests:
 p buy_fruit([['apples', 3], ['orange', 1], ['bananas', 2]]) == %w[apples apples apples orange bananas bananas]
