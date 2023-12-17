965a7930a3bf4c244f6b8a2b6e990103cfd43fc2
Justin Greger <justingreger@gmail.com>
2023-12-16 19:09:28 -0500

    # Make the requirements explicit Identify rules:

--- easy_9/8_grocery_list.rb
+++ easy_9/8_grocery_list.rb
@@ -16,7 +16,7 @@ p buy_fruit([['apples', 3], ['orange', 1], ['bananas', 2]]) ==
 # output:
 # an array of the correct number of each fruit.
 
-Make the requirements explicit Identify rules:
+# Make the requirements explicit Identify rules:
 # Write a method which takes a grocery list (array) of fruits with quantities
 and converts it into an array of the correct number of each fruit.
 
