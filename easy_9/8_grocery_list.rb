12a784d40661c7e81a918ef6c95049e3e3284285
Justin Greger <justingreger@gmail.com>
2023-12-16 19:09:53 -0500

    # and converts it into an array of the correct number of each fruit.

--- easy_9/8_grocery_list.rb
+++ easy_9/8_grocery_list.rb
@@ -18,11 +18,7 @@ p buy_fruit([['apples', 3], ['orange', 1], ['bananas', 2]]) ==
 
 # Make the requirements explicit Identify rules:
 # Write a method which takes a grocery list (array) of fruits with quantities
-and converts it into an array of the correct number of each fruit.
-
-
-
-
+# and converts it into an array of the correct number of each fruit.
 
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
