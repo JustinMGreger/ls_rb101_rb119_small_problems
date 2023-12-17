e6314ea5a654686c065dd89d7b8de435d7ab35d5
Justin Greger <justingreger@gmail.com>
2023-12-16 21:34:01 -0500

    deleted new_array = grocery_list in a single array of the result of fruit count times.

--- easy_9/8_grocery_list.rb
+++ easy_9/8_grocery_list.rb
@@ -33,7 +33,7 @@
 
 # Algorithm, Steps for converting input to output:
 # the buy_fruit method accepts the (grocery_list) parameter and is defined as the following:
-new_array = grocery_list in a single array of the result of fruit count times.
+
 
 
 
