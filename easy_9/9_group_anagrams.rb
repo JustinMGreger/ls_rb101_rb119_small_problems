a5aa8f89e29270093009a254e301fc730b8eef6d
Justin Greger <justingreger@gmail.com>
2023-12-17 21:03:04 -0500

    # Your output should look something like this:

--- easy_9/9_group_anagrams.rb
+++ easy_9/9_group_anagrams.rb
@@ -6,4 +6,4 @@
 
 # Write a program that prints out groups of words that are anagrams.
 # Anagrams are words that have the same exact letters in them but in a different order.
-Your output should look something like this:
+# Your output should look something like this:
