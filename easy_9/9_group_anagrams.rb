443d09cae28e467adf39d184919538496931ddcc
Justin Greger <justingreger@gmail.com>
2023-12-17 21:13:14 -0500

    %w[

--- easy_9/9_group_anagrams.rb
+++ easy_9/9_group_anagrams.rb
@@ -7,6 +7,6 @@
 # Write a program that prints out groups of words that are anagrams.
 # Anagrams are words that have the same exact letters in them but in a different order.
 # Your output should look something like this:
-["demo", "dome", "mode"]
+%w["demo", "dome", "mode"]
 # %w[neon none]
 # (etc)
