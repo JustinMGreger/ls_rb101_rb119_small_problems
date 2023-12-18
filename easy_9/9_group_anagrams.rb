c5622111c2e7c42bc296aa2457ad702b7f333a10
Justin Greger <justingreger@gmail.com>
2023-12-17 21:02:47 -0500

    # Anagrams are words that have the same exact letters in them but in a different order.

--- easy_9/9_group_anagrams.rb
+++ easy_9/9_group_anagrams.rb
@@ -5,4 +5,5 @@
 # words = %w[demo none tied evil dome mode live fowl veil wolf diet vile edit tide flow neon]
 
 # Write a program that prints out groups of words that are anagrams.
-Anagrams are words that have the same exact letters in them but in a different order. Your output should look something like this:
+# Anagrams are words that have the same exact letters in them but in a different order.
+Your output should look something like this:
