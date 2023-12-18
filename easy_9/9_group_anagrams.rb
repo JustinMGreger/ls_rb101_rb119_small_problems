0450d7d3b7aea0fc062668b539f664404db2e261
Justin Greger <justingreger@gmail.com>
2023-12-17 21:56:11 -0500

    # extract words from the words parameter.

--- easy_9/9_group_anagrams.rb
+++ easy_9/9_group_anagrams.rb
@@ -27,6 +27,7 @@
 words = %w[demo none tied evil dome mode live fowl veil wolf diet vile edit tide flow neon]
 
 # def anagrams(words)
+# extract words from the words parameter.
 prints out groups of words that are anagrams.
 Anagrams are words that have the same exact letters in them but in a different order.
 
