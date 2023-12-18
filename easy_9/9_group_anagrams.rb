73c330dcf7bcd569fbf6b239ee85c5d95db53cc3
Justin Greger <justingreger@gmail.com>
2023-12-17 20:41:27 -0500

    words =

--- easy_9/9_group_anagrams.rb
+++ easy_9/9_group_anagrams.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
 # Given the array...
+
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
