dc963312e0f05759e09949aa0830757aa89a25c6
Justin Greger <justingreger@gmail.com>
2023-12-17 20:44:00 -0500

    deleted quotes and commas

--- easy_9/9_group_anagrams.rb
+++ easy_9/9_group_anagrams.rb
@@ -2,6 +2,6 @@
 
 # Given the array...
 
-words = %w['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
-          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
-          'flow', 'neon']
+words = %w[demo none tied evil dome mode live
+          fowl veil wolf diet vile edit tide
+          flow neon]
