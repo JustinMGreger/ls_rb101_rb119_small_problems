2e2d15682bf82fe461fec54b0f4377e9c1d81ab8
Justin Greger <justingreger@gmail.com>
2023-12-18 19:04:48 -0500

    # Algorithm, Steps for converting input to output:

--- easy_9/9_group_anagrams.rb
+++ easy_9/9_group_anagrams.rb
@@ -32,10 +32,6 @@
 # create all combinations of the letters that are words.
 # print out groups of words that are anagrams.
 # Anagrams are words that have the same exact letters in them but in a different order.
-# p anagrams(words) ==
-# %w[demo dome mode]
-# %w[neon none]
-# (etc)
 
 # Examples / Test Cases, Validate understanding of the problem:
 # p anagrams(words) ==
@@ -46,7 +42,16 @@
 # Data Structure, How we represent data that we will work with when converting the input to output:
 # array.
 
-Algorithm, Steps for converting input to output:
+# Algorithm, Steps for converting input to output:
+words = %w[demo none tied evil dome mode live fowl veil wolf diet vile edit tide flow neon]
+def anagrams(words)
+extract words from the words parameter.
+a new array is created for each of the words.
+the words are then broken into letters.
+create all combinations of the letters that are words.
+print out groups of words that are anagrams.
+Anagrams are words that have the same exact letters in them but in a different order.
+
 and Code. Implementation of Algorithm:
 
 # test:
