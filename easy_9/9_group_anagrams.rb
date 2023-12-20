1a28e4a72a765259d8ba6654c1a89bbeb6499224
Justin Greger <justingreger@gmail.com>
2023-12-19 21:57:18 -0500

    # LS Solution:

--- easy_9/9_group_anagrams.rb
+++ easy_9/9_group_anagrams.rb
@@ -76,5 +76,22 @@
 #   %w[fowl wolf flow]
 # ]
 
+# LS Solution:
+result = {}
+
+words.each do |word|
+  key = word.split('').sort.join
+  if result.has_key?(key)
+    result[key].push(word)
+  else
+    result[key] = [word]
+  end
+end
+
+result.each_value do |v|
+  puts "------"
+  p v
+end
+
 # test:
 p anagrams(words).sort == expected_result.sort
