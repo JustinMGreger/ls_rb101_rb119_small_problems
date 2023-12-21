22987754492da8722ad04dcd1117a457f9ddb06d
Justin Greger <justingreger@gmail.com>
2023-12-20 21:02:08 -0500

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for [Understand the] Problem,

--- easy_9/10_sum_of_digits.rb
+++ easy_9/10_sum_of_digits.rb
@@ -10,3 +10,15 @@ puts sum(123_456_789) == 45
 # The tests above should print true.
 
 # For a challenge, try writing this without any basic looping constructs (while, until, loop, and each).
+
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for [Understand the] Problem,
+
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code. Implementation of Algorithm:
