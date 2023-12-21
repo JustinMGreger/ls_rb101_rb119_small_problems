4c2121efa0f2b6ef95d1e245c84a3178141b4864
Justin Greger <justingreger@gmail.com>
2023-12-20 21:22:36 -0500

    # Make the requirements explicit Identify rules:

--- easy_9/10_sum_of_digits.rb
+++ easy_9/10_sum_of_digits.rb
@@ -20,7 +20,10 @@ puts sum(123_456_789) == 45
 # output:
 # the sum of its digits.
 
-Make the requirements explicit Identify rules:
+# Make the requirements explicit Identify rules:
+Write a method that takes one argument, a positive integer, and returns the sum of its digits.
+For a challenge, try writing this without any basic looping constructs (while, until, loop, and each).
+
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
