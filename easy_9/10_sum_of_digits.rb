b6c5aee22ef80f0ecf9a3751efe3d3e072529e52
Justin Greger <justingreger@gmail.com>
2023-12-20 21:16:27 -0500

    # Identify expected input: # a positive integer argument.

--- easy_9/10_sum_of_digits.rb
+++ easy_9/10_sum_of_digits.rb
@@ -14,7 +14,9 @@ puts sum(123_456_789) == 45
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# a positive integer argument.
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
