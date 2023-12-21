020db8f72dd99e033d771faa421ccca9d4dc4040
Justin Greger <justingreger@gmail.com>
2023-12-20 21:32:09 -0500

    # Mental model of the problem (optional):

--- easy_9/10_sum_of_digits.rb
+++ easy_9/10_sum_of_digits.rb
@@ -24,7 +24,17 @@ puts sum(123_456_789) == 45
 # Write a method that takes one argument, a positive integer, and returns the sum of its digits.
 # For a challenge, try writing this without any basic looping constructs (while, until, loop, and each).
 
-Mental model of the problem (optional):
+# Mental model of the problem (optional):
+
+
+
+Write a method that takes one argument, a positive integer, and returns the sum of its digits.
+Examples:
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45
+For a challenge, try writing this without any basic looping constructs (while, until, loop, and each).
+
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
