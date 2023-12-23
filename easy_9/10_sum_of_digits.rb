734ce455321c699b0fb66815d10e68dbc94b7aef
Justin Greger <justingreger@gmail.com>
2023-12-22 19:28:12 -0500

    # Solution 1:

--- easy_9/10_sum_of_digits.rb
+++ easy_9/10_sum_of_digits.rb
@@ -51,16 +51,16 @@
 # end
 
 # Solution 1:
-def sum(number)
-  sum = 0
-  str_digits = number.to_s.chars
+# def sum(number)
+#   sum = 0
+#   str_digits = number.to_s.chars
 
-  str_digits.each do |str_digit|
-    sum += str_digit.to_i
-  end
+#   str_digits.each do |str_digit|
+#     sum += str_digit.to_i
+#   end
 
-  sum
-end
+#   sum
+# end
 
 # tests:
 puts sum(23) == 5
