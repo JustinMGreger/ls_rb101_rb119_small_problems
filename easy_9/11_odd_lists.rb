d2becc20fcceba7992945e45b4d33ec1d93e9ebe
Justin Greger <justingreger@gmail.com>
2023-12-22 21:37:40 -0500

    # p oddities

--- easy_9/11_odd_lists.rb
+++ easy_9/11_odd_lists.rb
@@ -6,12 +6,12 @@
 # elements of the argument Array.
 
 # Examples:
-p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
-p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
-p oddities(%w[abc def]) == ['abc']
-p oddities([123]) == [123]
-p oddities([]) == []
-p oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
+# p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+# p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+# p oddities(%w[abc def]) == ['abc']
+# p oddities([123]) == [123]
+# p oddities([]) == []
+# p oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
