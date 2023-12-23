8f9e633fd274d9ed565f13fe552db46fea6e3d66
Justin Greger <justingreger@gmail.com>
2023-12-22 21:56:16 -0500

    # Identify expected input: # Array argument.

--- easy_9/11_odd_lists.rb
+++ easy_9/11_odd_lists.rb
@@ -16,7 +16,9 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# Array argument.
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
