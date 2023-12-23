820c8862de3fe89abe852daded20940a8a2234c5
Justin Greger <justingreger@gmail.com>
2023-12-22 21:36:49 -0500

    # tests:

--- easy_9/11_odd_lists.rb
+++ easy_9/11_odd_lists.rb
@@ -32,3 +32,11 @@ p oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
+
+# tests:
+p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+p oddities(%w[abc def]) == ['abc']
+p oddities([123]) == [123]
+p oddities([]) == []
+p oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
