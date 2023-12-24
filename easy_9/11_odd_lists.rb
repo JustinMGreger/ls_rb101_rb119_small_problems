2a753d1a3bf6f3e31f6f3f510fe468c9d2f25fd5
Justin Greger <justingreger@gmail.com>
2023-12-23 21:50:02 -0500

    # LS Solution:

--- easy_9/11_odd_lists.rb
+++ easy_9/11_odd_lists.rb
@@ -52,6 +52,17 @@
 #   array.values_at(*array.each_index.select(&:even?))
 # end
 
+# LS Solution:
+def oddities(array)
+  odd_elements = []
+  index = 0
+  while index < array.size
+    odd_elements << array[index]
+    index += 2
+  end
+  odd_elements
+end
+
 # tests:
 p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
 p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
