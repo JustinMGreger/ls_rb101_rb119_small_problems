f6bac233cf42cb378f42a7ee9c53f157e1d1a6b0
Justin Greger <justingreger@gmail.com>
2023-12-23 19:05:43 -0500

    # Mental model of the problem (optional):

--- easy_9/11_odd_lists.rb
+++ easy_9/11_odd_lists.rb
@@ -28,8 +28,16 @@
 # The values in the returned list should be those values that are in the 1st, 3rd, 5th,
 # and so on elements of the argument Array.
 
+# Mental model of the problem (optional):
+Write a method that returns an Array
+that contains every other element of an Array that is passed in as an argument.
+The values in the returned list should be those values that are in the 1st, 3rd, 5th,
+and so on elements of the argument Array.
 
-Mental model of the problem (optional):
+
+
+
+
 
 # Examples / Test Cases, Validate understanding of the problem:
 # p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
