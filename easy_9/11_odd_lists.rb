e490878760fbeacab80d4d1aacdb3a1bd771a28b
Justin Greger <justingreger@gmail.com>
2023-12-23 19:10:32 -0500

    # def oddities(Array_parameter)

--- easy_9/11_odd_lists.rb
+++ easy_9/11_odd_lists.rb
@@ -29,6 +29,7 @@
 # and so on elements of the argument Array.
 
 # Mental model of the problem (optional):
+# def oddities(Array_parameter)
 Write a method that returns an Array
 that contains every other element of an Array that is passed in as an argument.
 The values in the returned list should be those values that are in the 1st, 3rd, 5th,
