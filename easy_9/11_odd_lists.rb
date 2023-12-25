78a9c2c450a54b35b298809617bdee7d7931091d
Justin Greger <justingreger@gmail.com>
2023-12-24 19:32:20 -0500

    # tests:

--- easy_9/11_odd_lists.rb
+++ easy_9/11_odd_lists.rb
@@ -63,6 +63,14 @@
 #   odd_elements
 # end
 
+# tests:
+# p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+# p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+# p oddities(%w[abc def]) == ['abc']
+# p oddities([123]) == [123]
+# p oddities([]) == []
+# p oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
+
 # Further Exploration
 
 Write a companion method that returns the 2nd, 4th, 6th, and so on elements of an array.
