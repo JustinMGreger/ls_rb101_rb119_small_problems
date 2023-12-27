4f770b36c47f72bcfa75e0d4f70be6693e375189
Justin Greger <justingreger@gmail.com>
2023-12-26 21:34:51 -0500

    # The original array should not be modified.

--- medium_1/1_rotation_part_1.rb
+++ medium_1/1_rotation_part_1.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Write a method that rotates an array by moving the first element to the end of the array.
-The original array should not be modified.
+# The original array should not be modified.
 
 Do not use the method Array#rotate or Array#rotate! for your implementation.
 
