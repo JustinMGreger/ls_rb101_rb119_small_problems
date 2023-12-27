1a38d6458da3b2bad3c7a37c65b9354b364b62df
Justin Greger <justingreger@gmail.com>
2023-12-26 21:35:11 -0500

    # Do not use the method Array#rotate or Array#rotate! for your implementation.

--- medium_1/1_rotation_part_1.rb
+++ medium_1/1_rotation_part_1.rb
@@ -3,6 +3,6 @@
 # Write a method that rotates an array by moving the first element to the end of the array.
 # The original array should not be modified.
 
-Do not use the method Array#rotate or Array#rotate! for your implementation.
+# Do not use the method Array#rotate or Array#rotate! for your implementation.
 
 Example:
