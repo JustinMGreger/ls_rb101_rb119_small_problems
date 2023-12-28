14692c1f38031e63936b1f6a47341e90dcf90b6d
Justin Greger <justingreger@gmail.com>
2023-12-27 20:52:15 -0500

    # Write a method that rotates an array by moving the first element to the end of the array.

--- medium_1/1_rotation_part_1.rb
+++ medium_1/1_rotation_part_1.rb
@@ -23,7 +23,8 @@
 # rotated array where the first element was moved to the end of the array. 
 
 # Make the requirements explicit Identify rules:
-Write a method that rotates an array by moving the first element to the end of the array. The original array should not be modified.
+# Write a method that rotates an array by moving the first element to the end of the array.
+The original array should not be modified.
 
 Do not use the method Array#rotate or Array#rotate! for your implementation.
 
