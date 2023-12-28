746b2f84612d197239c3ebb368da6a396a33f50d
Justin Greger <justingreger@gmail.com>
2023-12-27 19:20:12 -0500

    p rotate_array(%w[a b c]) == %w[b c a]

--- medium_1/1_rotation_part_1.rb
+++ medium_1/1_rotation_part_1.rb
@@ -7,7 +7,7 @@
 
 # Example:
 p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
-p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(%w[a b c]) == %w[b c a]
 p rotate_array(['a']) == ['a']
 p x = [1, 2, 3, 4]
 p rotate_array(x) == [2, 3, 4, 1]   # => true
