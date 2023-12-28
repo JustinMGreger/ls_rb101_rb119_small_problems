577e0b3d9fd58a3cd0756667800b5b425433dad3
Justin Greger <justingreger@gmail.com>
2023-12-27 20:54:52 -0500

    # Mental model of the problem (optional):

--- medium_1/1_rotation_part_1.rb
+++ medium_1/1_rotation_part_1.rb
@@ -27,7 +27,12 @@
 # The original array should not be modified.
 # Do not use the method Array#rotate or Array#rotate! for your implementation.
 
-Mental model of the problem (optional):
+# Mental model of the problem (optional):
+Write a method that rotates an array by moving the first element to the end of the array.
+The original array should not be modified.
+Do not use the method Array#rotate or Array#rotate! for your implementation.
+
+
 
 # Examples / Test Cases, Validate understanding of the problem:
 # p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
