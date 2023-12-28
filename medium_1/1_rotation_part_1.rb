adc4f8656b9b14db5e42c686f9ec0c96a0593500
Justin Greger <justingreger@gmail.com>
2023-12-27 19:14:29 -0500

    p rotate_array

--- medium_1/1_rotation_part_1.rb
+++ medium_1/1_rotation_part_1.rb
@@ -6,10 +6,9 @@
 # Do not use the method Array#rotate or Array#rotate! for your implementation.
 
 # Example:
-rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
-rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
-rotate_array(['a']) == ['a']
-
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
 x = [1, 2, 3, 4]
-rotate_array(x) == [2, 3, 4, 1]   # => true
+p rotate_array(x) == [2, 3, 4, 1]   # => true
 x == [1, 2, 3, 4]                 # => true
