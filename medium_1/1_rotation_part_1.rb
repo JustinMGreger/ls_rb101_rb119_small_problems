60b4dac6c04fedf9ec5fa77fd58afe97e75e5867
Justin Greger <justingreger@gmail.com>
2023-12-29 21:07:41 -0500

    # tests: # p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7] # p rotate_array(%w[a b c]) == %w[b c a] # p rotate_array([a]) == [a] # p x = [1, 2, 3, 4] # p rotate_array(x) == [2, 3, 4, 1]   # => true # p x == [1, 2, 3, 4]                 # => true

--- medium_1/1_rotation_part_1.rb
+++ medium_1/1_rotation_part_1.rb
@@ -83,6 +83,14 @@
 #   rotated_string = rotated_array.join
 # end
 
+# tests:
+# p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+# p rotate_array(%w[a b c]) == %w[b c a]
+# p rotate_array(['a']) == ['a']
+# p x = [1, 2, 3, 4]
+# p rotate_array(x) == [2, 3, 4, 1]   # => true
+# p x == [1, 2, 3, 4]                 # => true
+
 # Do the same thing for integers.
 # You may use rotate_array from inside your new method.
 def rotate_array(array)
