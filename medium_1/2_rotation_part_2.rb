0bea8b4e1e7f9039d5c2371e78eefd5141ac32f3
Justin Greger <justingreger@gmail.com>
2023-12-30 21:54:47 -0500

    # p rotate_rightmost_digits(735_291, 1) == 735_291 # p rotate_rightmost_digits(735_291, 2) == 735_219 # p rotate_rightmost_digits(735_291, 3) == 735_912 # p rotate_rightmost_digits(735_291, 4) == 732_915 # p rotate_rightmost_digits(735_291, 5) == 752_913 # p rotate_rightmost_digits(735_291, 6) == 352_917

--- medium_1/2_rotation_part_2.rb
+++ medium_1/2_rotation_part_2.rb
@@ -2,12 +2,12 @@
 
 # Write a method that can rotate the last n digits of a number.
 # For example:
-p rotate_rightmost_digits(735_291, 1) == 735_291
-p rotate_rightmost_digits(735_291, 2) == 735_219
-p rotate_rightmost_digits(735_291, 3) == 735_912
-p rotate_rightmost_digits(735_291, 4) == 732_915
-p rotate_rightmost_digits(735_291, 5) == 752_913
-p rotate_rightmost_digits(735_291, 6) == 352_917
+# p rotate_rightmost_digits(735_291, 1) == 735_291
+# p rotate_rightmost_digits(735_291, 2) == 735_219
+# p rotate_rightmost_digits(735_291, 3) == 735_912
+# p rotate_rightmost_digits(735_291, 4) == 732_915
+# p rotate_rightmost_digits(735_291, 5) == 752_913
+# p rotate_rightmost_digits(735_291, 6) == 352_917
 
 # Note that rotating just 1 digit results in the original number being returned.
 
