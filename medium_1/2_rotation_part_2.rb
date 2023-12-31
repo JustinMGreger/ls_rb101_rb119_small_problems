dc0672045c2c12dcea35d08bfbc65f27e7cb2a26
Justin Greger <justingreger@gmail.com>
2023-12-30 21:31:51 -0500

    752_913

--- medium_1/2_rotation_part_2.rb
+++ medium_1/2_rotation_part_2.rb
@@ -6,5 +6,5 @@ p rotate_rightmost_digits(735_291, 1) == 735_291
 p rotate_rightmost_digits(735_291, 2) == 735_219
 p rotate_rightmost_digits(735_291, 3) == 735_912
 p rotate_rightmost_digits(735_291, 4) == 732_915
-p rotate_rightmost_digits(735_291, 5) == 752913
+p rotate_rightmost_digits(735_291, 5) == 752_913
 p rotate_rightmost_digits(735_291, 6) == 352917
