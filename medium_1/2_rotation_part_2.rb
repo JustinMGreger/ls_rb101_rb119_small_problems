b7ecece0eb2387e069080fd5f4011b964057064e
Justin Greger <justingreger@gmail.com>
2023-12-30 21:55:56 -0500

    # p rotate_rightmost_digits(735_291, 1) == 735_291 # p rotate_rightmost_digits(735_291, 2) == 735_219 # p rotate_rightmost_digits(735_291, 3) == 735_912 # p rotate_rightmost_digits(735_291, 4) == 732_915 # p rotate_rightmost_digits(735_291, 5) == 752_913 # p rotate_rightmost_digits(735_291, 6) == 352_917

--- medium_1/2_rotation_part_2.rb
+++ medium_1/2_rotation_part_2.rb
@@ -24,12 +24,12 @@ Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
 
 # Examples / Test Cases, Validate understanding of the problem:
-p rotate_rightmost_digits(735_291, 1) == 735_291
-p rotate_rightmost_digits(735_291, 2) == 735_219
-p rotate_rightmost_digits(735_291, 3) == 735_912
-p rotate_rightmost_digits(735_291, 4) == 732_915
-p rotate_rightmost_digits(735_291, 5) == 752_913
-p rotate_rightmost_digits(735_291, 6) == 352_917
+# p rotate_rightmost_digits(735_291, 1) == 735_291
+# p rotate_rightmost_digits(735_291, 2) == 735_219
+# p rotate_rightmost_digits(735_291, 3) == 735_912
+# p rotate_rightmost_digits(735_291, 4) == 732_915
+# p rotate_rightmost_digits(735_291, 5) == 752_913
+# p rotate_rightmost_digits(735_291, 6) == 352_917
 
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
