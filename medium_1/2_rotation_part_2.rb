2db76c245c568532a4379cf520ad1d75e46b1d52
Justin Greger <justingreger@gmail.com>
2023-12-30 21:29:09 -0500

    735_291

--- medium_1/2_rotation_part_2.rb
+++ medium_1/2_rotation_part_2.rb
@@ -4,7 +4,7 @@
 # For example:
 p rotate_rightmost_digits(735_291, 1) == 735_291
 p rotate_rightmost_digits(735_291, 2) == 735219
-p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735_291, 3) == 735912
 p rotate_rightmost_digits(735291, 4) == 732915
 p rotate_rightmost_digits(735291, 5) == 752913
 p rotate_rightmost_digits(735291, 6) == 352917
