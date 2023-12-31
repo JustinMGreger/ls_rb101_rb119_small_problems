4e9183583c4ac62c1b867b64b46c52b680608d91
Justin Greger <justingreger@gmail.com>
2023-12-30 21:55:20 -0500

    # tests:

--- medium_1/2_rotation_part_2.rb
+++ medium_1/2_rotation_part_2.rb
@@ -34,3 +34,11 @@ p rotate_rightmost_digits(735_291, 6) == 352_917
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
+
+# tests:
+p rotate_rightmost_digits(735_291, 1) == 735_291
+p rotate_rightmost_digits(735_291, 2) == 735_219
+p rotate_rightmost_digits(735_291, 3) == 735_912
+p rotate_rightmost_digits(735_291, 4) == 732_915
+p rotate_rightmost_digits(735_291, 5) == 752_913
+p rotate_rightmost_digits(735_291, 6) == 352_917
