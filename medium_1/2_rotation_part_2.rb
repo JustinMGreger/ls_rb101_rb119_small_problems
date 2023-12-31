c8b329683303e20d9af25563434d8a443e8277b1
Justin Greger <justingreger@gmail.com>
2023-12-30 21:27:48 -0500

    p rotate_rightmost_digits

--- medium_1/2_rotation_part_2.rb
+++ medium_1/2_rotation_part_2.rb
@@ -2,3 +2,9 @@
 
 # Write a method that can rotate the last n digits of a number.
 # For example:
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
+p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
