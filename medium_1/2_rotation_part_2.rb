1a468a587ae2e42e338fdc4afb731c0b809e1dcd
Justin Greger <justingreger@gmail.com>
2023-12-30 21:36:42 -0500

    # Note that rotating just 1 digit results in the original number being returned.

--- medium_1/2_rotation_part_2.rb
+++ medium_1/2_rotation_part_2.rb
@@ -8,3 +8,9 @@ p rotate_rightmost_digits(735_291, 3) == 735_912
 p rotate_rightmost_digits(735_291, 4) == 732_915
 p rotate_rightmost_digits(735_291, 5) == 752_913
 p rotate_rightmost_digits(735_291, 6) == 352_917
+
+# Note that rotating just 1 digit results in the original number being returned.
+
+You may use the rotate_array method from the previous exercise if you want. (Recommended!)
+
+You may assume that n is always a positive integer.
