6fbc7695c4d2dbcc6edf0bd12911e762233550fc
Justin Greger <justingreger@gmail.com>
2023-12-31 19:08:24 -0500

    # You may assume that n is always a positive integer.

--- medium_1/2_rotation_part_2.rb
+++ medium_1/2_rotation_part_2.rb
@@ -28,14 +28,7 @@
 # Write a method that can rotate the last n digits of a number.
 # Note that rotating just 1 digit results in the original number being returned.
 # You may use the rotate_array method from the previous exercise if you want. (Recommended!)
-
-
-
-
-
-
-
-
+# You may assume that n is always a positive integer.
 
 Mental model of the problem (optional):
 
