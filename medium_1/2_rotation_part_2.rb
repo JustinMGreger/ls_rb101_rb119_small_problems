65b4e68e38e8a75825f68278d85ac9c4c6032fdb
Justin Greger <justingreger@gmail.com>
2024-01-02 20:55:28 -0500

    deleted trailing whitespace

--- medium_1/2_rotation_part_2.rb
+++ medium_1/2_rotation_part_2.rb
@@ -55,7 +55,7 @@
 # the new array created by returning the value located at the index location 0 in the array parameter.
 # this is the last line of the rotate_array method.
 # the rotate_integer method accepts the (integer) parameter and is defined as the following:
-# the digit_array varible is assigned the result of integer parameter in string form 
+# the digit_array varible is assigned the result of integer parameter in string form
 # then return an array where each element of this array is a single character from the string
 # then return a new array that is the result of converting every element to integer form.
 # the rotated_array varible is assigned the result of passing the digit_array varible to the rotate_array method.
