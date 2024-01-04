72698cf4e133230eb950f80c289a6e9958ac956f
Justin Greger <justingreger@gmail.com>
2024-01-03 20:42:04 -0500

    def rotate_array(array)   array[1..] + [array[0]] end

--- medium_1/2_rotation_part_2.rb
+++ medium_1/2_rotation_part_2.rb
@@ -102,6 +102,10 @@
 # end
 
 # LS Solution:
+def rotate_array(array)
+  array[1..] + [array[0]]
+end
+
 def rotate_rightmost_digits(number, n)
   all_digits = number.to_s.chars
   all_digits[-n..-1] = rotate_array(all_digits[-n..-1])
