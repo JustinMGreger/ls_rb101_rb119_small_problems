63d48f641bc632133364d1bc8d97b70320c44b64
Justin Greger <justingreger@gmail.com>
2024-01-04 20:38:40 -0500

    p max_rotation

--- medium_1/3_rotation_part_3.rb
+++ medium_1/3_rotation_part_3.rb
@@ -13,3 +13,8 @@
 # Note that you do not have to handle multiple 0s.
 
 # Example:
+p max_rotation(735291) == 321579
+p max_rotation(3) == 3
+p max_rotation(35) == 53
+p max_rotation(105) == 15 # the leading zero gets dropped
+p max_rotation(8_703_529_146) == 7_321_609_845
