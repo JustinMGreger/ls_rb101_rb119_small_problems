e82944630b016c3b6b8371f400ad6762bf824fbb
Justin Greger <justingreger@gmail.com>
2024-01-08 21:28:18 -0500

    # Can you create a solution that preserves zeros?

--- medium_1/3_rotation_part_3.rb
+++ medium_1/3_rotation_part_3.rb
@@ -142,4 +142,4 @@ p max_rotation(8_703_529_146) == 7_321_609_845
 # Does this 3 part approach make the problem easier to understand or harder?
 
 # There is an edge case in our problem when the number passed in as the argument has multiple consecutive zeros.
-Can you create a solution that preserves zeros?
+# Can you create a solution that preserves zeros?
