0f3d74cecfa52a931f4e39ee03762bdfb447b72a
Justin Greger <justingreger@gmail.com>
2024-01-08 21:34:13 -0500

    # p max_rotation(735_291) == 321_579 # p max_rotation(3) == 3 # p max_rotation(35) == 53 # p max_rotation(105) == 15 # the leading zero gets dropped # p max_rotation(8_703_529_146) == 7_321_609_845

--- medium_1/3_rotation_part_3.rb
+++ medium_1/3_rotation_part_3.rb
@@ -151,11 +151,11 @@ Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
 
 # Examples / Test Cases, Validate understanding of the problem:
-p max_rotation(735_291) == 321_579
-p max_rotation(3) == 3
-p max_rotation(35) == 53
-p max_rotation(105) == 15 # the leading zero gets dropped
-p max_rotation(8_703_529_146) == 7_321_609_845
+# p max_rotation(735_291) == 321_579
+# p max_rotation(3) == 3
+# p max_rotation(35) == 53
+# p max_rotation(105) == 15 # the leading zero gets dropped
+# p max_rotation(8_703_529_146) == 7_321_609_845
 
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
