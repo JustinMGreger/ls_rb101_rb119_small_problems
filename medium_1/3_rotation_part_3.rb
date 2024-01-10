44c98c23aa8ae747409f4cae26d4ae14ac19c333
Justin Greger <justingreger@gmail.com>
2024-01-09 21:50:24 -0500

    # Note that you do not have to handle multiple 0s.

--- medium_1/3_rotation_part_3.rb
+++ medium_1/3_rotation_part_3.rb
@@ -166,15 +166,7 @@ Keep the first 2 digits fixed in place and rotate again to 321759.
 Keep the first 3 digits fixed in place and rotate again to get 321597.
 Finally, keep the first 4 digits fixed in place and rotate the final 2 digits to get 321579.
 # return the resulting number which is the maximum rotation of the original number.
-Note that you do not have to handle multiple 0s.
-
-
-
-
-
-
-
-
+# Note that you do not have to handle multiple 0s.
 
 # Examples / Test Cases, Validate understanding of the problem:
 # p max_rotation(735_291) == 321_579
