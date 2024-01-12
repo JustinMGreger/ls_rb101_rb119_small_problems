5f161ce0d309d74781fe852c6f6ad39cb3000b7c
Justin Greger <justingreger@gmail.com>
2024-01-11 20:05:44 -0500

    # tests: # p max_rotation(735_291) == 321_579 # p max_rotation(3) == 3 # p max_rotation(35) == 53 # p max_rotation(105) == 15 # the leading zero gets dropped # p max_rotation(8_703_529_146) == 7_321_609_845

--- medium_1/3_rotation_part_3.rb
+++ medium_1/3_rotation_part_3.rb
@@ -223,11 +223,11 @@
 # end
 
 # tests:
-p max_rotation(735_291) == 321_579
-p max_rotation(3) == 3
-p max_rotation(35) == 53
-p max_rotation(105) == 15 # the leading zero gets dropped
-p max_rotation(8_703_529_146) == 7_321_609_845
+# p max_rotation(735_291) == 321_579
+# p max_rotation(3) == 3
+# p max_rotation(35) == 53
+# p max_rotation(105) == 15 # the leading zero gets dropped
+# p max_rotation(8_703_529_146) == 7_321_609_845
 
 # Would your solution look different?
 # No.
