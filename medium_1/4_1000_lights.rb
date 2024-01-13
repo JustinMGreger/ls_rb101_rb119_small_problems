d3cc706200070f2401e0b4fdeef3dd26cafb30e0
Justin Greger <justingreger@gmail.com>
2024-01-12 20:41:38 -0500

    # round 2: lights 2 and 4 are now off; 1, 3, 5 are on

--- medium_1/4_1000_lights.rb
+++ medium_1/4_1000_lights.rb
@@ -12,7 +12,7 @@
 
 # Example with n = 5 lights:
 # round 1: every light is turned on
-round 2: lights 2 and 4 are now off; 1, 3, 5 are on
+# round 2: lights 2 and 4 are now off; 1, 3, 5 are on
 round 3: lights 2, 3, and 4 are now off; 1 and 5 are on
 round 4: lights 2 and 3 are now off; 1, 4, and 5 are on
 round 5: lights 2, 3, and 5 are now off; 1 and 4 are on
