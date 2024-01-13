47cfd3dc2cdf4303cd716e665a5db64d352947e4
Justin Greger <justingreger@gmail.com>
2024-01-12 20:36:11 -0500

    # You repeat this process and keep going until you have been through n repetitions.

--- medium_1/4_1000_lights.rb
+++ medium_1/4_1000_lights.rb
@@ -5,4 +5,4 @@
 # You walk down the row of switches and toggle every one of them.
 # You go back to the beginning, and on this second pass, you toggle switches 2, 4, 6, and so on.
 # On the third pass, you go back again to the beginning and toggle switches 3, 6, 9, and so on.
-You repeat this process and keep going until you have been through n repetitions.
+# You repeat this process and keep going until you have been through n repetitions.
