fc1e6aceb3f3b43a9c54a587ea3073cb7e9fe64e
Justin Greger <justingreger@gmail.com>
2024-01-12 20:29:45 -0500

    # You walk down the row of switches and toggle every one of them.

--- medium_1/4_1000_lights.rb
+++ medium_1/4_1000_lights.rb
@@ -2,4 +2,5 @@
 
 # You have a bank of switches before you, numbered from 1 to n.
 # Each switch is connected to exactly one light that is initially off.
-You walk down the row of switches and toggle every one of them. You go back to the beginning, and on this second pass, you toggle switches 2, 4, 6, and so on. On the third pass, you go back again to the beginning and toggle switches 3, 6, 9, and so on. You repeat this process and keep going until you have been through n repetitions.
+# You walk down the row of switches and toggle every one of them.
+You go back to the beginning, and on this second pass, you toggle switches 2, 4, 6, and so on. On the third pass, you go back again to the beginning and toggle switches 3, 6, 9, and so on. You repeat this process and keep going until you have been through n repetitions.
