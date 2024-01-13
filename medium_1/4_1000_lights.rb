3223064fd55d1f89239148596737184da187c893
Justin Greger <justingreger@gmail.com>
2024-01-12 19:37:04 -0500

    # Each switch is connected to exactly one light that is initially off.

--- medium_1/4_1000_lights.rb
+++ medium_1/4_1000_lights.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 # You have a bank of switches before you, numbered from 1 to n.
-Each switch is connected to exactly one light that is initially off. You walk down the row of switches and toggle every one of them. You go back to the beginning, and on this second pass, you toggle switches 2, 4, 6, and so on. On the third pass, you go back again to the beginning and toggle switches 3, 6, 9, and so on. You repeat this process and keep going until you have been through n repetitions.
+# Each switch is connected to exactly one light that is initially off.
+You walk down the row of switches and toggle every one of them. You go back to the beginning, and on this second pass, you toggle switches 2, 4, 6, and so on. On the third pass, you go back again to the beginning and toggle switches 3, 6, 9, and so on. You repeat this process and keep going until you have been through n repetitions.
