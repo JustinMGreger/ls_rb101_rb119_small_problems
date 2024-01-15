30f7531d4525f3ee19efe7952c524867a52f3415
Justin Greger <justingreger@gmail.com>
2024-01-14 21:29:21 -0500

    # The result is that 2 lights are left on, lights 1 and 4. The return value is [1, 4]. # With 10 lights, 3 lights are left on: lights 1, 4, and 9. The return value is [1, 4, 9].

--- medium_1/4_1000_lights.rb
+++ medium_1/4_1000_lights.rb
@@ -51,8 +51,8 @@
 # round 3: lights 2, 3, and 4 are now off; 1 and 5 are on
 # round 4: lights 2 and 3 are now off; 1, 4, and 5 are on
 # round 5: lights 2, 3, and 5 are now off; 1 and 4 are on
-The result is that 2 lights are left on, lights 1 and 4. The return value is [1, 4].
-With 10 lights, 3 lights are left on: lights 1, 4, and 9. The return value is [1, 4, 9].
+# The result is that 2 lights are left on, lights 1 and 4. The return value is [1, 4].
+# With 10 lights, 3 lights are left on: lights 1, 4, and 9. The return value is [1, 4, 9].
 
 
 
