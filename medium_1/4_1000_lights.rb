21eb93302a637f67243a6774d00a6a591991ee85
Justin Greger <justingreger@gmail.com>
2024-01-14 21:12:49 -0500

    # round 1: every light is turned on

--- medium_1/4_1000_lights.rb
+++ medium_1/4_1000_lights.rb
@@ -46,7 +46,7 @@
 # the total number of switches,
 # and returns an Array that identifies which lights are on after n repetitions.
 # Example with n = 5 lights:
-round 1: every light is turned on
+# round 1: every light is turned on
 round 2: lights 2 and 4 are now off; 1, 3, 5 are on
 round 3: lights 2, 3, and 4 are now off; 1 and 5 are on
 round 4: lights 2 and 3 are now off; 1, 4, and 5 are on
