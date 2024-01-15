67b26e020b141cb23c6d548da2072057273a0a31
Justin Greger <justingreger@gmail.com>
2024-01-14 21:01:37 -0500

    # Each switch is connected to exactly one light that is initially off.

--- medium_1/4_1000_lights.rb
+++ medium_1/4_1000_lights.rb
@@ -37,7 +37,8 @@
 
 # Mental model of the problem (optional):
 # You have a bank of switches before you, numbered from 1 to n.
-Each switch is connected to exactly one light that is initially off. You walk down the row of switches and toggle every one of them. You go back to the beginning, and on this second pass, you toggle switches 2, 4, 6, and so on. On the third pass, you go back again to the beginning and toggle switches 3, 6, 9, and so on. You repeat this process and keep going until you have been through n repetitions.
+# Each switch is connected to exactly one light that is initially off.
+You walk down the row of switches and toggle every one of them. You go back to the beginning, and on this second pass, you toggle switches 2, 4, 6, and so on. On the third pass, you go back again to the beginning and toggle switches 3, 6, 9, and so on. You repeat this process and keep going until you have been through n repetitions.
 
 Write a method that takes one argument, the total number of switches, and returns an Array that identifies which lights are on after n repetitions.
 
