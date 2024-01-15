22339eb9550267b332cef07c0a84e702aa180b2a
Justin Greger <justingreger@gmail.com>
2024-01-14 21:11:06 -0500

    # You repeat this process and keep going until you have been through n repetitions.

--- medium_1/4_1000_lights.rb
+++ medium_1/4_1000_lights.rb
@@ -41,7 +41,7 @@
 # You walk down the row of switches and toggle every one of them.
 # You go back to the beginning, and on this second pass, you toggle switches 2, 4, 6, and so on.
 # On the third pass, you go back again to the beginning and toggle switches 3, 6, 9, and so on.
-You repeat this process and keep going until you have been through n repetitions.
+# You repeat this process and keep going until you have been through n repetitions.
 
 Write a method that takes one argument, the total number of switches, and returns an Array that identifies which lights are on after n repetitions.
 
