65554703ee2c00784b737c1fca7412f6cb0b8ed1
Justin Greger <justingreger@gmail.com>
2024-01-16 17:56:42 -0500

    # Factors come in pairs, and toggling occurs for each factor.

--- medium_1/4_1000_lights.rb
+++ medium_1/4_1000_lights.rb
@@ -137,6 +137,7 @@
 # Yes.
 # Every light that is on is a perfect square.
 # Why is that?
+# Factors come in pairs, and toggling occurs for each factor. 
 
 What are some alternatives for solving this exercise? What if we used an Array to represent our 1000 lights instead of a Hash, how would that change our code?
 We could have a method that replicates the output from the description of this problem (i.e. "lights 2, 3, and 5 are now off; 1 and 4 are on.") How would we go about writing that code?
