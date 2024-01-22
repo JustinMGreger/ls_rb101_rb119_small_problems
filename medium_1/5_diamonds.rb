8fe0268818a76a1e97abc64a813403c71bdc69a6
Justin Greger <justingreger@gmail.com>
2024-01-21 21:59:50 -0500

    # where n is an odd integer that is supplied as an argument to the method.

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Write a method that displays a 4-pointed diamond in an n x n grid,
-where n is an odd integer that is supplied as an argument to the method.
+# where n is an odd integer that is supplied as an argument to the method.
 You may assume that the argument will always be an odd integer.
 
 Examples
