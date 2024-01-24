26d02a008684fcdba81918b158115e9b56335dcf
Justin Greger <justingreger@gmail.com>
2024-01-23 21:59:02 -0500

    # Identify expected input: # the n odd integer.

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -25,7 +25,9 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# the n odd integer.
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
