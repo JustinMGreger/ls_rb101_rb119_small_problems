41ad3ce1f180a85c5c03d3f6c4f25baf019b96d7
Justin Greger <justingreger@gmail.com>
2024-01-24 20:31:30 -0500

    deleted Write a method that

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -38,7 +38,7 @@
 
 # Mental model of the problem (optional):
 # def diamond(n)
-Write a method that displays a 4-pointed diamond in an n x n grid,
+displays a 4-pointed diamond in an n x n grid,
 where n is an odd integer that is supplied as an argument to the method.
 You may assume that the argument will always be an odd integer.
 
