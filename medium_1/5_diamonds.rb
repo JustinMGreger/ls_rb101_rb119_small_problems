217c32c004b1682403c6829890ae5e4a0b5869b5
Justin Greger <justingreger@gmail.com>
2024-01-24 20:33:05 -0500

    deleted You may assume that the argument

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -40,7 +40,7 @@
 # def diamond(n)
 displays a 4-pointed diamond in an n x n grid,
 where n is an odd integer that is supplied as an argument to the method.
-You may assume that the argument will always be an odd integer.
+ will always be an odd integer.
 
 
 
