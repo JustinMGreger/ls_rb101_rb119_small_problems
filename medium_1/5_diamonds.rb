53ec6987a9ce7252ffd49f42054e29a9d6a6ccfa
Justin Greger <justingreger@gmail.com>
2024-01-24 19:17:35 -0500

    # Make the requirements explicit Identify rules: # Write a method that displays a 4-pointed diamond in an n x n grid,

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -31,7 +31,14 @@
 # output:
 # a 4-pointed diamond in an n x n grid.
 
-Make the requirements explicit Identify rules:
+# Make the requirements explicit Identify rules:
+# Write a method that displays a 4-pointed diamond in an n x n grid,
+where n is an odd integer that is supplied as an argument to the method. You may assume that the argument will always be an odd integer.
+
+
+
+
+
 Mental model of the problem (optional):
 
 # Examples / Test Cases, Validate understanding of the problem:
