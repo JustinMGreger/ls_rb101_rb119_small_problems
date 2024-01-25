2d7280e18b7b91914ec2901a1595fda7700f7383
Justin Greger <justingreger@gmail.com>
2024-01-24 21:15:23 -0500

    # Algorithm, Steps for converting input to output:

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -50,7 +50,12 @@
 # Data Structure, How we represent data that we will work with when converting the input to output:
 # Array.
 
-Algorithm, Steps for converting input to output:
+# Algorithm, Steps for converting input to output:
+
+
+
+
+
 and Code. Implementation of Algorithm:
 
 # tests:
