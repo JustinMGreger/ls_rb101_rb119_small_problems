0aa90b065ea9dd0af49b42a4f6803837f2c9885c
Justin Greger <justingreger@gmail.com>
2024-01-24 19:29:48 -0500

    # Mental model of the problem (optional): # def diamond(n)

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -36,7 +36,18 @@
 # where n is an odd integer that is supplied as an argument to the method.
 # You may assume that the argument will always be an odd integer.
 
-Mental model of the problem (optional):
+# Mental model of the problem (optional):
+# def diamond(n)
+Write a method that displays a 4-pointed diamond in an n x n grid,
+where n is an odd integer that is supplied as an argument to the method.
+You may assume that the argument will always be an odd integer.
+
+
+
+
+
+
+
 
 # Examples / Test Cases, Validate understanding of the problem:
 # diamond(1)
