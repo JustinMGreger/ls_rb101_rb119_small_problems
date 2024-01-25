7e7512b15154360a5fe39f0e9454b25e324c3901
Justin Greger <justingreger@gmail.com>
2024-01-24 19:20:25 -0500

    # where n is an odd integer that is supplied as an argument to the method.

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -33,7 +33,8 @@
 
 # Make the requirements explicit Identify rules:
 # Write a method that displays a 4-pointed diamond in an n x n grid,
-where n is an odd integer that is supplied as an argument to the method. You may assume that the argument will always be an odd integer.
+# where n is an odd integer that is supplied as an argument to the method.
+You may assume that the argument will always be an odd integer.
 
 
 
