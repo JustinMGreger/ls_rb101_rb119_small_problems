91237c17bfb940efc6fae7c141efa1e782d131ab
Justin Greger <justingreger@gmail.com>
2024-01-24 19:20:44 -0500

    # You may assume that the argument will always be an odd integer.

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -34,11 +34,7 @@
 # Make the requirements explicit Identify rules:
 # Write a method that displays a 4-pointed diamond in an n x n grid,
 # where n is an odd integer that is supplied as an argument to the method.
-You may assume that the argument will always be an odd integer.
-
-
-
-
+# You may assume that the argument will always be an odd integer.
 
 Mental model of the problem (optional):
 
