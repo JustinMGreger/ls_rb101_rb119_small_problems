ae4e3c562c72411c5676271e8088d5a4173a73ab
Justin Greger <justingreger@gmail.com>
2024-01-28 19:22:15 -0500

    # Further Exploration

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -87,7 +87,9 @@
 #   1.upto(max_distance)   { |distance| print_row(grid_size, distance) }
 # end
 
+# Further Exploration
 
+Try modifying your solution or our solution so it prints only the outline of the diamond:
 
 
 
