403fb39d868542e067b6c20b6b1bcb21bc7774de
Justin Greger <justingreger@gmail.com>
2024-01-28 19:19:58 -0500

    # def diamond(grid_size) #   max_distance = (grid_size - 1) / 2 #   max_distance.downto(0) { |distance| print_row(grid_size, distance) } #   1.upto(max_distance)   { |distance| print_row(grid_size, distance) } # end

--- medium_1/5_diamonds.rb
+++ medium_1/5_diamonds.rb
@@ -81,12 +81,11 @@
 #   stars = '*' * number_of_stars
 #   puts stars.center(grid_size)
 # end
-
-def diamond(grid_size)
-  max_distance = (grid_size - 1) / 2
-  max_distance.downto(0) { |distance| print_row(grid_size, distance) }
-  1.upto(max_distance)   { |distance| print_row(grid_size, distance) }
-end
+# def diamond(grid_size)
+#   max_distance = (grid_size - 1) / 2
+#   max_distance.downto(0) { |distance| print_row(grid_size, distance) }
+#   1.upto(max_distance)   { |distance| print_row(grid_size, distance) }
+# end
 
 
 
