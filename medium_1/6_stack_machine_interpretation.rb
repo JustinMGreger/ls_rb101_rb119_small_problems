bb02a9c99364182575a5eab0747bb126a42bf552
Justin Greger <justingreger@gmail.com>
2024-02-02 18:47:57 -0500

    # PRINT Print the register value

--- medium_1/6_stack_machine_interpretation.rb
+++ medium_1/6_stack_machine_interpretation.rb
@@ -33,7 +33,7 @@
 # MOD Pops a value from the stack and divides it into the register value,
 # storing the integer remainder of the division in the register.
 # POP Remove the topmost item from the stack and place in register
-PRINT Print the register value
+# PRINT Print the register value
 All operations are integer operations (which is only important with DIV and MOD).
 
 Programs will be supplied to your language method via a string passed in as an argument. Your program may assume that all programs are correct programs; that is, they won't do anything like try to pop a non-existent value from the stack, and they won't contain unknown tokens.
