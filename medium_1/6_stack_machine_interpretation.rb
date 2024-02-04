516f04b5932ccb5dbb92866d3a27de5517d86004
Justin Greger <justingreger@gmail.com>
2024-02-04 16:18:43 -0500

    # ADD Pops a value from the stack and adds it to the register value, storing the result in the register.

--- medium_1/6_stack_machine_interpretation.rb
+++ medium_1/6_stack_machine_interpretation.rb
@@ -84,7 +84,7 @@
 
 # n Place a value n in the "register". Do not modify the stack.
 # PUSH Push the register value on to the stack. Leave the value in the register.
-ADD Pops a value from the stack and adds it to the register value, storing the result in the register.
+# ADD Pops a value from the stack and adds it to the register value, storing the result in the register.
 SUB Pops a value from the stack and subtracts it from the register value, storing the result in the register.
 MULT Pops a value from the stack and multiplies it by the register value, storing the result in the register.
 DIV Pops a value from the stack and divides it into the register value, storing the integer result in the register.
