7ce5a9f09171ba5ddb7671e5049c7ed2945ccd71
Justin Greger <justingreger@gmail.com>
2024-02-04 16:13:12 -0500

    # PUSH Push the register value on to the stack. Leave the value in the register.

--- medium_1/6_stack_machine_interpretation.rb
+++ medium_1/6_stack_machine_interpretation.rb
@@ -83,7 +83,7 @@
 # that has the following commands:
 
 # n Place a value n in the "register". Do not modify the stack.
-PUSH Push the register value on to the stack. Leave the value in the register.
+# PUSH Push the register value on to the stack. Leave the value in the register.
 ADD Pops a value from the stack and adds it to the register value, storing the result in the register.
 SUB Pops a value from the stack and subtracts it from the register value, storing the result in the register.
 MULT Pops a value from the stack and multiplies it by the register value, storing the result in the register.
