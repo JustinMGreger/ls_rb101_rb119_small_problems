1434db9fc560133e87bf446a6b3e397d524561d7
Justin Greger <justingreger@gmail.com>
2024-02-07 21:24:54 -0500

    # def initialize_commands

--- medium_1/6_stack_machine_interpretation.rb
+++ medium_1/6_stack_machine_interpretation.rb
@@ -101,26 +101,24 @@
 # You should initialize the register to 0.
 
 # Mental model of the problem (optional):
-# the initialize_commands method is defined as the following:
-# the hash contains: the 'PUSH' string key which is assigned the value that is the
-# result of passing the :cmd_push symbol to the method method
-# which is followed by the 'ADD' string key which is assigned the value that is the
-# result of passing the :cmd_add symbol to the method method
-# which is followed by the 'SUB' string key which is assigned the value that is
-# the result of passing the :cmd_sub symbol to the method method
-# which is followed by the 'MULT' string ke which is assigned the value that is
-# the result of passing the :cmd_mult symbol to the method method
-# which is followed by the 'DIV' string key which is assigned the value that is the
-# result of passing the :cmd_div symbol to the method method
-# which is followed by the 'MOD' string key which is assigned the value that is the
-# result of passing the :cmd_mod symbol to the method method
-# which is followed by the 'POP' string key which is assigned the value that is the
-# result of passing the :cmd_pop symbol to the method method
-# which is followed by the 'PRINT' string key which is assigned the value that
-# is the result of passing the :cmd_print symbol to the method method and this is the
-# end of the hash.
-# this is the last line of the initialize_commands method.
-
+# def initialize_commands
+# that has the following commands:
+# n Place a value n in the "register". Do not modify the stack.
+# PUSH Push the register value on to the stack. Leave the value in the register.
+# ADD Pops a value from the stack and adds it to the register value, storing the result in the register.
+# SUB Pops a value from the stack and subtracts it from the register value, storing the result in the register.
+# MULT Pops a value from the stack and multiplies it by the register value, storing the result in the register.
+# DIV Pops a value from the stack and divides it into the register value, storing the integer result in the register.
+# MOD Pops a value from the stack and divides it into the register value,
+# storing the integer remainder of the division in the register.
+# POP Remove the topmost item from the stack and place in register
+# PRINT Print the register value
+# All operations are integer operations (which is only important with DIV and MOD).
+# Programs will be supplied to your language method via a string passed in as an argument.
+# Your program may assume that all programs are correct programs;
+# that is, they won't do anything like try to pop a non-existent value from the stack,
+# and they won't contain unknown tokens.
+# You should initialize the register to 0.
 
 
 
