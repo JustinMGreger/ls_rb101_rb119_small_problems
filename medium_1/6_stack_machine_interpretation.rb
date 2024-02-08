b60726ba2121543b6ff5728a569a35b83e22c830
Justin Greger <justingreger@gmail.com>
2024-02-07 20:43:09 -0500

    # which is followed by the MOD string key which is assigned the value that is the # result of passing the :cmd_mod symbol to the method method

--- medium_1/6_stack_machine_interpretation.rb
+++ medium_1/6_stack_machine_interpretation.rb
@@ -112,7 +112,8 @@
 # the result of passing the :cmd_mult symbol to the method method
 # which is followed by the 'DIV' string key which is assigned the value that is the
 # result of passing the :cmd_div symbol to the method method
-
+# which is followed by the 'MOD' string key which is assigned the value that is the
+# result of passing the :cmd_mod symbol to the method method
 
 
 
