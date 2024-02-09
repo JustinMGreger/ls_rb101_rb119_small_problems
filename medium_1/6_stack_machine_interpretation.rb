a13c57f0dec71d2ed9ad80042aef185ab54127ea
Justin Greger <justingreger@gmail.com>
2024-02-08 21:54:09 -0500

    # LS Solution:

--- medium_1/6_stack_machine_interpretation.rb
+++ medium_1/6_stack_machine_interpretation.rb
@@ -275,6 +275,25 @@
 #   end
 # end
 
+# LS Solution:
+def minilang(program)
+  stack = []
+  register = 0
+  program.split.each do |token|
+    case token
+    when 'ADD'   then register += stack.pop
+    when 'DIV'   then register /= stack.pop
+    when 'MULT'  then register *= stack.pop
+    when 'MOD'   then register %= stack.pop
+    when 'SUB'   then register -= stack.pop
+    when 'PUSH'  then stack.push(register)
+    when 'POP'   then register = stack.pop
+    when 'PRINT' then puts register
+    else              register = token.to_i
+    end
+  end
+end
+
 # tests:
 minilang('PRINT')
 # 0
