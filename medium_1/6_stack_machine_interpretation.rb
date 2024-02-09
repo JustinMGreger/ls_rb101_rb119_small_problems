d468065ae906d7f785701ad1ce762dfb69926869
Justin Greger <justingreger@gmail.com>
2024-02-08 19:10:43 -0500

    # return the result of removing and returning the last element of the stack parameter.

--- medium_1/6_stack_machine_interpretation.rb
+++ medium_1/6_stack_machine_interpretation.rb
@@ -193,7 +193,7 @@
 # returning the last element of the stack parameter.
 # this is the last line of the cmd_mod method.
 # the cmd_pop method accepts the (stack, _register) parameters and is defined as the following:
-
+# return the result of removing and returning the last element of the stack parameter.
 
 
 and Code. Implementation of Algorithm:
