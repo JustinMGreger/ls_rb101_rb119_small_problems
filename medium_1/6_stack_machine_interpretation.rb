16069b187859d421e62976bbfa0255f4b8554de5
Justin Greger <justingreger@gmail.com>
2024-02-09 21:32:27 -0500

    # minilang

--- medium_1/6_stack_machine_interpretation.rb
+++ medium_1/6_stack_machine_interpretation.rb
@@ -293,30 +293,29 @@
 #     end
 #   end
 # end
-
 # tests:
-minilang('PRINT')
+# minilang('PRINT')
 # 0
-minilang('5 PUSH 3 MULT PRINT')
+# minilang('5 PUSH 3 MULT PRINT')
 # 15
-minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+# minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
 # 5
 # 3
 # 8
-minilang('5 PUSH POP PRINT')
+# minilang('5 PUSH POP PRINT')
 # 5
-minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
 # 5
 # 10
 # 4
 # 7
-minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+# minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
 # 6
-minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+# minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
 # 12
-minilang('-3 PUSH 5 SUB PRINT')
+# minilang('-3 PUSH 5 SUB PRINT')
 # 8
-minilang('6 PUSH')
+# minilang('6 PUSH')
 # (nothing printed; no PRINT commands)
 
 # Further Exploration
