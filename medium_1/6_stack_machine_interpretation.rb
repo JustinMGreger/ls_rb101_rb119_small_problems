8fee731969951b3ae76f88b5b12005b8a21d6b0e
Justin Greger <justingreger@gmail.com>
2024-02-14 21:40:03 -0500

    3 4 5 * + 7 - 5 3 % /

--- medium_1/6_stack_machine_interpretation.rb
+++ medium_1/6_stack_machine_interpretation.rb
@@ -330,7 +330,9 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# the '3 4 5 * + 7 - 5 3 % /' string.
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
