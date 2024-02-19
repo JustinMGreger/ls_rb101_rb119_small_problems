de3db19e7399dc80b44580b4cf2f10ef1842a63b
Justin Greger <justingreger@gmail.com>
2024-02-18 21:31:24 -0500

    # p word_to_digit

--- medium_1/7_word_to_digit.rb
+++ medium_1/7_word_to_digit.rb
@@ -34,7 +34,7 @@
 # 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' converted to a string of digits.
 
 # Examples / Test Cases, Validate understanding of the problem:
-p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
+# p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
 
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
