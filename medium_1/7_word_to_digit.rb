19f2ce870eed4484c940fe7c4dd71bb218764cbd
Justin Greger <justingreger@gmail.com>
2024-02-18 20:43:00 -0500

    # Write a method that takes a sentence string as input,

--- medium_1/7_word_to_digit.rb
+++ medium_1/7_word_to_digit.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+# Write a method that takes a sentence string as input,
+and returns the same string with any sequence of the words 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' converted to a string of digits.
+
+Example:
