593972149601bbadd45ef802a4d01c98593c0ad8
Justin Greger <justingreger@gmail.com>
2024-02-18 21:04:14 -0500

    # Identify expected input: # a sentence string.

--- medium_1/7_word_to_digit.rb
+++ medium_1/7_word_to_digit.rb
@@ -17,7 +17,9 @@ word_to_digit('Please call me at five five five one two three four. Thanks.') ==
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# a sentence string.
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
