7188a5a8066d36e2d4c6da7f555d9d5445be7a66
Justin Greger <justingreger@gmail.com>
2024-02-18 21:03:05 -0500

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for [Understand the] Problem,

--- medium_1/7_word_to_digit.rb
+++ medium_1/7_word_to_digit.rb
@@ -13,3 +13,15 @@ word_to_digit('Please call me at five five five one two three four. Thanks.') ==
 # but it isn't necessary - this hint provides the only regular expression you need for this problem:
 # if you have a variable named foo that contains some sort of word and want to match that word in a longer string,
 # you can use the regex /\b#{foo}\b/.
+
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for [Understand the] Problem,
+
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code. Implementation of Algorithm:
