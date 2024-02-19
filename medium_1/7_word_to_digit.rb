dbaef6b6d5f5e10bd6affe820a10f7259a67c702
Justin Greger <justingreger@gmail.com>
2024-02-18 20:51:29 -0500

    # Example:

--- medium_1/7_word_to_digit.rb
+++ medium_1/7_word_to_digit.rb
@@ -5,3 +5,4 @@
 # 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' converted to a string of digits.
 
 # Example:
+word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
