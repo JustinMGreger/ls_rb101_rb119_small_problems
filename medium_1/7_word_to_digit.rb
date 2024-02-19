8d8e7b1cebed98a1a37a973843dd6915f628a906
Justin Greger <justingreger@gmail.com>
2024-02-18 20:43:33 -0500

    # and returns the same string with any sequence of the words

--- medium_1/7_word_to_digit.rb
+++ medium_1/7_word_to_digit.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # Write a method that takes a sentence string as input,
-and returns the same string with any sequence of the words 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' converted to a string of digits.
+# and returns the same string with any sequence of the words
+'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' converted to a string of digits.
 
 Example:
