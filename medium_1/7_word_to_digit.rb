adc3f060c2beb0003a4d6f84bfe15edf694877bb
Justin Greger <justingreger@gmail.com>
2024-02-21 21:00:03 -0500

    # p word_to_digit

--- medium_1/7_word_to_digit.rb
+++ medium_1/7_word_to_digit.rb
@@ -102,7 +102,7 @@
 # end
 
 # test:
-p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
+# p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
 
 # Further Exploration
 
