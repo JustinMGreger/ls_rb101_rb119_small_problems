aed72bf46dbb97fd5187374809fa5fa756833514
Justin Greger <justingreger@gmail.com>
2024-02-23 20:10:09 -0500

    #

--- medium_1/7_word_to_digit.rb
+++ medium_1/7_word_to_digit.rb
@@ -104,7 +104,8 @@
 # end
 
 # test:
-# p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
+# p word_to_digit('Please call me at five five five one two three four. Thanks.') ==
+# 'Please call me at 5 5 5 1 2 3 4. Thanks.'
 
 # Further Exploration
 
