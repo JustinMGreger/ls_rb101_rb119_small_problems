b2696964e5a1ea91708ec36c0d866ca643e063ec
Justin Greger <justingreger@gmail.com>
2024-02-27 20:43:01 -0500

    DIGIT_HASH = {

--- medium_1/7_word_to_digit.rb
+++ medium_1/7_word_to_digit.rb
@@ -178,3 +178,29 @@
 # Suppose the string already contains two or more space separated numbers (not words);
 # can you leave those spaces alone, while removing any spaces between numbers that you
 # create?
+DIGIT_HASH = {
+  'zero' => '0', 'one' => '1', 'two' => '2', 'three' => '3', 'four' => '4',
+  'five' => '5', 'six' => '6', 'seven' => '7', 'eight' => '8', 'nine' => '9'
+}.freeze
+
+def word_to_digit(sentence)
+  modified_sentence = sentence.dup
+
+  DIGIT_HASH.each do |word, digit|
+    modified_sentence.gsub!(/\b#{word}\b/i) { "|#{digit}|" }
+  end
+
+  modified_sentence.gsub!(/\|\s+\|/, '||')
+  modified_sentence.gsub!(/\|(\d)\|/, '\1')
+
+  modified_sentence
+end
+
+# tests:
+puts word_to_digit('Please call me at five five five one two three four. Thanks.') == "Please call me at 5551234. Thanks."
+puts word_to_digit('My number is four one five two three. Please call.')
+# Expected: "My number is 41523. Please call."
+puts word_to_digit('Call me on 123 456 or at seven eight nine.')
+# Expected: "Call me on 123 456 or at 789."
+puts word_to_digit('Please dial nine one one for emergency. Call five five five one two three four for non-emergencies. Dial 9 1 1 for direct emergencies.')
+# Expected: "Please dial 911 for emergency. Call 5551234 for non-emergencies. Dial 9 1 1 for direct emergencies."
