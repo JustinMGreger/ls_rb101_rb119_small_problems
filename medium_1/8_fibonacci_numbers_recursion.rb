d9c751dc95855df77aee909d216995d5fe2e4b48
Justin Greger <justingreger@gmail.com>
2024-03-02 19:49:59 -0500

    # the 3rd Fibonacci number is 1 + 1 = 2,

--- medium_1/8_fibonacci_numbers_recursion.rb
+++ medium_1/8_fibonacci_numbers_recursion.rb
@@ -2,4 +2,5 @@
 
 # The Fibonacci series is a sequence of numbers starting with 1 and 1
 # where each number is the sum of the two previous numbers:
- the 3rd Fibonacci number is 1 + 1 = 2, the 4th is 1 + 2 = 3, the 5th is 2 + 3 = 5, and so on. In mathematical terms:
+# the 3rd Fibonacci number is 1 + 1 = 2,
+the 4th is 1 + 2 = 3, the 5th is 2 + 3 = 5, and so on. In mathematical terms:
