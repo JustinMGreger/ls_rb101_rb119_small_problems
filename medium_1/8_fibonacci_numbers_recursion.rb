ba76d5dc6171ce24ab1a61dc5320d4f46d9fd77e
Justin Greger <justingreger@gmail.com>
2024-03-02 21:44:48 -0500

    # Examples:

--- medium_1/8_fibonacci_numbers_recursion.rb
+++ medium_1/8_fibonacci_numbers_recursion.rb
@@ -26,3 +26,10 @@
 
 # Write a recursive method that computes the nth Fibonacci number, where nth is an argument to the method.
 # Examples:
+fibonacci(1) == 1
+fibonacci(2) == 1
+fibonacci(3) == 2
+fibonacci(4) == 3
+fibonacci(5) == 5
+fibonacci(12) == 144
+fibonacci(20) == 6765
