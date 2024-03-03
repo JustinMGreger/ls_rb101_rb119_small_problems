50dcea68b19a20c61d5ac0344b36126a324d484d
Justin Greger <justingreger@gmail.com>
2024-03-02 20:20:50 -0500

    # They have a condition that stops the recursion (n == 1 above).

--- medium_1/8_fibonacci_numbers_recursion.rb
+++ medium_1/8_fibonacci_numbers_recursion.rb
@@ -19,5 +19,5 @@
 # sum is a recursive method that computes the sum of all integers between 1 and n.
 # Recursive methods have three primary qualities:
 # They call themselves at least once.
-They have a condition that stops the recursion (n == 1 above).
+# They have a condition that stops the recursion (n == 1 above).
 They use the result returned by themselves.
