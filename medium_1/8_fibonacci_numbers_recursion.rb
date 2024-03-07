13597130b7233f0c2cbe2a832375ef4300c0ce90
Justin Greger <justingreger@gmail.com>
2024-03-06 19:05:51 -0500

    # A tail recursive solution for this exercise is:

--- medium_1/8_fibonacci_numbers_recursion.rb
+++ medium_1/8_fibonacci_numbers_recursion.rb
@@ -92,6 +92,21 @@
 #   fibonacci(nth - 1) + fibonacci(nth - 2)
 # end
 
+# A tail recursive solution for this exercise is:
+def fibonacci_tail(nth, acc1, acc2)
+  if nth == 1
+    acc1
+  elsif nth == 2
+    acc2
+  else
+    fibonacci_tail(nth - 1, acc2, acc2 + acc1)
+  end
+end
+
+def fibonacci(nth)
+  fibonacci_tail(nth, 1, 1)
+end
+
 # tests:
 p fibonacci(1) == 1
 p fibonacci(2) == 1
