64747e99769591dc12cc721df33983c690b3749a
Justin Greger <justingreger@gmail.com>
2024-03-09 20:39:55 -0500

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for [Understand the] Problem,

--- medium_1/9_fibonacci_numbers_procedural.rb
+++ medium_1/9_fibonacci_numbers_procedural.rb
@@ -16,3 +16,15 @@
 fibonacci(20) == 6765
 fibonacci(100) == 354224848179261915075
 fibonacci(100_001) # => 4202692702.....8285979669707537501
+
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for [Understand the] Problem,
+
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code. Implementation of Algorithm:
