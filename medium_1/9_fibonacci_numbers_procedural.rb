9c326b76cbfde92bd66bac2a1f6290a00e61709a
Justin Greger <justingreger@gmail.com>
2024-03-09 20:40:53 -0500

    # Examples / Test Cases, Validate understanding of the problem:

--- medium_1/9_fibonacci_numbers_procedural.rb
+++ medium_1/9_fibonacci_numbers_procedural.rb
@@ -24,7 +24,12 @@ Identify expected input:
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
-Examples / Test Cases, Validate understanding of the problem:
+
+# Examples / Test Cases, Validate understanding of the problem:
+p fibonacci(20) == 6765
+p fibonacci(100) == 354224848179261915075
+p fibonacci(100_001) # => 4202692702.....8285979669707537501
+
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
