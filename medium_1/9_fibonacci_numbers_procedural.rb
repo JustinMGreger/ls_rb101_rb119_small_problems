f17452a8c89c90a6b611e2cf93d0817fb34743e6
Justin Greger <justingreger@gmail.com>
2024-03-09 20:44:02 -0500

    # fibonacci

--- medium_1/9_fibonacci_numbers_procedural.rb
+++ medium_1/9_fibonacci_numbers_procedural.rb
@@ -13,9 +13,9 @@
 # Rewrite your recursive fibonacci method so that it computes its results without recursion.
 
 # Examples:
-fibonacci(20) == 6765
-fibonacci(100) == 354224848179261915075
-fibonacci(100_001) # => 4202692702.....8285979669707537501
+# fibonacci(20) == 6765
+# fibonacci(100) == 354224848179261915075
+# fibonacci(100_001) # => 4202692702.....8285979669707537501
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
