76ac108695a51ac2d9ff06e3ae79a463d4681399
Justin Greger <justingreger@gmail.com>
2024-03-09 19:52:24 -0500

    # With higher values of nth, the recursive solution is impractical.

--- medium_1/9_fibonacci_numbers_procedural.rb
+++ medium_1/9_fibonacci_numbers_procedural.rb
@@ -5,7 +5,8 @@
 
 # Ruby does a reasonably good job of handling recursion, but it isn't designed for heavy recursion;
 # as a result, the Fibonacci solution is only useful up to about fibonacci(40).
-With higher values of nth, the recursive solution is impractical. (Our tail recursive solution did much better, but even that failed at around fibonacci(8200).)
+# With higher values of nth, the recursive solution is impractical.
+(Our tail recursive solution did much better, but even that failed at around fibonacci(8200).)
 
 Fortunately, every recursive method can be rewritten as a non-recursive (procedural) method.
 
