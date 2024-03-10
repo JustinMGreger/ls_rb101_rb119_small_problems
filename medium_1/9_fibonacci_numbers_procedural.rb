8d181a155aadd2951d3b5756667a631bcf2b7e85
Justin Greger <justingreger@gmail.com>
2024-03-09 19:44:51 -0500

    # In a language that is not optimized for recursion,

--- medium_1/9_fibonacci_numbers_procedural.rb
+++ medium_1/9_fibonacci_numbers_procedural.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 # In the previous exercise, we developed a recursive solution to calculating the nth Fibonacci number.
-In a language that is not optimized for recursion, some (not all) recursive methods can be extremely slow and require massive quantities of memory and/or stack space.
+# In a language that is not optimized for recursion,
+some (not all) recursive methods can be extremely slow and require massive quantities of memory and/or stack space.
 
 Ruby does a reasonably good job of handling recursion, but it isn't designed for heavy recursion; as a result, the Fibonacci solution is only useful up to about fibonacci(40). With higher values of nth, the recursive solution is impractical. (Our tail recursive solution did much better, but even that failed at around fibonacci(8200).)
 
