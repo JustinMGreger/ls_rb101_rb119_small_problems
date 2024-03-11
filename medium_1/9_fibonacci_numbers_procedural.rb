bb49115e78bf285092ff636b02b9d4a2d83809cf
Justin Greger <justingreger@gmail.com>
2024-03-10 21:42:07 -0400

    354_224_848_179_261_915_075

--- medium_1/9_fibonacci_numbers_procedural.rb
+++ medium_1/9_fibonacci_numbers_procedural.rb
@@ -47,5 +47,5 @@ and Code. Implementation of Algorithm:
 
 # tests:
 p fibonacci(20) == 6765
-p fibonacci(100) == 354224848179261915075
+p fibonacci(100) == 354_224_848_179_261_915_075
 p fibonacci(100_001) # => 4202692702.....8285979669707537501
