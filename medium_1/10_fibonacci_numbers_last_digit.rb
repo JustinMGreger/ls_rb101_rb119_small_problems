7679cd534f1389f4c602ac3e3355ef266136d9ad
Justin Greger <justingreger@gmail.com>
2024-03-14 19:34:05 -0400

    # Identify expected input: # nth fibonacci number.

--- medium_1/10_fibonacci_numbers_last_digit.rb
+++ medium_1/10_fibonacci_numbers_last_digit.rb
@@ -19,7 +19,8 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# nth fibonacci number.
 
 output:
 Make the requirements explicit Identify rules:
