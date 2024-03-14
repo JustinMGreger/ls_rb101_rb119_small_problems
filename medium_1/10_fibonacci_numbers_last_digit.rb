6c0a670ba8286e76b6757529b474ba6259c39ba3
Justin Greger <justingreger@gmail.com>
2024-03-13 21:49:54 -0400

    # fibonacci_last

--- medium_1/10_fibonacci_numbers_last_digit.rb
+++ medium_1/10_fibonacci_numbers_last_digit.rb
@@ -9,12 +9,12 @@
 # you are going to compute a method that returns the last digit of the nth Fibonacci number.
 
 # Examples:
-fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
-fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
-fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
-fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
-fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
-fibonacci_last(123456789) # -> 4
+# fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+# fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+# fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+# fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+# fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+# fibonacci_last(123456789) # -> 4
 
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
