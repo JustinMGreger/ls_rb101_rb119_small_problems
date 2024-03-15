d37ce7a45fa003b2ae6d32174472cf5b9e71a85e
Justin Greger <justingreger@gmail.com>
2024-03-14 20:58:08 -0400

    # Algorithm, Steps for converting input to output:

--- medium_1/10_fibonacci_numbers_last_digit.rb
+++ medium_1/10_fibonacci_numbers_last_digit.rb
@@ -44,7 +44,14 @@
 # Data Structure, How we represent data that we will work with when converting the input to output:
 # array.
 
-Algorithm, Steps for converting input to output:
+# Algorithm, Steps for converting input to output:
+
+
+
+
+
+
+
 and Code. Implementation of Algorithm:
 
 # tests:
