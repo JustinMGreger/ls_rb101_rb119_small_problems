03d96af4f8e1824b945f00db226e8104bb89b5c6
Justin Greger <justingreger@gmail.com>
2024-03-15 21:59:59 -0400

    123_456_789

--- medium_1/10_fibonacci_numbers_last_digit.rb
+++ medium_1/10_fibonacci_numbers_last_digit.rb
@@ -75,4 +75,4 @@ p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
 p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
 p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
 p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
-p fibonacci_last(123456789) # -> 4
+p fibonacci_last(123_456_789) # -> 4
