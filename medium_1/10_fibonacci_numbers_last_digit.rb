efda166d78baf42452f44f7444b0d45b61d244e8
Justin Greger <justingreger@gmail.com>
2024-03-16 21:57:19 -0400

    def fibonacci_last(nth)

--- medium_1/10_fibonacci_numbers_last_digit.rb
+++ medium_1/10_fibonacci_numbers_last_digit.rb
@@ -84,6 +84,14 @@
 # end
 
 # LS Solution Part 2:
+def fibonacci_last(nth)
+  last_2 = [1, 1]
+  3.upto(nth) do
+    last_2 = [last_2.last, (last_2.first + last_2.last) % 10]
+  end
+
+  last_2.last
+end
 
 # tests:
 p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
