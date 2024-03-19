da0449ee70affea55fb63d1f5b212304b7ff3444
Justin Greger <justingreger@gmail.com>
2024-03-18 20:04:56 -0400

    # tests: # p fibonacci_last

--- medium_1/10_fibonacci_numbers_last_digit.rb
+++ medium_1/10_fibonacci_numbers_last_digit.rb
@@ -93,6 +93,14 @@
 #   last_2.last
 # end
 
+# tests:
+# p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+# p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+# p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+# p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+# p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+# p fibonacci_last(123_456_789) # -> 4
+
 # Further Exploration
 
 # After a while, even this method starts to take too long to compute results.
