e0bd898cbae9b279dcc56dd3f12c020910eb35f5
Justin Greger <justingreger@gmail.com>
2024-03-25 21:24:10 -0400

    # that that nation might live. It is altogether fitting and

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -17,7 +17,7 @@
 # can long endure. We are met on a great battlefield of that
 # war. We have come to dedicate a portion of that field, as
 # a final resting place for those who here gave their lives
-that that nation might live. It is altogether fitting and
+# that that nation might live. It is altogether fitting and
 proper that we should do this.
 
 But, in a larger sense, we can not dedicate, we can not
