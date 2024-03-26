3b7d3549fd49d5537f0c550b2c6cd556d62c7e23
Justin Greger <justingreger@gmail.com>
2024-03-25 21:22:19 -0400

    # Now we are engaged in a great civil war, testing whether

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -12,7 +12,7 @@
 # dedicated to the proposition that all men are created
 # equal.
 
-Now we are engaged in a great civil war, testing whether
+# Now we are engaged in a great civil war, testing whether
 that nation, or any nation so conceived and so dedicated,
 can long endure. We are met on a great battlefield of that
 war. We have come to dedicate a portion of that field, as
