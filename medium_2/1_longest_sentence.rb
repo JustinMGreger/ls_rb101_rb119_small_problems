66e1c52ad9e0fb1d7657813fe9b3e9d9424a4dcf
Justin Greger <justingreger@gmail.com>
2024-03-25 21:22:55 -0400

    # that nation, or any nation so conceived and so dedicated,

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -13,7 +13,7 @@
 # equal.
 
 # Now we are engaged in a great civil war, testing whether
-that nation, or any nation so conceived and so dedicated,
+# that nation, or any nation so conceived and so dedicated,
 can long endure. We are met on a great battlefield of that
 war. We have come to dedicate a portion of that field, as
 a final resting place for those who here gave their lives
