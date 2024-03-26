689afe7099950d48d032e48bd346d2f9468ff350
Justin Greger <justingreger@gmail.com>
2024-03-25 21:23:53 -0400

    # a final resting place for those who here gave their lives

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -16,7 +16,7 @@
 # that nation, or any nation so conceived and so dedicated,
 # can long endure. We are met on a great battlefield of that
 # war. We have come to dedicate a portion of that field, as
-a final resting place for those who here gave their lives
+# a final resting place for those who here gave their lives
 that that nation might live. It is altogether fitting and
 proper that we should do this.
 
