d06d09dbd69f35d89824a1aad005058abc3f295f
Justin Greger <justingreger@gmail.com>
2024-03-25 21:14:14 -0400

    # on this continent a new nation, conceived in liberty, and

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -8,7 +8,7 @@
 
 # Example text:
 # Four score and seven years ago our fathers brought forth
-on this continent a new nation, conceived in liberty, and
+# on this continent a new nation, conceived in liberty, and
 dedicated to the proposition that all men are created
 equal.
 
