1bb210bf0335d0f065143d4efe5e13efc2dc8c48
Justin Greger <justingreger@gmail.com>
2024-03-26 19:33:42 -0400

    # before us -- that from these honored dead we take

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -30,7 +30,7 @@
 # here to the unfinished work which they who fought
 # here have thus far so nobly advanced. It is rather for
 # us to be here dedicated to the great task remaining
-before us -- that from these honored dead we take
+# before us -- that from these honored dead we take
 increased devotion to that cause for which they gave
 the last full measure of devotion -- that we here highly
 resolve that these dead shall not have died in vain
