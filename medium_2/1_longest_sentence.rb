40c18a81d54cf4fd62647ef46b5215e1aa455906
Justin Greger <justingreger@gmail.com>
2024-03-25 21:14:31 -0400

    # dedicated to the proposition that all men are created

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -9,7 +9,7 @@
 # Example text:
 # Four score and seven years ago our fathers brought forth
 # on this continent a new nation, conceived in liberty, and
-dedicated to the proposition that all men are created
+# dedicated to the proposition that all men are created
 equal.
 
 Now we are engaged in a great civil war, testing whether
