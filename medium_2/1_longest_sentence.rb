4bfd448d613a07e5c363fef7284e5f0dd799970c
Justin Greger <justingreger@gmail.com>
2024-03-25 21:04:32 -0400

    # You should also print the number of words in the longest sentence.

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -4,6 +4,6 @@
 # and then prints the longest sentence in the file based on number of words.
 # Sentences may end with periods (.), exclamation points (!), or question marks (?).
 # Any sequence of characters that are not spaces or sentence-ending characters should be treated as a word.
-You should also print the number of words in the longest sentence.
+# You should also print the number of words in the longest sentence.
 
 Example text:
