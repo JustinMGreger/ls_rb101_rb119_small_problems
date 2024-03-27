60cec00707d570e2687389a7679767fc56b8252c
Justin Greger <justingreger@gmail.com>
2024-03-27 19:27:49 -0400

    # sentences = text.split(/\.|\?|!/)

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -81,7 +81,7 @@
 # Sentences may end with periods (.), exclamation points (!), or question marks (?).
 # Any sequence of characters that are not spaces or sentence-ending characters should be treated as a word.
 # You should also print the number of words in the longest sentence.
-
+# sentences = text.split(/\.|\?|!/)
 
 
 
