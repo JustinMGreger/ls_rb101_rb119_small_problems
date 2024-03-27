0342bf53f29091d9cc71247242504510df664443
Justin Greger <justingreger@gmail.com>
2024-03-26 21:12:04 -0400

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for [Understand the] Problem,

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -57,3 +57,15 @@
 # If you haven't already read our book, Introduction to Regular Expressions,
 # you may do so now, but it isn't necessary
 # - this hint is all you need to know about regex for this problem.
+
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for [Understand the] Problem,
+
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code. Implementation of Algorithm:
