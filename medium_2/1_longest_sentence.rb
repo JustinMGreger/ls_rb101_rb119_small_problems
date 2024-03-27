e8e1d804fb2c3a6ce0141161bbd9f58bc39452bf
Justin Greger <justingreger@gmail.com>
2024-03-26 21:20:18 -0400

    # Data Structure, How we represent data that we will work with when converting the input to output: # an array.

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -66,6 +66,9 @@ output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
-Data Structure, How we represent data that we will work with when converting the input to output:
+
+# Data Structure, How we represent data that we will work with when converting the input to output:
+# an array.
+
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
