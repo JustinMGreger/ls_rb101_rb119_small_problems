a6c53e6203978097b8e3bac04cbd2a4b2d25a5f4
Justin Greger <justingreger@gmail.com>
2024-04-05 21:49:12 -0400

    # write a solution that keeps the period printed at the end of each sentence.

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -305,6 +305,12 @@
 
 
 Make the requirements explicit Identify rules:
+# write a solution that keeps the period printed at the end of each sentence.
+
+
+
+
+
 Mental model of the problem (optional):
 Examples / Test Cases, Validate understanding of the problem:
 Data Structure, How we represent data that we will work with when converting the input to output:
