9d13a3a541c454dbc53e52c022f80949d2f5c3e1
Justin Greger <justingreger@gmail.com>
2024-04-06 21:44:51 -0400

    # and Code. Implementation of Algorithm:

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -391,5 +391,19 @@
 # the Containing string followed by the #{number_of_words} string interpolation which
 # contains: the number_of_words variable followed by the words string.
 
-and Code. Implementation of Algorithm:
+# and Code. Implementation of Algorithm:
+text = File.read('sample_text.txt')
 
+sentences = text.split(/(?<=[.?!])/)
+
+sentences.map! do |sentence|
+  sentence.strip!
+  sentence.chomp!('!') || sentence.chomp!('?')
+  sentence
+end
+
+largest_sentence = sentences.max_by { |sentence| sentence.split.size }
+number_of_words = largest_sentence.split.size
+
+puts "#{largest_sentence}"
+puts "Containing #{number_of_words} words"
