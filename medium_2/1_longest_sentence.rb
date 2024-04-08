3d1a55fc9fd122412ad83fb98382e346985353da
Justin Greger <justingreger@gmail.com>
2024-04-07 21:26:44 -0400

    # Identify expected input: # sample_text.txt

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -415,7 +415,9 @@
 # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
 # PEDAC stands for [Understand the] Problem,
 
-Identify expected input:
+# Identify expected input:
+# sample_text.txt
+
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
