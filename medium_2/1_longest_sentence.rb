0b2373c531eefcc0817ce0cc7e5deddac9dade64
Justin Greger <justingreger@gmail.com>
2024-04-07 20:58:47 -0400

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for [Understand the] Problem,

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -412,6 +412,18 @@
 # What about finding the longest paragraph or longest word?
 # How would we go about solving that problem?
 
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for [Understand the] Problem,
+
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code. Implementation of Algorithm:
+
 puts "the longest sentence is: #{largest_sentence}"
 puts "Containing #{number_of_words} words"
 puts "The longest paragraph is: #{longest_paragraph}"
