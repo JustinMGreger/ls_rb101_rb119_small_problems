9d7d7d6c8224ab287bb06195e134121dd211bcf2
Justin Greger <justingreger@gmail.com>
2024-04-07 21:59:39 -0400

    # Write a program that reads the content of a text file # and then prints the longest sentence in the file based on number of words. # Sentences may end with periods (.), exclamation points (!), or question marks (?). # Any sequence of characters that are not spaces or sentence-ending characters should be treated as a word. # You should also print the number of words in the longest sentence. # keep the period printed at the end of each sentence. # print the longest paragraph and the longest word.

--- medium_2/1_longest_sentence.rb
+++ medium_2/1_longest_sentence.rb
@@ -434,6 +434,20 @@
 # print the longest paragraph and the longest word.
 
 Mental model of the problem (optional):
+# Write a program that reads the content of a text file
+# and then prints the longest sentence in the file based on number of words.
+# Sentences may end with periods (.), exclamation points (!), or question marks (?).
+# Any sequence of characters that are not spaces or sentence-ending characters should be treated as a word.
+# You should also print the number of words in the longest sentence.
+# keep the period printed at the end of each sentence.
+# print the longest paragraph and the longest word.
+
+
+
+
+
+
+
 
 # Examples / Test Cases, Validate understanding of the problem:
 # puts "the longest sentence is: #{largest_sentence}"
