de1a87a2ffa8ea746d8acf1485a5594197a1dc85
Justin Greger <justingreger@gmail.com>
2024-04-14 21:12:51 -0400

    # G:T   R:E   F:S   J:W   H:U

--- medium_2/2_now_i_know_my_abcs.rb
+++ medium_2/2_now_i_know_my_abcs.rb
@@ -2,5 +2,5 @@
 
 # A collection of spelling blocks has two letters per block, as shown in this list:
 # B:O   X:K   D:Q   C:P   N:A
-G:T   R:E   F:S   J:W   H:U
+# G:T   R:E   F:S   J:W   H:U
 V:I   L:Y   Z:M
