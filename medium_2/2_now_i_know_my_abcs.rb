d4b42f1e79f1da0add5cb07b07b4f5a0fe8288a7
Justin Greger <justingreger@gmail.com>
2024-04-14 21:09:20 -0400

    # B:O   X:K   D:Q   C:P   N:A

--- medium_2/2_now_i_know_my_abcs.rb
+++ medium_2/2_now_i_know_my_abcs.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
 # A collection of spelling blocks has two letters per block, as shown in this list:
+# B:O   X:K   D:Q   C:P   N:A
+G:T   R:E   F:S   J:W   H:U
+V:I   L:Y   Z:M
