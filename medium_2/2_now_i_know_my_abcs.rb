a396758543bbdffc95d64eaae598ed0e2acabb7a
Justin Greger <justingreger@gmail.com>
2024-04-16 20:23:07 -0400

    # Write a method that returns true if the word passed in as an argument can be # spelled from this set of blocks, false otherwise. # A collection of spelling blocks has two letters per block, as shown in this list: # B:O   X:K   D:Q   C:P   N:A # G:T   R:E   F:S   J:W   H:U # V:I   L:Y   Z:M # This limits the words you can spell with the blocks to just those words that do # not use both letters from any given block. # Each letter in each block can only be used once.

--- medium_2/2_now_i_know_my_abcs.rb
+++ medium_2/2_now_i_know_my_abcs.rb
@@ -39,6 +39,24 @@
 
 Mental model of the problem (optional):
 
+# Write a method that returns true if the word passed in as an argument can be
+# spelled from this set of blocks, false otherwise.
+# A collection of spelling blocks has two letters per block, as shown in this list:
+# B:O   X:K   D:Q   C:P   N:A
+# G:T   R:E   F:S   J:W   H:U
+# V:I   L:Y   Z:M
+# This limits the words you can spell with the blocks to just those words that do
+# not use both letters from any given block.
+# Each letter in each block can only be used once.
+
+
+
+
+
+
+
+
+
 # Examples / Test Cases, Validate understanding of the problem:
 # p block_word?('BATCH') == true
 # p block_word?('BUTCH') == false
