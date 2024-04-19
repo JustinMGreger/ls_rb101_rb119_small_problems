643ea4cfaaf7e9605c2af448d95bdcce87689f32
Justin Greger <justingreger@gmail.com>
2024-04-18 21:20:40 -0400

    # this is the last line of the if statement.

--- medium_2/2_now_i_know_my_abcs.rb
+++ medium_2/2_now_i_know_my_abcs.rb
@@ -68,7 +68,8 @@
 # if the result of counting the occurrence of the letter1 varible in 
 # the word parameter in upcase form is positive
 # and the result of counting the occurrence of of the letter2 varible in
-# the word parameter in upcase form is positive return false
+# the word parameter in upcase form is positive return false.
+# this is the last line of the if statement.
 
 and Code. Implementation of Algorithm:
 
