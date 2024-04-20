e8ceb114aae72da8e155cf79a375f1374f39f10d
Justin Greger <justingreger@gmail.com>
2024-04-19 20:26:58 -0400

    BLOCKS = %w(BO XK DQ CP NA GT RE FS JW HU VI LY ZM).freeze
    def block_word?(string)
      up_string = string.upcase
      BLOCKS.none? { |block| up_string.count(block) >= 2 }
    end

--- medium_2/2_now_i_know_my_abcs.rb
+++ medium_2/2_now_i_know_my_abcs.rb
@@ -87,6 +87,12 @@
 # end
 
 # LS Solution:
+BLOCKS = %w(BO XK DQ CP NA GT RE FS JW HU VI LY ZM).freeze
+
+def block_word?(string)
+  up_string = string.upcase
+  BLOCKS.none? { |block| up_string.count(block) >= 2 }
+end
 
 # tests:
 p block_word?('BATCH') == true
