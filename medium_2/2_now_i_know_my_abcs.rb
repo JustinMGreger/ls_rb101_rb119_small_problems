88258dd8ee7c10b6c6b7826bc241cdeb2d33b1ea
Justin Greger <justingreger@gmail.com>
2024-04-20 21:20:30 -0400

    # BLOCKS = %w(BO XK DQ CP NA GT RE FS JW HU VI LY ZM).freeze

--- medium_2/2_now_i_know_my_abcs.rb
+++ medium_2/2_now_i_know_my_abcs.rb
@@ -87,7 +87,7 @@
 # end
 
 # LS Solution:
-BLOCKS = %w(BO XK DQ CP NA GT RE FS JW HU VI LY ZM).freeze
+# BLOCKS = %w(BO XK DQ CP NA GT RE FS JW HU VI LY ZM).freeze
 
 def block_word?(string)
   up_string = string.upcase
