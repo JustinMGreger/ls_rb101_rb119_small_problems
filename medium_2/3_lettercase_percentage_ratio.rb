f55f1e112e426013345ca5493d83cb833f81038b
Justin Greger <justingreger@gmail.com>
2024-04-22 20:03:18 -0400

    # we worked on a problem where we had to count the number of uppercase and lowercase characters,

--- medium_2/3_lettercase_percentage_ratio.rb
+++ medium_2/3_lettercase_percentage_ratio.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 # In the easy exercises,
-we worked on a problem where we had to count the number of uppercase and lowercase characters, as well as characters that were neither of those two. Now we want to go one step further.
+# we worked on a problem where we had to count the number of uppercase and lowercase characters,
+as well as characters that were neither of those two. Now we want to go one step further.
 
 Write a method that takes a string, and then returns a hash that contains 3 entries: one represents the percentage of characters in the string that are lowercase letters, one the percentage of characters that are uppercase letters, and one the percentage of characters that are neither.
 
