2d532cbe6e75ce03f993c7e48ab377f3191b0165
Justin Greger <justingreger@gmail.com>
2024-04-23 18:54:24 -0400

    # Algorithm, Steps for converting input to output:

--- medium_2/3_lettercase_percentage_ratio.rb
+++ medium_2/3_lettercase_percentage_ratio.rb
@@ -55,7 +55,7 @@
 # Data Structure, How we represent data that we will work with when converting the input to output:
 # a hash.
 
-Algorithm, Steps for converting input to output:
+# Algorithm, Steps for converting input to output:
 
 # and Code. Implementation of Algorithm:
 
