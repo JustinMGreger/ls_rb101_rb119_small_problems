df4cbce9eb18f0226881d40fa4bd1fc8e22e4108
Justin Greger <justingreger@gmail.com>
2024-04-22 20:11:00 -0400

    # Examples / Test Cases, Validate understanding of the problem:

--- medium_2/3_lettercase_percentage_ratio.rb
+++ medium_2/3_lettercase_percentage_ratio.rb
@@ -25,7 +25,12 @@ Identify expected input:
 output:
 Make the requirements explicit Identify rules:
 Mental model of the problem (optional):
-Examples / Test Cases, Validate understanding of the problem:
+
+# Examples / Test Cases, Validate understanding of the problem:
+p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
+p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
+p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
+
 Data Structure, How we represent data that we will work with when converting the input to output:
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
