75f1eee9d8a4b498055742330583f99a22a6869b
Justin Greger <justingreger@gmail.com>
2024-04-22 20:09:27 -0400

    p letter_percentages

--- medium_2/3_lettercase_percentage_ratio.rb
+++ medium_2/3_lettercase_percentage_ratio.rb
@@ -14,3 +14,6 @@
 # You may assume that the string will always contain at least one character.
 
 # Examples:
+p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
+p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
+p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
