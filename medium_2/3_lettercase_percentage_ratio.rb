dc0224a2ab13f15e9ce21f201b0809e464aa3a72
Justin Greger <justingreger@gmail.com>
2024-05-01 19:47:11 -0400

    p letter_percentages

--- medium_2/3_lettercase_percentage_ratio.rb
+++ medium_2/3_lettercase_percentage_ratio.rb
@@ -141,7 +141,7 @@ def calculate_percentage(count, total)
 end
 
 # Tests:
-p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
-p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
-p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
-p letter_percentages('abcdefGHI') == { lowercase: 66.7, :uppercase: 33.3, neither: 0.0 }
+p letter_percentages('abCdef 123') == { :lowercase=>50.0, :uppercase=>10.0, :neither=>40.0 }
+p letter_percentages('AbCd +Ef') == { :lowercase=>37.5, :uppercase=>37.5, :neither=>25.0 }
+p letter_percentages('123') == { :lowercase=>0.0, :uppercase=>0.0, :neither=>100.0 }
+p letter_percentages('abcdefGHI') == { :lowercase=>66.7, :uppercase=>33.3, :neither=> 0.0 }
