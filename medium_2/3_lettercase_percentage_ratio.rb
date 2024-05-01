305aa53901d3cec42ee12a1bb7d2bf97b0a00ec0
Justin Greger <justingreger@gmail.com>
2024-04-30 21:12:50 -0400

    # Tests:

--- medium_2/3_lettercase_percentage_ratio.rb
+++ medium_2/3_lettercase_percentage_ratio.rb
@@ -88,6 +88,11 @@
 #   (count.to_f / total * 100).round(1)
 # end
 
+# Tests:
+p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
+p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
+p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
+
 # LS Solution:
 def letter_percentages(string)
   counts = {}
