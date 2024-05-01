a62772b5f94dc3dcacf328e9204b00f0ac085257
Justin Greger <justingreger@gmail.com>
2024-04-30 20:58:23 -0400

    # Try creating that solution on your own.

--- medium_2/3_lettercase_percentage_ratio.rb
+++ medium_2/3_lettercase_percentage_ratio.rb
@@ -120,4 +120,4 @@ p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 
 # {:lowercase=>66.66666666666666, :uppercase=>33.33333333333333, :neither=>0.0}.
 # It would be nicer if we could round these float numbers so that our solution looks like this
 # {:lowercase=>66.7, :uppercase=>33.3, :neither=>0.0}. 
-   Try creating that solution on your own.
+# Try creating that solution on your own.
