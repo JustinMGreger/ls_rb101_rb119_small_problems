7dc1f10d39d6f97f65e24b866ebdc2f9490cdf3a
Justin Greger <justingreger@gmail.com>
2024-04-30 21:27:44 -0400

    # Tests: # p letter_percentages

--- medium_2/3_lettercase_percentage_ratio.rb
+++ medium_2/3_lettercase_percentage_ratio.rb
@@ -116,9 +116,9 @@ def calculate(percentages, counts, length)
 end
 
 # Tests:
-p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
-p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
-p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
+# p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
+# p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
+# p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
 
 # Further Exploration:
 # If we passed a string 'abcdefGHI' as an argument to our method call the solution would be
