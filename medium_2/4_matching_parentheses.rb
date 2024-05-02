d883b3df52305f1e0e215ff43f1f52ed3ab50b7c
Justin Greger <justingreger@gmail.com>
2024-05-01 20:46:49 -0400

    # PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C). # PEDAC stands for [Understand the] Problem,

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -18,6 +18,18 @@
 
 # Note that balanced pairs must each start with a (, not a ).
 
+# PEDAC has two primary objectives: process the problem (PEDA) and code with intent (C).
+# PEDAC stands for [Understand the] Problem,
+
+Identify expected input:
+output:
+Make the requirements explicit Identify rules:
+Mental model of the problem (optional):
+Examples / Test Cases, Validate understanding of the problem:
+Data Structure, How we represent data that we will work with when converting the input to output:
+Algorithm, Steps for converting input to output:
+and Code. Implementation of Algorithm:
+
 # Tests:
 p balanced?('What (is) this?') == true
 p balanced?('What is) this?') == false
