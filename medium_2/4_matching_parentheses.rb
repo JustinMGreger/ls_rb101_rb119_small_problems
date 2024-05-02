1b7a3745267123b3df689fa575e720bdf3eff091
Justin Greger <justingreger@gmail.com>
2024-05-01 20:40:07 -0400

    # and returns true if all parentheses in the string are properly balanced,

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # Write a method that takes a string as an argument,
-and returns true if all parentheses in the string are properly balanced, false otherwise. To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
+# and returns true if all parentheses in the string are properly balanced,
+false otherwise. To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
 
 Examples:
