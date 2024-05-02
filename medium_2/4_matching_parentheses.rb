cd1f2d475bd5c39c66756732c4dce6b726d87d69
Justin Greger <justingreger@gmail.com>
2024-05-01 20:40:31 -0400

    # false otherwise.

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -2,6 +2,7 @@
 
 # Write a method that takes a string as an argument,
 # and returns true if all parentheses in the string are properly balanced,
-false otherwise. To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
+# false otherwise.
+To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
 
 Examples:
