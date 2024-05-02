7e29c528b376942fadb1bd2ce8ef8a11712b6b2b
Justin Greger <justingreger@gmail.com>
2024-05-01 20:44:06 -0400

    p balanced?

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -17,12 +17,12 @@ balanced?('What ((is))) up(') == false
 balanced?('What ())(is() up') == false
 
 # Tests:
-balanced?('What (is) this?') == true
-balanced?('What is) this?') == false
-balanced?('What (is this?') == false
-balanced?('((What) (is this))?') == true
-balanced?('((What)) (is this))?') == false
-balanced?('Hey!') == true
-balanced?(')Hey!(') == false
-balanced?('What ((is))) up(') == false
-balanced?('What ())(is() up') == false
+p balanced?('What (is) this?') == true
+p balanced?('What is) this?') == false
+p balanced?('What (is this?') == false
+p balanced?('((What) (is this))?') == true
+p balanced?('((What)) (is this))?') == false
+p balanced?('Hey!') == true
+p balanced?(')Hey!(') == false
+p balanced?('What ((is))) up(') == false
+p balanced?('What ())(is() up') == false
