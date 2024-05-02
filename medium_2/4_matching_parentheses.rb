6040f3269a97b710a6d4bc0a30935bcad244034e
Justin Greger <justingreger@gmail.com>
2024-05-01 20:26:25 -0400

    # Write a method that takes a string as an argument,

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+# Write a method that takes a string as an argument,
+and returns true if all parentheses in the string are properly balanced, false otherwise. To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
+
+Examples:
