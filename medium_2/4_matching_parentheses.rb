697b7189a7613991b506c67f215b72c7edd9b71a
Justin Greger <justingreger@gmail.com>
2024-05-02 20:07:58 -0400

    # true if all parentheses in the string are properly balanced

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -37,7 +37,7 @@
 
 # Mental model of the problem (optional):
 # def balanced?(string)
-# and returns true if all parentheses in the string are properly balanced,
+# true if all parentheses in the string are properly balanced
 # false otherwise.
 # To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
 # Note that balanced pairs must each start with a (, not a ).
