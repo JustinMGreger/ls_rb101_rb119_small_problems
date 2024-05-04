cede118eb0e08c8ec7842c2cbc9b6de339aa5f36
Justin Greger <justingreger@gmail.com>
2024-05-03 21:59:41 -0400

    # the stack variable is assigned the [] array.

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -58,6 +58,7 @@
 
 # Algorithm, Steps for converting input to output:
 # the balanced? method accepts the (string) parameter and is defined as the following:
+# the stack variable is assigned the [] array.
 
 # and Code. Implementation of Algorithm:
 
