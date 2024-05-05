a3c7b1168e619cdba90a23724dad38b226a8c751
Justin Greger <justingreger@gmail.com>
2024-05-04 21:46:54 -0400

    # Tests:

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -85,6 +85,17 @@ def balanced?(string)
   stack.empty?
 end
 
+# Tests:
+p balanced?('What (is) this?') == true
+p balanced?('What is) this?') == false
+p balanced?('What (is this?') == false
+p balanced?('((What) (is this))?') == true
+p balanced?('((What)) (is this))?') == false
+p balanced?('Hey!') == true
+p balanced?(')Hey!(') == false
+p balanced?('What ((is))) up(') == false
+p balanced?('What ())(is() up') == false
+
 # LS Solution:
 
 # Tests:
