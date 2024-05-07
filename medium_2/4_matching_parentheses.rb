39dbbef7c7b1406478e27897ea5ea2652a494e37
Justin Greger <justingreger@gmail.com>
2024-05-06 21:51:27 -0400

    # There are a few other characters that should be matching as well.

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -121,4 +121,5 @@ p balanced?('What ())(is() up') == false
 
 # Further Exploration
 
-There are a few other characters that should be matching as well. Square brackets and curly brackets normally come in pairs. Quotation marks(single and double) also typically come in pairs and should be balanced. Can you expand this method to take into account those characters?
+# There are a few other characters that should be matching as well.
+Square brackets and curly brackets normally come in pairs. Quotation marks(single and double) also typically come in pairs and should be balanced. Can you expand this method to take into account those characters?
