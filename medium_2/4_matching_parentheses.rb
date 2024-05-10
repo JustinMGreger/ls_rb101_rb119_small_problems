95bf83c57ffab7f12f3db71f7e4e6c71471f9e82
Justin Greger <justingreger@gmail.com>
2024-05-09 21:59:33 -0400

    # def balanced?

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -149,7 +149,7 @@
 # Can you expand this method to take into account those characters?
 
 # Mental model of the problem (optional):
-# Write a method that takes a string as an argument,
+# def balanced? that takes a string as an argument,
 # and returns true if all parentheses in the string are properly balanced,
 # false otherwise.
 # To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
