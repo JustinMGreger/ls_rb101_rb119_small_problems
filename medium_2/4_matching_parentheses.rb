60deb85386f3de71d2596ccc5542ce0513311af5
Justin Greger <justingreger@gmail.com>
2024-05-09 21:51:03 -0400

    # Data Structure, How we represent data that we will work with when converting the input to output: # arrays & hashes.

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -163,7 +163,9 @@ Mental model of the problem (optional):
 # puts balanced?('What is" this?') == false
 # puts balanced?('What "is this?') == false
 
-Data Structure, How we represent data that we will work with when converting the input to output:
+# Data Structure, How we represent data that we will work with when converting the input to output:
+# arrays & hashes.
+
 Algorithm, Steps for converting input to output:
 and Code. Implementation of Algorithm:
 
