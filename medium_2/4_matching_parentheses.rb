b2258e24a29b8c225749f4412d4df0129d5f9dd7
Justin Greger <justingreger@gmail.com>
2024-05-10 21:08:19 -0400

    # this is the last line of the closing? method.

--- medium_2/4_matching_parentheses.rb
+++ medium_2/4_matching_parentheses.rb
@@ -212,7 +212,7 @@
 # this is the last line of the opening? method.
 # the closing? method accepts the (char) parameter and is defined as the following:
 # return true if the [')', ']', '}'] array includes the (char) parameter.
-
+# this is the last line of the closing? method.
 
 
 and Code. Implementation of Algorithm:
