8a2e687571647007823456d651d0f16ed0b40339
Justin Greger <justingreger@gmail.com>
2024-05-15 19:59:02 -0400

    # and all sides must have lengths greater than 0:

--- medium_2/5_triangle_sides.rb
+++ medium_2/5_triangle_sides.rb
@@ -7,7 +7,8 @@
 # scalene All 3 sides are of different length
 # To be a valid triangle,
 # the sum of the lengths of the two shortest sides must be greater than the length of the longest side,
-and all sides must have lengths greater than 0: if either of these conditions is not satisfied, the triangle is invalid.
+# and all sides must have lengths greater than 0:
+ if either of these conditions is not satisfied, the triangle is invalid.
 
 Write a method that takes the lengths of the 3 sides of a triangle as arguments, and returns a symbol :equilateral, :isosceles, :scalene, or :invalid depending on whether the triangle is equilateral, isosceles, scalene, or invalid.
 
